b4742f9ab6f58d1fd4ddc046246432c22774814f
NickleDave <nicholdav@gmail.com>
2017-12-02 18:44:52 -0500

    factor loading training data out of load_mat_spects

--- load_mat_spects.py
+++ load_mat_spects.py
@@ -1,9 +1,17 @@
-"""load spectrograms generated in matlab
-into a numpy array and save for use as a training set
+"""load spectrograms and labeled timebin vectors generated in matlab
 
 takes as a command line argument the name of the directory with the .mat files
-containing the spectrograms and the trainkeys.mat file that specifies which of
-them to use for the training set.
+
+Each .mat file contains:
+    s : ndarray
+        the spectrogram
+    f : ndarray
+        vector of values at bin centers for frequencies in spectrogram
+    t : ndarray
+        vector of values for bin centers for times in spectrogram
+    labels : ndarray
+        vector of same length as t where each value is a label for that time bin
+containing the spectrograms.
 
 loads spectrograms in the .mat files specified in train_keys
 """
@@ -22,16 +30,25 @@ if not os.path.isdir(data_dir):
 else:
     os.chdir(data_dir)
 
-# get train_keys cell array out of .mat file and convert to list of str
-train_spect_files = loadmat('train_keys.mat',squeeze_me=True)['train_keys'].tolist()
+spect_files = glob('*.mat')
 
 spects = []
+spect_files_used = []
+all_time_bins = []
 labeled_timebins = []
 
-for counter, train_spect_file in enumerate(train_spect_files):
-    print(f'loading {train_spect_file}')
-    mat_dict = loadmat(train_spect_file, squeeze_me=True)
-    if counter == 0:
+for counter, spect_file in enumerate(train_spect_files):
+    print(f'loading {spect_file}')
+    mat_dict = loadmat(spect_file, squeeze_me=True)
+
+    if spect_file == 'train_keys.mat':
+        continue
+
+    if 's' not in mat_dict:
+        print(f'Did not find a spectrogram in {spect_file}. Skipping this file.')
+        continue
+
+    if 'freq_bins' not in locals() and 'time_bins' not in locals():
         freq_bins = mat_dict['f']        
         time_bins = mat_dict['t']
     else:
@@ -44,14 +61,21 @@ for counter, train_spect_file in enumerate(train_spect_files):
     # number of time bins should equal number of columns
     assert mat_dict['t'].shape[-1] == spect.shape[1]
     spects.append(spect)
+    all_time_bins.append(time_bins)
 
     assert labels.shape[-1] == mat_dict['t'].shape[-1]
     labeled_timebins.append(labels)
 
-training_spects = {'spects': spects,
-                   'freq_bins': freq_bins,
-                   'time_bins': time_bins}
-print(f'saving training_spects dicitonary in {data_dir}')
-joblib.dump(training_spects, 'training_spects')
-print(f'saving labeled_timebins in {data_dir}')
-joblib.dump(labeled_timebins, 'labeled_timebins')
+    spect_files_used.append(spect)
+
+data_dict = {'spects': spects,
+             'filenames': spect_files_used,
+             'freq_bins': freq_bins,
+             'time_bins': all_time_bins,
+             'labeled_timebins': labeled_timebins}
+
+print(f'saving data dictionary in {data_dir}')
+joblib.dump(data_dict, 'data_dict')
+
+# # get train_keys cell array out of .mat file and convert to list of str
+# train_spect_files = loadmat('train_keys.mat',squeeze_me=True)['train_keys'].tolist()
