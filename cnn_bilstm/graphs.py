a3cdcacf150f04721fa808578f13ccf1387a6fa8
David Nicholson <nicholdav@gmail.com>
2018-01-11 22:07:08 -0500

    num_hidden is now determined programaticaally by graphs.py, removed from config/learn_curve.py

--- cnn_bilstm/graphs.py
+++ cnn_bilstm/graphs.py
@@ -8,7 +8,6 @@ def out_width(in_width, filter_width, stride):
 def inference(spectrogram,
               seq_length,
               n_syllables,
-              num_hidden=None,
               batch_size=11,
               input_vec_size=513,
               conv1_filters=32,
@@ -16,7 +15,7 @@ def inference(spectrogram,
               pool1_size=(1,8),
               pool1_strides=(1,8),
               pool2_size=(1,8),
-              pool2_strides=(1, 8),):
+              pool2_strides=(1, 8)):
     """inference graph for 'inferring' labels of birdsong syllables
     hybrid convolutional neural net with bidirectional LSTM layer
 
@@ -51,38 +50,41 @@ def inference(spectrogram,
 
     """
 
-    if num_hidden is None:
-        freq_bins_after_pool1 = out_width(input_vec_size,
-                                          pool1_size[1],
-                                          pool1_strides[1])
-        freq_bins_after_pool2 = out_width(freq_bins_after_pool1,
-                                          pool2_size[1],
-                                          pool2_strides[1])
-        num_hidden = freq_bins_after_pool2 * conv2_filters
-
     # First convolutional layers
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
         inputs=tf.reshape(spectrogram, [batch_size, -1, input_vec_size, 1]),
-        filters=32,
+        filters=conv1_filters,
         kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu)
     # Pooling Layer #1
     pool1 = tf.layers.max_pooling2d(inputs=conv1,
-                                    pool_size=[1, 8],
-                                    strides=[1, 8])
+                                    pool_size=pool1_size,
+                                    strides=pool1_strides)
     # Convolutional Layer #2 and Pooling Layer #2
     conv2 = tf.layers.conv2d(
         inputs=pool1,
-        filters=64,
+        filters=conv2_filters,
         kernel_size=[5, 5],
         padding="same",
         activation=tf.nn.relu)
     pool2 = tf.layers.max_pooling2d(inputs=conv2,
-                                    pool_size=[1, 8],
-                                    strides=[1, 8])
-    # Second the dynamic bi-directional LSTM
+                                    pool_size=pool2_size,
+                                    strides=pool2_strides)
+
+    # Determine number of hidden units in bidirectional LSTM:
+    # uniquely determined by number of filters and frequency bins
+    # in output shape of pool2
+    freq_bins_after_pool1 = out_width(input_vec_size,
+                                      pool1_size[1],
+                                      pool1_strides[1])
+    freq_bins_after_pool2 = out_width(freq_bins_after_pool1,
+                                      pool2_size[1],
+                                      pool2_strides[1])
+    num_hidden = freq_bins_after_pool2 * conv2_filters
+
+    # dynamic bi-directional LSTM
     lstm_f1 = tf.contrib.rnn.BasicLSTMCell(num_hidden, forget_bias=1.0, state_is_tuple=True, reuse=None)
     lstm_b1 = tf.contrib.rnn.BasicLSTMCell(num_hidden, forget_bias=1.0, state_is_tuple=True, reuse=None)
     outputs, _states = tf.nn.bidirectional_dynamic_rnn(lstm_f1,
@@ -92,7 +94,7 @@ def inference(spectrogram,
                                                        dtype=tf.float32,
                                                        sequence_length=seq_length)
 
-    # Third, projection on the number of syllables creates logits time_steps
+    # projection on the number of syllables creates logits time_steps
     with tf.name_scope('Projection'):
         W_f = tf.Variable(tf.random_normal([num_hidden, n_syllables]))
         W_b = tf.Variable(tf.random_normal([num_hidden, n_syllables]))
@@ -108,6 +110,7 @@ def inference(spectrogram,
                         for ex1, ex2 in zip(expr1, expr2)],
                        0)
     return logits, outputs
+
 
 xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits
 
@@ -139,7 +142,7 @@ def train(logits, lbls, rate, batch_size):
     return train_op, cost
 
 
-def get_full_graph(input_vec_size=513, num_hidden=512, n_syllables=16,
+def get_full_graph(input_vec_size=513, n_syllables=16,
                    learning_rate=0.001, batch_size=11):
 
     full_graph = tf.Graph()
@@ -156,17 +159,16 @@ def get_full_graph(input_vec_size=513, num_hidden=512, n_syllables=16,
                                name="Ylabels")  # holds labels batch_size
             lng = tf.placeholder("int32",
                                  name="nSteps")  # holds the sequence length
-            tf.add_to_collection("specs", X)  # Remember this Op.
-            tf.add_to_collection("labels", Y)  # Remember this Op.
-            tf.add_to_collection("lng", lng)  # Remember this Op.
+            tf.add_to_collection("specs", X)
+            tf.add_to_collection("labels", Y)
+            tf.add_to_collection("lng", lng)
             # Build a Graph that computes predictions from the inference model.
             logits, outputs = inference(X,
-                                        num_hidden,
                                         lng,
                                         n_syllables,
                                         batch_size,
-                                        input_vec_size)  # lstm_size
-            tf.add_to_collection("logits", logits)  # Remember this Op.
+                                        input_vec_size)
+            tf.add_to_collection("logits", logits)
 
             # Add to the Graph the Ops that calculate and apply gradients.
             train_op, cost = train(logits, Y, learning_rate, batch_size)
