0f7774d85d11a5d6713320baea969e89534ccb4c
David Nicholson <nicholdav@gmail.com>
2018-10-28 15:58:26 -0400

    rename train_utils package to 'cli'

--- src/cnn_bilstm/cli/__init__.py
+++ src/cnn_bilstm/cli/__init__.py
@@ -1,3 +1,6 @@
+"""command-line interface functions for training,
+creating learning curves, etc."""
+
 from .make_data import make_data
 from .train import train
 from .learningcurve import learn_curve
