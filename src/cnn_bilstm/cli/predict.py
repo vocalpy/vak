c6f9dd0fade913e500113e0d267ac096684da04d
David Nicholson <nicholdav@gmail.com>
2018-10-28 20:49:37 -0400

    mostly working version of cnn_bilstm.cli.predict

--- src/cnn_bilstm/cli/predict.py
+++ src/cnn_bilstm/cli/predict.py
@@ -23,94 +23,85 @@ def predict(config_file):
     print('Using definitions in: ' + config_file)
     results_dirname = config['OUTPUT']['results_dir_made_by_main_script']
 
+
+    dir_to_predict = config['PREDICT']['dir_to_predict']
+    spect_file_list = glob(dir_to_predict + '*.spect')
+
+
     batch_size = int(config['NETWORK']['batch_size'])
     time_steps = int(config['NETWORK']['time_steps'])
+    n_syllables = int(config['NETWORK']['n_syllables'])
+    input_vec_size = int(config['NETWORK']['input_vec_size'])
 
-    labelset = list(config['DATA']['labelset'])
 
-    labels_mapping_file = os.path.join(results_dirname, 'labels_mapping')
-    with open(labels_mapping_file, 'rb') as labels_map_file_obj:
-        labels_mapping = pickle.load(labels_map_file_obj)
-        n_syllables = len(labels_mapping)
-    input_vec_size = joblib.load(
-        os.path.join(results_dirname,'X_train')).shape[-1]
-    print('vec_size: ' + str(input_vec_size))
-
-    data_folder = config['TRAIN']['test_data_path']
-    data_folder = data_folder[:-14]
-    spect_list = glob(data_folder + '*.spect')
-
-    true_labels = np.zeros((len(spect_list),), dtype=np.object)
-    estimates = np.zeros((len(TRAIN_SET_DURS),num_replicates,len(spect_list),), dtype=np.object)
-
-    Y_pred_test_this_dur = []
-    Y_pred_train_this_dur = []
-    Y_pred_test_labels_this_dur = []
-    Y_pred_train_labels_this_dur = []
-
-    training_records_dir = os.path.join(results_dirname,
-        ('records_for_training_set_with_duration_of_'
-        + str(train_set_dur) + '_sec_replicate_' + str(replicate)))
-
-    checkpoint_filename = ('checkpoint_train_set_dur_'
-       + str(train_set_dur) +
-       '_sec_replicate_'
-       + str(replicate))
-    meta_file = glob(os.path.join(training_records_dir, 'checkpoint*meta*'))[0]
-    data_file = glob(os.path.join(training_records_dir, 'checkpoint*data*'))[0]
-    #input_vec_size = X_train_subset.shape[-1]  # number of columns
     model = CNNBiLSTM(n_syllables=n_syllables,
         input_vec_size=input_vec_size,
         batch_size=batch_size)
+
     with tf.Session(graph=model.graph) as sess:
         tf.logging.set_verbosity(tf.logging.ERROR)
+
+        checkpoint_dir = config['PREDICT']['checkpoint_dir']
+        meta_file = glob(os.path.join(checkpoint_dir,
+                                      'checkpoint*meta*'))
+        if len(meta_file) > 1:
+            raise ValueError('found more than one .meta file in {}'
+                             .format(checkpoint_dir))
+        else:
+            meta_file = meta_file[0]
+
+        data_file = glob(os.path.join(training_records_dir,
+                                      'checkpoint*data*'))[0]
+        if len(data_file) > 1:
+            raise ValueError('found more than one .data file in {}'
+                             .format(checkpoint_dir))
+        else:
+            data_file = meta_file[0]
 
         model.restore(sess=sess,
         meta_file=meta_file,
         data_file=data_file)
-        for fnum in range(len(spect_list)):
-            data = joblib.load(spect_list[fnum])
+
+        num_spect_files = len(spect_file_list)
+        preds_dict = {}
+        for file_num, spect_file in enumerate(spect_file_list):
+            print('Predicting labels for {}, file {} of {}'
+                  .format(spect_file, file_num, num_spect_files))
+
+            data = joblib.load(spect_file)
             Xd = data['spect'].T
             Yd = data['labeled_timebins']
             (Xd_batch,
              Yd_batch,
-             num_batches_val) = reshape_data_for_batching(Xd,
-                                                          Yd,
-                                                          batch_size,
-                                                          time_steps,
-                                                          input_vec_size)
-        if 'Y_pred_train' in locals():
-          del Y_pred_train
+             num_batches) = reshape_data_for_batching(Xd,
+                                                      Yd,
+                                                      batch_size,
+                                                      time_steps,
+                                                      input_vec_size)
 
-        for b in range(num_batches_val):  # "b" is "batch number"
+            if 'Y_pred' in locals:
+                del Y_pred
+            # work through current spectrogram batch by batch
+            for b in range(num_batches):  # "b" is "batch number"
+                d = {model.X:Xd_batch[:, b * time_steps: (b + 1) * time_steps, :],
+                     model.lng: [time_steps] * batch_size}
+                if 'Y_pred' in locals():
+                    # if Y_pred exists, we concatenate with new predictions
+                    # for next batch
+                    preds = sess.run(model.predict, feed_dict=d)
+                    preds = preds.reshape( -1)  # batch_size
+                    Y_pred = np.concatenate((Y_pred, preds), axis=0)
+                else:  # if Y_pred doesn't exist yet
+                    Y_pred = sess.run(model.predict, feed_dict=d)
+                    Y_pred = Y_pred.reshape(-1)
 
-            d = {model.X:Xd_batch[:, b * time_steps: (b + 1) * time_steps, :],
-                         model.lng: [time_steps] * batch_size}
-            if 'Y_pred_test' in locals():
-                #preds = sess.run(eval_op, feed_dict=d)[1]
-                preds = sess.run(model.predict, feed_dict=d)
-                preds = preds.reshape( -1) #batch_size,
-                Y_pred_test = np.concatenate((Y_pred_test, preds), axis=0)
-                print(np.shape(Y_pred_test))
-            else:
-                Y_pred_test = sess.run(model.predict, feed_dict=d)
-                print(np.shape(Y_pred_test))
-                Y_pred_test = Y_pred_test.reshape( -1) #batch_size,
-                print(np.shape(Y_pred_test))
+            # remove zero padding added by reshape_data_for_batching function
+            Y_pred = Y_pred.ravel()
+            Y_pred = Y_pred[0:len(Yd)]
+            preds_dict[spect_file] = Y_pred
 
-        Y_pred_test = Y_pred_test.ravel()
-        Y_pred_test = Y_pred_test[0:len(Yd)]
-        estimates[dur_ind][replicate][fnum] = Y_pred_test
-        if replicate == 0 and dur_ind == 0:
-          true_labels[fnum] = Yd.ravel()
-        print('Duration: ' + str(dur_ind) + '/' + str(len(TRAIN_SET_DURS)) +
-                  ' replicate: ' + str(replicate) + '/'+ str(num_replicates) +
-                  'file: ' + str(fnum) + '/' + str(len(spect_list)))
-
-    fname = os.path.join(results_dirname,'summary_per_file.mat')
-    cpio.savemat(fname ,{'estimates':estimates, 'true_labels':true_labels})
-    fname = os.path.join(results_dirname,'summary_per_file')
-    joblib.dump({'estimates':estimates, 'true_labels':true_labels}, fname)
+    fname = os.path.join(results_dirname,'predictions')
+    joblib.dump(preds_dict, fname)
 
 
 if __name__ == '__main__':
