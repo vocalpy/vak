8e00083c3f6b6fa37abab9f87a916840a684df2a
David Nicholson <nicholdav@gmail.com>
2018-10-29 14:27:13 -0400

    rename cli functions again

--- src/cnn_bilstm/cli/__init__.py
+++ src/cnn_bilstm/cli/__init__.py
@@ -2,6 +2,6 @@
 creating learning curves, etc."""
 
 from .make_data import make_data
-from .train import train
-from .learningcurve import learn_curve
+from .learncurve import learncurve
+from .summary import summary
 from .predict import predict
