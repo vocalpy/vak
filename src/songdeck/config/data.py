aeaecd57b548e680106f8971624380ae4dc6e3fe
nickledave <nickledave@users.noreply.github.com>
2018-11-07 11:41:28 -0500

    add config module, make_data uses it, start tests for it

---
+++ src/songdeck/config/data.py
@@ -0,0 +1,98 @@
+"""parses [DATA] section of config"""
+import os
+from collections import namedtuple
+
+from ..utils import range_str
+
+DataConfig = namedtuple('DataConfig', ['labelset',
+                                       'all_labels_are_int',
+                                       'silent_gap_label',
+                                       'skip_files_with_labels_not_in_labelset',
+                                       'output_dir',
+                                       'mat_spect_files_path',
+                                       'data_dir',
+                                       'train_set_dur',
+                                       'val_dur',
+                                       'test_dur'])
+
+
+def parse_data_config(config, config_file):
+    """parse [DATA] section of config.ini file
+
+    Parameters
+    ----------
+    config : ConfigParser
+        containing config.ini file already loaded by parse function
+    config_file : str
+        path to config file (used for error messages)
+
+    Returns
+    -------
+    data_config : namedtuple
+        with fields:
+            labelset
+            all_labels_are_int
+            silent_gap_label
+            skip_files_with_labels_not_in_labelset
+            output_dir
+            mat_spect_files_path
+    """
+    labelset = config['DATA']['labelset']
+    # make mapping from syllable labels to consecutive integers
+    # start at 1, because 0 is assumed to be label for silent gaps
+    if '-' in labelset or ',' in labelset:
+        # if user specified range of ints using a str
+        labelset = range_str(labelset)
+    else:  # assume labelset is characters
+        labelset = list(labelset)
+
+    # to make type-checking consistent across .mat / .cbin / Koumura .wav files
+    # set all_labels_are_int flag
+    # currently only used with .mat files
+    if config.has_option('DATA', 'all_labels_are_int'):
+        all_labels_are_int = config.getboolean('DATA', 'all_labels_are_int')
+    else:
+        all_labels_are_int = False
+
+    if config.has_option('DATA', 'silent_gap_label'):
+        silent_gap_label = int(config['DATA']['silent_gap_label'])
+    else:
+        silent_gap_label = 0
+
+    skip_files_with_labels_not_in_labelset = config.getboolean(
+        'DATA',
+        'skip_files_with_labels_not_in_labelset')
+
+    if config.has_option('DATA', 'output_dir'):
+        output_dir = os.path.join(config['DATA']['output_dir'],
+                                  'spectrograms_' + timenow)
+    else:
+        output_dir = None
+
+    ### if using spectrograms from .mat files ###
+    if config.has_option('DATA', 'mat_spect_files_path'):
+        # make spect_files file from .mat spect files and annotation file
+        mat_spect_files_path = config['DATA']['mat_spect_files_path']
+    else:
+        mat_spect_files_path = None
+
+    data_dir = config['DATA']['data_dir']
+    if not os.path.isdir(data_dir):
+        raise NotADirectoryError('{} specified as data_dir in {}, '
+                                 'but not recognized as a directory'
+                                 .format(data_dir, config_file))
+
+    train_set_dur = float(config['DATA']['total_train_set_duration'])
+    val_dur = float(config['DATA']['validation_set_duration'])
+    test_dur = float(config['DATA']['test_set_duration'])
+
+    return DataConfig(labelset,
+                      all_labels_are_int,
+                      silent_gap_label,
+                      skip_files_with_labels_not_in_labelset,
+                      output_dir,
+                      mat_spect_files_path,
+                      data_dir,
+                      train_set_dur,
+                      val_dur,
+                      test_dur)
---
+++ tests/unit_tests/test_config_data.py
@@ -0,0 +1,67 @@
+import os
+import unittest
+from configparser import ConfigParser
+
+import songdeck.config.data
+import songdeck.utils
+
+HERE = os.path.dirname(__file__)
+
+
+def _base_config():
+    base_config = ConfigParser()
+    test_data_dir = os.path.join(HERE,
+                                 '..',
+                                 'test_data')
+    base_config['DATA'] = {
+        'labelset': 'iabcdefghjk',
+        'data_dir': test_data_dir,
+        'total_train_set_duration': '400',
+        'validation_set_duration': '100',
+        'test_set_duration': '400',
+        'skip_files_with_labels_not_in_labelset': 'Yes',
+    }
+    return base_config
+
+
+class TestData(unittest.TestCase):
+
+    def setUp(self):
+        self.get_config = _base_config()
+
+    def test_data_str_labelset(self):
+        config_obj = self.get_config
+
+        config_file = 'test'
+        data_config_tup = songdeck.config.data.parse_data_config(config_obj,
+                                                                 config_file)
+        self.assertEqual(data_config_tup.labelset,
+                         list(config_obj['DATA']['labelset'])
+                         )
+
+    def test_data_rangestr_labelset(self):
+        a_rangestr = '1-9, 12'
+        config_obj = self.get_config
+        config_obj['DATA']['labelset'] = a_rangestr
+
+        config_file = 'test'
+        data_config_tup = songdeck.config.data.parse_data_config(config_obj,
+                                                                 config_file)
+        self.assertEqual(data_config_tup.labelset,
+                         songdeck.utils.range_str(a_rangestr)
+                         )
+
+    def test_data_int_labelset(self):
+        int_labels = '01234567'
+        config_obj = self.get_config
+        config_obj['DATA']['labelset'] = int_labels
+
+        config_file = 'test'
+        data_config_tup = songdeck.config.data.parse_data_config(config_obj,
+                                                                 config_file)
+        self.assertEqual(data_config_tup.labelset,
+                         list(int_labels)
+                         )
+
+if __name__ == '__main__':
+    unittest.main()
