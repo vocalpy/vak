a1ef57ac2c91c15ddf61055693bb1e8d4f86221b
nickledave <nickledave@users.noreply.github.com>
2018-11-07 11:41:28 -0500

    add config module, make_data uses it, start tests for it

---
+++ src/songdeck/config/__init__.py
@@ -0,0 +1,2 @@
+"""sub-package with function that parses config.ini file and returns config object"""
+from .parse import parse
---
+++ src/songdeck/config/parse.py
@@ -0,0 +1,33 @@
+from configparser import ConfigParser
+from collections import namedtuple
+
+from .data import parse_data_config
+from .spectrogram import parse_spect_config
+
+ConfigTuple = namedtuple('ConfigTuple', ['data',
+                                         'spect_params'])
+
+
+def parse(config_file):
+    if not config_file.endswith('.ini'):
+        raise ValueError('{} is not a valid config file, '
+                         'must have .ini extension'.format(config_file))
+    if not os.path.isfile(config_file):
+        raise FileNotFoundError('config file {} is not found'
+                                .format(config_file))
+    config_obj = ConfigParser()
+    config_obj.read(config_file)
+
+    data = parse_data_config(config_obj, config_file)
+
+    ### if **not** using spectrograms from .mat files ###
+    if data.mat_spect_files_path is None:
+        # then user needs to specify spectrogram parameters
+        if not config_obj.has_section('SPECTROGRAM'):
+            raise ValueError('No annotation_path specified in config_file that '
+                             'would point to annotated spectrograms, but no '
+                             'parameters provided to generate spectrograms '
+                             'either.')
+        spect_params = parse_spect_config(config_obj)
+    else:
+        spect_params = None
