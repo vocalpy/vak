11ad86d4c0449cdbcd9fec75701086b26848a372
NickleDave <nickledave@users.noreply.github.com>
2018-11-08 17:36:11 -0500

    add config.predict and tests for it

---
+++ tests/unit_tests/test_config_predict.py
@@ -0,0 +1,69 @@
+"""tests for songdeck.config.predict module"""
+import os
+import tempfile
+import shutil
+import unittest
+from configparser import ConfigParser
+
+import songdeck.config.predict
+import songdeck.utils
+
+HERE = os.path.dirname(__file__)
+TEST_CONFIGS_PATH = os.path.join(HERE, '..', 'test_data', 'configs')
+
+
+def _base_config(tmp_checkpoint_dir,
+                 tmp_dir_to_predict):
+    base_config = ConfigParser()
+    base_config['PREDICT'] = {
+        'checkpoint_dir': str(tmp_checkpoint_dir),
+        'dir_to_predict': str(tmp_dir_to_predict),
+    }
+    return base_config
+
+
+class TestParsePredictConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp_checkpoint_dir = tempfile.mkdtemp()
+        self.tmp_dir_to_predict = tempfile.mkdtemp()
+        self.get_config = _base_config(self.tmp_checkpoint_dir,
+                                       self.tmp_dir_to_predict)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_checkpoint_dir)
+        shutil.rmtree(self.tmp_dir_to_predict)
+
+    def test_config_tuple_has_all_attrs(self):
+        config_obj = self.get_config
+        predict_config_tup = songdeck.config.predict.parse_predict_config(config_obj)
+        for field in songdeck.config.predict.PredictConfig._fields:
+            self.assertTrue(hasattr(predict_config_tup, field))
+
+    def test_missing_checkpoint_dir_raises(self):
+        config_obj = self.get_config
+        config_obj.remove_option('PREDICT', 'checkpoint_dir')
+        with self.assertRaises(KeyError):
+            songdeck.config.predict.parse_predict_config(config_obj)
+
+    def test_missing_dir_to_predict_raises(self):
+        config_obj = self.get_config
+        config_obj.remove_option('PREDICT', 'dir_to_predict')
+        with self.assertRaises(KeyError):
+            songdeck.config.predict.parse_predict_config(config_obj)
+
+    def test_nonexistent_checkpoint_dir_raises(self):
+        config_obj = self.get_config
+        config_obj['PREDICT']['checkpoint_dir'] = 'obviously/non/existent/dir'
+        with self.assertRaises(NotADirectoryError):
+            songdeck.config.predict.parse_predict_config(config_obj)
+
+    def test_nonexistent_dir_to_predict_raises(self):
+        config_obj = self.get_config
+        config_obj['PREDICT']['dir_to_predict'] = 'obviously/non/existent/dir'
+        with self.assertRaises(NotADirectoryError):
+            songdeck.config.predict.parse_predict_config(config_obj)
+
+
+if __name__ == '__main__':
+    unittest.main()
