d00e88420316ff0b05dfb6c18d84020b02850501
NickleDave <nickledave@users.noreply.github.com>
2018-11-12 19:26:02 -0500

    fix indentation bug leftover from hacking out config stuff

--- src/songdeck/cli/make_data.py
+++ src/songdeck/cli/make_data.py
@@ -19,27 +19,50 @@ def make_data(labelset,
               skip_files_with_labels_not_in_labelset=True,
               output_dir=None,
               mat_spect_files_path=None,
+              mat_spects_annotation_file=None,
               spect_params=None,
               ):
-    """make datasets for learning curves
+    """make datasets for training models to generate a learning curve
 
     Parameters
     ----------
-    labelset
-    all_labels_are_int
-    data_dir
-    total_train_set_dur
-    val_dur
-    test_dur
-    silent_gap_label
-    skip_files_with_labels_not_in_labelset
-    output_dir
-    mat_spect_files_path
-    spect_params
+    labelset : list
+        of str or int, set of labels for syllables
+    all_labels_are_int : bool
+        if True, labels are of type int, not str
+    data_dir : str
+        path to directory with audio files from which to make dataset
+    total_train_set_dur : int
+        total duration of training set, in seconds.
+        Training subsets of shorter duration will be drawn from this set.
+    val_dur : int
+        total duration of validation set, in seconds.
+    test_dur : int
+        total duration of test set, in seconds.
+    silent_gap_label : str
+        label for time bins of silent gaps between syllables. Default is '0'.
+    skip_files_with_labels_not_in_labelset : bool
+        if True, skip a file if the labels variable contains labels not
+        found in 'labelset'. Default is True.
+    output_dir : str
+        Path to location where data sets should be saved. Default is None,
+        in which case data sets are saved in the current working directory.
+    mat_spect_files_path : str
+        Path to a directory of .mat files that contain spectrograms.
+        Default is None (and this implies user is supplying audio files
+         instead of supplying spectrograms in .mat files).
+    mat_spects_annotation_file : str
+        Path to annotation file associated with .mat files.
+        Default is None.
+    spect_params : dict
+        Dictionary of parameters for creating spectrograms.
+        Default is None (implying that spectrograms are already made).
 
     Returns
     -------
+    None
 
+    Saves .spect files and data_dicts in output_dir specified by user.
     """
     logger = logging.getLogger(__name__)
     logger.setLevel('INFO')
@@ -65,8 +88,7 @@ def make_data(labelset,
     if mat_spect_files_path:
         print('will use spectrograms from .mat files in {}'
               .format(mat_spect_files_path))
-        mat_spect_files = glob(os.path.join(mat_spect_files_path,'*.mat'))
-        mat_spects_annotation_file = config['DATA']['mat_spect_files_annotation_file']
+        mat_spect_files = glob(os.path.join(mat_spect_files_path, '*.mat'))
         if output_dir is None:
             output_dir = os.path.join(mat_spect_files_path,
                                   'spectrograms_' + timenow)
@@ -77,9 +99,13 @@ def make_data(labelset,
                                                 output_dir,
                                                 labels_mapping=labels_mapping)
 
+    else:
         logger.info('will make training data from: {}'.format(data_dir))
         if output_dir is None:
             output_dir = os.path.join(data_dir,
+                                  'spectrograms_' + timenow)
+        else:
+            output_dir = os.path.join(output_dir,
                                   'spectrograms_' + timenow)
         os.mkdir(output_dir)
 
