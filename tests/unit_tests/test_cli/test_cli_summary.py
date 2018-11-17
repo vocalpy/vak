17dddcb81df7a5198787d0fbce07c83da4ffaa3e
NickleDave <nickledave@users.noreply.github.com>
2018-11-17 18:36:16 -0500

    summary runs without crash

--- tests/unit_tests/test_cli/test_cli_summary.py
+++ tests/unit_tests/test_cli/test_cli_summary.py
@@ -30,32 +30,28 @@ class TestSummary(unittest.TestCase):
         tmp_makefile_config = os.path.join(SETUP_SCRIPTS_DIR, 'tmp_Makefile_config.ini')
         # Now we want a copy (of the changed version) to use for tests
         # since this is what the test data was made with
-        self.tmp_config_path = os.path.join(TEST_DATA_DIR, 'configs', 'tmp_config.ini')
+        self.tmp_config_dir = tempfile.mkdtemp()
+        self.tmp_config_path = os.path.join(self.tmp_config_dir, 'tmp_config.ini')
         shutil.copy(tmp_makefile_config, self.tmp_config_path)
         test_data_spects_path = glob(os.path.join(TEST_DATA_DIR,
                                                   'spects',
-                                                  'spectrograms_*'))
-        self.assertTrue(len(test_data_spects_path) == 1)
-        test_data_spects_path = test_data_spects_path[0]
-        self.train_data_dict_path = os.path.join(test_data_spects_path,'train_data_dict')
-        self.assertTrue(os.path.isfile(self.train_data_dict_path))
-        self.val_data_dict_path = os.path.join(test_data_spects_path,'val_data_dict')
-        self.assertTrue(os.path.isfile(self.val_data_dict_path))
-
+                                                  'spectrograms_*'))[0]
+        self.test_data_dict_path = os.path.join(test_data_spects_path, 'test_data_dict')
 
     def tearDown(self):
         shutil.rmtree(self.tmp_output_dir)
-        os.remove(self.tmp_config_path)
+        shutil.rmtree(self.tmp_config_dir)
 
     def test_learncurve_func(self):
         # make sure cli.summary runs without crashing.
         config = songdeck.config.parse.parse_config(self.tmp_config_path)
-        songdeck.cli.summary(results_dirname=config.output.results_dir_made_by_main_script,
+        songdeck.cli.summary(results_dirname=config.output.results_dirname,
+                             train_data_dict_path=config.train.train_data_dict_path,
                              networks=config.networks,
                              train_set_durs=config.train.train_set_durs,
                              num_replicates=config.train.num_replicates,
                              labelset=config.data.labelset,
-                             test_data_dict_path=config.train.test_data_dict_path,
+                             test_data_dict_path=self.test_data_dict_path,
                              normalize_spectrograms=config.train.normalize_spectrograms)
 
 
