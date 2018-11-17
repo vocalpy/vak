a4a1d83bfa78ac9817a4790aa68aa4bc4ca99a17
NickleDave <nickledave@users.noreply.github.com>
2018-11-16 21:13:13 -0500

    fix call to make_data in if __name__==__main__ section cli.make_data

--- src/songdeck/cli/make_data.py
+++ src/songdeck/cli/make_data.py
@@ -194,4 +194,16 @@ def make_data(labelset,
 if __name__ == "__main__":
     config_file = os.path.normpath(sys.argv[1])
     config = songdeck.config.parse_config(config_file)
-    make_data(config)
+    make_data(labelset=config.data.labelset,
+              all_labels_are_int=config.data.all_labels_are_int,
+              data_dir=config.data.data_dir,
+              total_train_set_dur=config.data.total_train_set_dur,
+              val_dur=config.data.val_dur,
+              test_dur=config.data.test_dur,
+              config_file=config_file,
+              silent_gap_label=config.data.silent_gap_label,
+              skip_files_with_labels_not_in_labelset=config.data.skip_files_with_labels_not_in_labelset,
+              output_dir=config.data.output_dir,
+              mat_spect_files_path=config.data.mat_spect_files_path,
+              mat_spects_annotation_file=config.data.mat_spects_annotation_file,
+              spect_params=config.spect_params)
