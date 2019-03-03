beb6fa28c02ea2326927c46a08a55ad7b2ed6381
NickleDave <nicholdav@gmail.com>
2019-03-03 06:16:38 -0500

    change 'songdeck' to 'vak' in network.py

--- src/vak/network.py
+++ src/vak/network.py
@@ -34,7 +34,7 @@ def _load():
     networks = {
         entry_point.name: entry_point.load()
         for entry_point
-        in pkg_resources.iter_entry_points('songdeck.network')
+        in pkg_resources.iter_entry_points('vak.network')
     }
     return networks
 
