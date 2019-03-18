f0843a56b22bc0afba57b31130347ade3414e56d
NickleDave <nickledave@users.noreply.github.com>
2019-03-17 22:44:24 -0400

    bump pre-release version in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = 'https://github.com/NickleDave/vak'
 EMAIL = 'dnicho4@emory.edu'
 AUTHOR = 'David Nicholson, Yarden Cohen'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0a1'
+VERSION = '0.1.0a2'
 LICENSE = 'BSD'
 
 REQUIRED = [
