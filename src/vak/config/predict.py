e2f9d6e61a40596f851944346eba2c6b1bf98110
NickleDave <nickledave@users.noreply.github.com>
2019-03-23 13:45:52 -0400

    vak.config.predict knows how to parse new args for vak.cli.predict

--- src/vak/config/predict.py
+++ src/vak/config/predict.py
@@ -5,9 +5,11 @@ from configparser import NoOptionError
 
 from ..network import _load
 
-PredictConfig = namedtuple('OutputConfig', ['networks',
-                                            'checkpoint_dir',
-                                            'dir_to_predict',
+PredictConfig = namedtuple('PredictConfig', ['checkpoint_path',
+                                             'networks',
+                                             'labels_mapping_path',
+                                             'dir_to_predict',
+                                             'spect_scaler_path'
                                             ])
 
 
@@ -23,17 +25,31 @@ def parse_predict_config(config):
     -------
     predict_config : namedtuple
         with fields:
-            networks : str
-                Name of network which was trained and which should be used to make
-                predictions. This must match the original network that was trained
-                for the checkpoint file used and is required so vak knows which
-                type of network to load before loading the parameters saved in the
-                checkpoint file.
-            checkpoint_dir : str
-                directory with checkpoint files saved by Tensorflow, to reload model
+            checkpoint_path : str
+                path to directory with checkpoint files saved by Tensorflow, to reload model
+            networks : namedtuple
+                where each field is the Config tuple for a neural network and the name
+                of that field is the name of the class that represents the network.
+            labels_mapping_path : str
+                path to file that contains labels mapping, to convert output from consecutive
+                digits back to labels used for audio segments (e.g. birdsong syllables)
             dir_to_predict : str
-                directory with audio files for which predictions should be made
+                path to directory where input files are located
+            spect_scaler_path : str
+                path to a saved SpectScaler object used to normalize spectrograms.
+                If spectrograms were normalized and this is not provided, will give
+                incorrect results.
     """
+    try:
+        checkpoint_path = config['PREDICT']['checkpoint_path']
+        if not os.path.isdir(checkpoint_path):
+            raise NotADirectoryError("directory specified as saved model checkpoint, "
+                                     "{}, was not found."
+                                     .format(checkpoint_path))
+    except NoOptionError:
+        raise KeyError('must specify checkpoint_path in [PREDICT] section '
+                       'of config.ini file')
+
     # load entry points within function, not at module level,
     # to avoid circular dependencies
     # (user would be unable to import networks in other packages
@@ -52,14 +68,15 @@ def parse_predict_config(config):
         raise KeyError("'networks' option not found in [TRAIN] section of config.ini file. "
                        "Please add this option as a comma-separated list of neural network names, e.g.:\n"
                        "networks = TweetyNet, GRUnet, convnet")
+
     try:
-        checkpoint_dir = config['PREDICT']['checkpoint_dir']
-        if not os.path.isdir(checkpoint_dir):
-            raise NotADirectoryError('directory {}, specified as '
-                                     'checkpoint_dir, was not found.'
-                                     .format(checkpoint_dir))
+        labels_mapping_path = config['PREDICT']['labels_mapping_path']
+        if not os.path.isfile(labels_mapping_path):
+            raise NotADirectoryError("file specified as labels mapping, "
+                                     "{}, was not found."
+                                     .format(labels_mapping_path))
     except NoOptionError:
-        raise KeyError('must specify checkpoint_dir in [PREDICT] section '
+        raise KeyError('must specify checkpoint_path in [PREDICT] section '
                        'of config.ini file')
 
     try:
@@ -72,6 +89,17 @@ def parse_predict_config(config):
         raise KeyError('must specify dir_to_predict in [PREDICT] section '
                        'of config.ini file')
 
-    return PredictConfig(networks,
-                         checkpoint_dir,
-                         dir_to_predict)
+    try:
+        spect_scaler_path = config['PREDICT']['spect_scaler_path']
+        if not os.path.isfile(spect_scaler_path):
+            raise NotADirectoryError("file specified as spect scaler, "
+                                     "{}, was not found."
+                                     .format(spect_scaler_path))
+    except NoOptionError:
+        spect_scaler_path = None
+
+    return PredictConfig(checkpoint_path,
+                         networks,
+                         labels_mapping_path,
+                         dir_to_predict,
+                         spect_scaler_path)
