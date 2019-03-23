ad11ca1f43ca91ada2144b1b47abddaeb2e46e4c
NickleDave <nickledave@users.noreply.github.com>
2019-03-23 19:19:12 -0400

    vak.cli.cli() runs vak.cli.make_data when called with 'prep' command

--- src/vak/cli/cli.py
+++ src/vak/cli/cli.py
@@ -20,7 +20,19 @@ def cli(command, config_files):
         config = parse.parse_config(config_file)
 
         if command == 'prep':
-            raise NotImplementedError
+            make_data(labelset=config.data.labelset,
+                      all_labels_are_int=config.data.all_labels_are_int,
+                      data_dir=config.data.data_dir,
+                      total_train_set_dur=config.data.total_train_set_dur,
+                      val_dur=config.data.val_dur,
+                      test_dur=config.data.test_dur,
+                      config_file=config_file,
+                      silent_gap_label=config.data.silent_gap_label,
+                      skip_files_with_labels_not_in_labelset=config.data.skip_files_with_labels_not_in_labelset,
+                      output_dir=config.data.output_dir,
+                      mat_spect_files_path=config.data.mat_spect_files_path,
+                      mat_spects_annotation_file=config.data.mat_spects_annotation_file,
+                      spect_params=config.spect_params)
 
         elif command == 'train':
             train(train_data_dict_path=config.train.train_data_dict_path,
@@ -49,23 +61,6 @@ def cli(command, config_files):
                     spect_scaler_path=config.predict.spect_scaler_path)
 
         elif command == 'learncurve':
-                make_data(labelset=config.data.labelset,
-                          all_labels_are_int=config.data.all_labels_are_int,
-                          data_dir=config.data.data_dir,
-                          total_train_set_dur=config.data.total_train_set_dur,
-                          val_dur=config.data.val_dur,
-                          test_dur=config.data.test_dur,
-                          config_file=config_file,
-                          silent_gap_label=config.data.silent_gap_label,
-                          skip_files_with_labels_not_in_labelset=config.data.skip_files_with_labels_not_in_labelset,
-                          output_dir=config.data.output_dir,
-                          mat_spect_files_path=config.data.mat_spect_files_path,
-                          mat_spects_annotation_file=config.data.mat_spects_annotation_file,
-                          spect_params=config.spect_params)
-
-                # get config again
-                # because make_data changed train_data_dict_path and val_data_dict_path
-                config = parse.parse_config(config_file)
                 learncurve(train_data_dict_path=config.train.train_data_dict_path,
                            val_data_dict_path=config.train.val_data_dict_path,
                            spect_params=config.spect_params,
@@ -84,18 +79,6 @@ def cli(command, config_files):
                            previous_run_path=config.train.previous_run_path,
                            root_results_dir=config.output.root_results_dir,
                            save_transformed_data=config.data.save_transformed_data)
-
-                # get config again
-                # because learncurve changed results_dir_made_by_main_script
-                config = parse.parse_config(config_file)
-                summary(results_dirname=config.output.results_dirname,
-                        train_data_dict_path=config.train.train_data_dict_path,
-                        networks=config.networks,
-                        train_set_durs=config.train.train_set_durs,
-                        num_replicates=config.train.num_replicates,
-                        labelset=config.data.labelset,
-                        test_data_dict_path=config.train.test_data_dict_path,
-                        normalize_spectrograms=config.train.normalize_spectrograms)
 
         elif command == 'summary':
             summary(results_dirname=config.output.results_dirname,
