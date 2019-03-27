c2b3b692e05d5be2d9aa08d3e13eecb9151fc95c
NickleDave <nicholdav@gmail.com>
2019-03-27 17:51:24 -0400

    vak.config.output.parse_output_config returns OutputConfig instance

--- src/vak/config/output.py
+++ src/vak/config/output.py
@@ -1,11 +1,31 @@
 """parses [OUTPUT] section of config"""
 import os
-from collections import namedtuple
 from configparser import NoOptionError
 
-OutputConfig = namedtuple('OutputConfig', ['root_results_dir',
-                                           'results_dirname',
-                                           ])
+import attr
+from attr.validators import optional
+
+from .validators import is_a_directory
+
+
+@attr.s
+class OutputConfig:
+    """class that represents [OUTPUT] section of config.ini file
+
+    Attributes
+    ----------
+    root_results_dir : str
+        directory in which results *will* be created.
+        The vak.cli.learcurve function will create
+        a subdirectory in this directory each time it runs.
+    results_dirname : str
+        name of subdirectory created by vak.cli.learncurve.
+        This option is added programatically by that function
+        when it runs but can be changed (e.g. to run
+        vak.cli.summary on previous outputs of learncurve.)
+    """
+    root_results_dir = attr.ib(validator=optional(is_a_directory), default=None)
+    results_dirname = attr.ib(validator=optional(is_a_directory), default=None)
 
 
 def parse_output_config(config):
@@ -19,24 +39,10 @@ def parse_output_config(config):
     Returns
     -------
     output_config : namedtuple
-        with fields:
-            root_results_dir : str
-                directory in which results *will* be created.
-                The vak.cli.learcurve function will create
-                a subdirectory in this directory each time it runs.
-            results_dirname : str
-                name of subdirectory created by vak.cli.learncurve.
-                This option is added programatically by that function
-                when it runs but can be changed (e.g. to run
-                vak.cli.summary on previous outputs of learncurve.)
     """
     try:
         root_results_dir = config['OUTPUT']['root_results_dir']
         root_results_dir = os.path.expanduser(root_results_dir)
-        if not os.path.isdir(root_results_dir):
-            raise NotADirectoryError('directory {}, specified as '
-                                     'root_results_dir, was not found.'
-                                     .format(root_results_dir))
     except NoOptionError:
         raise KeyError('must specify root_results_dir in [OUTPUT] section '
                        'of config.ini file')
