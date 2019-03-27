50ea4f939bd825ac6bfa2326a48324e12d33579b
NickleDave <nicholdav@gmail.com>
2019-03-27 08:35:00 -0400

    vak/config/data.py uses attrs-based class to represent [DATA] section of config.ini files

--- src/vak/config/data.py
+++ src/vak/config/data.py
@@ -1,22 +1,57 @@
 """parses [DATA] section of config"""
 import os
-from collections import namedtuple
 
-from vak.utils.data import range_str
+import attr
+from attr.validators import instance_of, optional
 
-fields = ['labelset',
-          'all_labels_are_int',
-          'silent_gap_label',
-          'skip_files_with_labels_not_in_labelset',
-          'output_dir',
-          'mat_spect_files_path',
-          'mat_spects_annotation_file',
-          'data_dir',
-          'total_train_set_dur',
-          'val_dur',
-          'test_dur',
-          'save_transformed_data']
-DataConfig = namedtuple('DataConfig', fields)
+from ..utils.data import range_str
+
+
+@attr.s
+class DataConfig:
+    """class to represent [DATA] section of config.ini file
+
+    Attributes
+    ----------
+    labelset : list
+        of str or int, set of labels for syllables
+    all_labels_are_int : bool
+        if True, labels are of type int, not str
+    silent_gap_label  : int
+        label for time bins of silent gaps between syllables.
+        Type is int because labels are converted to a set of
+        n consecutive integers {0,1,2...n} where n is the number
+        of syllable classes + the silent gap class.
+        Default is 0 (in which case labels are {1,2,3,...,n}).
+    skip_files_with_labels_not_in_labelset
+    output_dir
+    mat_spect_files_path
+    mat_spects_annotation_file
+    data_dir
+    total_train_set_dur
+    val_dur
+    test_dur
+    freq_bins
+    """
+    labelset = attr.ib(validator=instance_of(list))
+    all_labels_are_int = attr.ib(validator=instance_of(bool), default=False)
+    silent_gap_label = attr.ib(validator=instance_of(bool), default=0)
+    skip_files_with_labels_not_in_labelset = attr.ib(validator=instance_of(bool), default=True)
+    output_dir = attr.ib(default=None)
+    @output_dir.validator.optional
+    def check_output_dir(self, attribute, value):
+        if not os.path.isdir(value):
+            raise NotADirectoryError(
+                f'{value} specified as output_dir but not recognized as a directory'
+            )
+
+    mat_spect_files_path = attr.ib()
+    mat_spects_annotation_file = attr.ib()
+    data_dir = attr.ib()
+    total_train_set_dur = attr.ib()
+    val_dur = attr.ib()
+    test_dur = attr.ib()
+    freq_bins = attr.ib()
 
 
 def parse_data_config(config, config_file):
@@ -31,60 +66,39 @@ def parse_data_config(config, config_file):
 
     Returns
     -------
-    data_config : DataConfig
-        namedtuple with fields:
-            labelset
-            all_labels_are_int
-            silent_gap_label
-            skip_files_with_labels_not_in_labelset
-            output_dir
-            mat_spect_files_path
-            mat_spects_annotation_file
-            data_dir
-            total_train_set_dur
-            val_dur
-            test_dur
-            save_transformed_data
+    data_config : vak.config.data.DataConfig
+        instance of class that represents [DATA] section of config.ini file
     """
+    config_dict = {}
+
     labelset = config['DATA']['labelset']
     # make mapping from syllable labels to consecutive integers
     # start at 1, because 0 is assumed to be label for silent gaps
     if '-' in labelset or ',' in labelset:
         # if user specified range of ints using a str
-        labelset = range_str(labelset)
+        config_dict['labelset'] = range_str(labelset)
     else:  # assume labelset is characters
-        labelset = list(labelset)
+        config_dict['labelset'] = list(labelset)
 
     # to make type-checking consistent across .mat / .cbin / Koumura .wav files
     # set all_labels_are_int flag
     # currently only used with .mat files
     if config.has_option('DATA', 'all_labels_are_int'):
-        all_labels_are_int = config.getboolean('DATA', 'all_labels_are_int')
-    else:
-        all_labels_are_int = False
+        config_dict['all_labels_are_int'] = config.getboolean('DATA', 'all_labels_are_int')
 
     if config.has_option('DATA', 'silent_gap_label'):
-        silent_gap_label = int(config['DATA']['silent_gap_label'])
-    else:
-        silent_gap_label = 0
+        config_dict['silent_gap_label'] = int(config['DATA']['silent_gap_label'])
 
     if config.has_option('DATA', 'skip_files_with_labels_not_in_labelset'):
-        skip_files_with_labels_not_in_labelset = config.getboolean(
-            'DATA',
-            'skip_files_with_labels_not_in_labelset')
-    else:
-        skip_files_with_labels_not_in_labelset = True
+        config_dict[
+            'skip_files_with_labels_not_in_labelset'
+        ] = config.getboolean('DATA',
+                              'skip_files_with_labels_not_in_labelset')
 
     if config.has_option('DATA', 'output_dir'):
         output_dir = config['DATA']['output_dir']
         output_dir = os.path.expanduser(output_dir)
         output_dir = os.path.abspath(output_dir)
-        if not os.path.isdir(output_dir):
-            raise NotADirectoryError('{} specified as output_dir in {}, '
-                                     'but not recognized as a directory'
-                                     .format(output_dir, config_file))
-    else:
-        output_dir = None
 
     # if using spectrograms from .mat files
     if config.has_option('DATA', 'mat_spect_files_path'):
