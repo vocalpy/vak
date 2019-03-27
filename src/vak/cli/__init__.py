cb6da607b56d5f39be789a839a6fbf44f3b698dc
NickleDave <nicholdav@gmail.com>
2019-03-27 11:12:31 -0400

    vak/cli/__init__.py imports make_data from .prep

--- src/vak/cli/__init__.py
+++ src/vak/cli/__init__.py
@@ -1,7 +1,7 @@
 """command-line interface functions for training,
 creating learning curves, etc."""
 
-from .make_data import make_data
+from .prep import make_data
 from .learncurve import learncurve
 from .summary import summary
 from .predict import predict
