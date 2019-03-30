6000b66b7b79984ca406ee8bfe4a4f2c9210c5da
NickleDave <nicholdav@gmail.com>
2019-03-30 08:37:43 -0400

    rewrite src/vak/config/train.py to use attrs-based class

--- src/vak/config/train.py
+++ src/vak/config/train.py
@@ -1,26 +1,90 @@
 """parses [TRAIN] section of config"""
-from collections import namedtuple
 from configparser import NoOptionError
+
+import attr
+from attr.validators import instance_of, optional
+
+from .validators import is_a_directory
 from ..network import _load
 
 
-TrainConfig = namedtuple('TrainConfig', [
-    'networks',
-    'train_data_dict_path',
-    'train_set_durs',
-    'num_replicates',
-    'val_data_dict_path',
-    'test_data_dict_path',
-    'val_error_step',
-    'checkpoint_step',
-    'save_only_single_checkpoint_file',
-    'num_epochs',
-    'patience',
-    'normalize_spectrograms',
-    'use_train_subsets_from_previous_run',
-    'previous_run_path',
-    'save_transformed_data'
-])
+@attr.s
+class TrainConfig:
+    """class that represents [TRAIN] section of config.ini file
+
+    Attributes
+    ----------
+    networks : namedtuple
+        where each field is the Config tuple for a neural network and the name
+        of that field is the name of the class that represents the network.
+    train_data_dict_path : str
+        path to training data
+    num_epochs : int
+        number of training epochs. One epoch = one iteration through the entire
+        training set.
+    normalize_spectrograms : bool
+        if True, use spect.utils.data.SpectScaler to normalize the spectrograms.
+        Normalization is done by subtracting off the mean for each frequency bin
+        of the training set and then dividing by the std for that frequency bin.
+        This same normalization is then applied to validation + test data.
+    val_data_dict_path : str
+        path to validation data. Default is None, in which case accuracy is not measured
+        on a validation set during training.
+    val_error_step : int
+        step/epoch at which to estimate accuracy using validation set.
+        Default is None, in which case no validation is done.
+    checkpoint_step : int
+        step/epoch at which to save to checkpoint file.
+        Default is None, in which case checkpoint is only saved at the last epoch.
+    patience : int
+        number of epochs to wait without the error dropping before stopping the
+        training. Default is None, in which case training continues for num_epochs
+    train_set_durs : list
+        of int, durations in seconds of subsets taken from training data
+        to create a learning curve, e.g. [5, 10, 15, 20]. Default is None
+        (when training a single model on all available training data).
+    num_replicates : int
+        number of times to replicate training for each training set duration
+        to better estimate mean accuracy for a training set of that size.
+        Each replicate uses a different randomly drawn subset of the training
+        data (but of the same duration).
+    save_only_single_checkpoint_file : bool
+        if True, save only one checkpoint file instead of separate files every time
+        we save. Default is True.
+    use_train_subsets_from_previous_run : bool
+        if True, use training subsets saved in a previous run. Default is False.
+        Requires setting previous_run_path option in config.ini file.
+    previous_run_path : str
+        path to results directory from a previous run.
+        Used for training if use_train_subsets_from_previous_run is True.
+    save_transformed_data : bool
+        if True, save transformed data (i.e. scaled, reshaped). The data can then
+        be used on a subsequent run of learncurve (e.g. if you want to compare results
+        from different hyperparameters across the exact same training set).
+        Also useful if you need to check what the data looks like when fed to networks.
+    """
+    # required for both train and learncurve
+    networks = attr.ib()
+    train_data_dict_path = attr.ib(validator=[instance_of(str), is_a_directory])
+    num_epochs = attr.ib(validator=instance_of(int))
+
+    # used for both train and learncurve, but optional
+    normalize_spectrograms = attr.ib(validator=optional(instance_of(bool)), default=False)
+    val_data_dict_path = attr.ib(validator=optional([instance_of(str), is_a_directory]), default=None)
+    test_data_dict_path = attr.ib(validator=optional([instance_of(str), is_a_directory]), default=None)
+    val_error_step = attr.ib(validator=optional(instance_of(int)), default=None)
+    checkpoint_step = attr.ib(validator=optional(instance_of(int)), default=None)
+    patience = attr.ib(validator=optional(instance_of(int)), default=None)
+
+    # used for learncurve, not train
+    train_set_durs = attr.ib(validator=optional(instance_of(list)), default=None)
+    num_replicates = attr.ib(validator=optional(instance_of(int)), default=None)
+
+    # things most users probably won't care about
+    save_only_single_checkpoint_file = attr.ib(validator=instance_of(bool), default=True)
+    use_train_subsets_from_previous_run = attr.ib(validator=instance_of(bool), default=False)
+    previous_run_path = attr.ib(validator=optional([instance_of(str), is_a_directory]), default=None)
+    save_transformed_data = attr.ib(validator=bool, default=False)
 
 
 def parse_train_config(config, config_file):
@@ -33,21 +97,10 @@ def parse_train_config(config, config_file):
 
     Returns
     -------
-    train_config : namedtuple
-        with fields:
-            networks :
-            train_data_dict_path :
-            train_set_durs :
-            num_replicates :
-            val_data_dict_path :
-            val_error_step :
-            checkpoint_step :
-            save_only_single_checkpoint_file :
-            num_epochs :
-            patience :
-            use_train_subsets_from_previous_run :
-            previous_run_path :
+    train_config : vak.config.train.TrainConfig
+        instance of TrainConfig class
     """
+    config_dict = {}
     # load entry points within function, not at module level,
     # to avoid circular dependencies
     # (user would be unable to import networks in other packages
@@ -62,61 +115,45 @@ def parse_train_config(config, config_file):
             if network.lower() not in NETWORK_NAMES:
                 raise TypeError('Neural network {} not found when importing installed networks.'
                                 .format(network))
+        config_dict['networks'] = networks
     except NoOptionError:
         raise KeyError("'networks' option not found in [TRAIN] section of config.ini file. "
                        "Please add this option as a comma-separated list of neural network names, e.g.:\n"
                        "networks = TweetyNet, GRUnet, convnet")
 
     try:
-        train_data_dict_path = config['TRAIN']['train_data_path']
+        config_dict['train_data_dict_path'] = config['TRAIN']['train_data_path']
     except NoOptionError:
         raise KeyError("'train_data_path' option not found in [TRAIN] section of config.ini file. "
                        "Please add this option.")
 
     if config.has_option('TRAIN', 'train_set_durs'):
-        train_set_durs = [int(element)
-                          for element in
-                          config['TRAIN']['train_set_durs'].split(',')]
-    else:
-        # set to None when training on entire dataset
-        train_set_durs = None
+        config_dict['train_set_durs'] = [int(element)
+                                         for element in
+                                         config['TRAIN']['train_set_durs'].split(',')]
 
     if config.has_option('TRAIN', 'replicates'):
-        num_replicates = int(config['TRAIN']['replicates'])
-    else:
-        # set to None when training on entire dataset
-        num_replicates = None
+        config_dict['num_replicates'] = int(config['TRAIN']['replicates'])
 
     if config.has_option('TRAIN', 'val_data_path'):
-        val_data_dict_path = config['TRAIN']['val_data_path']
-    else:
-        val_data_dict_path = None
+        config_dict['val_data_dict_path'] = config['TRAIN']['val_data_path']
 
     if config.has_option('TRAIN', 'test_data_path'):
         test_data_dict_path = config['TRAIN']['test_data_path']
-    else:
-        test_data_dict_path = None
 
     if config.has_option('TRAIN', 'val_error_step'):
-        val_error_step = int(config['TRAIN']['val_error_step'])
-    else:
-        val_error_step = None
+        config_dict['val_error_step'] = int(config['TRAIN']['val_error_step'])
 
     if config.has_option('TRAIN', 'checkpoint_step'):
-        checkpoint_step = int(config['TRAIN']['checkpoint_step'])
-    else:
-        checkpoint_step = None
+        config_dict['checkpoint_step'] = int(config['TRAIN']['checkpoint_step'])
 
     if config.has_option('TRAIN', 'save_only_single_checkpoint_file'):
-        save_only_single_checkpoint_file = config.getboolean('TRAIN',
-                                                             'save_only_single_checkpoint_file')
-    else:
-        save_only_single_checkpoint_file = True
+        config_dict['save_only_single_checkpoint_file'] = config.getboolean(
+            'TRAIN','save_only_single_checkpoint_file'
+        )
 
     if config.has_option('TRAIN', 'num_epochs'):
-        num_epochs = int(config['TRAIN']['num_epochs'])
-    else:
-        num_epochs = 18000
+        config_dict['num_epochs'] = int(config['TRAIN']['num_epochs'])
 
     if config.has_option('TRAIN', 'patience'):
         patience = config['TRAIN']['patience']
@@ -129,21 +166,19 @@ def parse_train_config(config, config_file):
                 raise TypeError('patience must be an int or None, but'
                                 'is {} and parsed as type {}'
                                 .format(patience, type(patience)))
-    else:
-        patience = None
+        config_dict['patience'] = patience
 
     if config.has_option('TRAIN', 'normalize_spectrograms'):
-        normalize_spectrograms = config.getboolean('TRAIN',
-                                                   'normalize_spectrograms')
-    else:
-        normalize_spectrograms = False
+        config_dict['normalize_spectrograms'] = config.getboolean(
+            'TRAIN', 'normalize_spectrograms'
+        )
 
     if config.has_option('TRAIN', 'use_train_subsets_from_previous_run'):
-        use_train_subsets_from_previous_run = config.getboolean(
+        config_dict['use_train_subsets_from_previous_run'] = config.getboolean(
             'TRAIN', 'use_train_subsets_from_previous_run')
-        if use_train_subsets_from_previous_run:
+        if config_dict['use_train_subsets_from_previous_run']:
             try:
-                previous_run_path = config['TRAIN']['previous_run_path']
+                config_dict['previous_run_path'] = config['TRAIN']['previous_run_path']
             except KeyError:
                 raise KeyError('In config.file {}, '
                                'use_train_subsets_from_previous_run = Yes, but '
@@ -151,39 +186,18 @@ def parse_train_config(config, config_file):
                                'Please add previous_run_path to config file.'
                                .format(config_file))
         else:
-            previous_run_path = None
-    else:
-        use_train_subsets_from_previous_run = False
-        if config.has_option('TRAIN', 'previous_run_path'):
-            raise ValueError('In config.file {}, '
-                             'use_train_subsets_from_previous_run = No, but '
-                             'previous_run_path option was specified as {}.\n'
-                             'Please fix argument or remove/comment out '
-                             'previous_run_path.'
-                             .format(config_file,
-                                     config['TRAIN']['previous_run_path'])
-                             )
-        else:
-            previous_run_path = None
+            if config.has_option('TRAIN', 'previous_run_path'):
+                raise ValueError('In config.file {}, '
+                                 'use_train_subsets_from_previous_run = No, but '
+                                 'previous_run_path option was specified as {}.\n'
+                                 'Please fix argument or remove/comment out '
+                                 'previous_run_path.'
+                                 .format(config_file,
+                                         config['TRAIN']['previous_run_path'])
+                                 )
 
     if config.has_option('TRAIN', 'save_transformed_data'):
-        save_transformed_data = config.getboolean(
+        config_dict['save_transformed_data'] = config.getboolean(
             'TRAIN', 'save_transformed_data')
-    else:
-        save_transformed_data = False
 
-    return TrainConfig(networks=networks,
-                       train_data_dict_path=train_data_dict_path,
-                       train_set_durs=train_set_durs,
-                       num_replicates=num_replicates,
-                       val_data_dict_path=val_data_dict_path,
-                       test_data_dict_path=test_data_dict_path,
-                       val_error_step=val_error_step,
-                       checkpoint_step=checkpoint_step,
-                       save_only_single_checkpoint_file=save_only_single_checkpoint_file,
-                       num_epochs=num_epochs,
-                       patience=patience,
-                       normalize_spectrograms=normalize_spectrograms,
-                       use_train_subsets_from_previous_run=use_train_subsets_from_previous_run,
-                       previous_run_path=previous_run_path,
-                       save_transformed_data=save_transformed_data)
+    return TrainConfig(**config_dict)
