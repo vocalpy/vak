572a83a652c88e4b7012d5e1708c74171345b3d9
NickleDave <nicholdav@gmail.com>
2019-03-30 08:38:10 -0400

    is_a_directory and is_a_file validators have clearer error messages

--- src/vak/config/validators.py
+++ src/vak/config/validators.py
@@ -5,12 +5,14 @@ def is_a_directory(instance, attribute, value):
     """check if given path is a directory"""
     if not os.path.isdir(value):
         raise NotADirectoryError(
-            f'{value} specified for {attribute} of {instance}, but not recognized as a directory'
+            f'Value specified for {attribute.name} of {type(instance)} not recognized as a directory:\n'
+            f'{value}'
         )
 
 def is_a_file(instance, attribute, value):
     """check if given path is a directory"""
     if not os.path.isfile(value):
         raise NotADirectoryError(
-            f'{value} specified for {attribute} of {instance}, but not recognized as a file'
+            f'Value specified for {attribute.name} of {type(instance)} not recognized as a file:\n'
+            f'{value}'
         )
