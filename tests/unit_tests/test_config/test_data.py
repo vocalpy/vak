64cd5deca23157a9f557e43f44da86e582ac5fa1
NickleDave <nickledave@users.noreply.github.com>
2019-04-02 12:07:38 -0400

    test_config/test_data.py has same structure as test_config/test_predict, et al.

--- tests/unit_tests/test_config/test_data.py
+++ tests/unit_tests/test_config/test_data.py
@@ -2,149 +2,120 @@
 import os
 import unittest
 from configparser import ConfigParser
+import tempfile
+import shutil
 
 import vak.config.data
 import vak.utils
 
 HERE = os.path.dirname(__file__)
-
-
-def _base_config():
-    base_config = ConfigParser()
-    test_data_dir = os.path.join(HERE,
-                                 '..',
-                                 '..',
-                                 'test_data')
-    base_config['DATA'] = {
-        'labelset': 'iabcdefghjk',
-        'data_dir': test_data_dir,
-        'total_train_set_duration': '400',
-        'validation_set_duration': '100',
-        'test_set_duration': '400',
-        'skip_files_with_labels_not_in_labelset': 'Yes',
-    }
-    return base_config
+TEST_DATA_DIR = os.path.join(HERE, '..', '..', 'test_data')
+TEST_CONFIGS_DIR = os.path.join(TEST_DATA_DIR, 'configs')
 
 
 class TestParseDataConfig(unittest.TestCase):
-
     def setUp(self):
-        self.get_config = _base_config()
+        self.config_file = os.path.join(TEST_DATA_DIR, 'configs', 'test_learncurve_config.ini')
+        self.config_obj = ConfigParser()
+        self.config_obj.read(self.config_file)
+
+        self.tmp_data_dir = tempfile.mkdtemp()
+        self.config_obj['DATA']['data_dir'] = self.tmp_data_dir
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_data_dir)
 
     def test_parse_data_config_returns_DataConfig_instance(self):
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_instance = vak.config.data.parse_data_config(config_obj, config_file)
-        self.assertTrue(type(data_config_instance) == vak.config.data.DataConfig)
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(type(data_config_obj) == vak.config.data.DataConfig)
 
     def test_str_labelset(self):
-        config_obj = self.get_config
-
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertEqual(data_config_tup.labelset,
-                         list(config_obj['DATA']['labelset'])
-                         )
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertEqual(
+            data_config_obj.labelset, list(self.config_obj['DATA']['labelset'])
+        )
 
     def test_rangestr_labelset(self):
         a_rangestr = '1-9, 12'
-        config_obj = self.get_config
-        config_obj['DATA']['labelset'] = a_rangestr
-
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertEqual(data_config_tup.labelset,
-                         vak.utils.data.range_str(a_rangestr)
-                         )
+        self.config_obj['DATA']['labelset'] = a_rangestr
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertEqual(
+            data_config_obj.labelset, vak.utils.data.range_str(a_rangestr)
+        )
 
     def test_int_labelset(self):
         int_labels = '01234567'
-        config_obj = self.get_config
-        config_obj['DATA']['labelset'] = int_labels
-
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertEqual(data_config_tup.labelset,
-                         list(int_labels)
-                         )
+        self.config_obj['DATA']['labelset'] = int_labels
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertEqual(
+            data_config_obj.labelset, list(int_labels)
+        )
 
     def test_all_labels_are_int_default(self):
         # test that all_labels_are_int is added
         # and set to False if we don't specify it
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertTrue(data_config_tup.all_labels_are_int is False)
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.all_labels_are_int is False)
 
     def test_silent_gap_label_default(self):
         # test that silent_gap_label is added
         # and set to 0 if we don't specify it
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertTrue(data_config_tup.silent_gap_label == 0)
+        if self.config_obj.has_option('DATA', 'silent_gap_label'):
+            self.config_obj.remove_option('DATA', 'silent_gap_label')
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.silent_gap_label == 0)
 
     def test_output_dir_default(self):
         # test that output_dir is added
         # and set to None if we don't specify it
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertTrue(data_config_tup.output_dir is None)
+        if self.config_obj.has_option('DATA', 'output_dir'):
+            self.config_obj.remove_option('DATA', 'output_dir')
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.output_dir is None)
 
     def test_mat_spect_files_path_default(self):
         # test that mat_spect_files_path is added
         # and set to None if we don't specify it
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertTrue(data_config_tup.mat_spect_files_path is None)
+        if self.config_obj.has_option('DATA', 'mat_spect_files_path'):
+            self.config_obj.remove_option('DATA', 'mat_spect_files_path')
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.mat_spect_files_path is None)
+
+    def test_data_dir_default(self):
+        # test that data_dir set to None if we don't specify it
+        self.config_obj.remove_option('DATA', 'data_dir')
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.data_dir is None)
 
     def test_nonexistent_data_dir_raises_error(self):
         # test that mate_spect_files_path is added
         # and set to None if we don't specify it
-        config_obj = self.get_config
-        config_obj['DATA']['data_dir'] = 'theres/no/way/this/is/a/dir'
-        config_file = 'test'
+        self.config_obj['DATA']['data_dir'] = 'theres/no/way/this/is/a/dir'
         with self.assertRaises(NotADirectoryError):
-            vak.config.data.parse_data_config(config_obj, config_file)
+            vak.config.data.parse_data_config(self.config_obj, self.config_file)
 
     def test_save_transformed_data(self):
-        # test that save_transformed_data is added
-        # and set to False, if we don't specify it
-        config_obj = self.get_config
-        config_obj['DATA']['save_transformed_data'] = 'True'
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj, config_file)
-        self.assertTrue(data_config_tup.save_transformed_data is True)
+        self.config_obj['DATA']['save_transformed_data'] = 'True'
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.save_transformed_data is True)
 
-        config_obj['DATA']['save_transformed_data'] = 'Yes'
-        data_config_tup = vak.config.data.parse_data_config(config_obj, config_file)
-        self.assertTrue(data_config_tup.save_transformed_data is True)
+        self.config_obj['DATA']['save_transformed_data'] = 'Yes'
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.save_transformed_data is True)
 
-        config_obj['DATA']['save_transformed_data'] = 'False'
-        data_config_tup = vak.config.data.parse_data_config(config_obj, config_file)
-        self.assertTrue(data_config_tup.save_transformed_data is False)
+        self.config_obj['DATA']['save_transformed_data'] = 'False'
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.save_transformed_data is False)
 
-        config_obj['DATA']['save_transformed_data'] = 'No'
-        data_config_tup = vak.config.data.parse_data_config(config_obj, config_file)
-        self.assertTrue(data_config_tup.save_transformed_data is False)
+        self.config_obj['DATA']['save_transformed_data'] = 'No'
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.save_transformed_data is False)
 
     def test_save_transformed_data_default(self):
         # test that save_transformed_data is added
         # and set to False, if we don't specify it
-        config_obj = self.get_config
-        config_file = 'test'
-        data_config_tup = vak.config.data.parse_data_config(config_obj,
-                                                                 config_file)
-        self.assertTrue(data_config_tup.save_transformed_data is False)
+        data_config_obj = vak.config.data.parse_data_config(self.config_obj, self.config_file)
+        self.assertTrue(data_config_obj.save_transformed_data is False)
 
 
 if __name__ == '__main__':
