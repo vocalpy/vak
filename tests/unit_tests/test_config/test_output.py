8255a50855dcd790e642281f737b67096feacec5
NickleDave <nickledave@users.noreply.github.com>
2019-04-01 20:26:40 -0400

    rewrite test_config/test_output.py to be more concise

--- tests/unit_tests/test_config/test_output.py
+++ tests/unit_tests/test_config/test_output.py
@@ -8,48 +8,37 @@ import vak.config.output
 import vak.utils
 
 
-def _base_config(tmp_root_dir,
-                 tmp_results_dir):
-    base_config = ConfigParser()
-    base_config['OUTPUT'] = {
-        'root_results_dir': str(tmp_root_dir),
-        'results_dir_made_by_main_script': str(tmp_results_dir),
-    }
-    return base_config
-
-
 class TestParseOutputConfig(unittest.TestCase):
 
     def setUp(self):
         self.tmp_root_dir = tempfile.mkdtemp()
         self.tmp_results_dir = tempfile.mkdtemp(dir=self.tmp_root_dir)
-        self.get_config = _base_config(self.tmp_root_dir,
-                                       self.tmp_results_dir)
+        self.config_obj = ConfigParser()
+        self.config_obj['OUTPUT'] = {
+            'root_results_dir': str(self.tmp_root_dir),
+            'results_dir_made_by_main_script': str(self.tmp_results_dir),
+        }
 
     def tearDown(self):
         shutil.rmtree(self.tmp_root_dir)
 
     def test_parse_output_config_returns_OutputConfig_instance(self):
-        config_obj = self.get_config
-        output_config_obj = vak.config.output.parse_output_config(config_obj)
+        output_config_obj = vak.config.output.parse_output_config(self.config_obj)
         self.assertTrue(type(output_config_obj) == vak.config.output.OutputConfig)
 
     def test_missing_root_results_dir_raises(self):
-        config_obj = self.get_config
-        config_obj.remove_option('OUTPUT', 'root_results_dir')
+        self.config_obj.remove_option('OUTPUT', 'root_results_dir')
         with self.assertRaises(KeyError):
-            vak.config.output.parse_output_config(config_obj)
+            vak.config.output.parse_output_config(self.config_obj)
 
     def test_nonexistent_root_results_dir_raises(self):
-        config_obj = self.get_config
-        config_obj['OUTPUT']['root_results_dir'] = 'obviously/non/existent/dir'
+        self.config_obj['OUTPUT']['root_results_dir'] = 'obviously/non/existent/dir'
         with self.assertRaises(NotADirectoryError):
-            vak.config.output.parse_output_config(config_obj)
+            vak.config.output.parse_output_config(self.config_obj)
 
     def test_no_results_dir_defaults_to_None(self):
-        config_obj = self.get_config
-        config_obj.remove_option('OUTPUT', 'results_dir_made_by_main_script')
-        output_config_tup = vak.config.output.parse_output_config(config_obj)
+        self.config_obj.remove_option('OUTPUT', 'results_dir_made_by_main_script')
+        output_config_tup = vak.config.output.parse_output_config(self.config_obj)
         self.assertTrue(output_config_tup.results_dirname is None)
 
 
