9540207a716526c34f98e7b2a02f6fd9c8c6f6e3
NickleDave <nickledave@users.noreply.github.com>
2019-04-04 20:36:47 -0400

    rewrite config/parse.py; gets net names from TRAIN and PREDICT then uses names to load configs for each net

--- src/vak/config/parse.py
+++ src/vak/config/parse.py
@@ -2,7 +2,6 @@ import os
 from configparser import ConfigParser
 from configparser import NoSectionError, MissingSectionHeaderError, ParsingError,\
     DuplicateOptionError, DuplicateSectionError
-from collections import namedtuple
 
 import attr
 from attr.validators import instance_of, optional
@@ -12,7 +11,7 @@ from .spectrogram import parse_spect_config, SpectConfig
 from .train import parse_train_config, TrainConfig
 from .output import parse_output_config, OutputConfig
 from .predict import parse_predict_config, PredictConfig
-from ..network import _load
+from .. import network
 
 
 @attr.s
@@ -32,16 +31,15 @@ class Config:
     output : vak.config.output.OutputConfig
         represents [OUTPUT] section of config.ini file
     networks : dict
-        represents neural network configuration sections of config.ini file.
-        These will vary depending on which network user specifies.
+        contains neural network configuration sections of config.ini file.
+        These will vary depending on which networks the user specifies.
     """
-    networks = attr.ib()
-
     data = attr.ib(validator=optional(instance_of(DataConfig)), default=None)
     spect_params = attr.ib(validator=optional(instance_of(SpectConfig)), default=None)
     train = attr.ib(validator=optional(instance_of(TrainConfig)), default=None)
     predict = attr.ib(validator=optional(instance_of(PredictConfig)), default=None)
     output = attr.ib(validator=optional(instance_of(OutputConfig)), default=None)
+    networks = attr.ib(validator=optional(instance_of(dict)), default=None)
 
 
 def parse_config(config_file):
@@ -84,54 +82,53 @@ def parse_config(config_file):
     if config_obj.has_section('SPECTROGRAM'):
         config_dict['spect_params'] = parse_spect_config(config_obj)
 
+    networks = []
     if config_obj.has_section('TRAIN'):
         config_dict['train'] = parse_train_config(config_obj, config_file)
-        networks = config_dict['train'].networks
+        networks += config_dict['train'].networks
 
     if config_obj.has_section('PREDICT'):
         config_dict['predict'] = parse_predict_config(config_obj)
-        networks = config_dict['predict'].networks
+        networks += config_dict['predict'].networks
 
-    # load entry points within function, not at module level,
-    # to avoid circular dependencies
-    # (user would be unable to import networks in other packages
-    # that subclass vak.network.AbstractVakNetwork
-    # since the module in the other package would need to `import vak`)
-    NETWORKS = _load()
-    sections = config_obj.sections()
-    # make tuple that will have network names as fields
-    # and a config tuple for each network as the value assigned to the corresponding field
-    NetworkTuple = namedtuple('NetworkTuple', [network for network in networks])
-    networks_dict = {}
-    for network in networks:
-        if network.lower() not in [section.lower() for section in sections]:
-            raise NoSectionError('No section found specifying parameters for network {}'
-                                 .format(network))
-        network_option_names = set(config_obj[network].keys())
-        config_field_names = set(NETWORKS[network].Config._fields)
-        # if some options in this network's section are not found in the Config tuple
-        # that is a class attribute for that network, raise an error because we don't
-        # know what to do with that option
-        if not network_option_names.issubset(config_field_names):
-            unknown_options = network_option_names - config_field_names
-            raise ValueError('The following option(s) in section for network {} are '
-                             'not found in the Config for that network: {}.\n'
-                             'Valid options are: {}'
-                             .format(network, unknown_options, config_field_names))
+    if networks:
+        # load entry points within function, not at module level,
+        # to avoid circular dependencies
+        # (user would be unable to import networks in other packages
+        # that subclass vak.network.AbstractVakNetwork
+        # since the module in the other package would need to `import vak`)
+        NETWORKS = network._load()
+        sections = config_obj.sections()
+        networks_dict = {}
+        for network_name in networks:
+            if network_name not in sections:
+                raise NoSectionError('No section found specifying parameters for network {}'
+                                     .format(network_name))
+            network_option_names = set(config_obj[network_name].keys())
+            config_field_names = set(NETWORKS[network_name].Config._fields)
+            # if some options in this network's section are not found in the Config tuple
+            # that is a class attribute for that network, raise an error because we don't
+            # know what to do with that option
+            if not network_option_names.issubset(config_field_names):
+                unknown_options = network_option_names - config_field_names
+                raise ValueError('The following option(s) in section for network {} are '
+                                 'not found in the Config for that network: {}.\n'
+                                 'Valid options are: {}'
+                                 .format(network_name, unknown_options, config_field_names))
 
-        options = {}
-        # do type conversion using the networks' Config typed namedtuple
-        # for the rest of the options
-        for option, value in config_obj[network].items():
-            option_type = NETWORKS[network].Config._field_types[option]
-            try:
-                options[option] = option_type(value)
-            except ValueError:
-                raise ValueError('Could not cast value {} for option {} in {} section '
-                                 ' to specified type {}.'
-                                 .format(value, option, network, option_type))
-        networks_dict[network] = NETWORKS[network].Config(**options)
-    config_dict['networks'] = NetworkTuple(**networks_dict)
+            options = {}
+            # do type conversion using the networks' Config typed namedtuple
+            # for the rest of the options
+            for option, value in config_obj[network_name].items():
+                option_type = NETWORKS[network_name].Config._field_types[option]
+                try:
+                    options[option] = option_type(value)
+                except ValueError:
+                    raise ValueError('Could not cast value {} for option {} in {} section '
+                                     ' to specified type {}.'
+                                     .format(value, option, network_name, option_type))
+            networks_dict[network_name] = NETWORKS[network_name].Config(**options)
+        config_dict['networks'] = networks_dict
 
     if config_obj.has_section('OUTPUT'):
         config_dict['output'] = parse_output_config(config_obj)
