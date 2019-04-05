e35cdab5ea122a903700dc367315734ec39a2951
NickleDave <nickledave@users.noreply.github.com>
2019-04-05 11:03:16 -0400

    vak.cli.cli uses _get_net_config helper func to pass configs for just nets spec'd by user to each cli func

--- src/vak/cli/cli.py
+++ src/vak/cli/cli.py
@@ -6,6 +6,18 @@ from .prep import make_data
 from ..config import parse
 
 
+def _get_net_config(command, config):
+    """helper function to get configuration for only networks that user specified
+    in a specific section of config.ini file, e.g. in the TRAIN section"""
+    command_config = getattr(config, command)
+    net_names = getattr(command_config, 'networks')
+    network_config = {net_name: net_config
+                      for net_name, net_config in config.networks.items()
+                      if net_name in net_names
+                      }
+    return network_config
+
+
 def cli(command, config_files):
     """command-line interface
 
@@ -42,10 +54,11 @@ def cli(command, config_files):
                       spect_params=config.spect_params)
 
         elif command == 'train':
+            network_config = _get_net_config(command, config)
             train(train_data_dict_path=config.train.train_data_dict_path,
                   val_data_dict_path=config.train.val_data_dict_path,
                   spect_params=config.spect_params,
-                  networks=config.networks,
+                  networks=network_config,
                   num_epochs=config.train.num_epochs,
                   config_file=config_file,
                   val_error_step=config.train.val_error_step,
@@ -60,8 +73,9 @@ def cli(command, config_files):
             raise NotImplementedError
 
         elif command == 'predict':
+            network_config = _get_net_config(command, config)
             predict(checkpoint_path=config.predict.checkpoint_path,
-                    networks=config.networks,
+                    networks=network_config,
                     labels_mapping_path=config.predict.labels_mapping_path,
                     spect_params=config.spect_params,
                     dir_to_predict=config.predict.dir_to_predict,
@@ -74,7 +88,7 @@ def cli(command, config_files):
             if config.train.val_data_dict_path is None:
                 raise ValueError("must set 'val_data_path' option in [TRAIN] section of config.ini file "
                                  "before running 'learncurve'")
-
+            network_config = _get_net_config(command, config)
             learncurve(train_data_dict_path=config.train.train_data_dict_path,
                        val_data_dict_path=config.train.val_data_dict_path,
                        spect_params=config.spect_params,
@@ -83,7 +97,7 @@ def cli(command, config_files):
                        num_replicates=config.train.num_replicates,
                        num_epochs=config.train.num_epochs,
                        config_file=config_file,
-                       networks=config.networks,
+                       networks=network_config,
                        val_error_step=config.train.val_error_step,
                        checkpoint_step=config.train.checkpoint_step,
                        patience=config.train.patience,
@@ -95,9 +109,10 @@ def cli(command, config_files):
                        save_transformed_data=config.data.save_transformed_data)
 
         elif command == 'summary':
+            network_config = _get_net_config('train', config)
             summary(results_dirname=config.output.results_dirname,
                     train_data_dict_path=config.train.train_data_dict_path,
-                    networks=config.networks,
+                    networks=network_config,
                     train_set_durs=config.train.train_set_durs,
                     num_replicates=config.train.num_replicates,
                     labelset=config.data.labelset,
