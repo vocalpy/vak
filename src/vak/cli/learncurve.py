388af7ae54dd28e9f3bcea773b4698939c721e8e
NickleDave <nickledave@users.noreply.github.com>
2019-04-06 14:52:30 -0400

    in vak.cli.learncurve, change order of args passed to reshape_data_for_batching

--- src/vak/cli/learncurve.py
+++ src/vak/cli/learncurve.py
@@ -349,9 +349,9 @@ def learncurve(train_data_dict_path,
                 (X_val_batch,
                  Y_val_batch,
                  num_batches_val) = utils.data.reshape_data_for_batching(X_val,
-                                                                         Y_val,
                                                                          net_config.batch_size,
-                                                                         net_config.time_bins)
+                                                                         net_config.time_bins,
+                                                                         Y_val)
 
                 # save scaled reshaped data
                 scaled_reshaped_data_filename = os.path.join(training_records_path,
