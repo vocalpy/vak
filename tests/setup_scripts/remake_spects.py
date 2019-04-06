664efaa856b73607dbd7a989014008e698a17306
NickleDave <nickledave@users.noreply.github.com>
2019-04-06 14:54:21 -0400

    rewrite remake_spects so it just gets config for data section and spect params

--- tests/setup_scripts/remake_spects.py
+++ tests/setup_scripts/remake_spects.py
@@ -1,20 +1,24 @@
 import os
+from configparser import ConfigParser
 
 import vak
 
 HERE = os.path.dirname(__file__)
 config_file = os.path.join(HERE, 'tmp_Makefile_config.ini')
-config = vak.config.parse.parse_config(config_file)
-vak.cli.make_data(labelset=config.data.labelset,
-                  all_labels_are_int=config.data.all_labels_are_int,
-                  data_dir=config.data.data_dir,
-                  total_train_set_dur=config.data.total_train_set_dur,
-                  val_dur=config.data.val_dur,
-                  test_dur=config.data.test_dur,
+config = ConfigParser()
+config.read(config_file)
+data_config = vak.config.parse.parse_data_config(config, config_file)
+spect_params = vak.config.parse.parse_spect_config(config)
+vak.cli.make_data(labelset=data_config.labelset,
+                  all_labels_are_int=data_config.all_labels_are_int,
+                  data_dir=data_config.data_dir,
+                  total_train_set_dur=data_config.total_train_set_dur,
+                  val_dur=data_config.val_dur,
+                  test_dur=data_config.test_dur,
                   config_file=config_file,
-                  silent_gap_label=config.data.silent_gap_label,
-                  skip_files_with_labels_not_in_labelset=config.data.skip_files_with_labels_not_in_labelset,
-                  output_dir=config.data.output_dir,
-                  mat_spect_files_path=config.data.mat_spect_files_path,
-                  mat_spects_annotation_file=config.data.mat_spects_annotation_file,
-                  spect_params=config.spect_params)
+                  silent_gap_label=data_config.silent_gap_label,
+                  skip_files_with_labels_not_in_labelset=data_config.skip_files_with_labels_not_in_labelset,
+                  output_dir=data_config.output_dir,
+                  mat_spect_files_path=data_config.mat_spect_files_path,
+                  mat_spects_annotation_file=data_config.mat_spects_annotation_file,
+                  spect_params=spect_params)
