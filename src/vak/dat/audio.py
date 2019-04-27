04e71ee1aee8e136c52088d4208278e111b5a409
NickleDave <nickledave@users.noreply.github.com>
2019-04-26 22:24:09 -0400

    add initial attempt at vak.data.audio.from_audio function

--- src/vak/dat/audio.py
+++ src/vak/dat/audio.py
@@ -1,4 +1,9 @@
-def from_audio(audio_dir, audio_format, annot_format):
+import os
+from glob import glob
+
+from ..utils.data import make_spects_from_list_of_files
+
+def from_audio(audio_dir, audio_format, annot_files, annot_format):
     """create .vak.dat files from already-made spectrograms that are in files containing arrays
     (e.g. a .mat file created by Matlab or a .npy file created by numpy)
 
@@ -6,10 +11,17 @@ def from_audio(audio_dir, audio_format, annot_format):
     ----------
     audio_dir
     audio_format
+    annot_files
     annot_format
 
     Returns
     -------
     vakdat_path
     """
+    audio_files = glob(
+        os.path.join(audio_dir, '*' + audio_format)
+    )
+
+    vakdat_path = make_spects_from_list_of_files(filelist=audio_files)
+
     return vakdat_path
