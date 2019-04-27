658d8bf334d11785bdb4a5dcf8da0e676673ed54
NickleDave <nickledave@users.noreply.github.com>
2019-04-26 21:00:18 -0400

    add initial drafts of array.py and audio.py in vak/data/

---
+++ src/vak/dat/array.py
@@ -0,0 +1,23 @@
+from crowsetta import Transcriber
+
+def from_array(array_dir, array_format, annot_format):
+    """create .vak.dat files from already-made spectrograms that are in files containing arrays
+    (e.g. a .mat file created by Matlab or a .npy file created by numpy)
+
+    Parameters
+    ----------
+    array_dir : str
+        path to directory of files containing spectrograms as arrays.
+    array_format : str
+        format of array files. One of {'mat', 'npy'}
+    annot_format : str
+        format of annotation.
+
+    Returns
+    -------
+    vakdat_path
+    """
+    scribe = Transcriber(user_config=)
+    seqs = scribe.to_seq(file=annotation_files, format=annot_format)
+
+    return vakdat_path
