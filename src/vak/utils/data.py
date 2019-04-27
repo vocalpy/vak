dad2e77e8afc5e0da087657b9ef01cd49d38cdcd
NickleDave <nickledave@users.noreply.github.com>
2019-04-27 11:47:50 -0400

    move label_timebins to vak/utils/labels.py

--- src/vak/utils/data.py
+++ src/vak/utils/data.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.io import wavfile, loadmat
 from tqdm import tqdm
 
+from .labels import label_timebins
 from . import spect as spect_utils  # so as not to confuse with variable name `spect`
 from vak import evfuncs
 from vak.koumura_utils import load_song_annot
@@ -16,48 +17,6 @@ from vak.koumura_utils import load_song_annot
 
 def vec_translate(a, my_dict):
     return np.vectorize(my_dict.__getitem__)(a)
-
-
-def label_timebins(labels,
-                   onsets,
-                   offsets,
-                   time_bins,
-                   silent_gap_label=0):
-    """makes a vector of labels for each timebin from a spectrogram,
-    given labels for syllables plus onsets and offsets of syllables
-
-    Parameters
-    ----------
-    labels : ints
-        should be mapping returned by make_labels_mapping
-    onsets : ndarray
-        1d vector of floats, syllable onsets in seconds
-    offsets : ndarray
-        1d vector of floats, offsets in seconds
-    time_bins : ndarray
-        1d vector of floats,
-        time in seconds for each time bin of a spectrogram
-    silent_gap_label : int
-        label assigned to silent gaps
-        default is 0
-
-    Returns
-    -------
-    label_vec : ndarray
-        same length as time_bins, with each element a label for
-        each time bin
-    """
-
-    labels = [int(label) for label in labels]
-    label_vec = np.ones((time_bins.shape[-1], 1), dtype='int8') * silent_gap_label
-    onset_inds = [np.argmin(np.abs(time_bins - onset))
-                  for onset in onsets]
-    offset_inds = [np.argmin(np.abs(time_bins - offset))
-                   for offset in offsets]
-    for label, onset, offset in zip(labels, onset_inds, offset_inds):
-        label_vec[onset:offset+1] = label
-        # offset_inds[ind]+1 because of Matlab one-indexing
-    return label_vec
 
 
 def make_spects_from_list_of_files(filelist,
---
+++ src/vak/utils/labels.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def label_timebins(labels,
+                   onsets,
+                   offsets,
+                   time_bins,
+                   silent_gap_label=0):
+    """makes a vector of labels for each timebin from a spectrogram,
+    given labels for syllables plus onsets and offsets of syllables
+
+    Parameters
+    ----------
+    labels : ints
+        should be mapping returned by make_labels_mapping
+    onsets : ndarray
+        1d vector of floats, syllable onsets in seconds
+    offsets : ndarray
+        1d vector of floats, offsets in seconds
+    time_bins : ndarray
+        1d vector of floats,
+        time in seconds for each time bin of a spectrogram
+    silent_gap_label : int
+        label assigned to silent gaps
+        default is 0
+
+    Returns
+    -------
+    label_vec : ndarray
+        same length as time_bins, with each element a label for
+        each time bin
+    """
+    labels = [int(label) for label in labels]
+    label_vec = np.ones((time_bins.shape[-1], 1), dtype='int8') * silent_gap_label
+    onset_inds = [np.argmin(np.abs(time_bins - onset))
+                  for onset in onsets]
+    offset_inds = [np.argmin(np.abs(time_bins - offset))
+                   for offset in offsets]
+    for label, onset, offset in zip(labels, onset_inds, offset_inds):
+        label_vec[onset:offset+1] = label
+        # offset_inds[ind]+1 because of Matlab one-indexing
+    return label_vec
