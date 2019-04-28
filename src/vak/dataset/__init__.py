a6b38d7384b277e65e06441bb59b439382fb2a96
NickleDave <nickledave@users.noreply.github.com>
2019-04-28 17:32:34 -0400

    change module docstring in vak/dataset/__init__.py

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,3 +1,4 @@
-"""sub-package that handles flow of data"""
+"""module that handles datasets:
+spectrograms made from audio files of vocalizations, and associated annotations"""
 from .array import from_array
 from .audio import from_audio
