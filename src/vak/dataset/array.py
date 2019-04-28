b3505e089a3ef5de49fe87dd27d0432cf31c4885
NickleDave <nickledave@users.noreply.github.com>
2019-04-28 16:27:36 -0400

    rename vak/dat to vak/dataset

--- src/vak/dataset/array.py
+++ src/vak/dataset/array.py
@@ -1,5 +1,6 @@
 from crowsetta import Transcriber
 
+
 def from_array(array_dir, array_format, annot_format):
     """create .vak.dat files from already-made spectrograms that are in files containing arrays
     (e.g. a .mat file created by Matlab or a .npy file created by numpy)
@@ -17,7 +18,7 @@ def from_array(array_dir, array_format, annot_format):
     -------
     vakdat_path
     """
-    scribe = Transcriber(user_config=)
+    scribe = Transcriber()
     seqs = scribe.to_seq(file=annotation_files, format=annot_format)
 
     return vakdat_path
