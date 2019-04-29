541f75140e33f39ccad95715f461dc6a92e284d6
NickleDave <nickledave@users.noreply.github.com>
2019-04-28 22:20:33 -0400

    add is_audio_format and is_annot_format to vak/config/validators.py

--- src/vak/config/validators.py
+++ src/vak/config/validators.py
@@ -18,3 +18,26 @@ def is_a_file(instance, attribute, value):
             f'Value specified for {attribute.name} of {type(instance)} not recognized as a file:\n'
             f'{value}'
         )
+
+from ..dataset.audio import VALID_AUDIO_FORMATS
+
+
+def is_audio_format(instance, attribute, value):
+    """check if valid audio format"""
+    if value not in VALID_AUDIO_FORMATS:
+        raise ValueError(
+            f'{value} is not a valid format for audio files'
+        )
+
+from crowsetta import Transcriber
+scribe = Transcriber()
+VALID_ANNOT_FORMATS = scribe._config.keys()
+
+
+def is_annot_format(instance, attribute, value):
+    """check if valid annotation format"""
+    if value not in VALID_ANNOT_FORMATS:
+        raise ValueError(
+            f'{value} is not a valid format for annotation files.\n'
+            f'Valid formats are: {VALID_ANNOT_FORMATS}'
+        )
