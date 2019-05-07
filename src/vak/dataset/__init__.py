9f690b70307408ead552b62944a6743be6615668
NickleDave <nickledave@users.noreply.github.com>
2019-05-07 10:32:01 -0400

    vak/dataset/__init__.py imports array.from_arr_files

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,5 +1,4 @@
 """module that handles datasets:
 spectrograms made from audio files of vocalizations, and associated annotations"""
-from .array import from_array
+from .array import from_arr_files
 from .audio import from_dir
-
