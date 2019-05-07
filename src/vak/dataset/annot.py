a3a55cbc989701f42678e016cbe3287a10aa4a7f
NickleDave <nickledave@users.noreply.github.com>
2019-05-07 18:24:02 -0400

    rewrite dataset.annot.files_from_dir to use utils.general._files_from_dir

--- src/vak/dataset/annot.py
+++ src/vak/dataset/annot.py
@@ -1,13 +1,12 @@
-from glob import glob
-import os
-
 import crowsetta
 
 from ..config import validators
+from ..utils.general import _files_from_dir
 
 
 def files_from_dir(annot_dir, annot_format):
-    """get all annotation files from a directory,
+    """get all annotation files of a given format
+    from a directory or its sub-directories,
     using the file extension associated with that annotation format.
     """
     if annot_format not in validators.VALID_ANNOT_FORMATS:
@@ -18,5 +17,6 @@ def files_from_dir(annot_dir, annot_format):
 
     format_module = getattr(crowsetta.formats, annot_format)
     ext = format_module.meta.ext
-    annot_files = glob(os.path.join(annot_dir, '*.' + ext))
+    annot_files = _files_from_dir(annot_dir, ext)
     return annot_files
+
