b929c933ef4358ed7be7a7d0cfb0c29ad335768a
NickleDave <nickledave@users.noreply.github.com>
2019-05-08 20:19:12 -0400

    start rewriting test_cli_funcs/test_prep.py

--- tests/unit_tests/test_cli_funcs/test_prep.py
+++ tests/unit_tests/test_cli_funcs/test_prep.py
@@ -26,27 +26,50 @@ class TestMakeData(unittest.TestCase):
         shutil.rmtree(self.tmp_output_dir)
         os.remove(self.tmp_config_path)
 
-    def test_prep_func(self):
+    def test_prep_with_audio_cbin(self):
         # all this does is make sure the functions runs without crashing.
         # need to write tests for functions that prep calls as well
         data_dir = os.path.join(TEST_DATA_DIR, 'cbins', 'gy6or6', '032312')
         spect_params = SpectConfig(fft_size=512, step_size=64, freq_cutoffs=(500, 10000), thresh=6.25,
                                    transform_type='log_spect')
-        vak.cli.prep(labelset=list('iabcdefghjk'),
-                     all_labels_are_int=False,
+        labelset = list('iabcdefghjk')
+
+        vak.cli.prep(labelset=labelset,
                      data_dir=data_dir,
                      total_train_set_dur=20,
                      val_dur=10,
                      test_dur=20,
                      config_file=self.tmp_config_path,
+                     annot_format='notmat',
                      silent_gap_label=0,
                      skip_files_with_labels_not_in_labelset=True,
                      output_dir=self.tmp_output_dir,
-                     mat_spect_files_path=None,
-                     mat_spects_annotation_file=None,
+                     audio_format='cbin',
+                     array_format=None,
+                     annot_file=None,
                      spect_params=spect_params)
+
         data_dicts = glob(os.path.join(self.tmp_output_dir, 'spectrograms*', '*dict*'))
         assert len(data_dicts) == 3
+
+    def test_prep_with_array_mat(self):
+        data_dir = os.path.join(TEST_DATA_DIR, 'mat', 'llb11', 'spect')
+        annot_file = os.path.join(TEST_DATA_DIR, 'mat', 'llb11', 'llb11_annot_subset.mat')
+        labelset = {1, 4, 5, 9, 10, 13, 15, 16, 17, 19, 20, 21, 23, 27}
+        vak.cli.prep(labelset=labelset,
+                     data_dir=data_dir,
+                     total_train_set_dur=20,
+                     val_dur=10,
+                     test_dur=20,
+                     config_file=self.tmp_config_path,
+                     annot_format='yarden',
+                     silent_gap_label=0,
+                     skip_files_with_labels_not_in_labelset=True,
+                     output_dir=self.tmp_output_dir,
+                     audio_format=None,
+                     array_format='mat',
+                     annot_file=annot_file,
+                     spect_params=None)
 
 
 if __name__ == '__main__':
