fa0012b0ab8eeb8c38c79102ee5d5eb9721c1ac8
NickleDave <nickledave@users.noreply.github.com>
2019-05-11 16:35:09 -0400

    still rewriting classes

--- src/vak/dataset/classes.py
+++ src/vak/dataset/classes.py
@@ -1,18 +1,22 @@
 import json
-from json import JSONEncoder, JSONDecoder
-import copy
+from json import JSONEncoder
 
 import numpy as np
 import attr
 from attr.validators import optional, instance_of
 from crowsetta import Sequence
 
+from ..utils.general import timebin_dur_from_vec
+
 
 def asarray_if_not(val):
-    if type(val) == np.ndarray:
-        return val
+    if val is None:
+        return None
     else:
-        return np.asarray(val)
+        if type(val) == np.ndarray:
+            return val
+        else:
+            return np.asarray(val)
 
 
 @attr.s(cmp=False)
@@ -27,16 +31,77 @@ class Spectrogram:
         vector of times in spectrogram, where each value is a bin center.
     timebin_dur : numpy.ndarray
         duration of a timebin in seconds from spectrogram
-    duration : numpy.ndarray
-        duration of spectrogram, i.e. len(time_bins) * timebin_dur
     array : numpy.ndarray
         spectrogram contained in an array
     """
     freq_bins = attr.ib(validator=instance_of(np.ndarray), converter=asarray_if_not)
     time_bins = attr.ib(validator=instance_of(np.ndarray), converter=asarray_if_not)
     timebin_dur = attr.ib(validator=instance_of(float))
-    duration = attr.ib(validator=instance_of(float))
     array = attr.ib(validator=optional(instance_of(np.ndarray)), converter=asarray_if_not)
+
+    @classmethod
+    def from_arr_file_dict(cls,
+                           arr_file_dict,
+                           freqbins_key='f',
+                           timebins_key='t',
+                           spect_key='s',
+                           timebin_dur=None,
+                           n_decimals_trunc=3):
+        """create a Spectrogram instance from a dictionary-like object that
+        provides access to arrays loaded from a file, e.g. a .mat or .npz file
+
+        Parameters
+        ----------
+        arr_file_dict : dict-like
+            dictionary-like object providing access to .mat or .npz file
+        freqbins_key : str
+            key for accessing vector of frequency bins in files. Default is 'f'.
+        timebins_key : str
+            key for accessing vector of time bins in files. Default is 't'.
+        spect_key : str
+            key for accessing spectrogram in files. Default is 's'.
+        timebin_dur : float
+            duration of time bins. Default is None. If None, then
+        n_decimals_trunc : int
+            number of decimal places to keep when truncating the timebin duration calculated from
+            the spectrogram arrays.
+            Default is 3, i.e. assumes milliseconds is the last significant digit.
+
+        Returns
+        -------
+        spect : vak.dataset.classes.Spectrogram
+            a Spectrogram instance with attributes freq_bins, time_bins, array, and timebin_dur
+        """
+        if timebin_dur is None:
+            timebin_dur = timebin_dur_from_vec(time_bins=arr_file_dict[timebins_key],
+                                               n_decimals_trunc=n_decimals_trunc)
+
+        return cls(freq_bins=arr_file_dict[freqbins_key],
+                   time_bins=arr_file_dict[timebins_key],
+                   array=arr_file_dict[spect_key],
+                   timebin_dur=timebin_dur)
+
+
+def voc_file_validator(instance, attribute, value):
+    if (
+            (attribute.name == 'audio_file' and value is None) and (instance.spect_file is None)
+        or
+            (attribute.name == 'spect_file' and value is None) and (instance.audio_file is None)
+    ):
+        raise ValueError(
+            'a vocalization must have either an audio_file or spect_file associated with it'
+            )
+
+
+def voc_data_validator(instance, attribute, value):
+    if (
+            (attribute.name == 'audio' and value is None) and (instance.spect is None)
+        or
+            (attribute.name == 'spect' and value is None) and (instance.audio is None)
+    ):
+        raise ValueError(
+            'a vocalization must have either an audio_file or spect_file associated with it'
+            )
 
 
 @attr.s(cmp=False)
@@ -56,7 +121,15 @@ class Vocalization:
     spect : vak.dataset.Spectrogram
         spectrogram of vocalization. Represented as an instance of the
         Spectrogram class, see docstring of that class for its attributes.
+    duration : float
+        duration of audio file and/or spectrogram associated with vocalization
     """
+    # mandatory attribute: duration
+    duration = attr.ib(validator=instance_of(float))
+
+    # less mandatory: annotation
+    # need at least labels from annotation + duration to split a dataset up by duration
+    # while maintaining class balance
     annotation = attr.ib(validator=optional(instance_of(Sequence)), default=None)
     @annotation.validator
     def is_list_tup_or_seq(self, attribute, value):
@@ -64,10 +137,16 @@ class Vocalization:
             raise TypeError(
                 f'annotations for Vocalization must be a crowsetta.Sequence'
             )
-    audio_file = attr.ib(validator=instance_of(str), default=None)
-    audio = attr.ib(validator=optional(instance_of(np.ndarray)), converter=asarray_if_not, default=None)
-    spect = attr.ib(validator=optional(instance_of(Spectrogram)), default=None)
-    spect_file = attr.ib(validator=optional(instance_of(str)), default=None)
+    # optional: need *one of* audio_file + audio or spect + spect_file
+    audio = attr.ib(validator=optional([instance_of(np.ndarray), voc_data_validator]),
+                    converter=asarray_if_not,
+                    default=None)
+    audio_file = attr.ib(validator=optional([instance_of(str), voc_file_validator]),
+                         default=None)
+    spect = attr.ib(validator=optional([instance_of(Spectrogram), voc_data_validator]),
+                    default=None)
+    spect_file = attr.ib(validator=optional([instance_of(str), voc_file_validator]),
+                         default=None)
 
 
 class VocalDatasetJSONEncoder(JSONEncoder):
@@ -130,5 +209,6 @@ class VocalDataset:
     def save(self, json_fname):
         self.to_json(json_fname)
 
-    def load(self, json_fname):
-        return self.from_json(json_fname)
+    @classmethod
+    def load(cls, json_fname):
+        return cls.from_json(json_fname=json_fname)
