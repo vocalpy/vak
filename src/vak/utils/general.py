3c16ae3c441a0c6d780fbab783dac52e36c9f4a4
NickleDave <nickledave@users.noreply.github.com>
2019-05-11 14:10:18 -0400

    add timebin_dur_from_vec function to vak.utils.general

--- src/vak/utils/general.py
+++ src/vak/utils/general.py
@@ -1,6 +1,8 @@
 import os
 from glob import glob
 
+import numpy as np
+
 
 def _files_from_dir(dir_path, ext):
     """helper function that gets all files with a given extension
@@ -44,3 +46,37 @@ def _files_from_dir(dir_path, ext):
         )
 
     return files
+
+
+def timebin_dur_from_vec(time_bins, n_decimals_trunc=3):
+    """compute duration of a time bin, given the
+    vector of time bin centers associated with a spectrogram
+
+    Parameters
+    ----------
+    time_bins : numpy.ndarray
+        vector of times in spectrogram, where each value is a bin center.
+    n_decimals_trunc : int
+        number of decimal places to keep when truncating the timebin duration calculated from
+        the spectrogram arrays.
+        Default is 3, i.e. assumes milliseconds is the last significant digit.
+
+    Returns
+    -------
+    timebin_dur : float
+        duration of a timebin, estimated from vector of times
+
+    Notes
+    -----
+    takes mean of pairwise difference between neighboring time bins,
+    to deal with floating point error, then rounds and truncates to specified decimal place
+    """
+    # first we round to the given number of decimals
+    timebin_dur = np.around(
+        np.mean(np.diff(time_bins)),
+        decimals=n_decimals_trunc
+    )
+    # only after rounding do we truncate any decimal place past decade
+    decade = 10 ** n_decimals_trunc
+    timebin_dur = np.trunc(timebin_dur * decade) / decade
+    return timebin_dur
