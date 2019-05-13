cd51870a29432d3575a1c349188bd11b2e0e117b
NickleDave <nickledave@users.noreply.github.com>
2019-05-12 21:19:10 -0400

    vak/dataset/__init__.py imports split

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,3 +1,3 @@
 """module that handles datasets:
 spectrograms made from audio files of vocalizations, and associated annotations"""
-from . import array, audio, annot
+from . import array, audio, annot, split
