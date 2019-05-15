e911cb917c5ab475f1f7eb832263731b3134a4e4
NickleDave <nickledave@users.noreply.github.com>
2019-05-15 18:12:08 -0400

    test_dataset/__init__.py imports all modules from sub-package

--- tests/unit_tests/test_dataset/__init__.py
+++ tests/unit_tests/test_dataset/__init__.py
@@ -1,5 +1,7 @@
 from .test_annot import TestAnnot
 from .test_array import TestArray
-from .test_classes import TestClasses
 from .test_audio import TestAudio
+from .test_classes import TestClasses
+from .test_prep import TestPrep
+from .test_split import TestSplit
 
