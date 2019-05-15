b71dc5e21d051415da8fcb530bce4a33f30b8a8e
NickleDave <nickledave@users.noreply.github.com>
2019-05-15 15:39:17 -0400

    dataset/__init__.py imports prep from .prep

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,3 +1,4 @@
 """module that handles datasets:
 spectrograms made from audio files of vocalizations, and associated annotations"""
 from . import array, audio, annot, split
+from .prep import prep
