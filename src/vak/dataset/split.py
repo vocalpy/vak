c746e77463bc38fae7cb9eb5e352a6622266f5ce
NickleDave <nickledave@users.noreply.github.com>
2019-05-16 15:01:56 -0400

    train_test_dur_split_inds uses functions from splitalgos sub-package

--- src/vak/dataset/split.py
+++ src/vak/dataset/split.py
@@ -3,6 +3,7 @@ import itertools
 import warnings
 
 from .classes import VocalizationDataset
+from .splitalgos import brute_force
 
 
 def train_test_dur_split_inds(durs,
@@ -10,7 +11,8 @@ def train_test_dur_split_inds(durs,
                               labelset,
                               train_dur=None,
                               test_dur=None,
-                              val_dur=None):
+                              val_dur=None,
+                              algo='brute_force'):
     """return indices to split a dataset into training, test, and validation sets of specified durations.
 
     Given the durations of a set of vocalizations, and labels from the annotations for those vocalizations,
@@ -36,6 +38,9 @@ def train_test_dur_split_inds(durs,
     val_dur : float
         Target duration for validation set, in seconds. Default is None.
         If None, no indices are returned for validation set.
+    algo : str
+        algorithm to use. One of {'brute_force', 'inc_freq'}. Default is 'brute_force'. For more information
+        on the algorithms, see the docstrings, e.g., vak.dataset.splitalgos.brute_force
 
     Returns
     -------
@@ -83,133 +88,12 @@ def train_test_dur_split_inds(durs,
             f'training, test, and (if specified) validation sets: {total_target_dur}'
         )
 
-    iter = 1
-    all_labels_err = ('Did not successfully divide data into training, '
-                      'validation, and test sets of sufficient duration '
-                      'after 1000 iterations.'
-                      ' Try increasing the total size of the data set.')
-
-    durs_labels_list = list(zip(durs, labels))
-
-    while 1:
-        train_inds = []
-        test_inds = []
-        if val_dur:
-            val_inds = []
-        else:
-            val_inds = None
-
-        total_train_dur = 0
-        total_val_dur = 0
-        total_test_dur = 0
-
-        choice = ['train', 'test']
-        if val_dur:
-            choice.append('val')
-
-        durs_labels_inds = list(range(len(durs_labels_list)))
-        random.shuffle(durs_labels_inds)
-        while 1:
-            # pop durations off list and append to randomly-chosen
-            # list, either train, val, or test set.
-            # Do this until the total duration for each data set is equal
-            # to or greater than the target duration for each set.
-            try:
-                ind = durs_labels_inds.pop()
-            except IndexError:
-                if len(durs_labels_inds) == 0:
-                    print(
-                        'Ran out of elements while dividing dataset into subsets of specified durations.'
-                        f'Iteration {iter}'
-                          )
-                    iter += 1
-                    break  # do next iteration
-                else:
-                    raise
-
-            which_set = random.randint(0, len(choice)-1)
-            which_set = choice[which_set]
-            if which_set == 'train':
-                train_inds.append(ind)
-                total_train_dur += durs_labels_list[ind][0]  # ind 0 is duration
-                if total_train_dur >= train_dur:
-                    choice.pop(choice.index('train'))
-            elif which_set == 'val':
-                val_inds.append(ind)
-                total_val_dur += durs_labels_list[ind][0]  # ind 0 is duration
-                if total_val_dur >= val_dur:
-                    choice.pop(choice.index('val'))
-            elif which_set == 'test':
-                test_inds.append(ind)
-                total_test_dur += durs_labels_list[ind][0]  # ind 0 is duration
-                if total_test_dur >= test_dur:
-                    choice.pop(choice.index('test'))
-
-            if len(choice) < 1:
-                total_all_durs = total_train_dur + total_test_dur
-                if val_dur:
-                    total_all_durs += total_val_dur
-                if total_all_durs < total_target_dur:
-                    raise ValueError(
-                        f'Loop to find subsets completed but total duration of subsets, {total_all_durs} seconds, '
-                        f'is less than total duration specified: {total_target_dur} seconds.')
-                else:
-                    break
-
-            if iter > 1000:
-                raise ValueError('Could not find subsets of sufficient duration in '
-                                 'less than 1000 iterations.')
-
-        # make sure that each set contains all classes we
-        # want the network to learn
-        train_tups = [durs_labels_list[ind] for ind in train_inds]  # tup = a tuple
-        test_tups = [durs_labels_list[ind] for ind in test_inds]
-        if val_dur:
-            val_tups = [durs_labels_list[ind] for ind in val_inds]
-
-        train_labels = itertools.chain.from_iterable(
-            [tup[1] for tup in train_tups])
-        train_labels = set(train_labels)  # make set to get unique values
-
-        test_labels = itertools.chain.from_iterable(
-            [tup[1] for tup in test_tups])
-        test_labels = set(test_labels)
-
-        if val_dur:
-            val_labels = itertools.chain.from_iterable(
-                [tup[1] for tup in val_tups])
-            val_labels = set(val_labels)
-
-        if train_labels != set(labelset):
-            iter += 1
-            if iter > 1000:
-                raise ValueError(all_labels_err)
-            else:
-                print('Train labels did not contain all labels in labelset. '
-                      'Getting new training set. Iteration {}'
-                      .format(iter))
-                continue
-        elif test_labels != set(labelset):
-            iter += 1
-            if iter > 1000:
-                raise ValueError(all_labels_err)
-            else:
-                print('Test labels did not contain all labels in labelset. '
-                      'Getting new test set. Iteration {}'
-                      .format(iter))
-                continue
-        elif val_dur is not None and val_labels != set(labelset):
-            iter += 1
-            if iter > 1000:
-                raise ValueError(all_labels_err)
-            else:
-                print('Validation labels did not contain all labels in labelset. '
-                      'Getting new validation set. Iteration {}'
-                      .format(iter))
-                continue
-
-        else:
-            break
+    if algo='brute_force':
+        train_inds, test_inds, val_inds = brute_force(durs, labels, labelset, train_dur, test_dur, val_dur)
+    else:
+        raise NotImplementedError(
+            f'algorithm {algo} not implemented'
+        )
 
     return train_inds, test_inds, val_inds
 
