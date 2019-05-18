5b67ba9c315025c33b3434190bb2a4a6d2e3a211
NickleDave <nickledave@users.noreply.github.com>
2019-05-17 20:23:48 -0400

    change 'test_array' to 'test_spect' in test_dataset/__init__.py

--- tests/unit_tests/test_dataset/__init__.py
+++ tests/unit_tests/test_dataset/__init__.py
@@ -1,7 +1,6 @@
 from .test_annot import TestAnnot
-from .test_array import TestArray
+from .test_spect import TestSpect
 from .test_audio import TestAudio
 from .test_classes import TestClasses
 from .test_prep import TestPrep
 from .test_split import TestSplit
-
