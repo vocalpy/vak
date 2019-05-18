8cb27973443bcdd46a22345f0b41e1d210d039cf
NickleDave <nickledave@users.noreply.github.com>
2019-05-17 20:24:42 -0400

    fix tests in test_dataset/test_classes.py

--- tests/unit_tests/test_dataset/test_classes.py
+++ tests/unit_tests/test_dataset/test_classes.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy.io import loadmat
 import crowsetta
 
+from vak.evfuncs import load_cbin
 import vak.dataset.spect
 import vak.dataset.annot
 from vak.dataset.classes import VocalizationDataset, Vocalization, SpectrogramFile
@@ -25,8 +26,8 @@ SETUP_SCRIPTS_DIR = os.path.join(HERE,
 
 class TestClasses(unittest.TestCase):
     def setUp(self):
-        self.array_dir_mat = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
-        self.array_list_mat = glob(os.path.join(self.array_dir_mat, '*.mat'))
+        self.spect_dir_mat = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
+        self.spect_list_mat = glob(os.path.join(self.spect_dir_mat, '*.mat'))
 
         self.annot_mat = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'llb3_annot_subset.mat')
         self.scribe = crowsetta.Transcriber(voc_format='yarden')
@@ -51,70 +52,70 @@ class TestClasses(unittest.TestCase):
         self.labelset_cbin = list('iabcdefghjk')
 
     def test_Spectrogram_init(self):
-        for arr_file in self.array_list_mat:
-            arr = loadmat(arr_file, squeeze_me=True)
-            a_spect = SpectrogramFile(freq_bins=arr['f'],
-                                      time_bins=arr['t'],
+        for spect_path in self.spect_list_mat:
+            spect_dict = loadmat(spect_path, squeeze_me=True)
+            a_spect = SpectrogramFile(freq_bins=spect_dict['f'],
+                                      time_bins=spect_dict['t'],
                                       timebin_dur=0.002,
-                                      array=arr['s']
+                                      spect=spect_dict['s']
                                       )
-            for attr in ['freq_bins', 'time_bins', 'timebin_dur', 'array']:
+            for attr in ['freq_bins', 'time_bins', 'timebin_dur', 'spect']:
                 self.assertTrue(hasattr(a_spect, attr))
-                if attr in ['freq_bins', 'time_bins', 'array']:
+                if attr in ['freq_bins', 'time_bins', 'spect']:
                     self.assertTrue(type(getattr(a_spect, attr)) == np.ndarray)
                 elif attr == 'timebin_dur':
                     self.assertTrue(type(getattr(a_spect, attr)) == float)
 
-    def test_Spectrogram_from_arr_file_dict(self):
-        for arr_file in self.array_list_mat:
-            arr_file_dict = loadmat(arr_file, squeeze_me=True)
-            a_spect = SpectrogramFile.from_arr_file_dict(arr_file_dict=arr_file_dict,
-                                                         freqbins_key='f',
-                                                         timebins_key='t',
-                                                         spect_key='s',
-                                                         timebin_dur=None,
-                                                         n_decimals_trunc=3)
-            for attr in ['freq_bins', 'time_bins', 'timebin_dur', 'array']:
-                self.assertTrue(hasattr(a_spect, attr))
-                if attr in ['freq_bins', 'time_bins', 'array']:
-                    self.assertTrue(type(getattr(a_spect, attr)) == np.ndarray)
+    def test_SpectrogramFile_from_dict(self):
+        for spect_path in self.spect_list_mat:
+            spect_dict = loadmat(spect_path, squeeze_me=True)
+            spect_file = SpectrogramFile.from_dict(spect_file_dict=spect_dict,
+                                                   freqbins_key='f',
+                                                   timebins_key='t',
+                                                   spect_key='s',
+                                                   timebin_dur=None,
+                                                   n_decimals_trunc=3)
+            for attr in ['freq_bins', 'time_bins', 'timebin_dur', 'spect']:
+                self.assertTrue(hasattr(spect_file, attr))
+                if attr in ['freq_bins', 'time_bins', 'spect']:
+                    self.assertTrue(type(getattr(spect_file, attr)) == np.ndarray)
                 elif attr == 'timebin_dur':
-                    self.assertTrue(type(getattr(a_spect, attr)) in (float, np.float16, np.float32, np.float64))
+                    self.assertTrue(type(getattr(spect_file, attr)) in (float, np.float16, np.float32, np.float64))
 
     def test_Vocalization_init(self):
-        for arr_file, annot in zip(self.array_list_mat, self.annot_list):
-            arr_file_dict = loadmat(arr_file, squeeze_me=True)
-            spect = SpectrogramFile.from_arr_file_dict(arr_file_dict=arr_file_dict,
-                                                       freqbins_key='f',
-                                                       timebins_key='t',
-                                                       spect_key='s',
-                                                       timebin_dur=None,
-                                                       n_decimals_trunc=3)
-            dur = spect.timebin_dur * spect.spect.shape[-1]
+        for spect_path, annot in zip(self.spect_list_mat, self.annot_list):
+            spect_dict = loadmat(spect_path, squeeze_me=True)
+            spect_file = SpectrogramFile.from_dict(spect_file_dict=spect_dict,
+                                                   freqbins_key='f',
+                                                   timebins_key='t',
+                                                   spect_key='s',
+                                                   timebin_dur=None,
+                                                   n_decimals_trunc=3)
+            dur = spect_file.timebin_dur * spect_file.spect.shape[-1]
             voc = Vocalization(annot=annot,
                                duration=dur,
-                               spect=spect,
-                               spect_file=arr_file)
-            for attr in ['annot', 'duration', 'spect', 'spect_file', 'audio', 'audio_file']:
+                               spect_file=spect_file,
+                               spect_path=spect_path)
+            for attr in ['annot', 'duration', 'spect_path', 'spect_file', 'audio', 'audio_path']:
                 self.assertTrue(hasattr(voc, attr))
             self.assertTrue(voc.duration == dur)
-            self.assertTrue(voc.spect_file == arr_file)
+            self.assertTrue(voc.spect_path == spect_path)
             self.assertTrue(voc.audio is None)
-            self.assertTrue(voc.audio_file is None)
+            self.assertTrue(voc.audio_path is None)
 
-        for cbin_file, annot in zip(self.array_list_mat, self.annot_list):
-            arr_file_dict = loadmat(arr_file, squeeze_me=True)
-            dur = spect.timebin_dur * spect.spect.shape[-1]
-            voc = Vocalization(annot=self.annot_list[0],
+        for audio_path, annot in zip(self.audio_files_cbin, self.annot_list_cbin):
+            fs, audio = load_cbin(audio_path)
+            dur = audio.shape[-1] / fs
+            voc = Vocalization(annot=annot,
                                duration=dur,
-                               spect=spect,
-                               spect_file=arr_file)
-            for attr in ['annot', 'duration', 'spect', 'spect_file', 'audio', 'audio_file']:
+                               audio=audio,
+                               audio_path=audio_path)
+            for attr in ['annot', 'duration', 'spect_path', 'spect_file', 'audio', 'audio_path']:
                 self.assertTrue(hasattr(voc, attr))
             self.assertTrue(voc.duration == dur)
-            self.assertTrue(voc.spect_file == arr_file)
-            self.assertTrue(voc.audio is None)
-            self.assertTrue(voc.audio_file is None)
+            self.assertTrue(voc.spect_path is None)
+            self.assertTrue(np.array_equal(voc.audio, audio))
+            self.assertTrue(voc.audio_path == audio_path)
 
         with self.assertRaises(ValueError):
             # because we didn't specify audio or spect or audio_file or spect_file
@@ -123,13 +124,13 @@ class TestClasses(unittest.TestCase):
                          duration=dur)
 
         with self.assertRaises(ValueError):
-            # because we didn't specify spect file
+            # because we didn't specify spect path
             Vocalization(annot=annot,
                          duration=dur,
-                         spect=spect)
+                         spect_file=spect_file)
 
         with self.assertRaises(ValueError):
-            # because we didn't specify audio file
+            # because we didn't specify audio path
             Vocalization(annot=annot,
                          duration=dur,
                          audio=np.random.normal(size=(1000, 1)))
@@ -138,43 +139,43 @@ class TestClasses(unittest.TestCase):
         # without loading the spectrogram into it
         a_voc = Vocalization(annot=annot,
                              duration=dur,
-                             spect_file=arr_file)
-        for attr in ['annot', 'duration', 'spect', 'spect_file', 'audio', 'audio_file']:
-            self.assertTrue(hasattr(voc, attr))
+                             spect_path=spect_path)
+        for attr in ['annot', 'duration', 'spect_path', 'spect_file', 'audio', 'audio_path']:
+            self.assertTrue(hasattr(a_voc, attr))
         self.assertTrue(a_voc.duration == dur)
-        self.assertTrue(a_voc.spect is None)
-        self.assertTrue(a_voc.spect_file == arr_file)
+        self.assertTrue(a_voc.spect_file is None)
+        self.assertTrue(a_voc.spect_path == spect_path)
         self.assertTrue(a_voc.audio is None)
-        self.assertTrue(a_voc.audio_file is None)
+        self.assertTrue(a_voc.audio_path is None)
 
         # this should work, because we want to be able to have a Vocalization
         # without loading the audio into it
         a_voc = Vocalization(annot=annot,
                              duration=dur,
-                             audio_file=self.audio_files_cbin[0])
-        for attr in ['annot', 'duration', 'spect', 'spect_file', 'audio', 'audio_file']:
-            self.assertTrue(hasattr(voc, attr))
+                             audio_path=self.audio_files_cbin[0])
+        for attr in ['annot', 'duration', 'spect_path', 'spect_file', 'audio', 'audio_path']:
+            self.assertTrue(hasattr(a_voc, attr))
         self.assertTrue(a_voc.duration == dur)
-        self.assertTrue(a_voc.spect is None)
         self.assertTrue(a_voc.spect_file is None)
+        self.assertTrue(a_voc.spect_path is None)
         self.assertTrue(a_voc.audio is None)
-        self.assertTrue(a_voc.audio_file == self.audio_files_cbin[0])
+        self.assertTrue(a_voc.audio_path == self.audio_files_cbin[0])
 
     def test_VocalizationDataset_init(self):
         voc_list = []
-        for arr_file, annot in zip(self.array_list_mat, self.annot_list):
-            arr_file_dict = loadmat(arr_file, squeeze_me=True)
-            spect = SpectrogramFile.from_arr_file_dict(arr_file_dict=arr_file_dict,
-                                                       freqbins_key='f',
-                                                       timebins_key='t',
-                                                       spect_key='s',
-                                                       timebin_dur=None,
-                                                       n_decimals_trunc=3)
-            dur = spect.timebin_dur * spect.spect.shape[-1]
+        for spect_path, annot in zip(self.spect_list_mat, self.annot_list):
+            spect_dict = loadmat(spect_path, squeeze_me=True)
+            spect_file = SpectrogramFile.from_dict(spect_file_dict=spect_dict,
+                                                   freqbins_key='f',
+                                                   timebins_key='t',
+                                                   spect_key='s',
+                                                   timebin_dur=None,
+                                                   n_decimals_trunc=3)
+            dur = spect_file.timebin_dur * spect_file.spect.shape[-1]
             voc = Vocalization(annot=annot,
                                duration=dur,
-                               spect=spect,
-                               spect_file=arr_file)
+                               spect_file=spect_file,
+                               spect_path=spect_path)
             voc_list.append(voc)
 
         vds = VocalizationDataset(voc_list=voc_list)
@@ -195,7 +196,7 @@ class TestClasses(unittest.TestCase):
 
     def test_VocalizationDataset_json(self):
         vds = vak.dataset.spect.from_files(spect_format='mat',
-                                           spect_dir=self.array_dir_mat,
+                                           spect_dir=self.spect_dir_mat,
                                            annot_list=self.annot_list,
                                            load_spects=True)
         vds_json_str = vds.to_json(json_fname=None)
@@ -206,7 +207,7 @@ class TestClasses(unittest.TestCase):
         voc_list = vds_json['voc_list']
         self.assertTrue(type(voc_list) == list)
         for voc in voc_list:
-            for key in ['annot', 'duration', 'spect', 'spect_file', 'audio', 'audio_file']:
+            for key in ['annot', 'duration', 'spect_path', 'spect_file', 'audio', 'audio_path']:
                 self.assertTrue(key in voc)
 
         vds_from_json = VocalizationDataset.from_json(json_str=vds_json_str)
@@ -218,41 +219,44 @@ class TestClasses(unittest.TestCase):
 
     def test_VocalizationDataset_load_spects(self):
         vds = vak.dataset.spect.from_files(spect_format='mat',
-                                           spect_dir=self.array_dir_mat,
+                                           spect_dir=self.spect_dir_mat,
                                            annot_list=self.annot_list,
                                            load_spects=False)
         self.assertTrue(
-            all([voc.spect is None for voc in vds.voc_list])
+            all([voc.spect_file is None for voc in vds.voc_list])
         )
 
-        spect_files_before = [voc.spect_file for voc in vds.voc_list]
+        # check whether order of spect paths changes because we convert voc_list
+        # to a dask bag to parallelize loading and order is usually preserved but this
+        # is not guaranteed
+        spect_paths_before = [voc.spect_path for voc in vds.voc_list]
         vds.load_spects()
         self.assertTrue(
-            all([type(voc.spect) == SpectrogramFile for voc in vds.voc_list])
+            all([type(voc.spect_file) == SpectrogramFile for voc in vds.voc_list])
         )
-        spect_files_after = [voc.spect_file for voc in vds.voc_list]
-        for before, after in zip(spect_files_before, spect_files_after):
+        spect_paths_after = [voc.spect_path for voc in vds.voc_list]
+        for before, after in zip(spect_paths_before, spect_paths_after):
             self.assertTrue(
                 before == after
             )
 
     def test_VocalizationDataset_clear_spects(self):
         vds = vak.dataset.spect.from_files(spect_format='mat',
-                                           spect_dir=self.array_dir_mat,
+                                           spect_dir=self.spect_dir_mat,
                                            annot_list=self.annot_list,
                                            load_spects=True)
         self.assertTrue(
-            all([type(voc.spect) == SpectrogramFile for voc in vds.voc_list])
+            all([type(voc.spect_file) == SpectrogramFile for voc in vds.voc_list])
         )
 
         vds.clear_spects()
         self.assertTrue(
-            all([voc.spect is None for voc in vds.voc_list])
+            all([voc.spect_file is None for voc in vds.voc_list])
         )
 
     def test_VocalizationDataset_are_spects_loaded(self):
         vds = vak.dataset.spect.from_files(spect_format='mat',
-                                           spect_dir=self.array_dir_mat,
+                                           spect_dir=self.spect_dir_mat,
                                            annot_list=self.annot_list,
                                            load_spects=False)
         self.assertTrue(
@@ -266,7 +270,7 @@ class TestClasses(unittest.TestCase):
 
     def test_VocalizationDataset_spects_list(self):
         vds = vak.dataset.spect.from_files(spect_format='mat',
-                                           spect_dir=self.array_dir_mat,
+                                           spect_dir=self.spect_dir_mat,
                                            annot_list=self.annot_list,
                                            load_spects=True)
         spects_list = vds.spects_list()
