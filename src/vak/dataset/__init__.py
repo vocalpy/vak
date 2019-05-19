f6f5d36ed8f4bc8a48cebd2fe087ebe9badbe69d
NickleDave <nickledave@users.noreply.github.com>
2019-05-19 13:54:26 -0400

    make classes sub-package in vak/dataset/

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,4 +1,5 @@
 """module that handles datasets:
 spectrograms made from audio files of vocalizations, and associated annotations"""
+from .classes import MetaSpect, Vocalization, VocalizationDataset
 from . import spect, audio, annot, split
 from .prep import prep
