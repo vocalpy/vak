5163241705cef1af158a3ea5353b1c0a2139ce22
NickleDave <nickledave@users.noreply.github.com>
2019-06-13 12:11:54 -0400

    switch to using pathlib in test_utils/test_general.py

--- tests/unit_tests/test_utils/test_general.py
+++ tests/unit_tests/test_utils/test_general.py
@@ -1,5 +1,4 @@
-import os
-from glob import glob
+from pathlib import Path
 import unittest
 
 import numpy as np
@@ -7,26 +6,28 @@ import numpy as np
 import vak.utils.general
 
 
-HERE = os.path.dirname(__file__)
-TEST_DATA_DIR = os.path.join(HERE, '..', '..', 'test_data')
+HERE = Path(__file__).parent
+TEST_DATA_DIR = HERE.joinpath('..', '..', 'test_data')
 
 
 class TestGeneral(unittest.TestCase):
     def setUp(self):
-        self.mat_spect_path = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
-        self.cbin_path = os.path.join(TEST_DATA_DIR, 'cbins', 'gy6or6', '032312')
+        self.mat_spect_path = TEST_DATA_DIR.joinpath('mat', 'llb3', 'spect')
+        self.cbin_path = TEST_DATA_DIR.joinpath('cbins', 'gy6or6', '032312')
 
     def test_files_from_dir(self):
         # check works for .mat files
-        mat_files = glob(os.path.join(self.mat_spect_path, '*.mat'))
+        mat_files = self.mat_spect_path.glob('*.mat')
+        mat_files = [str(path) for path in mat_files]
         files = vak.utils.general._files_from_dir(self.mat_spect_path, 'mat')
         self.assertTrue(
             sorted(mat_files) == sorted(files)
         )
 
-        # no reason logically why it shouldn't also work for other file types, but just in case ...
-        # we check for an audio file type too
-        cbin_files = glob(os.path.join(self.cbin_path, '*.cbin'))
+        # no reason logically why it shouldn't also work for other file types,
+        # but just in case ... we check for an audio file type too
+        cbin_files = self.cbin_path.glob('*.cbin')
+        cbin_files = [str(path) for path in cbin_files]
         files = vak.utils.general._files_from_dir(self.cbin_path, 'cbin')
         self.assertTrue(
             sorted(cbin_files) == sorted(files)
