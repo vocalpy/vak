256649ef9356fde5320cf5f9fbf6b7eddbc5c058
NickleDave <nickledave@users.noreply.github.com>
2019-06-23 09:12:51 -0400

    single-source version
    - add __about__.py to package root that defines __version__ and other
      info used in setup.py
    - vak.__init__ imports this
    - and setup.py loads the same variables into memory by running exec on the
      __about__.py file
    - basically the "warehouse" approach suggested on the PyPA page:
      https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version

--- src/vak/__init__.py
+++ src/vak/__init__.py
@@ -1,3 +1,16 @@
+from .__about__ import (
+    __author__,
+    __commit__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
+
 from . import __main__
 from . import cli
 from . import config
