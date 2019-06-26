c23c9d64b4282acc598dcf2e4e35e8b9cae5b0aa
NickleDave <nickledave@users.noreply.github.com>
2019-06-26 12:20:34 -0400

    rewrite vak.cli.predict to work with VocalizationDatasets

--- src/vak/cli/predict.py
+++ src/vak/cli/predict.py
@@ -1,227 +1,185 @@
-import os
 from glob import glob
-from datetime import datetime
-import pickle
+import os
 
 import joblib
 import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
-from ..utils.data import reshape_data_for_batching
-from ..dataset.mat import from_mat
+from ..dataset import VocalizationDataset
 from .. import network
+from ..utils.data import reshape_data_for_batching
+from ..utils.labels import lbl_tb2labels
 
 
-def predict(checkpoint_path,
+def predict(predict_vds_path,
+            checkpoint_path,
             networks,
-            labels_mapping_path,
-            spect_params,
-            dir_to_predict=None,
-            mat_spect_files_path=None,
-            spect_scaler_path=None
+            labelmap,
+            spect_scaler_path=None,
+            save_predict_vds=True
             ):
     """make predictions with one trained model
 
     Parameters
     ----------
+    predict_vds_path : str, list
+        path or list of paths to VocalizationDataset(s) for which
+        annotation should be predicted.
     checkpoint_path : str
         path to directory with saved model
     networks : dict
         where each key is the name of a neural network and the corresponding
         value is the configuration for that network (in a namedtuple or a dict)
-    labels_mapping_path : str
-        path to file that contains labels mapping, to convert output from consecutive
-        digits back to labels used for audio segments (e.g. birdsong syllables)
-    spect_params : dict
-        Dictionary of parameters for creating spectrograms.
-    dir_to_predict : str
-        path to directory where input files are located
-    mat_spect_files_path
-        path to directory with .mat files containing spectrograms that should be used
-        as data for which predictions are made
+    labelmap : dict
+        maps set of labels for vocalizations to consecutive integer
+        values {0,1,2,...N}, where N is the number of classes, i.e., label types
     spect_scaler_path : str
         path to a saved SpectScaler object used to normalize spectrograms.
         If spectrograms were normalized and this is not provided, will give
         incorrect results.
         Default is None.
+    save_predict_vds : bool
+        if True, save VocalizationDataset after predicting labels.
+        Default is True.
 
     Returns
     -------
     None
     """
-    timenow = datetime.now().strftime('%y%m%d_%H%M%S')
-
-    if dir_to_predict is None and mat_spect_files_path is None:
-        raise ValueError('must specify either dir_to_predict or mat_spect_files_path but both are None')
-
-    if dir_to_predict and mat_spect_files_path:
-        raise ValueError('got values for both dir_to_predict and mat_spect_files_path, not clear which to use')
-
-    with open(labels_mapping_path, 'rb') as labels_map_file_obj:
-        labels_mapping = pickle.load(labels_map_file_obj)
-    n_syllables = len(labels_mapping)
-
-    if mat_spect_files_path:
-        print('will use spectrograms from .mat files in {}'
-              .format(mat_spect_files_path))
-        mat_spect_files = glob(os.path.join(mat_spect_files_path, '*.mat'))
-        spects_dir = os.path.join(mat_spect_files_path,
-                                  'spectrograms_' + timenow)
-        os.mkdir(spects_dir)
-        spect_files_path = from_mat(mat_spect_files,
-                                    mat_spects_annotation_file,
-                                    spects_dir,
-                                    labels_mapping=labels_mapping)
-        dir_to_predict = spect_files_path
-
-    else:
-        if not os.path.isdir(dir_to_predict):
-            raise FileNotFoundError('directory {}, specified as '
-                                    'dir_to_predict, is not found.'
-                                    .format(dir_to_predict))
-
-        spects_dir = os.path.join(dir_to_predict,
-                              'spectrograms_' + timenow)
-        os.mkdir(spects_dir)
-
-        cbins = glob(os.path.join(dir_to_predict, '*.cbin'))
-        if cbins == []:
-            # if we don't find .cbins in data_dir, look in sub-directories
-            cbins = []
-            subdirs = glob(os.path.join(dir_to_predict, '*/'))
-            for subdir in subdirs:
-                cbins.extend(glob(os.path.join(dir_to_predict,
-                                               subdir,
-                                               '*.cbin')))
-        if cbins == []:
-            # try looking for .wav files
-            wavs = glob(os.path.join(dir_to_predict, '*.wav'))
-
-            if cbins == [] and wavs == []:
-                raise FileNotFoundError('No .cbin or .wav files found in {} or'
-                                        'immediate sub-directories'
-                                        .format(dir_to_predict))
-
-        if cbins:
-            spect_files_path = \
-                from_list(cbins,
-                          spect_params,
-                          spects_dir,
-                          labels_mapping,
-                          skip_files_with_labels_not_in_labelset=False,
-                          is_for_predict=True)
-        elif wavs:
-            spect_files_path = \
-                from_list(wavs,
-                          spect_params,
-                          spects_dir,
-                          labels_mapping,
-                          skip_files_with_labels_not_in_labelset=False,
-                          is_for_predict=True)
-
-    # TODO should be able to just call dataset here, right?
-    # instead of forcing user to specify spect_file_list
-    # should give them option to do either
-    spect_file_list = glob(os.path.join(spects_dir,
-                                        '*.spect'))
-    if spect_file_list == []:
-        raise ValueError('did not find any .spect files in {}'
-                         .format(dir_to_predict))
-
-    X_data = []
-    for spect_file in spect_file_list:
-        spect_dict = joblib.load(spect_file)
-        X_data.append(spect_dict['spect'].T)
-    freq_bins_all_spects = [Xd.shape[-1] for Xd in X_data]
-    uniq_freq_bins = set(freq_bins_all_spects)
-    if len(uniq_freq_bins) != 1:
-        raise ValueError('Found spectrograms with different numbers of frequency bins in dir_to_predict.\n'
-                         f'Different values for numbers of frequency bins found were: {uniq_freq_bins}')
-    else:
-        freq_bins = list(uniq_freq_bins)[0]
-
     if not os.path.isdir(checkpoint_path):
         raise FileNotFoundError('directory {}, specified as '
                                 'checkpoint_path, is not found.'
                                 .format(checkpoint_path))
-    checkpoint_file = tf.train.latest_checkpoint(checkpoint_path)
-    meta_file = glob(checkpoint_file + '*meta*')
-    if len(meta_file) > 1:
-        raise ValueError('found more than one .meta file in {}'
-                         .format(checkpoint_path))
-    elif len(meta_file) < 1:
-        raise ValueError('did not find .meta file in {}'
-                         .format(checkpoint_path))
+
+    if spect_scaler_path:
+        if not os.path.isfile(spect_scaler_path):
+            raise FileNotFoundError(
+                f'file for spect_scaler not found at path:\n{spect_scaler_path}'
+            )
+        spect_scaler = joblib.load(spect_scaler_path)
     else:
-        meta_file = meta_file[0]
+        spect_scaler = None
 
-    data_file = glob(checkpoint_file + '*data*')
-    if len(data_file) > 1:
-        raise ValueError('found more than one .data file in {}'
-                         .format(checkpoint_dir))
-    elif len(data_file) < 1:
-        raise ValueError('did not find .data file in {}'
-                         .format(checkpoint_dir))
+    if type(predict_vds_path) is str:
+        predict_vds_path = [predict_vds_path]
+    elif type(predict_vds_path) is list:
+        pass
     else:
-        data_file = data_file[0]
+        raise TypeError(
+            'predict_vds_path should be a string path or list '
+            f'of string paths, but type was {type(predict_vds_path)}'
+        )
 
-    num_spect_files = len(spect_file_list)
+    n_classes = len(labelmap)  # used below when instantiating network
+    predict_vds_out = []
+    for p_vds_path in predict_vds_path:
+        predict_vds = VocalizationDataset.load(json_fname=p_vds_path)
 
-    NETWORKS = network._load()
+        if predict_vds.are_spects_loaded() is False:
+            predict_vds = predict_vds.load_spects()
 
-    for net_name, net_config in networks.items():
-        net_config_dict = net_config._asdict()
-        net_config_dict['n_syllables'] = n_syllables
-        if 'freq_bins' in net_config_dict:
-            net_config_dict['freq_bins'] = freq_bins
-        net = NETWORKS[net_name](**net_config_dict)
+        X_data = predict_vds.spects_list()
+        X_data_spect_ID_vector = np.concatenate(
+            [np.ones((spect.shape[-1],), dtype=np.int64) * ind
+             for ind, spect in enumerate(X_data)]
+        )
+        X_data = np.concatenate(X_data, axis=1)
 
-        if spect_scaler_path:
-            spect_scaler = joblib.load(spect_scaler_path)
+        timebin_dur = set([voc.metaspect.timebin_dur for voc in predict_vds.voc_list])
+        if len(timebin_dur) > 1:
+            raise ValueError(
+                'found more than one time bin duration in '
+                f'VocalizationDataset: {timebin_dur}'
+            )
+        elif len(timebin_dur) == 1:
+            timebin_dur = timebin_dur.pop()
+        else:
+            raise ValueError(
+                f'invalid time bin durations from training set: {timebin_dur}'
+            )
 
-        with tf.Session(graph=net.graph) as sess:
-            tf.logging.set_verbosity(tf.logging.ERROR)
+        # transpose X_data, so rows are timebins and columns are frequency bins
+        # because networks expect this orientation for input
+        X_data = X_data.T
+        freq_bins = X_data.shape[-1]  # needed for net config
 
-            net.restore(sess=sess,
-                        meta_file=meta_file,
-                        data_file=data_file)
+        checkpoint_file = tf.train.latest_checkpoint(checkpoint_path)
+        meta_file = glob(checkpoint_file + '*meta*')
+        if len(meta_file) > 1:
+            raise ValueError('found more than one .meta file in {}'
+                             .format(checkpoint_path))
+        elif len(meta_file) < 1:
+            raise ValueError('did not find .meta file in {}'
+                             .format(checkpoint_path))
+        else:
+            meta_file = meta_file[0]
 
-            preds_dict = {}
-            pbar = tqdm(spect_file_list)
-            for file_num, spect_file in enumerate(pbar):
-                pbar.set_description(
-                    f'Predicting labels for {os.path.basename(spect_file)}, file {file_num} of {num_spect_files}'
-                )
+        data_file = glob(checkpoint_file + '*data*')
+        if len(data_file) > 1:
+            raise ValueError('found more than one .data file in {}'
+                             .format(checkpoint_path))
+        elif len(data_file) < 1:
+            raise ValueError('did not find .data file in {}'
+                             .format(checkpoint_path))
+        else:
+            data_file = data_file[0]
 
-                Xd = X_data[file_num]
-                if spect_scaler_path:
-                    Xd = spect_scaler.transform(Xd)
-                (Xd_batch,
-                 num_batches) = reshape_data_for_batching(Xd,
-                                                          batch_size=net_config.batch_size,
-                                                          time_steps=net_config.time_bins)
+        NETWORKS = network._load()
 
-                if 'Y_pred' in locals():
-                    del Y_pred
-                # work through current spectrogram batch by batch
-                for b in range(num_batches):  # "b" is "batch number"
-                    d = {net.X: Xd_batch[:, b * net_config.time_bins: (b + 1) * net_config.time_bins, :],
-                         net.lng: [net_config.time_bins] * net_config.batch_size}
-                    if 'Y_pred' in locals():
-                        # if Y_pred exists, we concatenate with new predictions
-                        # for next batch
-                        preds = sess.run(net.predict, feed_dict=d)
-                        preds = preds.reshape(-1)  # batch_size
-                        Y_pred = np.concatenate((Y_pred, preds), axis=0)
-                    else:  # if Y_pred doesn't exist yet
-                        Y_pred = sess.run(net.predict, feed_dict=d)
-                        Y_pred = Y_pred.reshape(-1)
+        for net_name, net_config in networks.items():
+            net_config_dict = net_config._asdict()
+            net_config_dict['n_syllables'] = n_classes
+            if 'freq_bins' in net_config_dict:
+                net_config_dict['freq_bins'] = freq_bins
+            net = NETWORKS[net_name](**net_config_dict)
+
+            if spect_scaler:
+                X_data = spect_scaler.transform(X_data)
+
+            (Xd_batch,
+             num_batches) = reshape_data_for_batching(X_data,
+                                                      batch_size=net_config.batch_size,
+                                                      time_steps=net_config.time_bins)
+
+            time_bins = net_config.time_bins  # for brevity below
+            with tf.Session(graph=net.graph) as sess:
+                tf.logging.set_verbosity(tf.logging.ERROR)
+
+                net.restore(sess=sess,
+                            meta_file=meta_file,
+                            data_file=data_file)
+
+                Y_pred = []
+                pbar = tqdm(range(num_batches))
+                for b in pbar:
+                    pbar.set_description(
+                        f'Predicting labels for {predict_vds_path}'
+                    )
+
+                    d = {net.X: Xd_batch[:, b * time_bins: (b + 1) * time_bins, :],
+                         net.lng: [time_bins] * net_config.batch_size}
+
+                    # if Y_pred exists, we concatenate with new predictions
+                    # for next batch
+                    preds = sess.run(net.predict, feed_dict=d)
+                    preds = preds.reshape(-1)  # batch_size
+                    Y_pred.append(preds)
 
                 # remove zero padding added by reshape_data_for_batching function
                 Y_pred = Y_pred.ravel()
-                Y_pred = Y_pred[0:Xd.shape[0]]
-                preds_dict[spect_file] = Y_pred
+                Y_pred = Y_pred[0:X_data.shape[0]]
 
-        fname = os.path.join(dir_to_predict, 'predictions')
-        joblib.dump(preds_dict, fname)
+            lbl_tbs = [Y_pred[X_data_spect_ID_vector == ind]
+                       for ind in np.unique(X_data_spect_ID_vector)]
+            for lbl_tb, voc in zip(lbl_tbs, predict_vds.voc_list):
+                voc.metaspect.lbl_tb = lbl_tb
+                labels = lbl_tb2labels(lbl_tb, labelmap)
+                voc.annot.labels = labels
+
+            if save_predict_vds:
+                predict_vds.save(json_fname=predict_vds_path)
+
+            predict_vds_out.append(predict_vds)
+
+        return predict_vds_out
