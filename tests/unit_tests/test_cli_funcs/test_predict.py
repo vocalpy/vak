c49d59b83bc418355225228f61c80addeedc8ea4
NickleDave <nickledave@users.noreply.github.com>
2019-06-26 21:11:22 -0400

    rewrite test_cli_funcs/test_predict.py

--- tests/unit_tests/test_cli_funcs/test_predict.py
+++ tests/unit_tests/test_cli_funcs/test_predict.py
@@ -1,21 +1,21 @@
 """tests for vak.cli.predict module"""
-import os
-import tempfile
-import shutil
-import unittest
 from configparser import ConfigParser
-from glob import glob
+import os
+from pathlib import Path
+import shutil
+import tempfile
+import unittest
 
-import vak.utils
+import numpy as np
+
 import vak.cli.predict
 from vak.core.learncurve import LEARN_CURVE_DIR_STEM
+from vak.dataset import VocalizationDataset
+import vak.utils
 
-HERE = os.path.dirname(__file__)
-TEST_CONFIGS_PATH = os.path.join(HERE, '..', 'test_data', 'configs')
-TEST_DATA_DIR = os.path.join(HERE,
-                             '..',
-                             '..',
-                             'test_data')
+HERE = Path(__file__).parent
+TEST_DATA_DIR = HERE.joinpath('..', '..', 'test_data')
+TEST_CONFIGS_PATH = TEST_DATA_DIR.joinpath('configs')
 
 
 def copydir(src, dst):
@@ -25,53 +25,64 @@ def copydir(src, dst):
         shutil.copy2(s, d)
 
 
-@unittest.skip('need to fix predict')
 class TestPredict(unittest.TestCase):
     def setUp(self):
         self.tmp_output_dir = tempfile.mkdtemp()
-        a_config = os.path.join(TEST_DATA_DIR, 'configs', 'test_predict_config.ini')
-        self.tmp_config_path = os.path.join(TEST_DATA_DIR, 'configs', 'tmp_config.ini')
-        shutil.copy(a_config, self.tmp_config_path)
 
-        # copy some data to predict to a temporary dir
-        self.tmp_dir_to_predict = tempfile.mkdtemp()
-        self.tmp_dir_to_predict = os.path.join(self.tmp_dir_to_predict, '032312')
-        os.makedirs(self.tmp_dir_to_predict)
-        src = os.path.join(TEST_DATA_DIR, 'cbins', 'gy6or6', '032312')
-        copydir(src=src, dst=self.tmp_dir_to_predict)
-
-        a_results_dir = glob(os.path.join(TEST_DATA_DIR,
-                                          'results',
-                                          f'{LEARN_CURVE_DIR_STEM}*'))[0]
-        a_training_records_dir = glob(os.path.join(a_results_dir,
-                                                   'train',
-                                                   'records_for_training_set*'))[0]
-        checkpoint_path = os.path.join(a_training_records_dir, 'TweetyNet', 'checkpoints')
-        spect_scaler = glob(os.path.join(a_training_records_dir, 'spect_scaler_*'))[0]
+        a_results_dir = list(
+            TEST_DATA_DIR.joinpath('results').glob(
+                f'{LEARN_CURVE_DIR_STEM}*'))[0]
+        a_training_records_dir = list(
+            Path(a_results_dir).joinpath(
+                'train').glob('records_for_training_set*'))[0]
+        checkpoint_path = str(Path(a_training_records_dir).joinpath(
+            'TweetyNet', 'checkpoints'))
+        spect_scaler = list(
+            Path(a_training_records_dir).glob('spect_scaler_*'))[0]
+        spect_scaler = str(spect_scaler)
 
         # rewrite config so it points to data for testing + temporary output dirs
+        # rewrite config so it points to data for testing + temporary output dirs
+        a_config = str(TEST_CONFIGS_PATH.joinpath('test_predict_config.ini'))
         config = ConfigParser()
-        config.read(self.tmp_config_path)
-        config['DATA']['data_dir'] = TEST_DATA_DIR
+        config.read(a_config)
         config['PREDICT']['checkpoint_path'] = checkpoint_path
-        config['PREDICT']['dir_to_predict'] = self.tmp_dir_to_predict
         config['PREDICT']['spect_scaler_path'] = spect_scaler
-        with open(self.tmp_config_path, 'w') as fp:
-            config.write(fp)
+
+        test_data_vds_path = list(TEST_DATA_DIR.glob('vds'))[0]
+        for stem in ['train', 'test']:
+            vds_path = list(test_data_vds_path.glob(f'*.{stem}.vds.json'))
+            self.assertTrue(len(vds_path) == 1)
+            vds_path = vds_path[0]
+            if stem == 'train':
+                self.train_vds_path = str(shutil.copy(vds_path, self.tmp_output_dir))
+                config['PREDICT']['train_vds_path'] = self.train_vds_path
+            elif stem == 'test':
+                # pretend test data is data we want to predict
+                self.predict_vds_path = str(shutil.copy(vds_path, self.tmp_output_dir))
+                dst = self.predict_vds_path.replace('test', 'predict_before')
+                self.predict_vds_path_before = shutil.copyfile(self.predict_vds_path,
+                                                               dst=dst)
+                config['PREDICT']['predict_vds_path'] = self.predict_vds_path
+
+        self.config_obj = config
 
     def tearDown(self):
-        shutil.rmtree(self.tmp_dir_to_predict)
-        os.remove(self.tmp_config_path)
+        shutil.rmtree(self.tmp_output_dir)
 
     def test_predict_func(self):
-        # make sure predict runs without crashing.
-        config = vak.config.parse.parse_config(self.tmp_config_path)
-        vak.cli.predict(checkpoint_path=config.predict.checkpoint_path,
-                        networks=config.networks,
-                        labels_mapping_path=config.predict.labels_mapping_path,
-                        spect_params=config.spect_params,
-                        dir_to_predict=config.predict.dir_to_predict,
-                        spect_scaler_path=config.predict.spect_scaler_path)
+        predict_config = vak.config.predict.parse_predict_config(self.config_obj)
+        networks = vak.config.parse._get_nets_config(self.config_obj,
+                                                     predict_config.networks)
+        vak.cli.predict(predict_vds_path=predict_config.predict_vds_path,
+                        train_vds_path=predict_config.train_vds_path,
+                        checkpoint_path=predict_config.checkpoint_path,
+                        networks=networks,
+                        spect_scaler_path=predict_config.spect_scaler_path)
+        predict_vds_after = VocalizationDataset.load(self.predict_vds_path)
+        for voc in predict_vds_after.voc_list:
+            self.assertTrue(type(voc.metaspect.lbl_tb) is np.ndarray)
+            self.assertTrue(type(voc.annot.labels) is np.ndarray)
 
 
 if __name__ == '__main__':
