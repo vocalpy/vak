ab0aa8242c393fdcccf460ff78338a42b367b0fb
NickleDave <nickledave@users.noreply.github.com>
2019-07-16 13:07:21 -0500

    add find_audio_fname function to vak/dataset/spect.py module

--- src/vak/dataset/spect.py
+++ src/vak/dataset/spect.py
@@ -1,6 +1,7 @@
-import os
 from glob import glob
 import logging
+import os
+from pathlib import Path
 
 import numpy as np
 from scipy.io import loadmat
@@ -10,7 +11,57 @@ from dask.diagnostics import ProgressBar
 from .annotation import source_annot_map
 from .classes import MetaSpect, Vocalization, VocalizationDataset
 from ..config import validators
-from ..utils.general import timebin_dur_from_vec
+from ..utils.general import find_fname, timebin_dur_from_vec
+
+
+def find_audio_fname(spect_path, audio_ext=None):
+    """finds name of audio file in a path to a spectogram file,
+    if one is present.
+
+    Checks for any extension that is a valid audio file format
+    and returns path up to and including that extension,
+    i.e. with the spectrogram file extension removed.
+
+    Parameters
+    ----------
+    spect_path : str
+        path to a spectrogram file
+    audio_ext : str
+        extension associated with an audio file format, used to
+        find audio file name in spect_path.
+        Default is None. If None, search for any valid audio format
+        (as defined by vak.config.validators.VALID_AUDIO_FORMATS)
+
+    Returns
+    -------
+    audio_fname : str
+        name of audio file found in spect_path
+    """
+    if audio_ext is None:
+        audio_ext = validators.VALID_AUDIO_FORMATS
+    elif type(audio_ext) is str:
+        audio_ext = [audio_ext]
+    else:
+        raise TypeError(
+            f'invalid type for audio_ext: {type(audio_ext)}'
+        )
+
+    audio_fnames = []
+    for ext in audio_ext:
+        audio_fnames.append(
+            find_fname(spect_path, ext)
+        )
+    # remove Nones
+    audio_fnames = [path for path in audio_fnames if path is not None]
+    # keep just file name from spect path
+    audio_fnames = [Path(path).name for path in audio_fnames]
+
+    if len(audio_fnames) == 1:
+        return audio_fnames[0]
+    else:
+        raise ValueError(
+            f'unable to determine filename of audio file from: {spect_path}'
+        )
 
 
 def from_files(spect_format,
