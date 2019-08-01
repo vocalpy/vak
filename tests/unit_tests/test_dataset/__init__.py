e0c6f9c915c5d82c9e17996c31923db7705fe10f
NickleDave <nickledave@users.noreply.github.com>
2019-08-01 14:02:20 -0400

    fix import of test_Dataset in test_dataset/__init__.py

--- tests/unit_tests/test_dataset/__init__.py
+++ tests/unit_tests/test_dataset/__init__.py
@@ -3,6 +3,6 @@ from .test_spect import TestFindAudioFname, TestFromFiles
 from .test_audio import TestAudio
 from .test_MetaSpect import TestMetaSpect
 from .test_Vocalization import TestVocalization
-from .test_VocalizationDataset import TestVocalizationDataset
+from .test_Dataset import TestDataset
 from .test_prep import TestPrep
 from .test_split import TestSplit
