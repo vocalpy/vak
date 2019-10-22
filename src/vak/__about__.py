e666aa2b43932ab751623d4817f3cde64dc6285d
NickleDave <nickledave@users.noreply.github.com>
2019-10-21 23:33:32 -0400

    bump version to 0.2.0 in vak/__about__.py

--- src/vak/__about__.py
+++ src/vak/__about__.py
@@ -23,7 +23,7 @@ __title__ = "vak"
 __summary__ = "automated annotation of vocalizations for everybody"
 __uri__ = "https://github.com/NickleDave/vak"
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
     with open(os.path.join(base_dir, ".commit")) as fp:
