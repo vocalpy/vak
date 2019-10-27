e37c8bd14347ed460606f93359eec8bebccd82da
NickleDave <nicholdav@gmail.com>
2019-10-27 19:27:52 -0400

    change 'data' to 'prep' in import section of config/__init__.py

--- src/vak/config/__init__.py
+++ src/vak/config/__init__.py
@@ -1,6 +1,6 @@
 """sub-package with function that parses config.ini file and returns config object"""
 from .parse import parse_config
-from .data import parse_data_config
+from .prep import parse_prep_config
 from .output import parse_output_config
 from .predict import parse_predict_config
 from .spectrogram import parse_spect_config
