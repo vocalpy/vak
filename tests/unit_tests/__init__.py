f1bb1b3fd7a87e7207e5020bd140077319a014d7
NickleDave <nicholdav@gmail.com>
2019-10-27 20:14:12 -0400

    add import statements to __init__.py files in tests

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -1,2 +1,5 @@
+from . import test_cli_funcs
 from . import test_config
+from . import test_core
+from . import test_dataset
 from . import test_utils
---
+++ tests/unit_tests/test_config/__init__.py
@@ -0,0 +1,7 @@
+from . import test_output
+from . import test_parse
+from . import test_predict
+from . import test_prep
+from . import test_spect
+from . import test_train
+from . import test_validators
