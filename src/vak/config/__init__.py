cc78901d5734f5cd0e11ad877a1f8ce2c92c49ed
NickleDave <nicholdav@gmail.com>
2019-11-03 13:55:14 -0500

    remove import of parse_output_config from vak/config/__init__.py

--- src/vak/config/__init__.py
+++ src/vak/config/__init__.py
@@ -1,9 +1,7 @@
 """sub-package with function that parses config.ini file and returns config object"""
 from .parse import parse_config
 from .prep import parse_prep_config
-from .output import parse_output_config
 from .predict import parse_predict_config
 from .spectrogram import parse_spect_config
 from .train import parse_train_config
 from . import validators
-
