0b61c26ef33258932a05cbb498ed01f81d7c3fc4
NickleDave <nicholdav@gmail.com>
2019-11-05 17:35:16 -0500

    change 'train_config' to 'learncurve_config' where appropriate in vak/cli/cli.py

--- src/vak/cli/cli.py
+++ src/vak/cli/cli.py
@@ -93,30 +93,30 @@ def cli(command, config_file):
                 "file with a new name and remove it from that file.\n"
                 f"Currently the option is set to: {config_obj['TRAIN']['results_dir_made_by_main_script']}"
             )
-        train_config = parse_train_config(config_obj, config_file)
-        nets_config = _get_nets_config(config_obj, train_config.networks)
+        learncurve_config = parse_learncurve_config(config_obj, config_file)
+        nets_config = _get_nets_config(config_obj, learncurve_config.networks)
         prep_config = parse_prep_config(config_obj, config_file)
-        if train_config.train_vds_path is None:
-            raise ValueError("must set 'train_vds_path' option in [TRAIN] section of config.ini file "
+        if learncurve_config.train_vds_path is None:
+            raise ValueError("must set 'train_vds_path' option in [LEARNCURVE] section of config.ini file "
                              "before running 'learncurve'")
-        if train_config.val_vds_path is None:
-            raise ValueError("must set 'val_vds_path' option in [TRAIN] section of config.ini file "
+        if learncurve_config.val_vds_path is None:
+            raise ValueError("must set 'val_vds_path' option in [LEARNCURVE] section of config.ini file "
                              "before running 'learncurve'")
-        learning_curve(train_vds_path=train_config.train_vds_path,
-                       val_vds_path=train_config.val_vds_path,
-                       test_vds_path=train_config.test_vds_path,
+        learning_curve(train_vds_path=learncurve_config.train_vds_path,
+                       val_vds_path=learncurve_config.val_vds_path,
+                       test_vds_path=learncurve_config.test_vds_path,
                        total_train_set_duration=prep_config.total_train_set_dur,
-                       train_set_durs=train_config.train_set_durs,
-                       num_replicates=train_config.num_replicates,
-                       num_epochs=train_config.num_epochs,
+                       train_set_durs=learncurve_config.train_set_durs,
+                       num_replicates=learncurve_config.num_replicates,
+                       num_epochs=learncurve_config.num_epochs,
                        config_file=config_file,
                        networks=nets_config,
-                       val_error_step=train_config.val_error_step,
-                       checkpoint_step=train_config.checkpoint_step,
-                       patience=train_config.patience,
-                       save_only_single_checkpoint_file=train_config.save_only_single_checkpoint_file,
-                       normalize_spectrograms=train_config.normalize_spectrograms,
-                       use_train_subsets_from_previous_run=train_config.use_train_subsets_from_previous_run,
-                       previous_run_path=train_config.previous_run_path,
-                       root_results_dir=train_config.root_results_dir,
-                       save_transformed_data=train_config.save_transformed_data)
+                       val_error_step=learncurve_config.val_error_step,
+                       checkpoint_step=learncurve_config.checkpoint_step,
+                       patience=learncurve_config.patience,
+                       save_only_single_checkpoint_file=learncurve_config.save_only_single_checkpoint_file,
+                       normalize_spectrograms=learncurve_config.normalize_spectrograms,
+                       use_train_subsets_from_previous_run=learncurve_config.use_train_subsets_from_previous_run,
+                       previous_run_path=learncurve_config.previous_run_path,
+                       root_results_dir=learncurve_config.root_results_dir,
+                       save_transformed_data=learncurve_config.save_transformed_data)
