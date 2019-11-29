0411e12507784740e87d3c9d0e8ab102b4dd3716
David Nicholson <nickledave@users.noreply.github.com>
2019-11-29 10:40:30 -0500

    fix import statement in vak/dataset/__init__.py

--- src/vak/dataset/__init__.py
+++ src/vak/dataset/__init__.py
@@ -1,5 +1,5 @@
 """module that handles datasets:
 spectrograms made from audio files of vocalizations, and associated annotations"""
 from .classes import MetaSpect, Vocalization, Dataset
-from . import spect, audio, annotation, split
+from . import dataframe, audio, annotation, split
 from .prep import prep
