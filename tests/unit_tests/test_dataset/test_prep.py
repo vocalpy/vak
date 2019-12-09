3086d5911f731a479e210b9eed60d83a9a64c30f
David Nicholson <nickledave@users.noreply.github.com>
2019-12-08 22:13:20 -0500

    fix test_prep.py tests after rewriting dataset/prep.py

--- tests/unit_tests/test_dataset/test_prep.py
+++ tests/unit_tests/test_dataset/test_prep.py
@@ -4,9 +4,10 @@ import tempfile
 import shutil
 import unittest
 
+import pandas as pd
+
 import vak.dataset.prep
 from vak.config.spectrogram import SpectConfig
-from vak.dataset.classes import Dataset
 
 HERE = os.path.dirname(__file__)
 TEST_DATA_DIR = os.path.join(HERE, '..', '..', 'test_data')
@@ -25,35 +26,26 @@ class TestPrep(unittest.TestCase):
                                    transform_type='log_spect')
         annot_format = 'notmat'
         labelset = list('iabcdefghjk')
-        vds_fname = 'test.json'
+        csv_fname = 'test.csv'
 
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         labelset=labelset,
-                                         annot_format=annot_format,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format='cbin',
-                                         spect_format=None,
-                                         annot_file=None,
-                                         spect_params=spect_params)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            labelset=labelset,
+                                            annot_format=annot_format,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format='cbin',
+                                            spect_format=None,
+                                            annot_file=None,
+                                            spect_params=spect_params)
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        self.assertTrue(type(vak_df) == pd.DataFrame)
+        json_fname = os.path.join(self.tmp_output_dir, csv_fname)
         self.assertTrue(
             os.path.isfile(json_fname)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
 
     def test_prep_with_audio_cbin_no_annot(self):
         data_dir = os.path.join(TEST_DATA_DIR, 'cbins', 'gy6or6', '032312')
@@ -61,35 +53,28 @@ class TestPrep(unittest.TestCase):
                                    transform_type='log_spect')
         annot_format = None
         labelset = None
-        vds_fname = 'test.json'
+        csv_fname = 'test.csv'
 
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         annot_format=annot_format,
-                                         labelset=labelset,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format='cbin',
-                                         spect_format=None,
-                                         annot_file=None,
-                                         spect_params=spect_params)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            annot_format=annot_format,
+                                            labelset=labelset,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format='cbin',
+                                            spect_format=None,
+                                            annot_file=None,
+                                            spect_params=spect_params)
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        self.assertTrue(type(vak_df) == pd.DataFrame)
         self.assertTrue(
-            os.path.isfile(json_fname)
+            os.path.isfile(csv_path)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
+        self.assertTrue(
+            csv_path == os.path.join(self.tmp_output_dir, csv_fname)
+        )
 
     def test_prep_with_audio_cbin_no_labelset(self):
         data_dir = os.path.join(TEST_DATA_DIR, 'cbins', 'gy6or6', '032312')
@@ -97,35 +82,28 @@ class TestPrep(unittest.TestCase):
                                    transform_type='log_spect')
         annot_format = 'notmat'
         labelset = None
-        vds_fname = 'test.json'
+        csv_fname = 'test.csv'
 
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         annot_format=annot_format,
-                                         labelset=labelset,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format='cbin',
-                                         spect_format=None,
-                                         annot_file=None,
-                                         spect_params=spect_params)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            annot_format=annot_format,
+                                            labelset=labelset,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format='cbin',
+                                            spect_format=None,
+                                            annot_file=None,
+                                            spect_params=spect_params)
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        self.assertTrue(type(vak_df) == pd.DataFrame)
         self.assertTrue(
-            os.path.isfile(json_fname)
+            os.path.isfile(csv_path)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
+        self.assertTrue(
+            csv_path == os.path.join(self.tmp_output_dir, csv_fname)
+        )
 
     def test_prep_with_spect_mat(self):
         data_dir = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
@@ -133,74 +111,56 @@ class TestPrep(unittest.TestCase):
 
         labelset = {1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19}
         annot_format = 'yarden'
-        vds_fname = 'test.json'
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         labelset=labelset,
-                                         annot_format=annot_format,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format=None,
-                                         spect_format='mat',
-                                         annot_file=annot_file,
-                                         spect_params=None)
+        csv_fname = 'test.csv'
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            labelset=labelset,
+                                            annot_format=annot_format,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format=None,
+                                            spect_format='mat',
+                                            annot_file=annot_file,
+                                            spect_params=None)
+
+        self.assertTrue(type(vak_df) == pd.DataFrame)
         self.assertTrue(
-            os.path.abspath(vds_path) == os.path.abspath(json_fname)
+            os.path.isfile(csv_path)
         )
         self.assertTrue(
-            os.path.isfile(json_fname)
+            csv_path == os.path.join(self.tmp_output_dir, csv_fname)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
 
     def test_prep_with_spect_mat_no_annot(self):
         data_dir = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
         annot_file = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'llb3_annot_subset.mat')
         labelset = None
         annot_format = None
-        vds_fname = 'test.json'
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         labelset=labelset,
-                                         annot_format=annot_format,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format=None,
-                                         spect_format='mat',
-                                         annot_file=annot_file,
-                                         spect_params=None)
+        csv_fname = 'test.csv'
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            labelset=labelset,
+                                            annot_format=annot_format,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format=None,
+                                            spect_format='mat',
+                                            annot_file=annot_file,
+                                            spect_params=None)
+
+        self.assertTrue(type(vak_df) == pd.DataFrame)
         self.assertTrue(
-            os.path.abspath(vds_path) == os.path.abspath(json_fname)
+            os.path.isfile(csv_path)
         )
         self.assertTrue(
-            os.path.isfile(json_fname)
+            csv_path == os.path.join(self.tmp_output_dir, csv_fname)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
 
     def test_prep_with_spect_mat_no_labelset(self):
         data_dir = os.path.join(TEST_DATA_DIR, 'mat', 'llb3', 'spect')
@@ -208,37 +168,28 @@ class TestPrep(unittest.TestCase):
 
         labelset = None
         annot_format = 'yarden'
-        vds_fname = 'test.json'
-        vds, vds_path = vak.dataset.prep(data_dir=data_dir,
-                                         labelset=labelset,
-                                         annot_format=annot_format,
-                                         output_dir=self.tmp_output_dir,
-                                         save_vds=True,
-                                         vds_fname=vds_fname,
-                                         return_vds=True,
-                                         return_path=True,
-                                         load_spects=False,
-                                         audio_format=None,
-                                         spect_format='mat',
-                                         annot_file=annot_file,
-                                         spect_params=None)
+        csv_fname = 'test.csv'
 
-        self.assertTrue(type(vds) == Dataset)
-        json_fname = os.path.join(self.tmp_output_dir, vds_fname)
+        vak_df, csv_path = vak.dataset.prep(data_dir=data_dir,
+                                            labelset=labelset,
+                                            annot_format=annot_format,
+                                            output_dir=self.tmp_output_dir,
+                                            save_csv=True,
+                                            csv_fname=csv_fname,
+                                            return_df=True,
+                                            return_path=True,
+                                            audio_format=None,
+                                            spect_format='mat',
+                                            annot_file=annot_file,
+                                            spect_params=None)
+
+        self.assertTrue(type(vak_df) == pd.DataFrame)
         self.assertTrue(
-            os.path.abspath(vds_path) == os.path.abspath(json_fname)
+            os.path.isfile(csv_path)
         )
         self.assertTrue(
-            os.path.isfile(json_fname)
+            csv_path == os.path.join(self.tmp_output_dir, csv_fname)
         )
-        vds_loaded = Dataset.load(json_fname=json_fname)
-        for voc, voc_loaded in zip(vds.voc_list, vds_loaded.voc_list):
-            self.assertTrue(
-                voc.audio_path == voc_loaded.audio_path
-            )
-            self.assertTrue(
-                voc.spect_path == voc_loaded.spect_path
-            )
 
 
 if __name__ == '__main__':
