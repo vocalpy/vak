8a69a0ddb033510ef067550ac12b0f6b0db01a11
David Nicholson <nickledave@users.noreply.github.com>
2019-12-15 13:07:03 -0500

    rename io.dataset again, to dataframe
    to avoid namespace clash with Dataset classes used when working with networks

--- tests/unit_tests/test_io/__init__.py
+++ tests/unit_tests/test_io/__init__.py
@@ -4,4 +4,4 @@ from .test_audio import TestAudio
 from .test_MetaSpect import TestMetaSpect
 from .test_Vocalization import TestVocalization
 from .test_Dataset import TestDataset
-from .test_dataset import TestFromFiles
+from .test_dataframe import TestFromFiles
