4183790fc7181c1bc38cb099c28e54888acd2ea3
David Nicholson <nickledave@users.noreply.github.com>
2019-12-16 18:34:04 -0500

    add import of dataset sub-package to vak/io/__init_.py

--- src/vak/io/__init__.py
+++ src/vak/io/__init__.py
@@ -3,5 +3,6 @@
 - spectrograms made from audio files of vocalizations
 - associated annotation files
 - .csv files that represent a dataset of vocalizations that combines all those files together"""
+from . import dataset
 from .dataset import MetaSpect, Vocalization, Dataset
 from . import annotation, audio, dataframe, spect
