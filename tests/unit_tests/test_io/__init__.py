f886fc8ece888d4100d85f1843a23edc213c72d3
David Nicholson <nickledave@users.noreply.github.com>
2019-12-21 18:48:25 -0500

    fix imports in test_io/__init__.py

--- tests/unit_tests/test_io/__init__.py
+++ tests/unit_tests/test_io/__init__.py
@@ -1,6 +1,7 @@
 from .test_annot import TestAnnot
-from .test_spect import TestFindAudioFname, TestFromFiles
+from .test_spect import TestFindAudioFname
 from .test_audio import TestAudio
+from .test_dataframe import TestFromFiles
 from .test_MetaSpect import TestMetaSpect
 from .test_Vocalization import TestVocalization
 from .test_Dataset import TestDataset
