d1cf4527e008fcbea52aba6fb482691ee5b86d22
David Nicholson <nickledave@users.noreply.github.com>
2019-12-22 09:47:21 -0500

    rewrite rerun_prep.py to use prep_*_config.ini scripts

--- tests/setup_scripts/rerun_prep.py
+++ tests/setup_scripts/rerun_prep.py
@@ -1,47 +1,43 @@
-import os
-from pathlib import Path
 from configparser import ConfigParser
+from pathlib import Path
+import shutil
 
 import vak
 
 HERE = Path(__file__).parent
-TEST_DATA_DIR = HERE.joinpath('..', 'test_data')
+# convention is that all the config.ini files in setup_scripts/ that should be
+# run when setting up for development have filenames of the form `prep_*_config.ini'
+# e.g., 'prep_learncurve_config.ini'
+PREP_CONFIGS_TO_RUN = HERE.glob('prep_*_config.ini')
 
 
 def main():
-    config_file = str(HERE.joinpath('tmp_Makefile_config.ini'))
-    if not os.path.isfile(config_file):
-        raise FileNotFoundError(
-            f'{config_file} not found')
+    for config_path in PREP_CONFIGS_TO_RUN:
+        if not config_path.exists():
+            raise FileNotFoundError(
+                f'{config_path} not found')
 
-    config = ConfigParser()
-    config.read(config_file)
-    prep_config = vak.config.parse.parse_prep_config(config, config_file)
-    spect_params = vak.config.parse.parse_spect_config(config)
-    vak.cli.prep(data_dir=prep_config.data_dir,
-                 labelset=prep_config.labelset,
-                 config_file=config_file,
-                 annot_format=prep_config.annot_format,
-                 train_dur=prep_config.total_train_set_dur,
-                 test_dur=prep_config.test_dur,
-                 val_dur=prep_config.val_dur,
-                 output_dir=prep_config.output_dir,
-                 audio_format=prep_config.audio_format,
-                 spect_format=prep_config.spect_format,
-                 annot_file=prep_config.annot_file,
-                 spect_params=spect_params)
+        print(f"preparing datasets for tests using config: {config_path}")
+        tmp_config_path = Path(config_path.parent).joinpath(f'tmp_{config_path.name}')
+        print(f"\tcopying to {tmp_config_path}")
+        shutil.copy(src=config_path, dst=tmp_config_path)
 
-    # make a "predict.vds.json" to use when testing predict functions
-    dir_to_predict = TEST_DATA_DIR.joinpath('cbins', 'gy6or6', '032412')
-    for_vds_fname = dir_to_predict.name
-    csv_path = TEST_DATA_DIR.joinpath(f'{for_vds_fname}.predict.csv')
-
-    vak_df = vak.io.dataframe.from_files(data_dir=str(dir_to_predict),
-                                         output_dir=prep_config.output_dir,
-                                         audio_format='cbin',
-                                         spect_params=spect_params)
-    vak_df = vak.io.dataframe.add_split_col(vak_df, split='predict')
-    vak_df.to_csv(csv_path)
+        config = ConfigParser()
+        config.read(tmp_config_path)
+        prep_config = vak.config.parse.parse_prep_config(config, tmp_config_path)
+        spect_params = vak.config.parse.parse_spect_config(config)
+        vak.cli.prep(data_dir=prep_config.data_dir,
+                     labelset=prep_config.labelset,
+                     config_file=tmp_config_path,
+                     annot_format=prep_config.annot_format,
+                     train_dur=prep_config.total_train_set_dur,
+                     test_dur=prep_config.test_dur,
+                     val_dur=prep_config.val_dur,
+                     output_dir=prep_config.output_dir,
+                     audio_format=prep_config.audio_format,
+                     spect_format=prep_config.spect_format,
+                     annot_file=prep_config.annot_file,
+                     spect_params=spect_params)
 
 
 if __name__ == '__main__':
