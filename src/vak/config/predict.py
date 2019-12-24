da103b4ae82176d66558a8941037955b18532760
David Nicholson <nickledave@users.noreply.github.com>
2019-12-24 13:13:32 -0500

    modify config.predict to work with vak.models module

--- src/vak/config/predict.py
+++ src/vak/config/predict.py
@@ -19,9 +19,8 @@ class PredictConfig:
         path to where dataset was saved as a csv.
     checkpoint_path : str
         path to directory with checkpoint files saved by Tensorflow, to reload model
-    networks : namedtuple
-        where each field is the Config tuple for a neural network and the name
-        of that field is the name of the class that represents the network.
+    models : list
+        of model names. e.g., 'models = TweetyNet, GRUNet, ConvNet'
     spect_scaler_path : str
         path to a saved SpectScaler object used to normalize spectrograms.
         If spectrograms were normalized and this is not provided, will give
@@ -58,6 +57,7 @@ def parse_predict_config(config):
     except KeyError:
         raise KeyError("'predict_vds_path' option not found in [PREDICT] section of "
                             "config.ini file. Please add this option.")
+
     try:
         config_dict['train_vds_path'] = os.path.expanduser(
             config['PREDICT']['train_vds_path']
@@ -74,23 +74,20 @@ def parse_predict_config(config):
 
     # load entry points within function, not at module level,
     # to avoid circular dependencies
-    # (user would be unable to import networks in other packages
-    # that subclass vak.network.AbstractVakNetwork
-    # since the module in the other package would need to `import vak`)
-    NETWORKS = models._load()
-    NETWORK_NAMES = NETWORKS.keys()
+    MODEL_NAMES = [model_name for model_name, model_builder in models.find()]
     try:
-        networks = [network_name for network_name in
-                    config['PREDICT']['networks'].split(',')]
-        for network_name in networks:
-            if network_name not in NETWORK_NAMES:
-                raise TypeError('Neural network {} not found when importing installed networks.'
-                                .format(models))
-        config_dict['networks'] = networks
+        model_names = [model_name
+                       for model_name in config['PREDICT']['models'].split(',')]
     except NoOptionError:
-        raise KeyError("'networks' option not found in [PREDICT] section of config.ini file. "
-                       "Please add this option as a comma-separated list of neural network names, e.g.:\n"
-                       "networks = TweetyNet, GRUnet, convnet")
+        raise KeyError("'models' option not found in [PREDICT] section of config.ini file. "
+                       "Please add this option as a comma-separated list of model names, e.g.:\n"
+                       "models = TweetyNet, GRUnet, convnet")
+    for model_name in model_names:
+        if model_name not in MODEL_NAMES:
+            raise ValueError(
+                f'Model {model_name} not found when importing installed models.'
+            )
+    config_dict['models'] = model_names
 
     if config.has_option('PREDICT', 'spect_scaler_path'):
         config_dict['spect_scaler_path'] = config['PREDICT']['spect_scaler_path']
