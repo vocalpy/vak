02a3bb7d900cbead4c40c5727a274a90692f8df6
David Nicholson <nickledave@users.noreply.github.com>
2019-12-23 23:13:24 -0500

    rewrite vak.models functions for finding entry points

--- src/vak/models.py
+++ src/vak/models.py
@@ -1,23 +1,15 @@
-"""module that contains helper function to load networks, and
-the AbstractVakNetwork class.
-Networks in separate packages should subclass this class, and
-then make themselves available to vak by including
-'vak.network' in the entry_points value of their setup.py
-file.
+"""module that contains helper function to load models
 
-The reason for subclassing is simply to set a
-standard interface for networks to use. vak will check
-that any network implements all the methods listed here,
-since it requires those methods to carry out commands
-in its command-line interface.
+Models in separate packages should make themselves available to vak by including
+'vak.models' in the entry_points of their setup.py file.
 
-For example, if you had a package `grunet` containing a class
-`GRUnet`, then that package would include the following in its
-setup.py file:
+For example, if you had a package `grunet` containing a model
+that was instantiated by a function `GRUnet`,
+then that package would include the following in its setup.py file:
 
 setup(
     ...
-    entry_points={'vak.network': 'GRUnet = grunet.model:GRUnet'},
+    entry_points={'vak.models': 'GRUnet = grunet:GRUnet'},
     ...
 )
 
@@ -26,57 +18,64 @@ https://packaging.python.org/guides/creating-and-discovering-plugins/#using-pack
 https://setuptools.readthedocs.io/en/latest/setuptools.html#dynamic-discovery-of-services-and-plugins
 https://amir.rachum.com/blog/2017/07/28/python-entry-points/
 """
-import typing
-import pkg_resources
+import attr
 
 
-def _load():
-    networks = {
-        entry_point.name: entry_point.load()
-        for entry_point
-        in pkg_resources.iter_entry_points('vak.network')
-    }
-    return networks
+MODELS_ENTRY_POINT = 'vak.models'
+MODEL_CONFIG_PARSERS_ENTRY_POINT = 'vak.model_config_parsers'
 
 
-class AbstractVakNetwork:
+def iter_entry_points(group_name):
+    try:
+        import pkg_resources
+    except (ImportError, IOError):
+        return []
 
-    Config = typing.NamedTuple('Config',
-                               [('conv_layers', int),
-                                ('kernels', int),
-                                ('learning_rate', float)])
+    return pkg_resources.iter_entry_points(group_name)
 
-    def __init__(self):
-        pass
 
-    def inference(self):
-        """inference method, forward pass through network
-        with output that is used with loss function for
-        training (e.g., softmax layer)"""
-        pass
+def find():
+    for entrypoint in iter_entry_points(MODELS_ENTRY_POINT):
+        yield entrypoint.name, entrypoint.load()
 
-    def optimize(self):
-        """optimize method, that runs optimizer on one cost value, given
-        ground truth as expected output"""
-        pass
 
-    def error(self):
-        """error method, that returns mean error given as input a spectrogram
-        and the true and predicted labels"""
+def find_config_parsers():
+    for entrypoint in iter_entry_points(MODEL_CONFIG_PARSERS_ENTRY_POINT):
+        yield entrypoint.name, entrypoint.load()
 
-    def train(self):
-        """method that uses error and optimize methods to iteratively
-        train the network"""
-        pass
 
-    def predict(self):
-        """predict method, that (usually) returns argmax(inference)
-        for all outputs"""
-        pass
+def from_model_config_map(model_config_map, num_classes, input_shape):
+    """get models that are ready to train, given their names and configurations.
 
-    def save(self):
-        """method to save network"""
-        pass
+    Given a dictionary that maps model names to configurations,
+    along with the number of classes they should be trained to discriminate and their input shape,
+    return a dictionary that maps model names to instances of the model
 
-    def restore(self):
-        pass
+    Parameters
+    ----------
+    model_config_map : dict
+    num_classes : int
+    input_shape : tuple, list
+
+    Returns
+    -------
+    models_map : dict
+        where keys are model names and values are instances of the models, ready for training
+    """
+    MODELS = {model_name: model_builder for model_name, model_builder in find()}
+    MODEL_CONFIG_PARSERS = {model_name: model_config_parser
+                            for model_name, model_config_parser in find_config_parsers()}
+
+    models_map = {}
+    for model_name, model_config_kwargs in model_config_map.items():
+        # pass section dict as kwargs to config parser function
+        model_config = MODEL_CONFIG_PARSERS[model_name](num_classes=num_classes,
+                                                        input_shape=input_shape,
+                                                        **model_config_kwargs)
+        # now convert the config itself to a dict and pass as kwargs to the model
+        if type(model_config) != dict:
+            # if not a dict, assume an attrs-based class for config, and convert to dict
+            model_config = attr.asdict(model_config)
+        model = MODELS[model_name](**model_config)
+        models_map[model_name] = model
+    return models_map
