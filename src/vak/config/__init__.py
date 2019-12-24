947f930c769e8c664edca907adc11cd65b6917ae
David Nicholson <nickledave@users.noreply.github.com>
2019-12-24 14:42:11 -0500

    import models module in config/__init__.py

--- src/vak/config/__init__.py
+++ src/vak/config/__init__.py
@@ -1,8 +1,10 @@
 """sub-package with function that parses config.ini file and returns config object"""
+from . import models
+from . import validators
+
 from .learncurve import parse_learncurve_config, LearncurveConfig
 from .parse import parse_config, Config
 from .prep import parse_prep_config, PrepConfig
 from .predict import parse_predict_config, PredictConfig
 from .spectrogram import parse_spect_config, SpectConfig
 from .train import parse_train_config, TrainConfig
-from . import validators
