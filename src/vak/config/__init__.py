f47bb82b62533fb973b2ac30f35825517414fc67
David Nicholson <nickledave@users.noreply.github.com>
2019-12-25 16:03:51 -0500

    change imports in vak/config/__init__.py
    - import entire parse module, so user writes config.parse.from_file
    - import Config class from .parse module, just so it's there

--- src/vak/config/__init__.py
+++ src/vak/config/__init__.py
@@ -1,9 +1,10 @@
-"""sub-package with function that parses config.ini file and returns config object"""
+"""sub-package that parses config.ini files and returns config object"""
 from . import models
+from . import parse
 from . import validators
 
 from .learncurve import parse_learncurve_config, LearncurveConfig
-from .parse import parse_config, Config
+from .parse import Config
 from .prep import parse_prep_config, PrepConfig
 from .predict import parse_predict_config, PredictConfig
 from .spectrogram import parse_spect_config, SpectConfig
