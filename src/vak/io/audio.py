a10ffce190c522b5f172cb543eb0f95d339ef54a
David Nicholson <nickledave@users.noreply.github.com>
2019-12-29 18:06:52 -0500

    rewrite how io.audio.to_spect uses spect_params
    - remove separate arguments for spect_key, timebins_key, freqbins_key,
      and audio_path
    - instead use them from spect_params passed in
    - assume spect_params will be a SpectParamsConfig object, or that
      we can convert it into one (i.e. it's a dict with keyword arg-value
      pairs)
    - this blurs the lines around what Config classes are for --
      not just for parsing config.ini files
    - but it keeps this function from having three thousand arguments

--- src/vak/io/audio.py
+++ src/vak/io/audio.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-import attr
 import numpy as np
 import dask.bag as db
 from dask.diagnostics import ProgressBar
@@ -46,17 +45,21 @@ def to_spect(audio_format,
              audio_files=None,
              annot_list=None,
              audio_annot_map=None,
-             labelset=None,
-             freqbins_key='f',
-             timebins_key='t',
-             spect_key='s',
-             audio_path_key='audio_path'):
+             labelset=None):
     """makes spectrograms from audio files and saves in array files
 
     Parameters
     ----------
     audio_format : str
         format of audio files. One of {'wav', 'cbin'}
+    spect_params : dict or vak.config.spectrogram.SpectConfig
+        parameters for computing spectrogram, from .ini file.
+        To see all related parameters, run:
+        >>> help(vak.config.spect_params.SpectParamConfig)
+        To get a default configuration, create a SpectParamConfig
+        with no arguments and then pass that to `to_spect`:
+        >>> default_spect_params = vak.config.spect_params.SpectParamConfig()
+        >>> to_spect(audio_format='wav', spect_params=default_spect_params, output_dir='.')
     audio_dir : str
         path to directory containing audio files from which to make spectrograms
     audio_files : list
@@ -67,23 +70,12 @@ def to_spect(audio_format,
         Where keys are paths to array files and value corresponding to each key is
         the annotation for that array file.
         Default is None.
-    spect_params : dict or vak.config.spectrogram.SpectConfig
-        parameters for computing spectrogram, from .ini file
     output_dir : str
         directory in which to save .spect.npz file generated for each audio file.
     labelset : set
         of str or int, set of unique labels for vocalizations. Default is None.
         If not None, then files will be skipped where the 'labels' array in the
         corresponding annotation contains labels that are not found in labelset
-    freqbins_key : str
-        key for accessing vector of frequency bins in files. Default is 'f'.
-    timebins_key : str
-        key for accessing vector of time bins in files. Default is 't'.
-    spect_key : str
-        key for accessing spectrogram in files. Default is 's'.
-    audio_path_key : str
-        key for accessing path to source audio file for spectogram in files.
-        Default is 'audio_path'.
 
     Returns
     -------
@@ -103,8 +95,8 @@ def to_spect(audio_format,
         audio_path : numpy.ndarray
             path to source audio file used to create spectrogram
 
-    The names of the arrays are defaults, and will change if different values are specified for
-    'spect_key', 'freqbins_key', 'timebins_key', or 'audio_path_key'.
+    The names of the arrays are defaults, and will change if different values are specified
+    in spect_params for 'spect_key', 'freqbins_key', 'timebins_key', or 'audio_path_key'.
     """
     if audio_format not in validators.VALID_AUDIO_FORMATS:
         raise ValueError(
@@ -135,8 +127,14 @@ def to_spect(audio_format,
                 f'type of labelset must be set, but was: {type(labelset)}'
             )
 
-    if type(spect_params) == SpectParamsConfig:
-        spect_params = attr.asdict(spect_params)
+    if type(spect_params) not in [dict, SpectParamsConfig]:
+        raise TypeError(
+            'type of spect_params must be an instance of vak.config.spect_params.SpectParamsConfig, '
+            'or a dict that can be converted to a SpectParamsConfig instance, '
+            f'but was {type(spect_params)}'
+        )
+    if type(spect_params) is dict:
+        spect_params = SpectParamsConfig(**spect_params)
 
     # validate audio files if supplied by user
     if audio_files:
@@ -196,11 +194,16 @@ def to_spect(audio_format,
         files containing spectrograms.
         Accepts path to audio file, saves .npz file with spectrogram"""
         fs, dat = AUDIO_FORMAT_FUNC_MAP[audio_format](audio_file)
-        s, f, t = spectrogram(dat, fs, **spect_params)
-        spect_dict = {spect_key: s,
-                      freqbins_key: f,
-                      timebins_key: t,
-                      audio_path_key: audio_file}
+        s, f, t = spectrogram(dat, fs,
+                              spect_params.fft_size,
+                              spect_params.step_size,
+                              spect_params.thresh,
+                              spect_params.transform_type,
+                              spect_params.freq_cutoffs)
+        spect_dict = {spect_params.spect_key: s,
+                      spect_params.freqbins_key: f,
+                      spect_params.timebins_key: t,
+                      spect_params.audio_path_key: audio_file}
         basename = os.path.basename(audio_file)
         npz_fname = os.path.join(os.path.normpath(output_dir),
                                  basename + '.spect.npz')
