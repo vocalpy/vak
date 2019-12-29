32f861e4ed67c2767f322c156e1830eda907effc
David Nicholson <nickledave@users.noreply.github.com>
2019-12-29 09:26:20 -0500

    add dataloader module to config.py
    seems like least painful way to share parameters across train/predict etc
    for dataloader even though it is adding yet another section to the
    config.ini file. Will have a default so in theory some users might not
    have to think about it at first

---
+++ src/vak/config/dataloader.py
@@ -0,0 +1,27 @@
+import attr
+from attr.validators import instance_of
+
+
+@attr.s
+class DataLoaderConfig:
+    """represents options for DataLoaders specified in config.ini file
+
+    Attributes
+    ----------
+    window_size : int
+        size of windows taken from spectrograms, in number of time bins,
+        shonw to neural networks
+    """
+    window_size = attr.ib(converter=int,
+                          validator=instance_of(int),
+                          default=88)
+
+
+def parse_dataloader_config(config, config_path):
+    # return defaults if config doesn't have SPECTROGRAM section
+    dataloader_section = {}
+    if 'DATALOADER' in config:
+        dataloader_section.update(
+            config['DATALOADER'].items()
+        )
+    return DataLoaderConfig(**dataloader_section)
