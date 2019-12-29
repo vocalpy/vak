9464537fa981c7730a1e66244e65f3c7b7f2ff8d
David Nicholson <nickledave@users.noreply.github.com>
2019-12-29 11:20:10 -0500

    rewrite vak/config/predict.py module
    - to use the same logic + structure as the rewritten train/prep modules
      + let attrs do as much as work as possible for converting/validating
    - stop checking for train/predict_vds paths

--- src/vak/config/predict.py
+++ src/vak/config/predict.py
@@ -5,7 +5,8 @@ import os
 import attr
 from attr.validators import instance_of, optional
 
-from .validators import is_a_directory, is_a_file
+from .converters import comma_separated_list, expanded_user_path
+from .validators import is_a_directory, is_a_file, is_valid_model_name
 from .. import models
 
 
@@ -26,21 +27,31 @@ class PredictConfig:
         If spectrograms were normalized and this is not provided, will give
         incorrect results.
     """
-    csv_path = attr.ib(validator=[instance_of(str), is_a_file])
-    checkpoint_path = attr.ib(validator=[instance_of(str), is_a_directory])
-    networks = attr.ib()
-
+    csv_path = attr.ib(converter=expanded_user_path, validator=is_a_file)
+    checkpoint_path = attr.ib(converter=expanded_user_path,
+                              validator=is_a_directory)
+    models = attr.ib(converter=comma_separated_list,
+                     validator=[instance_of(list), is_valid_model_name])
     spect_scaler_path = attr.ib(validator=optional([instance_of(str), is_a_file]),
                                 default=None)
 
 
-def parse_predict_config(config_obj, config_file):
+REQUIRED_PREDICT_OPTIONS = [
+    'csv_path',
+    'checkpoint_path',
+    'models',
+]
+
+
+def parse_predict_config(config_obj, config_path):
     """parse [PREDICT] section of config.ini file
 
     Parameters
     ----------
-    config : ConfigParser
+    config_obj : ConfigParser
         containing config.ini file already loaded by parse function
+    config_path : str
+        path to config.ini file (used for error messages)
 
     Returns
     -------
@@ -48,48 +59,14 @@ def parse_predict_config(config_obj, config_file):
         instance of PredictConfig class that represents [PREDICT] section
         of config.ini file
     """
-    config_dict = {}
+    predict_section = dict(
+        config_obj['PREDICT'].items()
+    )
 
-    try:
-        config_dict['predict_vds_path'] = os.path.expanduser(
-            config_obj['PREDICT']['predict_vds_path']
-        )
-    except KeyError:
-        raise KeyError("'predict_vds_path' option not found in [PREDICT] section of "
-                            "config.ini file. Please add this option.")
-
-    try:
-        config_dict['train_vds_path'] = os.path.expanduser(
-            config_obj['PREDICT']['train_vds_path']
-        )
-    except KeyError:
-        raise KeyError("'train_vds_path' option not found in [PREDICT] section of "
-                            "config.ini file. Please add this option.")
-
-    try:
-        config_dict['checkpoint_path'] = config_obj['PREDICT']['checkpoint_path']
-    except KeyError:
-        raise KeyError('must specify checkpoint_path in [PREDICT] section '
-                       'of config.ini file')
-
-    # load entry points within function, not at module level,
-    # to avoid circular dependencies
-    MODEL_NAMES = [model_name for model_name, model_builder in models.find()]
-    try:
-        model_names = [model_name
-                       for model_name in config_obj['PREDICT']['models'].split(',')]
-    except NoOptionError:
-        raise KeyError("'models' option not found in [PREDICT] section of config.ini file. "
-                       "Please add this option as a comma-separated list of model names, e.g.:\n"
-                       "models = TweetyNet, GRUnet, convnet")
-    for model_name in model_names:
-        if model_name not in MODEL_NAMES:
-            raise ValueError(
-                f'Model {model_name} not found when importing installed models.'
+    for required_option in REQUIRED_PREDICT_OPTIONS:
+        if required_option not in predict_section:
+            raise NoOptionError(
+                f"the '{required_option}' option is required but was not found in the "
+                f"PREDICT section of the config.ini file: {config_path}"
             )
-    config_dict['models'] = model_names
-
-    if config_obj.has_option('PREDICT', 'spect_scaler_path'):
-        config_dict['spect_scaler_path'] = config_obj['PREDICT']['spect_scaler_path']
-
-    return PredictConfig(**config_dict)
+    return PredictConfig(**predict_section)
