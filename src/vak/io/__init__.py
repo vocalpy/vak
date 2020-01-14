020a23cc22b2e48faa70c8e16cffb60288acca47
David Nicholson <nickledave@users.noreply.github.com>
2020-01-13 22:00:06 -0500

    move datasets/ up to vak/

--- src/vak/io/__init__.py
+++ src/vak/io/__init__.py
@@ -3,5 +3,4 @@
 - spectrograms made from audio files of vocalizations
 - associated annotation files
 - .csv files that represent a dataset of vocalizations that combines all those files together"""
-from . import datasets
 from . import annotation, audio, dataframe, spect
