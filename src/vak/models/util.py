131d30f230a6d914091e31fe0dc086b14f206176
David Nicholson <nickledave@users.noreply.github.com>
2020-01-25 21:48:42 -0500

    rewrite src/vak/models/util.py
    - remove constant + functions having to do with config parsers,
      users will not have to write their own config parsers
    - rewrite from_model_config_map to just use dicts, not expect
      any attrs-based config class it needs to cast to a dict, and
      not have to parse any section using some config_parser function.
      Just use the dict straight from toml as kwargs to the model's
      __init__ function

--- src/vak/models/util.py
+++ src/vak/models/util.py
@@ -18,11 +18,7 @@ https://packaging.python.org/guides/creating-and-discovering-plugins/#using-pack
 https://setuptools.readthedocs.io/en/latest/setuptools.html#dynamic-discovery-of-services-and-plugins
 https://amir.rachum.com/blog/2017/07/28/python-entry-points/
 """
-import attr
-
-
 MODELS_ENTRY_POINT = 'vak.models'
-MODEL_CONFIG_PARSERS_ENTRY_POINT = 'vak.model_config_parsers'
 
 
 def iter_entry_points(group_name):
@@ -44,15 +40,6 @@ def find():
         yield entrypoint.name, entrypoint.load()
 
 
-def find_config_parsers():
-    """find installed vak.model config parsers
-
-    returns generator that yields model name and function for loading
-    """
-    for entrypoint in iter_entry_points(MODEL_CONFIG_PARSERS_ENTRY_POINT):
-        yield entrypoint.name, entrypoint.load()
-
-
 def from_model_config_map(model_config_map, num_classes, input_shape):
     """get models that are ready to train, given their names and configurations.
 
@@ -63,8 +50,12 @@ def from_model_config_map(model_config_map, num_classes, input_shape):
     Parameters
     ----------
     model_config_map : dict
+        where each key-value pair is model name : dict of config parameters
     num_classes : int
+        number of classes model will be trained to classify
     input_shape : tuple, list
+        e.g. (channels, height, width).
+        Batch size is not required for input shape.
 
     Returns
     -------
@@ -72,19 +63,17 @@ def from_model_config_map(model_config_map, num_classes, input_shape):
         where keys are model names and values are instances of the models, ready for training
     """
     MODELS = {model_name: model_builder for model_name, model_builder in find()}
-    MODEL_CONFIG_PARSERS = {model_name: model_config_parser
-                            for model_name, model_config_parser in find_config_parsers()}
 
     models_map = {}
-    for model_name, model_config_kwargs in model_config_map.items():
+    for model_name, model_config in model_config_map.items():
         # pass section dict as kwargs to config parser function
-        model_config = MODEL_CONFIG_PARSERS[model_name](num_classes=num_classes,
-                                                        input_shape=input_shape,
-                                                        **model_config_kwargs)
-        # now convert the config itself to a dict and pass as kwargs to the model
-        if type(model_config) != dict:
-            # if not a dict, assume an attrs-based class for config, and convert to dict
-            model_config = attr.asdict(model_config)
-        model = MODELS[model_name](**model_config)
+        model_config['network'].update(
+            num_classes=num_classes,
+            input_shape=input_shape,
+        )
+        try:
+            model = MODELS[model_name].from_config(config=model_config)
+        except KeyError:
+            model = MODELS[f'{model_name}Model'].from_config(config=model_config)
         models_map[model_name] = model
     return models_map
