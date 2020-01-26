26162ed1bbfb4a36be3b39951f5c1d96211be98f
David Nicholson <nickledave@users.noreply.github.com>
2020-01-25 21:43:20 -0500

    rewrite config/models.py to work with toml

--- src/vak/config/models.py
+++ src/vak/config/models.py
@@ -1,30 +1,29 @@
-from configparser import ConfigParser, NoSectionError
 from pathlib import Path
+
+import toml
 
 from .. import models
 
 
-def map_from_config(config_obj, model_names):
+def map_from_config_dict(config_dict, model_names):
     """map a list of model names to model configuration sections from a
-    config.ini file.
+    config.toml file.
 
-    Given a ConfigParser instance and a list of model names, returns dict that
-    maps model names to config sections. If no section in the config.ini file
-    matches the model name, the name will map the value None.
+    Given the configuraiton in a dict and a list of model names, returns dict that
+    maps model names to config sections. If no section in the config.toml file
+    matches the model name, an error is raised.
 
-    Can be used to get configuration for only the models specified
-    in a certain section of config.ini file, e.g. in the TRAIN section.
+    Used to get configuration for only the models specified
+    in a certain section of config.toml file, e.g. in the TRAIN section.
 
     The returned model-config map can be used with vak.models.from_model_config_map
     (and the number of classes and input shape) to get a list of model instances
-    ready for training. Any model name that maps to None will use the default
-    values defined for the config.
+    ready for training.
 
     Parameters
     ----------
-    config_obj : configparser.ConfigParser
-        instance of ConfigParser with config.ini file already read into it
-        that has sections representing configurations for models
+    config_dict : dict
+        configuration from a .toml file, loaded into a dictionary
     model_names : list
         of str, i.e. names of models specified by a section
         (such as TRAIN or PREDICT) that should each have corresponding sections
@@ -34,50 +33,51 @@ def map_from_config(config_obj, model_names):
     -------
     model_config_map : dict
         where each key is the name of a model and the corresponding value is
-        a section from a config.ini file.
+        a section from a config.toml file.
     """
+    # first check whether models in list are installed
+
     # load entry points within function, not at module level,
     # to avoid circular dependencies
     # (user would be unable to import models in other packages
     # if the module in the other package needed to `import vak`)
     MODELS = {model_name: model_builder for model_name, model_builder in models.find()}
-    MODEL_CONFIG_PARSERS = {model_name: model_config_parser
-                            for model_name, model_config_parser in models.find_config_parsers()}
-
     for model_name in model_names:
         if model_name not in MODELS:
-            raise ValueError(
-                f'Model not installed: {model_name}. Installed models are: {list(MODELS.keys())}'
-            )
-        if model_name not in MODEL_CONFIG_PARSERS:
-            raise ValueError(
-                f'Could not config parser for model: {model_name}. '
-                f'Installed model config parsers are: {list(MODEL_CONFIG_PARSERS.keys())}'
-            )
+            # try appending 'Model' to name
+            tmp_model_name = f'{model_name}Model'
+            if tmp_model_name not in MODELS:
+                raise ValueError(
+                    f"Did not find an installed model named {model_name} or {tmp_model_name}. "
+                    f"Installed models are: {list(MODELS.keys())}"
+                )
 
-    sections = config_obj.sections()
+    # now see if we can find corresponding sections in config
+    sections = list(config_dict.keys())
     model_config_map = {}
     for model_name in model_names:
         if model_name in sections:
-            model_section_dict = dict(config_obj[model_name].items())
-            model_config_map[model_name] = model_section_dict
+            model_config_map[model_name] = config_dict[model_name]
         else:
-            model_config_map[model_name] = {}  # will use default config
+            # try appending 'Model' to name
+            tmp_model_name = f'{model_name}Model'
+            if tmp_model_name not in sections:
+                raise ValueError(
+                    f'did not find section named {model_name} or {tmp_model_name} '
+                    f'in config'
+                )
+            model_config_map[tmp_model_name] = config_dict[tmp_model_name]
     return model_config_map
 
 
-def map_from_path(config_path, model_names):
-    """map a list of model names to model configuration sections from a
-    config.ini file.
-
-    Convenience function that wraps config.models.map and accepts
-    path to config.ini file (instead of an instance of a ConfigParser
-    with the config.ini file already loaded into it)
+def map_from_path(toml_path, model_names):
+    """map a list of model names to sections from a .toml configuration file
+     that specify parameters for those models.
 
     Parameters
     ----------
-    config_path : str, Path
-        to config.ini file
+    toml_path : str, Path
+        to configuration file in .toml format
      model_names : list
         of str, i.e. names of models specified by a section
         (such as TRAIN or PREDICT) that should each have corresponding sections
@@ -91,10 +91,10 @@ def map_from_path(config_path, model_names):
     """
     # check config_path is a file,
     # because if it doesn't ConfigParser will just return an "empty" instance w/out sections or options
-    config_path = Path(config_path)
-    if not config_path.is_file():
-        raise FileNotFoundError(f'path not recognized as a file: {config_path}')
+    toml_path = Path(toml_path)
+    if not toml_path.is_file():
+        raise FileNotFoundError(f'file not found, or not recognized as a file: {toml_path}')
 
-    config_obj = ConfigParser()
-    config_obj.read(config_path)
-    return map_from_config(config_obj, model_names)
+    with toml_path.open('r') as fp:
+        config_dict = toml.load(fp)
+    return map_from_config_dict(config_dict, model_names)
