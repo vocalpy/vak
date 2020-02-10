2b5ef7f03c1747bdb460ef45276fcf846fdefb82
David Nicholson <nickledave@users.noreply.github.com>
2020-02-09 19:49:10 -0500

    fix cli function so user can call predict

--- src/vak/cli/cli.py
+++ src/vak/cli/cli.py
@@ -1,11 +1,7 @@
-from pathlib import Path
-
 from .train import train
-from .predict import predict
 from .learncurve import learning_curve
+from .predict import predict
 from .prep import prep
-from .. import config
-from ..config import parse_learncurve_config, parse_predict_config, parse_prep_config
 
 
 def cli(command, config_file):
@@ -18,12 +14,6 @@ def cli(command, config_file):
     config_file : str, Path
         path to a config.toml file
     """
-    # check config_file exists,
-    # because if it doesn't ConfigParser will just return an "empty" instance w/out sections or options
-    config_file = Path(config_file)
-    if not config_file.is_file():
-        raise FileNotFoundError(f'file not found, or not recognized as a file: {config_file}')
-
     if command == 'prep':
         prep(toml_path=config_file)
 
@@ -33,15 +23,9 @@ def cli(command, config_file):
     elif command == 'finetune':
         raise NotImplementedError
 
-    # elif command == 'predict':
-    #     predict_config = parse_predict_config(config_obj)
-    #     models = config.models.from_config(config_obj, predict_config.models)
-    #     predict(predict_vds_path=predict_config.predict_vds_path,
-    #             train_vds_path=predict_config.train_vds_path,
-    #             checkpoint_path=predict_config.checkpoint_path,
-    #             networks=models,
-    #             spect_scaler_path=predict_config.spect_scaler_path)
-    #
+    elif command == 'predict':
+        predict(toml_path=config_file)
+
     # elif command == 'learncurve':
     #     if config_obj.has_option('TRAIN', 'results_dir_made_by_main_script'):
     #         raise ValueError(
@@ -78,3 +62,7 @@ def cli(command, config_file):
     #                    previous_run_path=learncurve_config.previous_run_path,
     #                    root_results_dir=learncurve_config.root_results_dir,
     #                    save_transformed_data=learncurve_config.save_transformed_data)
+    else:
+        raise ValueError(
+            f'command not recognized: {command}'
+        )
