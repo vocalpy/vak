cb1cf2a366a790513776b9d0f6c244803f9d4439
David Nicholson <NickleDave@users.noreply.github.com>
2020-03-29 22:23:04 -0400

    add 'labelset_from_toml_value' function to config/converters.py

--- src/vak/config/converters.py
+++ src/vak/config/converters.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from distutils.util import strtobool
 
+from ..util.general import range_str
+
 
 def bool_from_str(value):
     if type(value) == bool:
@@ -23,3 +25,36 @@ def comma_separated_list(value):
 def expanded_user_path(value):
     return Path(value).expanduser()
 
+
+def labelset_from_toml_value(value):
+    """convert value for 'labelset' option from config file into a set
+
+    Parameters
+    ----------
+    value : str, list
+        if value is a list, returns set(value).
+        If value is a str, and it starts with "range:", then everything after range is passed to
+        vak.util.general.range_str, and the returned list is converted to a set.
+        Other strings that do not start with "range:" are just converted to a set.
+
+    Returns
+    -------
+    labelset : set
+
+    Examples
+    --------
+    >>> labelset_from_toml_value([1, 2, 3])
+    {1, 2, 3}
+    >>> labelset_from_toml_value('range: 1-3, 5')
+    {'1', '2', '3', '5'}
+    >>> labelset_from_toml_value('1235')
+    {'1', '2', '3', '5'}
+    """
+    if type(value) is list:
+        return set(value)
+    elif type(value) is str:
+        if value.startswith('range:'):
+            value = value.replace('range:', '')
+            return set(range_str(value))
+        else:
+            return set(value)
