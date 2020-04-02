4cd7eab09970cfa5f7ca8ba73a8e5c9d68391395
David Nicholson <NickleDave@users.noreply.github.com>
2020-04-02 18:25:03 -0400

    change behavior of labelset_from_toml_value converter
    Now always returns labelset as single-character strings
    (e.g. {'a', 'b', '1', '3', '5'}). Handles strings like "abcde"
    and a "range string" like "range: 1-20, 22" as before, but now
    casts lists of ints to string, and parses each element of a
    list of strings according to the rules for single strings--i.e.,
    labelset=["range:1-3", "a-"] would become {'1', '2', '3', 'a', '-'}.

--- src/vak/config/converters.py
+++ src/vak/config/converters.py
@@ -27,34 +27,61 @@ def expanded_user_path(value):
 
 
 def labelset_from_toml_value(value):
-    """convert value for 'labelset' option from config file into a set
+    """convert value for 'labelset' option from .toml config file into a set
 
     Parameters
     ----------
     value : str, list
-        if value is a list, returns set(value).
-        If value is a str, and it starts with "range:", then everything after range is passed to
-        vak.util.general.range_str, and the returned list is converted to a set.
-        Other strings that do not start with "range:" are just converted to a set.
+        value assigned to 'labelset' option in a .toml file
 
     Returns
     -------
     labelset : set
+        of single-character strings.
+
+    Notes
+    -----
+    If value is a str, and it starts with "range:", then everything after range is converted to
+    some range of integers, by passing the string to vak.util.general.range_str,
+    and the returned list is converted to a set. E.g. "range: 1-5" becomes {'1', '2', '3', '4', '5'}.
+    Other strings that do not start with "range:" are just converted to a set. E.g. "abc" becomes {'a', 'b', 'c'}.
+
+    If value is a list, returns set(value). If all values in list are int,
+    they will be converted to strings, e.g. [1, 2, 3, 4, 5] becomes {'1', '2', '3', '4', '5'}.
+    If all values are string, any that begin with "range:" will be passed to vak.util.general.range_str.
+    Any other strings will be
+
 
     Examples
     --------
-    >>> labelset_from_toml_value([1, 2, 3])
-    {1, 2, 3}
-    >>> labelset_from_toml_value('range: 1-3, 5')
-    {'1', '2', '3', '5'}
     >>> labelset_from_toml_value('1235')
     {'1', '2', '3', '5'}
+    >>> labelset_from_toml_value('range: 1-3, 5')
+    {'1', '2', '3', '5'}
+    >>> labelset_from_toml_value([1, 2, 3])
+    {'1', '2', '3'}
+    >>> labelset_from_toml_value(['a', 'b', 'c'])
+    {'a', 'b', 'c'}
+    >>> labelset_from_toml_value(['range: 1-3', 'a-'])
+    {'-', '1', '2', '3', 'a'}
     """
-    if type(value) is list:
-        return set(value)
-    elif type(value) is str:
+    if type(value) is str:
         if value.startswith('range:'):
             value = value.replace('range:', '')
             return set(range_str(value))
         else:
             return set(value)
+    elif type(value) is list:
+        if all([type(el) == int for el in value]):
+            return set([str(el) for el in value])
+        elif all([type(el) == str for el in value]):
+            labelset = []
+            for el in value:
+                if el.startswith('range:'):
+                    el = el.replace('range:', '')
+                    labelset.extend(range_str(el))
+                else:
+                    labelset.extend(
+                        list(el)  # in case element is a string with multiple characters
+                    )
+            return set(labelset)
