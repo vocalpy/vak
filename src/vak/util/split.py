vocalpy/vak

commit 6f827133316047dcc76b2dd25b076caa70c9c494

remove redundant validation, fix #141

src/vak/util/split.py
@@ -126,13 +126,6 @@ def train_test_dur_split(vak_df,
     """
     labels = labels_from_df(vak_df)
 
-    durs = vak_df['duration'].values
-    total_dataset_dur = durs.sum()
-    train_dur, val_dur, test_dur = validate_durations_convert_nonnegative(train_dur,
-                                                                          val_dur,
-                                                                          test_dur,
-                                                                          total_dataset_dur)
-
     train_inds, val_inds, test_inds = train_test_dur_split_inds(durs=durs,
                                                                 labels=labels,
                                                                 labelset=labelset,

commit b29b5a7e1b2f5fddd6418da8e208d8589cc7c840

fix bug introduced by 6f82713
accidentally took out the line that creates 'durs' variable

src/vak/util/split.py
@@ -126,6 +126,7 @@ def train_test_dur_split(vak_df,
     """
     labels = labels_from_df(vak_df)
 
+    durs = vak_df['duration'].values
     train_inds, val_inds, test_inds = train_test_dur_split_inds(durs=durs,
                                                                 labels=labels,
                                                                 labelset=labelset,