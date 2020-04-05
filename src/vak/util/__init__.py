ee7ed07a42d08087190ad8165e0de4d32dad7340
David Nicholson <nickledave@users.noreply.github.com>
2020-04-04 22:26:29 -0400

    add summary_writer module to vak/util/

--- src/vak/util/__init__.py
+++ src/vak/util/__init__.py
@@ -9,6 +9,7 @@ from . import logging
 from . import path
 from . import splitalgos
 from . import spect
+from . import summary_writer
 from . import validation
 
 from .split import train_test_dur_split
