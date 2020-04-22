2a764df7d95a0d96cd0aa016e1da23ddb1500b17
David Nicholson <nickledave@users.noreply.github.com>
2020-04-22 11:14:20 -0400

    rewrite cli/prep.py to use core.prep

--- src/vak/cli/prep.py
+++ src/vak/cli/prep.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from datetime import datetime
 import warnings
@@ -6,9 +5,8 @@ import warnings
 import toml
 
 from .. import config
+from .. import core
 from .. import util
-from ..io import dataframe
-from ..util import train_test_dur_split
 
 
 def prep(toml_path):
@@ -49,51 +47,17 @@ def prep(toml_path):
 
     if cfg.prep is None:
         raise ValueError(
-            f'prep called with a config.ini file that does not have a PREP section: {toml_path}'
+            f'prep called with a config.toml file that does not have a PREP section: {toml_path}'
         )
 
-    # pre-conditions ---------------------------------------------------------------------------------------------------
-    if cfg.prep.audio_format is None and cfg.prep.spect_format is None:
-        raise ValueError("Must specify either audio_format or spect_format")
-
-    if cfg.prep.audio_format and cfg.prep.spect_format:
-        raise ValueError("Cannot specify both audio_format and spect_format, "
-                         "unclear whether to create spectrograms from audio files or "
-                         "use already-generated spectrograms from array files")
-
-    if cfg.prep.labelset is not None:
-        if type(cfg.prep.labelset) not in (set, list):
-            raise TypeError(
-                f"type of labelset must be set or list, but type was: {type(labelset)}"
-            )
-
-        if type(cfg.prep.labelset) == list:
-            labelset_set = set(cfg.prep.labelset)
-            if len(cfg.prep.labelset) != len(labelset_set):
-                raise ValueError(
-                    'labelset contains repeated elements, should be a set (i.e. all members unique.\n'
-                    f'Labelset was: {cfg.prep.labelset}'
-                )
-            else:
-                cfg.prep.labelset = labelset_set
-
-    if cfg.prep.output_dir:
-        if not os.path.isdir(cfg.prep.output_dir):
-            raise NotADirectoryError(
-                f'output_dir not found: {cfg.prep.output_dir}'
-            )
-    elif cfg.prep.output_dir is None:
-        cfg.prep.output_dir = cfg.prep.data_dir
-
     # ---- set up logging ----------------------------------------------------------------------------------------------
-    timenow = datetime.now().strftime('%y%m%d_%H%M%S')  # also used below for csv filename
+    timenow = datetime.now().strftime('%y%m%d_%H%M%S')
     logger = util.logging.get_logger(log_dst=cfg.prep.output_dir,
                                      caller='prep',
                                      timestamp=timenow,
                                      logger_name=__name__)
 
-    # ---- figure out what section we will be saving path in, and prefix of option name in that section ----------------
-    # (e.g., if it's PREDICT section then the prefix will be 'predict' for 'predict_vds_path' option
+    # ---- figure out purpose of config file from sections; will save csv path in that section -------------------------
     with toml_path.open('r') as fp:
         config_toml = toml.load(fp)
     if 'TRAIN' in config_toml:
@@ -118,64 +82,21 @@ def prep(toml_path):
         )
     logger.info(f'determined that config file has section: {section}\nWill add csv_path option to that section')
 
-    # ---- figure out file name ----------------------------------------------------------------------------------------
-    _, tail = os.path.split(cfg.prep.data_dir)
-    csv_fname_stem = f'{tail}_prep_{timenow}'
-    csv_path = os.path.join(cfg.prep.output_dir, f'{csv_fname_stem}.csv')
-
-    # ---- figure out if we're going to split into train / val / test sets ---------------------------------------------
-    # catch case where user specified duration for just training set, raise a helpful error instead of failing silently
-    if ((section == 'TRAIN' or section == 'LEARNCURVE') and
-            ((cfg.prep.train_dur is not None and cfg.prep.train_dur > 0) and
-             (cfg.prep.val_dur is None or cfg.prep.val_dur == 0) and
-             (cfg.prep.test_dur is None or cfg.prep.val_dur == 0)
-            )):
-        raise ValueError(
-            'duration specified for just training set, but prep function does not currently support creating a '
-            'single split of a specified duration. Either remove the train_dur option from the prep section and '
-            'rerun, in which case all data will be included in the training set, or specify values greater than '
-            'zero for test_dur (and val_dur, if a validation set will be used)'
-        )
-
-    if all([dur is None for dur in (cfg.prep.train_dur,
-                                    cfg.prep.val_dur,
-                                    cfg.prep.test_dur)]):
-        # then we're not going to split
-        logger.info('will not split dataset')
-        do_split = False
-    else:
-        if cfg.prep.val_dur is not None and cfg.prep.train_dur is None and cfg.prep.test_dur is None:
-            raise ValueError('cannot specify only val_dur, unclear how to split dataset into training and test sets')
-        else:
-            logger.info('will split dataset')
-            do_split = True
-
-    # ---- actually make the dataset -----------------------------------------------------------------------------------
-    vak_df = dataframe.from_files(labelset=cfg.prep.labelset,
-                                  data_dir=cfg.prep.data_dir,
-                                  annot_format=cfg.prep.annot_format,
-                                  output_dir=cfg.prep.output_dir,
-                                  annot_file=cfg.prep.annot_file,
-                                  audio_format=cfg.prep.audio_format,
-                                  spect_format=cfg.prep.spect_format,
-                                  spect_params=cfg.spect_params,
-                                  logger=logger)
-
-    if do_split:
-        # save before splitting, jic duration args are not valid (we can't know until we make dataset)
-        vak_df.to_csv(csv_path)
-        vak_df = train_test_dur_split(vak_df,
-                                      labelset=cfg.prep.labelset,
-                                      train_dur=cfg.prep.train_dur,
-                                      val_dur=cfg.prep.val_dur,
-                                      test_dur=cfg.prep.test_dur)
-
-    elif do_split is False:
-        # add a split column, but assign everything to the same 'split'
-        vak_df = dataframe.add_split_col(vak_df, split=section.lower())
-
-    logger.info(f'saving dataset as a .csv file: {csv_path}')
-    vak_df.to_csv(csv_path)
+    purpose = section.lower()
+    vak_df, csv_path = core.prep(data_dir=cfg.prep.data_dir,
+                                 purpose=purpose,
+                                 audio_format=cfg.prep.audio_format,
+                                 spect_format=cfg.prep.spect_format,
+                                 spect_params=cfg.spect_params,
+                                 annot_format=cfg.prep.annot_format,
+                                 annot_file=cfg.prep.annot_file,
+                                 labelset=cfg.prep.labelset,
+                                 output_dir=cfg.prep.output_dir,
+                                 train_dur=cfg.prep.train_dur,
+                                 val_dur=cfg.prep.val_dur,
+                                 test_dur=cfg.prep.test_dur,
+                                 logger=logger,
+                                 )
 
     # use config and section from above to add csv_path to config.ini file
     config_toml[section]['csv_path'] = str(csv_path)
