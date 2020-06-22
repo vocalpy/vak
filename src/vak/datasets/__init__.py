1a5a9650b486d6981237db79c2e161f1fedf5fd7
David Nicholson <NickleDave@users.noreply.github.com>
2020-06-21 23:11:18 -0400

    import all classes, add __all__ to datasets.__init__.py

--- src/vak/datasets/__init__.py
+++ src/vak/datasets/__init__.py
@@ -1,1 +1,9 @@
+from .unannotated_dataset import UnannotatedDataset
+from .vocal_dataset import VocalDataset
 from .window_dataset import WindowDataset
+
+__all__ = [
+    'UnannotatedDataset',
+    'VocalDataset',
+    'WindowDataset'
+]
