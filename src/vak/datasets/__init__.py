7dd2a21d98f42d7e6fad1f39a4cfef6bfbea5a8f
David Nicholson <nickledave@users.noreply.github.com>
2020-07-13 09:24:21 -0400

    remove UnannotatedDataset, no longer used

--- src/vak/datasets/__init__.py
+++ src/vak/datasets/__init__.py
@@ -1,9 +1,7 @@
-from .unannotated_dataset import UnannotatedDataset
 from .vocal_dataset import VocalDataset
 from .window_dataset import WindowDataset
 
 __all__ = [
-    'UnannotatedDataset',
     'VocalDataset',
     'WindowDataset'
 ]
