66739a9f404e2de5e1a47d0b24e35a6396db2de4
David Nicholson <nickledave@users.noreply.github.com>
2020-07-31 15:42:28 -0400

    add imports and __all__ to vak/plot/__init__.py

---
+++ src/vak/plot/__init__.py
@@ -0,0 +1,11 @@
+from . import learncurve
+from .annot import annotation, plot_labels, plot_segments
+from .spect import spect, spect_annot
+
+__all__ = [
+    'learncurve',
+    'plot_labels',
+    'plot_segments',
+    'spect',
+    'spect_annot',
+]
