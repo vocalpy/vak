6341c8d4991a4e51565953f8e15d40f13419e6d5
David Nicholson <nickledave@users.noreply.github.com>
2020-12-12 19:23:38 -0500

    fix + organize imports in vak/cli/__init__.py
    - import eval module -- the import statement was missing
    - alphabetize import statements
    - add an `__all__` declaration

--- src/vak/cli/__init__.py
+++ src/vak/cli/__init__.py
@@ -1,7 +1,18 @@
 """command-line interface functions for training,
 creating learning curves, etc."""
-from .prep import prep
+
+from .cli import cli
+from .eval import eval
 from .learncurve import learning_curve
 from .predict import predict
-from .cli import cli
+from .prep import prep
 from .train import train
+
+__all__ = [
+    'cli',
+    'eval',
+    'learncurve',
+    'predict',
+    'prep',
+    'train',
+]
