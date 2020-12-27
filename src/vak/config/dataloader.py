1012d7ef2832e6148ed670369d89d9d7fe63dee0
David Nicholson <nickledave@users.noreply.github.com>
2020-12-26 20:38:59 -0500

    clean up parse_dataloader_config function

--- src/vak/config/dataloader.py
+++ src/vak/config/dataloader.py
@@ -17,11 +17,30 @@ class DataLoaderConfig:
                           default=88)
 
 
-def parse_dataloader_config(config, config_path):
-    # return defaults if config doesn't have SPECTROGRAM section
+def parse_dataloader_config(config_toml, toml_path):
+    """parse [DATALOADER] section of config.toml file
+
+    Parameters
+    ----------
+    config_toml : dict
+        containing configuration file in TOML format, already loaded by parse function
+    toml_path : Path
+        path to a configuration file in TOML format.
+        Note that this is not actually used but the function has this parameter
+        for consistency with other functions.
+        **Removing it will cause ``vak.config.parse.from_toml`` to crash, since it
+        tries to loop through all the section parser functions,
+        nd pass them both arguments.**
+
+    Returns
+    -------
+    dataloader_config : vak.config.dataloader.DataloaderConfig
+        instance of Dataloader class
+    """
+    # return defaults if config doesn't have DATALOADER section
     dataloader_section = {}
-    if 'DATALOADER' in config:
+    if 'DATALOADER' in config_toml:
         dataloader_section.update(
-            config['DATALOADER'].items()
+            config_toml['DATALOADER'].items()
         )
     return DataLoaderConfig(**dataloader_section)
