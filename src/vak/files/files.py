f751a527bd47d94062a06372940f217b39fc16f4
David Nicholson <nickledave@users.noreply.github.com>
2021-01-02 15:27:20 -0500

    make `files.from_dir` case insensitive, fix #275

--- src/vak/files/files.py
+++ src/vak/files/files.py
@@ -1,6 +1,6 @@
-import os
+import fnmatch
+from pathlib import Path
 import re
-from glob import glob
 
 
 def find_fname(fname, ext):
@@ -19,7 +19,6 @@ def find_fname(fname, ext):
     -------
     sub_fname : str or None
 
-
     Examples
     --------
     >>> sub_fname(fname='llb3_0003_2018_04_23_14_18_54.wav.mat', ext='wav')
@@ -36,16 +35,23 @@ def from_dir(dir_path, ext):
     """helper function that gets all files with a given extension
     from a directory or its sub-directories.
 
-    If no files with the specified extension are found in the directory, then
-    the function recurses into all sub-directories and returns any files with
-    the extension in those sub-directories.
+    ``from_dir`` is case insensitive. For example, if you specify the extension
+    as ``wav`` then it will return files that end in ``.wav`` or ``.WAV``.
+    Similarly, if you specify ``TextGrid`` as the extension,
+    it will return files that end in ``.textgrid`` and ``.TextGrid``.
+
+    If no files with the specified extension are found in the directory,
+    then the function looks in all directories within ``dir_path``
+    and returns any files with the extension in those directories.
+    Currently the function does not recurse, i.e., it does not look
+    any deeper than one level below ``dir_path``.
 
     Parameters
     ----------
     dir_path : str
         path to target directory
     ext : str
-        file extension to search for
+        file extension to search for. E.g.,
 
     Returns
     -------
@@ -56,17 +62,34 @@ def from_dir(dir_path, ext):
     -----
     used by vak.io.audio.files_from_dir and vak.io.annot.files_from_dir
     """
-    wildcard_with_extension = f'*.{ext}'
-    files = sorted(
-        glob(os.path.join(dir_path, wildcard_with_extension))
-    )
+    dir_path = Path(dir_path)
+    if not dir_path.is_dir():
+        raise NotADirectoryError(
+            f'dir_path not recognized as a directory: {dir_path}'
+        )
+
+    # use fnmatch + re to make search case-insensitive
+    # adopted from:
+    # https://gist.github.com/techtonik/5694830
+    # https://jdhao.github.io/2019/06/24/python_glob_case_sensitivity/
+    glob_pat = f'*.{ext}'
+    rule = re.compile(fnmatch.translate(glob_pat), re.IGNORECASE)
+
+    files = [
+        file
+        for file in dir_path.iterdir()
+        if file.is_file() and rule.match(file.name)
+    ]
+
     if len(files) == 0:
         # if we don't any files with extension, look in sub-directories
         files = []
-        subdirs = glob(os.path.join(dir_path, '*/'))
+        subdirs = [subdir for subdir in dir_path.iterdir() if subdir.is_dir()]
         for subdir in subdirs:
             files.extend(
-                glob(os.path.join(dir_path, subdir, wildcard_with_extension))
+                [file
+                 for file in subdir.iterdir()
+                 if file.is_file() and rule.match(file.name)]
             )
 
     if len(files) == 0:
@@ -75,4 +98,5 @@ def from_dir(dir_path, ext):
             f'{dir_path} or immediate sub-directories'
         )
 
-    return files
+    # TODO: use / return Path instead of strings
+    return [str(file) for file in files]
