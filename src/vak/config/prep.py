6e278076f528e92a23cabf2023bf0264f3873946
David Nicholson <nickledave@users.noreply.github.com>
2021-01-24 08:55:44 -0500

    add `spect_output_dir` option to PREP section, fix #289
    - add `spect_output_dir` attribute to `vak.config.prep.PrepConfig`
    - add as an option in PREP section to `vak/config/valid.toml`

--- src/vak/config/prep.py
+++ src/vak/config/prep.py
@@ -50,6 +50,11 @@ class PrepConfig:
     spect_format : str
         format of files containg spectrograms as 2-d matrices.
         One of {'mat', 'npy'}.
+    spect_output_dir : str
+        path to directory where array files containing spectrograms
+        should be saved, when generated from audio files.
+        Default is None, in which case the spectrogram files
+        are saved in ``data_dir`` by ``vak.io.dataframe.from_files``.
     annot_format : str
         format of annotations. Any format that can be used with the
         crowsetta library is valid.
@@ -79,6 +84,9 @@ class PrepConfig:
 
     audio_format = attr.ib(validator=validators.optional(is_audio_format), default=None)
     spect_format = attr.ib(validator=validators.optional(is_spect_format), default=None)
+    spect_output_dir = attr.ib(converter=converters.optional(expanded_user_path),
+                               validator=validators.optional(is_a_directory),
+                               default=None)
     annot_file = attr.ib(converter=converters.optional(expanded_user_path),
                          validator=validators.optional(is_a_file), default=None)
     annot_format = attr.ib(validator=validators.optional(is_annot_format), default=None)
