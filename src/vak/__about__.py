f5520a7acebccea7ca1bb77cd0840b65e32975bd
David Nicholson <nickledave@users.noreply.github.com>
2021-01-24 23:49:37 -0500

    bump version to 0.4.0dev1

--- src/vak/__about__.py
+++ src/vak/__about__.py
@@ -23,7 +23,7 @@ __title__ = "vak"
 __summary__ = "neural network toolbox for animal communication and bioacoustics"
 __uri__ = "https://github.com/NickleDave/vak"
 
-__version__ = "0.3.3"
+__version__ = "0.4.0dev1"
 
 if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
     with open(os.path.join(base_dir, ".commit")) as fp:
