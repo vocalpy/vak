b97e7f231e7ee5c2234915d51fc03eff7ce60595
David Nicholson <nickledave@users.noreply.github.com>
2021-02-20 10:25:58 -0500

    add timenow.py

---
+++ src/vak/timenow.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from .constants import STRFTIME_TIMESTAMP
+
+
+def get_timenow_as_str():
+    f"""returns current time as a string, 
+    with the format specified by ``vak.constants.STRFTIME_TIMESTAMP``"""
+    return datetime.now().strftime(STRFTIME_TIMESTAMP)
