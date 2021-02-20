cf83d504f4dbe9e70c34f4b3bedc5d421fcb5007
David Nicholson <nickledave@users.noreply.github.com>
2021-02-20 10:17:46 -0500

    TST: rewrite test_core to use pytest, add tests
    - rewrite existing modules to work with pytest
    - remove two unused/out-of-date unit test modules
    - add tests/unit/test_core/test_train.py
    - add tests/unit/test_core/test_eval.py
    - add tests/unit/test_core/test_prep.py

--- tests/unit/test_core/test_learncurve.py
+++ tests/unit/test_core/test_learncurve.py
@@ -1,188 +1,137 @@
 """tests for vak.core.learncurve module"""
-import os
-from pathlib import Path
-import tempfile
-import shutil
-import unittest
-from datetime import datetime
-from configparser import ConfigParser
-
-import vak.core.learncurve
 import vak.config
-from vak.core.learncurve import LEARN_CURVE_DIR_STEM
-
-HERE = Path(__file__).parent
-TEST_DATA_DIR = HERE.joinpath('..', '..', 'test_data')
-TEST_CONFIGS_DIR = TEST_DATA_DIR.joinpath('configs')
-SETUP_SCRIPTS_DIR = HERE.joinpath('..', '..', 'setup_scripts')
+import vak.constants
+import vak.core.learncurve
+import vak.paths
 
 
-class TestLearncurve(unittest.TestCase):
-    def setUp(self):
-        self.tmp_output_dir = tempfile.mkdtemp()
+def learncurve_output_matches_expected(cfg,
+                                       model_config_map,
+                                       results_path):
+    assert results_path.joinpath('learning_curve.csv').exists()
 
-        test_learncurve_config = TEST_CONFIGS_DIR.joinpath('test_learncurve_config.ini')
-        # Now we want a copy (of the changed version) to use for tests
-        # since this is what the test data was made with
-        self.tmp_config_dir = tempfile.mkdtemp()
-        self.tmp_config_path = Path(self.tmp_config_dir).joinpath('tmp_test_learncurve_config.ini')
-        shutil.copy(test_learncurve_config, self.tmp_config_path)
+    for train_set_dur in cfg.learncurve.train_set_durs:
+        train_set_dur_root = results_path.joinpath(f'train_dur_{train_set_dur}s')
+        assert train_set_dur_root.exists()
 
-        # rewrite config so it points to data for testing + temporary output dirs
-        config = ConfigParser()
-        config.read(self.tmp_config_path)
-        test_data_vds_path = list(TEST_DATA_DIR.glob('vds'))[0]
-        for stem in ['train', 'test', 'val']:
-            vds_path = list(test_data_vds_path.glob(f'*.{stem}.vds.json'))
-            self.assertTrue(len(vds_path) == 1)
-            vds_path = vds_path[0]
-            config['LEARNCURVE'][f'{stem}_vds_path'] = str(vds_path)
+        for replicate_num in range(1, cfg.learncurve.num_replicates + 1):
+            replicate_path = train_set_dur_root.joinpath(f'replicate_{replicate_num}')
+            assert replicate_path.exists()
 
-        config['PREP']['output_dir'] = str(self.tmp_output_dir)
-        config['PREP']['data_dir'] = str(TEST_DATA_DIR.joinpath('cbins', 'gy6or6', '032312'))
-        config['LEARNCURVE']['root_results_dir'] = str(self.tmp_output_dir)
-        with open(self.tmp_config_path, 'w') as fp:
-            config.write(fp)
+            prep_csv = sorted(replicate_path.glob('*prep*csv'))
+            assert len(prep_csv) == 1
 
-    def tearDown(self):
-        shutil.rmtree(self.tmp_output_dir)
-        shutil.rmtree(self.tmp_config_dir)
+            assert replicate_path.joinpath('labelmap.json').exists()
 
-    def _check_learncurve_output(self, learncurve_config, nets_config, time_before, time_after):
-        output_dir_after = os.listdir(learncurve_config.root_results_dir)
-        self.assertTrue(len(output_dir_after) == 1)
+            assert replicate_path.joinpath('spect_id_vector.npy').exists()
+            assert replicate_path.joinpath('spect_inds_vector.npy').exists()
+            assert replicate_path.joinpath('x_inds.npy').exists()
 
-        results_dir = output_dir_after[0]
-        self.assertTrue(LEARN_CURVE_DIR_STEM in results_dir)
+            if cfg.learncurve.normalize_spectrograms:
+                assert replicate_path.joinpath('StandardizeSpect').exists()
 
-        time_str_results_dir = results_dir.replace(LEARN_CURVE_DIR_STEM, '')  # to get just datestr
-        time_results_dir = datetime.strptime(time_str_results_dir, '%y%m%d_%H%M%S')
-        self.assertTrue(time_before <= time_results_dir <= time_after)
+            for model_name in model_config_map.keys():
+                eval_csv = sorted(replicate_path.glob(f'eval_{model_name}*csv'))
+                assert len(eval_csv) == 1
 
-        # -------- test output of learncurve.train --------------------------------------------------------------------
-        train_dirname = os.path.join(learncurve_config.root_results_dir, results_dir, 'train')
-        train_dir_list = os.listdir(train_dirname)
-        records_dirs = [item for item in train_dir_list if 'records' in item]
-        self.assertTrue(
-            len(records_dirs) == len(learncurve_config.train_set_durs) * learncurve_config.num_replicates
-        )
+                model_path = replicate_path.joinpath(model_name)
+                assert model_path.exists()
 
-        for record_dir in records_dirs:
-            records_path = os.path.join(train_dirname, record_dir)
-            records_dir_list = os.listdir(records_path)
+                tensorboard_log = sorted(model_path.glob(f'events.out.tfevents.*{model_name}'))
+                assert len(tensorboard_log) == 1
 
-            self.assertTrue('train_inds' in records_dir_list)
+                checkpoints_path = model_path.joinpath('checkpoints')
+                assert checkpoints_path.exists()
+                assert checkpoints_path.joinpath('checkpoint.pt').exists()
+                if cfg.learncurve.val_step is not None:
+                    assert checkpoints_path.joinpath('max-val-acc-checkpoint.pt').exists()
 
-            for net_name in nets_config.keys():
-                self.assertTrue(
-                    # make everything lowercase
-                    net_name.lower() in [item.lower() for item in records_dir_list]
-                )
-
-            if learncurve_config.val_vds_path:
-                self.assertTrue('val_errs' in records_dir_list)
-
-            if learncurve_config.save_transformed_data:
-                self.assertTrue('X_train' in records_dir_list)
-                self.assertTrue('Y_train' in records_dir_list)
-                if learncurve_config.val_vds_path:
-                    self.assertTrue('X_val' in records_dir_list)
-                    self.assertTrue('Y_val' in records_dir_list)
-                self.assertTrue('scaled_spects' in records_dir_list)
-                self.assertTrue('scaled_reshaped_spects' in records_dir_list)
-
-        # -------- test output of learncurve.test ---------------------------------------------------------------------
-        test_dirname = os.path.join(learncurve_config.root_results_dir, results_dir, 'test')
-        self.assertTrue(os.path.isdir(test_dirname))
-        test_dir_list = os.listdir(test_dirname)
-        self.assertTrue('test_err' in test_dir_list)
-        self.assertTrue('train_err' in test_dir_list)
-        self.assertTrue('Y_pred_test_all' in test_dir_list)
-        self.assertTrue('Y_pred_train_all' in test_dir_list)
-        self.assertTrue('y_preds_and_err_for_train_and_test' in test_dir_list)
-
-        return True
-
-    def test_learncurve_func(self):
-        # this kind of repeats what happens in self.setUp, but
-        # this is the way cli does it using what user passed in
-        # so we repeat that logic here
-        config_file = self.tmp_config_path
-        config_obj = ConfigParser()
-        config_obj.read(config_file)
-        learncurve_config = vak.config.parse_learncurve_config(config_obj, config_file)
-        nets_config = vak.config.parse._get_nets_config(config_obj, learncurve_config.networks)
-        prep_config = vak.config.parse_prep_config(config_obj, config_file)
-
-        # want time to make sure results dir generated has correct time;
-        # have to drop microseconds from datetime object because we don't include that in
-        # the string format that's in the directory name, and if we keep it here then
-        # the time recovered from the directory name can be "less than" the time
-        # from before starting--i.e. some datetime with microseconds is less than the
-        # exact same date time but with some number of microseconds
-        time_before = datetime.now().replace(microsecond=0)
-        vak.core.learning_curve(train_vds_path=learncurve_config.train_vds_path,
-                                test_vds_path=learncurve_config.test_vds_path,
-                                total_train_set_duration=prep_config.total_train_set_dur,
-                                train_set_durs=learncurve_config.train_set_durs,
-                                num_replicates=learncurve_config.num_replicates,
-                                num_epochs=learncurve_config.num_epochs,
-                                networks=nets_config,
-                                val_vds_path=learncurve_config.val_vds_path,
-                                val_step=learncurve_config.val_step,
-                                ckpt_step=learncurve_config.ckpt_step,
-                                patience=learncurve_config.patience,
-                                save_only_single_checkpoint_file=learncurve_config.save_only_single_checkpoint_file,
-                                normalize_spectrograms=learncurve_config.normalize_spectrograms,
-                                use_train_subsets_from_previous_run=learncurve_config.use_train_subsets_from_previous_run,
-                                previous_run_path=learncurve_config.previous_run_path,
-                                output_dir=learncurve_config.root_results_dir,
-                                save_transformed_data=learncurve_config.save_transformed_data)
-        time_after = datetime.now().replace(microsecond=0)
-        self.assertTrue(self._check_learncurve_output(
-            learncurve_config, nets_config, time_before, time_after
-        ))
-
-    def test_learncurve_no_validation(self):
-        # this kind of repeats what happens in self.setUp, but
-        # this is the way cli does it using what user passed in
-        # so we repeat that logic here
-        config_file = self.tmp_config_path
-        config_obj = ConfigParser()
-        config_obj.read(config_file)
-        learncurve_config = vak.config.parse_learncurve_config(config_obj, config_file)
-        nets_config = vak.config.parse._get_nets_config(config_obj, learncurve_config.networks)
-        prep_config = vak.config.parse_prep_config(config_obj, config_file)
-
-        # want time to make sure results dir generated has correct time;
-        # have to drop microseconds from datetime object because we don't include that in
-        # the string format that's in the directory name, and if we keep it here then
-        # the time recovered from the directory name can be "less than" the time
-        # from before starting--i.e. some datetime with microseconds is less than the
-        # exact same date time but with some number of microseconds
-        time_before = datetime.now().replace(microsecond=0)
-        vak.core.learning_curve(train_vds_path=learncurve_config.train_vds_path,
-                                test_vds_path=learncurve_config.test_vds_path,
-                                total_train_set_duration=prep_config.total_train_set_dur,
-                                train_set_durs=learncurve_config.train_set_durs,
-                                num_replicates=learncurve_config.num_replicates,
-                                num_epochs=learncurve_config.num_epochs,
-                                networks=nets_config,
-                                val_vds_path=None,
-                                val_step=None,
-                                ckpt_step=learncurve_config.ckpt_step,
-                                patience=learncurve_config.patience,
-                                save_only_single_checkpoint_file=learncurve_config.save_only_single_checkpoint_file,
-                                normalize_spectrograms=learncurve_config.normalize_spectrograms,
-                                use_train_subsets_from_previous_run=learncurve_config.use_train_subsets_from_previous_run,
-                                previous_run_path=learncurve_config.previous_run_path,
-                                output_dir=learncurve_config.root_results_dir,
-                                save_transformed_data=learncurve_config.save_transformed_data)
-        time_after = datetime.now().replace(microsecond=0)
-        self.assertTrue(self._check_learncurve_output(
-            learncurve_config, nets_config, time_before, time_after
-        ))
+    return True
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_learncurve(specific_config,
+                    tmp_path):
+    toml_path = specific_config(config_type='learncurve',
+                                audio_format='cbin',
+                                annot_format='notmat')
+    cfg = vak.config.parse.from_toml_path(toml_path)
+    model_config_map = vak.config.models.map_from_path(toml_path, cfg.learncurve.models)
+    results_path = vak.paths.generate_results_dir_name_as_path(tmp_path)
+    results_path.mkdir()
+
+    vak.core.learning_curve(model_config_map,
+                            train_set_durs=cfg.learncurve.train_set_durs,
+                            num_replicates=cfg.learncurve.num_replicates,
+                            csv_path=cfg.learncurve.csv_path,
+                            labelset=cfg.prep.labelset,
+                            window_size=cfg.dataloader.window_size,
+                            batch_size=cfg.learncurve.batch_size,
+                            num_epochs=cfg.learncurve.num_epochs,
+                            num_workers=cfg.learncurve.num_workers,
+                            root_results_dir=None,
+                            results_path=results_path,
+                            previous_run_path=cfg.learncurve.previous_run_path,
+                            spect_key=cfg.spect_params.spect_key,
+                            timebins_key=cfg.spect_params.timebins_key,
+                            normalize_spectrograms=cfg.learncurve.normalize_spectrograms,
+                            shuffle=cfg.learncurve.shuffle,
+                            val_step=cfg.learncurve.val_step,
+                            ckpt_step=cfg.learncurve.ckpt_step,
+                            patience=cfg.learncurve.patience,
+                            device=cfg.learncurve.device,
+                            logger=None,
+                            )
+
+    assert learncurve_output_matches_expected(cfg,
+                                              model_config_map,
+                                              results_path)
+
+
+def test_learncurve_no_results_path(specific_config,
+                                    tmp_path):
+    root_results_dir = tmp_path.joinpath('test_learncurve_no_results_path')
+    root_results_dir.mkdir()
+
+    options_to_change = {
+        'section': 'LEARNCURVE',
+        'option': 'root_results_dir',
+        'value': str(root_results_dir)
+    }
+    toml_path = specific_config(config_type='learncurve',
+                                audio_format='cbin',
+                                annot_format='notmat',
+                                options_to_change=options_to_change)
+
+    cfg = vak.config.parse.from_toml_path(toml_path)
+    model_config_map = vak.config.models.map_from_path(toml_path, cfg.learncurve.models)
+
+    vak.core.learning_curve(model_config_map,
+                            train_set_durs=cfg.learncurve.train_set_durs,
+                            num_replicates=cfg.learncurve.num_replicates,
+                            csv_path=cfg.learncurve.csv_path,
+                            labelset=cfg.prep.labelset,
+                            window_size=cfg.dataloader.window_size,
+                            batch_size=cfg.learncurve.batch_size,
+                            num_epochs=cfg.learncurve.num_epochs,
+                            num_workers=cfg.learncurve.num_workers,
+                            root_results_dir=cfg.learncurve.root_results_dir,
+                            results_path=None,
+                            previous_run_path=cfg.learncurve.previous_run_path,
+                            spect_key=cfg.spect_params.spect_key,
+                            timebins_key=cfg.spect_params.timebins_key,
+                            normalize_spectrograms=cfg.learncurve.normalize_spectrograms,
+                            shuffle=cfg.learncurve.shuffle,
+                            val_step=cfg.learncurve.val_step,
+                            ckpt_step=cfg.learncurve.ckpt_step,
+                            patience=cfg.learncurve.patience,
+                            device=cfg.learncurve.device,
+                            logger=None,
+                            )
+
+    results_path = sorted(root_results_dir.glob(f'{vak.constants.RESULTS_DIR_PREFIX}*'))
+    assert len(results_path) == 1
+    results_path = results_path[0]
+
+    assert learncurve_output_matches_expected(cfg,
+                                              model_config_map,
+                                              results_path)
--- tests/unit/test_core/test_predict.py
+++ tests/unit/test_core/test_predict.py
@@ -1,103 +1,61 @@
-"""tests for vak.cli.predict module"""
-from configparser import ConfigParser
-import os
-from pathlib import Path
-import shutil
-import tempfile
-import unittest
+"""tests for vak.core.predict module"""
+import pytest
 
-import crowsetta
-import numpy as np
-
+import vak.config
 import vak.core.predict
-from vak.core.learncurve import LEARN_CURVE_DIR_STEM
-from vak.io import Dataset
-import vak.split
-
-HERE = Path(__file__).parent
-TEST_DATA_DIR = HERE.joinpath('..', '..', 'test_data')
-TEST_CONFIGS_PATH = TEST_DATA_DIR.joinpath('configs')
 
 
-def copydir(src, dst):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        shutil.copy2(s, d)
+# written as separate function so we can re-use in tests/unit/test_cli/test_predict.py
+def predict_output_matches_expected(output_dir, annot_csv_filename):
+    annot_csv = output_dir.joinpath(annot_csv_filename)
+    assert annot_csv.exists()
+
+    return True
 
 
-class TestPredict(unittest.TestCase):
-    def setUp(self):
-        self.tmp_output_dir = tempfile.mkdtemp()
+@pytest.mark.parametrize(
+    'audio_format, spect_format, annot_format',
+    [
+        ('cbin', None, 'notmat'),
+        ('wav', None, 'koumura'),
+    ]
+)
+def test_predict(audio_format,
+                 spect_format,
+                 annot_format,
+                 specific_config,
+                 tmp_path):
+    output_dir = tmp_path.joinpath(f'test_predict_{audio_format}_{spect_format}_{annot_format}')
+    output_dir.mkdir()
 
-        a_results_dir = list(
-            TEST_DATA_DIR.joinpath('results').glob(
-                f'{LEARN_CURVE_DIR_STEM}*'))[0]
-        a_training_records_dir = list(
-            Path(a_results_dir).joinpath(
-                'train').glob('records_for_training_set*'))[0]
-        self.checkpoint_path = str(Path(a_training_records_dir).joinpath(
-            'TweetyNet', 'checkpoints'))
-        spect_scaler = list(
-            Path(a_training_records_dir).glob('spect_scaler_*'))[0]
-        self.spect_scaler_path = str(spect_scaler)
+    options_to_change = {
+        'section': 'PREDICT',
+        'option': 'output_dir',
+        'value': str(output_dir)
+    }
+    toml_path = specific_config(config_type='predict',
+                                audio_format=audio_format,
+                                annot_format=annot_format,
+                                options_to_change=options_to_change)
+    cfg = vak.config.parse.from_toml_path(toml_path)
 
-        # rewrite config so it points to data for testing + temporary output dirs
-        # rewrite config so it points to data for testing + temporary output dirs
-        a_config = str(TEST_CONFIGS_PATH.joinpath('test_predict_config.ini'))
-        config = ConfigParser()
-        config.read(a_config)
-        config['PREDICT']['checkpoint_path'] = self.checkpoint_path
-        config['PREDICT']['spect_scaler_path'] = self.spect_scaler_path
+    model_config_map = vak.config.models.map_from_path(toml_path, cfg.predict.models)
 
-        test_data_vds_path = list(TEST_DATA_DIR.glob('vds'))[0]
+    vak.core.predict(csv_path=cfg.predict.csv_path,
+                     checkpoint_path=cfg.predict.checkpoint_path,
+                     labelmap_path=cfg.predict.labelmap_path,
+                     model_config_map=model_config_map,
+                     window_size=cfg.dataloader.window_size,
+                     num_workers=cfg.predict.num_workers,
+                     spect_key=cfg.spect_params.spect_key,
+                     timebins_key=cfg.spect_params.timebins_key,
+                     spect_scaler_path=cfg.predict.spect_scaler_path,
+                     device=cfg.predict.device,
+                     annot_csv_filename=cfg.predict.annot_csv_filename,
+                     output_dir=cfg.predict.output_dir,
+                     min_segment_dur=cfg.predict.min_segment_dur,
+                     majority_vote=cfg.predict.majority_vote,
+                     logger=None
+                     )
 
-        vds_path = list(test_data_vds_path.glob(f'*.train.vds.json'))
-        self.assertTrue(len(vds_path) == 1)
-        vds_path = vds_path[0]
-        self.train_vds_path = str(shutil.copy(vds_path, self.tmp_output_dir))
-        config['PREDICT']['train_vds_path'] = self.train_vds_path
-
-        train_vds = Dataset.load(json_fname=vds_path)
-        if train_vds.are_spects_loaded() is False:
-            train_vds = train_vds.load_spects()
-        self.labelmap = train_vds.labelmap
-        del train_vds
-
-        vds_path = list(test_data_vds_path.glob(f'*.predict.vds.json'))
-        self.assertTrue(len(vds_path) == 1)
-        vds_path = vds_path[0]
-        self.predict_vds_path = str(shutil.copy(vds_path, self.tmp_output_dir))
-        config['PREDICT']['predict_vds_path'] = self.predict_vds_path
-
-        self.config_obj = config
-
-    def tearDown(self):
-        shutil.rmtree(self.tmp_output_dir)
-
-    def test_predict_func(self):
-        # make sure we're working with a vds that needs prediction
-        predict_vds_before = Dataset.load(self.predict_vds_path)
-        predict_vds_before = predict_vds_before.load_spects()
-        for voc in predict_vds_before.voc_list:
-            self.assertTrue(voc.metaspect.lbl_tb is None)
-            self.assertTrue(voc.annot is None)
-
-        predict_config = vak.config.predict.parse_predict_config(self.config_obj)
-        networks = vak.config.parse._get_nets_config(self.config_obj,
-                                                     predict_config.networks)
-        vak.core.predict(predict_vds_path=self.predict_vds_path,
-                         labelmap=self.labelmap,
-                         checkpoint_path=self.checkpoint_path,
-                         networks=networks,
-                         spect_scaler_path=self.spect_scaler_path)
-        predict_vds_after = Dataset.load(self.predict_vds_path)
-        predict_vds_after = predict_vds_after.load_spects()
-
-        for voc in predict_vds_after.voc_list:
-            self.assertTrue(type(voc.annot) == crowsetta.Sequence)
-            self.assertTrue(type(voc.annot.labels) is np.ndarray)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert predict_output_matches_expected(output_dir, cfg.predict.annot_csv_filename)
