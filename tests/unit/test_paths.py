a849083eb465277fd0ab668e6882a768636bb0c3
David Nicholson <nickledave@users.noreply.github.com>
2021-02-23 00:05:24 -0500

    TST: fix unit tests in two modules
    - fix unit tests in test_paths.py
    - fix unit test int test_timenow.py

--- tests/unit/test_paths.py
+++ tests/unit/test_paths.py
@@ -12,22 +12,23 @@ def results_path_matches_expected(results_path,
                                   root_results_dir,
                                   before_timestamp,
                                   after_timestamp):
-    assert results_path.parents == root_results_dir
+    assert results_path.parent == root_results_dir
     assert results_path.name.startswith(vak.constants.RESULTS_DIR_PREFIX)
     results_dirname_parts = results_path.name.split(sep='_')
-    assert len(results_dirname_parts) == 2
-    timenow_str = results_dirname_parts[1]
-    assert len(timenow_str) == vak.constants.STRFTIME_TIMESTAMP
+    assert len(results_dirname_parts) == 3   # 'results', 'date', 'time'
+    timenow_str = results_dirname_parts[1] + '_' + results_dirname_parts[2]
+    assert len(timenow_str) == len(vak.constants.STRFTIME_TIMESTAMP)
     timenow_str_as_timestamp = time.mktime(
         datetime.strptime(timenow_str, vak.constants.STRFTIME_TIMESTAMP).timetuple()
     )
     assert before_timestamp <= timenow_str_as_timestamp <= after_timestamp
+    return True
 
 
 def test_generate_results_dir_name_as_path(tmp_path):
-    before_timestamp = time.mktime(datetime.now())
+    before_timestamp = time.mktime(datetime.now().timetuple())
     results_path = vak.paths.generate_results_dir_name_as_path(root_results_dir=tmp_path)
-    after_timestamp = time.mktime(datetime.now())
+    after_timestamp = time.mktime(datetime.now().timetuple())
 
     assert results_path_matches_expected(results_path,
                                          root_results_dir=tmp_path,
@@ -36,15 +37,18 @@ def test_generate_results_dir_name_as_path(tmp_path):
 
 
 def test_generate_results_dir_name_as_path_no_root_results_dir(tmp_path):
-    before_timestamp = time.mktime(datetime.now())
+    before_timestamp = time.mktime(datetime.now().timetuple())
+    home = os.getcwd()
     os.chdir(tmp_path)
     results_path = vak.paths.generate_results_dir_name_as_path(root_results_dir=None)
-    after_timestamp = time.mktime(datetime.now())
+    after_timestamp = time.mktime(datetime.now().timetuple())
 
+    results_path = results_path.resolve()
     assert results_path_matches_expected(results_path,
                                          root_results_dir=tmp_path,
                                          before_timestamp=before_timestamp,
                                          after_timestamp=after_timestamp)
+    os.chdir(home)
 
 
 def test_generate_results_dir_name_as_path_nonexistent_root_raises():
--- tests/unit/test_timenow.py
+++ tests/unit/test_timenow.py
@@ -6,11 +6,11 @@ import vak.timenow
 
 
 def test_timenow():
-    before_timestamp = time.mktime(datetime.now())
+    before_timestamp = time.mktime(datetime.now().timetuple())
     timenow_str = vak.timenow.get_timenow_as_str()
     assert len(timenow_str) == len(vak.constants.STRFTIME_TIMESTAMP)
     timenow_str_as_timestamp = time.mktime(
         datetime.strptime(timenow_str, vak.constants.STRFTIME_TIMESTAMP).timetuple()
     )
-    after_timestamp = time.mktime(datetime.now())
+    after_timestamp = time.mktime(datetime.now().timetuple())
     assert before_timestamp <= timenow_str_as_timestamp <= after_timestamp
