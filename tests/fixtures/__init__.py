8702d56af66f7ee71eee02eb69505f224d70766e
David Nicholson <nickledave@users.noreply.github.com>
2021-02-27 12:52:01 -0500

    TST: add `device` module to fixtures

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -2,6 +2,7 @@ from .annot import *
 from .audio import *
 from .config import *
 from .dataframe import *
+from .device import *
 from .spect import *
 from .split import *
 from .test_data import *
