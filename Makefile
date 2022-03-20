0cc14db33af3af560c1664824354705189efdce8
David Nicholson <nickledave@users.noreply.github.com>
2022-03-20 14:14:15 -0400

    BUG/DEV: require 'evfuncs >=0.3.4', fixes #466
    fix bug where `vak prep` crashed because
    `evfuncs` returned a scalar value when
    a .not.mat file only had a single annotated segment
    
    - DEV: require 'evfuncs >=0.3.4', fixes #466
    - DEV: update url for SOURCE_TEST_DATA in Makefile
      - new source test data includes a .cbin + .not.mat
        edited to just have a single segment,
        with label in the `labelset`
    - TST: add regression test in test_core/test_prep for #466

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ SOURCE_TEST_DATA_TAR=${DATA_FOR_TESTS_DIR}source/source_test_data.tar.gz
 GENERATED_TEST_DATA_CI_TAR=${GENERATED_TEST_DATA_DIR}generated_test_data.ci.tar.gz
 GENERATED_TEST_DATA_ALL_TAR=${GENERATED_TEST_DATA_DIR}generated_test_data.tar.gz
 
-SOURCE_TEST_DATA_URL=https://osf.io/ubhsj/download
+SOURCE_TEST_DATA_URL=https://osf.io/mjksu/download
 GENERATED_TEST_DATA_ALL_URL=https://osf.io/8swue/download
 GENERATED_TEST_DATA_CI_URL=https://osf.io/vsk9x/download
 
--- tests/test_core/test_prep.py
+++ tests/test_core/test_prep.py
@@ -162,3 +162,62 @@ def test_prep_with_single_audio_and_annot(source_test_data_root,
     )
 
     assert len(vak_df) == 1
+
+
+def test_prep_when_annot_has_single_segment(source_test_data_root,
+                                            specific_config,
+                                            default_model,
+                                            tmp_path):
+    """
+    regression test, checks that we avoid a repeat of
+    https://github.com/NickleDave/vak/issues/466
+    """
+    data_dir = source_test_data_root / 'audio_cbin_annot_notmat' / 'gy6or6-song-edited-to-have-single-segment'
+
+    output_dir = tmp_path.joinpath(
+        f"test_prep_eval_annot_with_single_segment"
+    )
+    output_dir.mkdir()
+
+    options_to_change = [
+        {
+            "section": "PREP",
+            "option": "data_dir",
+            "value": str(data_dir),
+        },
+        {
+            "section": "PREP",
+            "option": "output_dir",
+            "value": str(output_dir),
+        },
+    ]
+
+    toml_path = specific_config(
+        config_type='eval',
+        model=default_model,
+        audio_format='cbin',
+        annot_format='notmat',
+        spect_format=None,
+        options_to_change=options_to_change,
+    )
+    cfg = vak.config.parse.from_toml_path(toml_path)
+
+    purpose = 'eval'
+    vak_df, csv_path = vak.core.prep(
+        data_dir=cfg.prep.data_dir,
+        purpose=purpose,
+        audio_format=cfg.prep.audio_format,
+        spect_format=cfg.prep.spect_format,
+        spect_output_dir=cfg.prep.spect_output_dir,
+        spect_params=cfg.spect_params,
+        annot_format=cfg.prep.annot_format,
+        annot_file=cfg.prep.annot_file,
+        labelset=cfg.prep.labelset,
+        output_dir=cfg.prep.output_dir,
+        train_dur=cfg.prep.train_dur,
+        val_dur=cfg.prep.val_dur,
+        test_dur=cfg.prep.test_dur,
+        logger=None,
+    )
+
+    assert len(vak_df) == 1
