9201822325e661501f174f04e98fb4b6e6656465
kaiyaprovost <17089935+kaiyaprovost@users.noreply.github.com>
2022-04-20 14:32:51 -0400

    fixed typo

--- src/vak/cli/__init__.py
+++ src/vak/cli/__init__.py
@@ -1,7 +1,7 @@
 """command-line interface functions for training,
 creating learning curves, etc."""
 
-from . import cli, eval, learncurve, predict, prep, train, tain_checkpoint
+from . import cli, eval, learncurve, predict, prep, train, train_checkpoint
 
 
 __all__ = [
