623be9596f41a03444e5fd48cd4b794cde93ae5c
David Nicholson <nickledave@users.noreply.github.com>
2022-07-03 15:04:17 -0400

    TST: Add tests/test_console_script.py, fixes #369 [skip ci]

---
+++ tests/test_console_script.py
@@ -0,0 +1,23 @@
+import subprocess
+
+
+def capture(command):
+    result = subprocess.run(command, capture_output=True, text=True)
+    return result.stdout, result.stderr, result.returncode
+
+
+def test_cli_just_vak():
+    command = ["vak"]
+    out, err, returncode = capture(command)
+    assert returncode == 2
+    assert out == ''
+    assert err.startswith('usage: vak ')
+
+
+def test_cli_help():
+    command = ["vak", "-h"]
+    out, err, returncode = capture(command)
+    assert returncode == 0
+    breakpoint()
+    assert out.startswith('usage: vak ')
+    assert err == ''
