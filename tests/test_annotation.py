f6eb41a5101f4f38067181bd717cc7e839ceb1f9
David Nicholson <nickledave@users.noreply.github.com>
2022-08-17 21:58:10 -0400

    TST: Fix names in tests/test_annotation.py
    Fix parameter names, rename variables to match

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -23,7 +23,7 @@ def test_files_from_dir(annot_dir_notmat, annot_files_notmat):
         ("spect", "mat", "yarden"),
     ],
 )
-def test_source_annot_map(
+def test_annotated_annot_map(
     source_type,
     source_format,
     annot_format,
@@ -32,26 +32,26 @@ def test_source_annot_map(
     specific_annot_list,
 ):
     if source_type == "audio":
-        source_files = audio_list_factory(source_format)
+        annotated_files = audio_list_factory(source_format)
     else:
-        source_files = spect_list_mat
+        annotated_files = spect_list_mat
     annot_list = specific_annot_list(annot_format)
-    source_annot_map = vak.annotation.map_annotated_to_annot(
-        source_files=source_files, annot_list=annot_list
+    annotated_annot_map = vak.annotation.map_annotated_to_annot(
+        annotated_files=annotated_files, annot_list=annot_list
     )
 
     # test all the audio paths made it into the map
-    source_files_from_map = list(source_annot_map.keys())
-    for source_file in source_files:
-        assert source_file in source_files_from_map
+    annotated_files_from_map = list(annotated_annot_map.keys())
+    for source_file in annotated_files:
+        assert source_file in annotated_files_from_map
 
     # test all the annots made it into the map
-    annot_list_from_map = list(source_annot_map.values())
+    annot_list_from_map = list(annotated_annot_map.values())
     for annot in annot_list:
         assert annot in annot_list_from_map
 
     # test all mappings are correct
-    for source_path, annot in list(source_annot_map.items()):
+    for source_path, annot in list(annotated_annot_map.items()):
         assert vak.annotation.audio_stem_from_path(
             annot.audio_path
         ) == vak.annotation.audio_stem_from_path(source_path)
