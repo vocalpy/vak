9576b68509dc037e2bdaca3a432f3c2cc7022aa0
David Nicholson <nickledave@users.noreply.github.com>
2023-02-20 19:53:30 -0500

    BUG: Fix default for post_tfm_kwargs in config classes
    Default should be None, not an empty dict.
    Setting it to an empty dict causes logic in core/eval
    to instantiate a PostProcess transform
    with majority_vote = False and min_segment_dur = None,
    so that we end up applying post-processing that does nothing,
    and the computed metric is the same as
    without "post-processing".
    This sets the default to None,
    which fixes the issue.

--- src/vak/config/eval.py
+++ src/vak/config/eval.py
@@ -127,7 +127,7 @@ class EvalConfig:
     post_tfm_kwargs = attr.ib(
         validator=validators.optional(are_valid_post_tfm_kwargs),
         converter=converters.optional(convert_post_tfm_kwargs),
-        default={},  # empty dict so we can pass into transform with **kwargs expansion
+        default=None,
     )
 
     # optional, data loader
