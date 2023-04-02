06c79bbef615e41f2f431df998ed5fcef73fd6e4
David Nicholson <nickledave@users.noreply.github.com>
2023-04-02 15:32:54 -0400

    Add vak/datasets/metadata.py with Metadata class
    that we will use when loading datasets
    in core/train, core/predict, etc.
    
    - Import metadata module in datasets/__init__.py

--- src/vak/datasets/__init__.py
+++ src/vak/datasets/__init__.py
@@ -1,12 +1,14 @@
 from .vocal_dataset import VocalDataset
 from .window_dataset import WindowDataset
 from . import (
+    metadata,
     seq,
     window_dataset,  # to give access to `helper` module
 )
 
 
 __all__ = [
+    "metadata",
     "seq",
     "VocalDataset",
     "WindowDataset",
