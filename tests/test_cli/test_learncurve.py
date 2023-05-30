11c69d98ed4ee1761e6c80b99ccf5cbf692122dc
David Nicholson <nickledave@users.noreply.github.com>
2023-05-29 21:15:12 -0400

    Fix unit tests in tests/test_cli/test_learncurve.py

--- tests/test_cli/test_learncurve.py
+++ tests/test_cli/test_learncurve.py
@@ -31,53 +31,7 @@ def test_learncurve(specific_config, tmp_path, model, device):
         options_to_change=options_to_change,
     )
 
-    with mock.patch('vak.core.learning_curve', autospec=True) as mock_core_learning_curve:
-        vak.cli.learncurve.learning_curve(toml_path)
-        assert mock_core_learning_curve.called
-
-    results_path = sorted(root_results_dir.glob(f"{vak.constants.RESULTS_DIR_PREFIX}*"))
-    assert len(results_path) == 1
-    results_path = results_path[0]
-
-    assert cli_asserts.toml_config_file_copied_to_results_path(results_path, toml_path)
-    assert cli_asserts.log_file_created(command="learncurve", output_path=results_path)
-    assert cli_asserts.log_file_contains_version(command="learncurve", output_path=results_path)
-
-
-@pytest.mark.parametrize("window_size", [44, 88, 176])
-def test_learncurve_previous_run_path(
-    specific_config, tmp_path, model, device, previous_run_path_factory, window_size
-):
-    root_results_dir = tmp_path.joinpath("test_learncurve_root_results_dir")
-    root_results_dir.mkdir()
-
-    options_to_change = [
-        {
-            "section": "LEARNCURVE",
-            "option": "root_results_dir",
-            "value": str(root_results_dir),
-        },
-        {
-            "section": "LEARNCURVE",
-            "option": "device",
-            "value": device},
-        {
-            "section": "LEARNCURVE",
-            "option": "previous_run_path",
-            "value": str(previous_run_path_factory(model)),
-        },
-        {"section": "DATALOADER", "option": "window_size", "value": window_size},
-    ]
-
-    toml_path = specific_config(
-        config_type="learncurve",
-        model=model,
-        audio_format="cbin",
-        annot_format="notmat",
-        options_to_change=options_to_change,
-    )
-
-    with mock.patch('vak.core.learning_curve', autospec=True) as mock_core_learning_curve:
+    with mock.patch('vak.core.learncurve.learning_curve', autospec=True) as mock_core_learning_curve:
         vak.cli.learncurve.learning_curve(toml_path)
         assert mock_core_learning_curve.called
 
