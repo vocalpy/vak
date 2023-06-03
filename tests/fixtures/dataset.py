a5fe61388fa1ff9cd2625f2da5cc485b52b29d51
David Nicholson <nickledave@users.noreply.github.com>
2023-06-03 16:39:53 -0400

    Add tests/fixtures/dataset.py with fixture `specific_dataset_path`

---
+++ tests/fixtures/dataset.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def specific_dataset_path(specific_config_toml):
+    """Returns a function that will return the
+    ``dataset_path`` corresponding to the ``prep``ared dataset
+    from a specific configuration file,
+    determined by characteristics specified by the caller:
+    `config_type`, `audio_format`, `spect_format`, `annot_format`
+    """
+
+    def _specific_dataset_path(
+        config_type,
+        model,
+        annot_format,
+        audio_format=None,
+        spect_format=None,
+    ):
+        config_toml = specific_config_toml(
+            config_type, model, annot_format, audio_format, spect_format
+        )
+        dataset_path = Path(config_toml[config_type.upper()]["dataset_path"])
+        return dataset_path
+
+    return _specific_dataset_path
