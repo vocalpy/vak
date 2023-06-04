26d20538c39758789a2e77e1ef4462186af75c04
David Nicholson <nickledave@users.noreply.github.com>
2023-06-04 17:56:37 -0400

    Fix how we build paths for tests in test_core/test_prep/test_prep_helper.py

--- tests/test_core/test_prep/test_prep_helper.py
+++ tests/test_core/test_prep/test_prep_helper.py
@@ -6,7 +6,7 @@ import pytest
 import vak.core.prep.prep_helper
 
 
-def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, tmp_path_data_dir):
+def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_path_data_dir):
     """Copy all the files in a dataset DataFrame to a `tmp_path_data_dir`,
     and change the paths in the Dataframe, so that we can then call
     `vak.core.prep.prep_helper.move_files_into_split_subdirs`."""
@@ -15,7 +15,7 @@ def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, tmp_path_data_dir):
         paths = dataset_df[paths_col].values
         new_paths = []
         for path in paths:
-            new_path = shutil.copy(src=path, dst=tmp_path_data_dir)
+            new_path = shutil.copy(src=dataset_path / path, dst=tmp_path_data_dir)
             new_paths.append(new_path)
         dataset_df[paths_col] = new_paths
     return dataset_df
@@ -29,11 +29,12 @@ def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, tmp_path_data_dir):
     ]
 )
 def test_move_files_into_split_subdirs(config_type, model_name, audio_format, spect_format, annot_format,
-                                       tmp_path, specific_dataset_df):
+                                       tmp_path, specific_dataset_df, specific_dataset_path):
     dataset_df = specific_dataset_df(config_type, model_name, annot_format, audio_format, spect_format)
+    dataset_path = specific_dataset_path(config_type, model_name, annot_format, audio_format, spect_format)
     tmp_path_data_dir = tmp_path / 'data_dir'
     tmp_path_data_dir.mkdir()
-    copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, tmp_path_data_dir)
+    copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_path_data_dir)
 
     tmp_dataset_path = tmp_path / 'dataset_dir'
     tmp_dataset_path.mkdir()
@@ -55,7 +56,7 @@ def test_move_files_into_split_subdirs(config_type, model_name, audio_format, sp
             # --> there is a single annotation file associated with all rows
             # in this case we copy the single annotation file to the root of the dataset directory
             annot_path = dataset_df["annot_path"].unique().item()
-            assert pathlib.Path(annot_path).exists()
+            assert (tmp_dataset_path / annot_path).exists()
         elif len(dataset_df["annot_path"].unique()) == len(dataset_df):
             annot_paths = split_df['annot_path'].values
             for annot_path in annot_paths:
