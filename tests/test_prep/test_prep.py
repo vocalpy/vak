0889f3be028a8cb50d00031d6fcf6173806d67bb
David Nicholson <nickledave@users.noreply.github.com>
2023-06-05 13:39:42 -0400

    Move test_prep/ up to tests/ from test_core/, fix vak.core.prep -> vak.prep in tests

--- tests/test_prep/test_prep.py
+++ tests/test_prep/test_prep.py
@@ -1,4 +1,4 @@
-"""tests for vak.core.prep module"""
+"""tests for vak.prep module"""
 import json
 import pathlib
 import shutil
@@ -94,7 +94,7 @@ def test_prep(
     cfg = vak.config.parse.from_toml_path(toml_path)
 
     purpose = config_type.lower()
-    dataset_df, dataset_path = vak.core.prep.prep(
+    dataset_df, dataset_path = vak.prep.prep(
         data_dir=cfg.prep.data_dir,
         purpose=purpose,
         audio_format=cfg.prep.audio_format,
@@ -168,7 +168,7 @@ def test_prep_raises_when_labelset_required_but_is_none(
 
     purpose = config_type.lower()
     with pytest.raises(ValueError):
-        vak.core.prep.prep(
+        vak.prep.prep(
             data_dir=cfg.prep.data_dir,
             purpose=purpose,
             audio_format=cfg.prep.audio_format,
@@ -234,7 +234,7 @@ def test_prep_with_single_audio_and_annot(source_test_data_root,
     cfg = vak.config.parse.from_toml_path(toml_path)
 
     purpose = 'eval'
-    dataset_df, dataset_path = vak.core.prep.prep(
+    dataset_df, dataset_path = vak.prep.prep(
         data_dir=cfg.prep.data_dir,
         purpose=purpose,
         audio_format=cfg.prep.audio_format,
@@ -291,7 +291,7 @@ def test_prep_when_annot_has_single_segment(source_test_data_root,
     cfg = vak.config.parse.from_toml_path(toml_path)
 
     purpose = 'eval'
-    dataset_df, dataset_path = vak.core.prep.prep(
+    dataset_df, dataset_path = vak.prep.prep(
         data_dir=cfg.prep.data_dir,
         purpose=purpose,
         audio_format=cfg.prep.audio_format,
@@ -338,7 +338,7 @@ def test_prep_raises_not_a_directory(
 
     purpose = "train"
     with pytest.raises(NotADirectoryError):
-        vak.core.prep.prep(
+        vak.prep.prep(
             data_dir=cfg.prep.data_dir,
             purpose=purpose,
             audio_format=cfg.prep.audio_format,
@@ -385,7 +385,7 @@ def test_prep_raises_file_not_found(
 
     purpose = "train"
     with pytest.raises(FileNotFoundError):
-        vak.core.prep.prep(
+        vak.prep.prep(
             data_dir=cfg.prep.data_dir,
             purpose=purpose,
             audio_format=cfg.prep.audio_format,
