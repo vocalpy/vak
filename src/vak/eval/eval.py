ad6aa751aa1a716251d8b73c2d358c3dd5a48f52
David Nicholson <nickledave@users.noreply.github.com>
2023-06-05 14:45:19 -0400

    Fix an import in eval/eval.py

--- src/vak/eval/eval.py
+++ src/vak/eval/eval.py
@@ -13,8 +13,8 @@ from .. import (
     datasets,
     models,
     transforms,
-    validators
 )
+from ..common import validators
 from ..datasets.vocal_dataset import VocalDataset
 from ..prep.prep_helper import validate_and_get_timebin_dur
 
