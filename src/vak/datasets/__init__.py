6c45a403e4ddb13233d85b49e5c9d8d2e8a6e731
David Nicholson <nickledave@users.noreply.github.com>
2023-06-11 19:16:31 -0400

    Change imports in src/vak/datasets/__init__.py, import frame_classification sub-package

--- src/vak/datasets/__init__.py
+++ src/vak/datasets/__init__.py
@@ -1,16 +1,12 @@
-from .vocal_dataset import VocalDataset
-from .window_dataset import WindowDataset
 from . import (
+    frame_classification,
     metadata,
     seq,
-    window_dataset,  # to give access to `helper` module
 )
 
 
 __all__ = [
+    "frame_classification",
     "metadata",
     "seq",
-    "VocalDataset",
-    "WindowDataset",
-    "window_dataset",
 ]
