3a908e6fc3137e4bdf9bf3901cfda72a946c5fd9
David Nicholson <nickledave@users.noreply.github.com>
2023-06-15 10:22:01 -0400

    Import constants module in vak/prep/__init__.py

--- src/vak/prep/__init__.py
+++ src/vak/prep/__init__.py
@@ -1,5 +1,6 @@
 from . import (
     audio_dataset,
+    constants,
     frame_classification,
     learncurve,
     spectrogram_dataset,
@@ -9,6 +10,7 @@ from .prep import prep
 
 __all__ = [
     'audio_dataset',
+    'constants',
     'frame_classification',
     'learncurve',
     'prep',
