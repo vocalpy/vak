fda603a2926347dd19f017b3695ab25387112659
David Nicholson <nickledave@users.noreply.github.com>
2023-06-19 10:43:52 -0400

    Fix `vak.common.learncurve.get_train_dur_replicate_split_name` to cast `train_dur` to float and `replicate_num` to int

--- src/vak/common/learncurve.py
+++ src/vak/common/learncurve.py
@@ -5,4 +5,4 @@ def get_train_dur_replicate_split_name(train_dur: int, replicate_num: int) -> st
     Used when preparing the training set splits for a learning curve,
     and when training models to generate the results for the curve.
     """
-    return f"train-dur-{train_dur}-replicate-{replicate_num}"
+    return f"train-dur-{float(train_dur)}-replicate-{int(replicate_num)}"
