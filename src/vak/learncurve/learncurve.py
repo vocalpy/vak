0f1e0b1ce300abf5747a122c3dbbc961d65335e1
David Nicholson <nickledave@users.noreply.github.com>
2023-06-19 11:16:16 -0400

    Fix how vak.learncurve.learncurve builds dataframe of results

--- src/vak/learncurve/learncurve.py
+++ src/vak/learncurve/learncurve.py
@@ -25,19 +25,19 @@ __all__ = [
 logger = logging.getLogger(__name__)
 
 
-def train_dur_dirname(train_dur: int) -> str:
+def train_dur_dirname(train_dur: float) -> str:
     """Returns name of directory for all replicates
     trained with a training set of a specified duration,
     ``f"train_dur_{train_dur}s"``.
     """
-    return f"train_dur_{train_dur}s"
+    return f"train_dur_{float(train_dur)}s"
 
 
 def replicate_dirname(replicate_num: int) -> str:
     """Returns name of directory for a replicate,
     ``f"replicate_{replicate_num}``.
     """
-    return f"replicate_{replicate_num}"
+    return f"replicate_{int(replicate_num)}"
 
 
 def learning_curve(
@@ -183,8 +183,12 @@ def learning_curve(
     )
 
     # ---- get training set subsets ------------------------------------------------------------------------------------
+    dataset_df = dataset_df[
+        (dataset_df.train_dur.notna()) & (dataset_df.replicate_num.notna())
+    ]
     train_durs = sorted(dataset_df['train_dur'].unique())
-    replicate_nums = sorted(dataset_df['replicate_num'].unique())
+    replicate_nums = [int(replicate_num)
+                      for replicate_num in sorted(dataset_df['replicate_num'].unique())]
     to_do = []
     for train_dur in train_durs:
         for replicate_num in replicate_nums:
@@ -296,33 +300,28 @@ def learning_curve(
         )
 
     # ---- make a csv for analysis -------------------------------------------------------------------------------------
-    reg_exp_num = re.compile(
-        r"[-+]?\d*\.\d+|\d+"
-    )  # to extract train set dur and replicate num from paths
-
+    # use one of the eval csvs just to get columns, that we use below to re-order
     eval_csv_paths = sorted(results_path.glob("**/eval*.csv"))
-    eval_df_0 = pd.read_csv(eval_csv_paths[0])  # use to just get columns
-    eval_columns = eval_df_0.columns.tolist()  # will use below to re-order
+    eval_df_0 = pd.read_csv(eval_csv_paths[0])
+    eval_columns = eval_df_0.columns.tolist()
+
     eval_dfs = []
-    for eval_csv_path in eval_csv_paths:
-        train_set_dur = reg_exp_num.findall(eval_csv_path.parents[1].name)
-        if len(train_set_dur) != 1:
+    for train_dur, replicate_num in to_do:
+        results_path_this_train_dur = results_path / train_dur_dirname(train_dur)
+        results_path_this_replicate = results_path_this_train_dur / replicate_dirname(replicate_num)
+        eval_csv_path = sorted(results_path_this_replicate.glob('eval*.csv'))
+        if not len(eval_csv_path) == 1:
             raise ValueError(
-                f"unable to determine training set duration from .csv path: {train_set_dur}"
+                "Did not find exactly one eval results csv file in replicate directory after running learncurve. "
+                f"Directory is: {results_path_this_replicate}."
+                f"Result of globbing directory for eval csv file: {eval_csv_path}"
             )
-        else:
-            train_set_dur = float(train_set_dur[0])
-        replicate_num = reg_exp_num.findall(eval_csv_path.parents[0].name)
-        if len(replicate_num) != 1:
-            raise ValueError(
-                f"unable to determine replicate number from .csv path: {train_set_dur}"
-            )
-        else:
-            replicate_num = int(replicate_num[0])
+        eval_csv_path = eval_csv_path[0]
         eval_df = pd.read_csv(eval_csv_path)
-        eval_df["train_set_dur"] = train_set_dur
+        eval_df["train_set_dur"] = train_dur
         eval_df["replicate_num"] = replicate_num
         eval_dfs.append(eval_df)
+
     all_eval_df = pd.concat(eval_dfs)
     all_eval_columns = ["train_set_dur", "replicate_num", *eval_columns]
     all_eval_df = all_eval_df[all_eval_columns]
