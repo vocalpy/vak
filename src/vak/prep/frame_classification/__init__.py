6b0631c5793562981935bddc995a7c45806296c8
David Nicholson <nickledave@users.noreply.github.com>
2023-06-20 19:54:01 -0400

    Fix imports and __all__ in vak/prep/frame_classification/__init__.py

--- src/vak/prep/frame_classification/__init__.py
+++ src/vak/prep/frame_classification/__init__.py
@@ -1,8 +1,14 @@
 from .frame_classification import prep
-from . import helper
+from . import (
+    dataset_arrays,
+    frame_classification,
+    learncurve,
+)
 
 
 __all__ = [
+    'dataset_arrays',
+    'frame_classification',
+    'learncurve',
     'prep',
-    'helper',
 ]
