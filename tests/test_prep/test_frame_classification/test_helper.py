316c7ec81995d5ee5f62ea60496c22b673154093
David Nicholson <nickledave@users.noreply.github.com>
2023-06-20 13:12:11 -0400

    Delete function `prep.frame_classification.helper.move_files_into_split_subdirs`, no longer used

--- tests/test_prep/test_frame_classification/test_helper.py
+++ tests/test_prep/test_frame_classification/test_helper.py
@@ -80,51 +80,6 @@ def test_make_frame_classification_arrays_from_spectrogram_dataset(
 
 
 @pytest.mark.parametrize(
-    'config_type, model_name, audio_format, spect_format, annot_format',
-    [
-        ('train', 'teenytweetynet', 'cbin', None, 'notmat'),
-        ('train', 'teenytweetynet', None, 'mat', 'yarden'),
-    ]
-)
-def test_move_files_into_split_subdirs(config_type, model_name, audio_format, spect_format, annot_format,
-                                       tmp_path, specific_dataset_df, specific_dataset_path):
-    dataset_df = specific_dataset_df(config_type, model_name, annot_format, audio_format, spect_format)
-    dataset_path = specific_dataset_path(config_type, model_name, annot_format, audio_format, spect_format)
-    tmp_path_data_dir = tmp_path / 'data_dir'
-    tmp_path_data_dir.mkdir()
-    copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_path_data_dir)
-
-    tmp_dataset_path = tmp_path / 'dataset_dir'
-    tmp_dataset_path.mkdir()
-
-    vak.prep.frame_classification.helper.move_files_into_split_subdirs(
-        dataset_df, tmp_dataset_path, purpose=config_type
-    )
-
-    for split in dataset_df['split'].dropna().unique():
-        split_subdir = tmp_dataset_path / split
-        assert split_subdir.exists()
-
-        split_df = dataset_df[dataset_df['split'] == split]
-        # for path_col in ('spect_path', 'annot_path'):
-        spect_paths = split_df['spect_path'].values
-        for spect_path in spect_paths:
-            new_path = split_subdir / pathlib.Path(spect_path).name
-            assert new_path.exists()
-
-        if len(dataset_df["annot_path"].unique()) == 1:
-            # --> there is a single annotation file associated with all rows
-            # in this case we copy the single annotation file to the root of the dataset directory
-            annot_path = dataset_df["annot_path"].unique().item()
-            assert (tmp_dataset_path / annot_path).exists()
-        elif len(dataset_df["annot_path"].unique()) == len(dataset_df):
-            annot_paths = split_df['annot_path'].values
-            for annot_path in annot_paths:
-                new_path = split_subdir / pathlib.Path(annot_path).name
-                assert new_path.exists()
-
-
-@pytest.mark.parametrize(
     'data_dir_name, timestamp',
     [
         ('bird1', '230319_115852')
