48ecd15b710600f6eb3d2dd61cc4e348f477923c
David Nicholson <nickledave@users.noreply.github.com>
2023-06-20 20:03:59 -0400

    Fix import in vak/prep/__init__.py

--- src/vak/prep/__init__.py
+++ src/vak/prep/__init__.py
@@ -1,7 +1,7 @@
 from . import (
     audio_dataset,
     constants,
-    dataset_df,
+    dataset_df_helper,
     frame_classification,
     spectrogram_dataset,
 )
@@ -11,7 +11,7 @@ from .prep import prep
 __all__ = [
     'audio_dataset',
     'constants',
-    'dataset_df',
+    'dataset_df_helper',
     'frame_classification',
     'prep',
     'spectrogram_dataset',
