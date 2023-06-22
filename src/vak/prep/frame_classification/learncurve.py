5705738b08c2539b818a40a8398ce2b952fb2bfc
David Nicholson <nickledave@users.noreply.github.com>
2023-06-22 09:43:33 -0400

    Rewrite vak.prep.frame_classification.learncurve.make_learncurve_splits_from_dataset_df to use vak.prep.frame_classification.dataset_arrays.make_npy_files_for_each_split

--- src/vak/prep/frame_classification/learncurve.py
+++ src/vak/prep/frame_classification/learncurve.py
@@ -6,16 +6,13 @@ import logging
 import pathlib
 from typing import Sequence
 
-import crowsetta
-import numpy as np
 import pandas as pd
 
 from .dataset_arrays import (
-    sort_source_paths_and_annots_by_label_freq,
-    make_from_source_paths_and_annots
+    make_npy_files_for_each_split,
 )
 from .. import split
-from ... import common, datasets
+from ... import common
 
 
 logger = logging.getLogger(__name__)
@@ -23,7 +20,6 @@ logger = logging.getLogger(__name__)
 
 def make_learncurve_splits_from_dataset_df(
     dataset_df: pd.DataFrame,
-    csv_path: str | pathlib.Path,
     input_type: str,
     train_set_durs: Sequence[float],
     num_replicates: int,
@@ -32,22 +28,20 @@ def make_learncurve_splits_from_dataset_df(
     audio_format: str | None = None,
     spect_key: str = "s",
     timebins_key: str = "t",
-):
-    """Make splits for a learning curve from a dataframe representing the entire dataset.
+) -> pd.DataFrame:
+    """Make splits for a learning curve
+    from a dataframe representing the entire dataset.
 
-    Uses :func:`vak.split.dataframe` to make splits from ``dataset_df``.
-    Then makes a new directory inside ``dataset_path`` for each training set split,
-    one split for each combination of training set duration and replicate number.
-    In each directory, it saves the three array files representing the frame
-    classification dataset, produced by calling
-    :func:`vak.prep.frame_classification.helper.make_frame_classification_arrays_from_source_paths_and_annots`.
+    Uses :func:`vak.prep.split.dataframe` to make
+    splits/subsets of the training data
+    from ``dataset_df``, and then uses
+    :func:`vak.prep.frame_classification.dataset_arrays.make_npy_files_for_each_split`
+    to make the array files for each split.
 
     Parameters
     ----------
     dataset_df : pandas.DataFrame
         Representing an entire dataset of vocalizations.
-    csv_path : pathlib.Path
-        Path to where dataset was saved as a csv file.
     input_type : str
         The type of input to the neural network model.
         One of {'audio', 'spect'}.
@@ -62,11 +56,31 @@ def make_learncurve_splits_from_dataset_df(
     dataset_path : str, pathlib.Path
         Directory where splits will be saved.
     labelmap : dict
-        that maps labelset to consecutive integers
+        A :class:`dict` that maps a set of human-readable
+        string labels to the integer classes predicted by a neural
+        network model. As returned by :func:`vak.labels.to_map`.
+    audio_format : str
+        A :class:`string` representing the format of audio files.
+        One of :constant:`vak.common.constants.VALID_AUDIO_FORMATS`.
     spect_key : str
-        key for accessing spectrogram in files. Default is 's'.
+        Key for accessing spectrogram in files. Default is 's'.
     timebins_key : str
-        key for accessing vector of time bins in files. Default is 't'.
+        Key for accessing vector of time bins in files. Default is 't'.
+
+    Returns
+    -------
+    dataset_df_out : pandas.DataFrame
+        A pandas.DataFrame that has the original splits
+        from ``dataset_df`` as well as the additional subsets
+        of the training data added, along with additional
+        'train_dur' and 'replicate_num' columns
+        that can be used during analysis.
+        Other functions like :func:`vak.learncurve.learncurve`
+        specify a specific subset of the training data
+        by getting the split name with the function
+        :func:`vak.common.learncurve.get_train_dur_replicate_split_name`,
+        and then filtering ``dataset_df_out`` with that name
+        using the 'split' column.
     """
     dataset_path = pathlib.Path(dataset_path)
 
@@ -93,7 +107,7 @@ def make_learncurve_splits_from_dataset_df(
             )
             # remove rows where split set to 'None'
             train_dur_replicate_df = train_dur_replicate_df[train_dur_replicate_df.split == "train"]
-            # next line, make split name in csv math split name used for directory in dataset dir
+            # next line, make split name in csv match the split name used for directory in dataset dir
             train_dur_replicate_df['split'] = train_dur_replicate_split_name
             train_dur_replicate_df['train_dur'] = train_dur
             train_dur_replicate_df['replicate_num'] = replicate_num
@@ -101,75 +115,24 @@ def make_learncurve_splits_from_dataset_df(
                 train_dur_replicate_df
             )
 
-            metadata = datasets.metadata.Metadata.from_dataset_path(dataset_path)
-            frame_dur = metadata.frame_dur
-
-            if input_type == 'audio':
-                source_paths = train_dur_replicate_df['audio_path'].values
-            elif input_type == 'spect':
-                source_paths = train_dur_replicate_df['spect_path'].values
-            else:
-                raise ValueError(
-                    f"Invalid ``input_type``: {input_type}"
-                )
-            annots = common.annotation.from_df(train_dur_replicate_df)
-
-            # sort to minimize chance that cropping removes classes
-            source_paths, annots = sort_source_paths_and_annots_by_label_freq(
-                source_paths,
-                annots
-            )
-
-            (inputs,
-             source_id_vec,
-             frame_labels) = make_from_source_paths_and_annots(
-                source_paths,
-                input_type,
-                annots,
-                labelmap,
-                train_dur,
-                frame_dur,
-                audio_format,
-                spect_key,
-                timebins_key,
-            )
-
-            train_dur_replicate_root = dataset_path / train_dur_replicate_split_name
-            train_dur_replicate_root.mkdir()
-
-            logger.info(
-                "Saving ``inputs`` vector for frame classification dataset with size "
-                f"{round(inputs.nbytes * 1e-6, 2)} MB."
-            )
-            np.save(train_dur_replicate_root / datasets.frame_classification.constants.INPUT_ARRAY_FILENAME, inputs)
-            logger.info(
-                "Saving ``source_id`` vector for frame classification dataset with size "
-                f"{round(source_id_vec.nbytes * 1e-6, 2)} MB."
-            )
-            np.save(train_dur_replicate_root / datasets.frame_classification.constants.SOURCE_IDS_ARRAY_FILENAME,
-                    source_id_vec)
-            logger.info(
-                "Saving ``frame_labels`` vector (targets) for frame classification dataset "
-                f"with size {round(frame_labels.nbytes * 1e-6, 2)} MB."
-            )
-            np.save(train_dur_replicate_root / datasets.frame_classification.constants.FRAME_LABELS_ARRAY_FILENAME,
-                    frame_labels)
-            logger.info(
-                "Saving csv file of annotations for frame classification dataset"
-            )
-            generic_seq = crowsetta.formats.seq.GenericSeq(annots=annots)
-            generic_seq.to_file(
-                train_dur_replicate_root / datasets.frame_classification.constants.ANNOTATION_CSV_FILENAME
-            )
+    all_train_durs_and_replicates_df = pd.concat(all_train_durs_and_replicates_df)
+    dataset_df = make_npy_files_for_each_split(
+        all_train_durs_and_replicates_df,
+        dataset_path,
+        input_type,
+        'learncurve',  # purpose
+        labelmap,
+        audio_format,
+        spect_key,
+        timebins_key,
+    )
 
     # keep the same validation and test set by concatenating them with the train subset
-    all_train_durs_and_replicates_df = pd.concat(all_train_durs_and_replicates_df)
     all_train_durs_and_replicates_df = pd.concat(
         (
             all_train_durs_and_replicates_df,
-            dataset_df[dataset_df.split == "val"],
-            dataset_df[dataset_df.split == "test"],
+            dataset_df,
         )
     )
+    return all_train_durs_and_replicates_df
 
-    all_train_durs_and_replicates_df.to_csv(csv_path, index=False)
