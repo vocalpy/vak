fd2e7015081e527d1260b7707361840c36f18e32
David Nicholson <nickledave@users.noreply.github.com>
2023-06-22 13:22:27 -0400

    Add constants +FRAMES_NPY_PATH_COL_NAME and FRAME_LABELS_NPY_PATH_COL_NAME to datasets/frame_classification/constants.py

--- src/vak/datasets/frame_classification/constants.py
+++ src/vak/datasets/frame_classification/constants.py
@@ -1,5 +1,7 @@
 FRAMES_ARRAY_EXT = '.frames.npy'
+FRAMES_NPY_PATH_COL_NAME = 'frames_npy_path'
 FRAME_LABELS_EXT = '.frame_labels.npy'
+FRAME_LABELS_NPY_PATH_COL_NAME = 'frame_labels_npy_path'
 ANNOTATION_CSV_FILENAME = 'y.csv'
 SAMPLE_IDS_ARRAY_FILENAME = 'sample_ids.npy'
 INDS_IN_SAMPLE_ARRAY_FILENAME = 'inds_in_sample.npy'
