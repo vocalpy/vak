c9b549ab5f21ed6ac8a0b52c4777de7c37c80c10
David Nicholson <nickledave@users.noreply.github.com>
2023-06-25 13:17:46 -0400

    Rename vak.datasets.metadata.Metadata -> datasets.frame_classification_dataset.FrameClassificationDatasetMetadata, add attribute 'input_type'

--- src/vak/datasets/frame_classification/__init__.py
+++ src/vak/datasets/frame_classification/__init__.py
@@ -1,10 +1,12 @@
 from . import constants
 from .frames_dataset import FramesDataset
+from .metadata import FrameClassificationDatasetMetadata
 from .window_dataset import WindowDataset
 
 
 __all__ = [
     "constants",
+    "FrameClassificationDatasetMetadata",
     "FramesDataset",
     "WindowDataset"
 ]
