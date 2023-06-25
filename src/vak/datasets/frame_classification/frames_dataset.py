9d5bfd2e4a5561e9ceefb2e77d0c4137c9cae527
David Nicholson <nickledave@users.noreply.github.com>
2023-06-25 13:59:47 -0400

    Fix frames_dataset to use FrameClassificationDatasetMetadata, add input_type as parameter to dataset __init__, and use to add source_paths attribute that is used by __getitem__

--- src/vak/datasets/frame_classification/frames_dataset.py
+++ src/vak/datasets/frame_classification/frames_dataset.py
@@ -8,7 +8,7 @@ import numpy.typing as npt
 import pandas as pd
 
 from . import constants
-from ..metadata import Metadata
+from .metadata import FrameClassificationDatasetMetadata
 
 
 class FramesDataset:
@@ -18,49 +18,14 @@ class FramesDataset:
     where the source data consists of audio signals
     or spectrograms of varying lengths.
 
-    The underlying data consists of single arrays
-    for both the input to the network ``X``
-    and the targets for the network output ``Y``.
-    These single arrays ``X`` and ``Y`` are
-    created by concatenating samples from the source
-    data, e.g., audio files or spectrogram arrays.
-    The last dimension of ``X`` will always be the
-    number of total frames in the dataset,
-    either audio samples or spectrogram time bins,
-    and ``Y`` will be the same size, containing
-    an integer class label for each frame.
-
-    To recover the original samples from the
-    concatenated array, a ``sample_ids`` vector
-    is used with integer elements
-    :math:`(0, 0, 0, ..., 1, 1, 1, ..., i, i, i)```
-    for a dataset with :math:`M` samples
-    :math:`(1, 2, ..., i)`.
-    The vector ``sample_ids`` will be the same length
-    as ``X`` and ``Y``. Each element in ``sample_ids``
-    indicates for each frame in the total dataset
-    which original sample that frame belongs to.
-
     Attributes
     ----------
+    dataset_path
     dataset_df
-    Y : numpy.ndarray
     frame_dur : float
         Duration of a single frame, in seconds.
     duration : float
         Total duration of the dataset.
-
-    Examples
-    --------
-
-    This snippet demonstrates how samples are recovered by array indexing.
-    In the snippet we get the first sample using its id number, ``0``
-    (because Python uses zero indexing).
-    This is basically what :meth:`FramesDataset.__getitem__` does
-    when called with an ``idx``.
-
-    >>> dataset = FramesDataset(X, Y, sample_ids)
-    >>> x1, y1 = dataset.X[dataset.sample_ids == 0], dataset.Y[dataset.sample_ids == 0]
     """
     def __init__(
         self,
@@ -70,6 +35,7 @@ class FramesDataset:
         sample_ids: npt.NDArray,
         inds_in_sample: npt.NDArray,
         frame_dur: float,
+        input_type: str,
         item_transform: Callable | None = None,
     ):
         self.dataset_path = pathlib.Path(dataset_path)
@@ -79,6 +45,15 @@ class FramesDataset:
         self.dataset_df = dataset_df
         self.frames_paths = self.dataset_df[constants.FRAMES_NPY_PATH_COL_NAME].values
         self.frame_labels_paths = self.dataset_df[constants.FRAME_LABELS_NPY_PATH_COL_NAME].values
+
+        if input_type == 'audio':
+            self.source_paths = self.dataset_df['audio_path'].values
+        elif input_type == 'spect':
+            self.source_paths = self.dataset_df['audio_path'].values
+        else:
+            raise ValueError(
+                f"Invalid `input_type`: {input_type}. Must be one of {{'audio', 'spect'}}."
+            )
 
         self.sample_ids = sample_ids
         self.inds_in_sample = inds_in_sample
@@ -96,16 +71,18 @@ class FramesDataset:
         return tmp_item["frames"].shape
 
     def __getitem__(self, idx):
+        source_path = self.source_paths[idx]
         frames = np.load(self.dataset_path / self.frames_paths[idx])
-        frame_labels = np.load(self.dataset_path / self.frame_labels_paths[idx])
+        item = {
+            'frames': frames,
+            'source_path': source_path
+        }
+        if self.frame_labels is not None:
+            frame_labels = np.load(self.dataset_path / self.frame_labels_paths[idx])
+            item['frame_labels'] = frame_labels
 
         if self.item_transform:
-            item = self.item_transform(frames, frame_labels)
-        else:
-            item = {
-                "frames": frames,
-                "frame_labels": frame_labels,
-            }
+            item = self.item_transform(**item)
 
         return item
 
@@ -121,8 +98,9 @@ class FramesDataset:
         item_transform: Callable | None = None,
     ):
         dataset_path = pathlib.Path(dataset_path)
-        metadata = Metadata.from_dataset_path(dataset_path)
+        metadata = FrameClassificationDatasetMetadata.from_dataset_path(dataset_path)
         frame_dur = metadata.frame_dur
+        input_type = metadata.input_type
 
         dataset_csv_path = dataset_path / metadata.dataset_csv_filename
         dataset_df = pd.read_csv(dataset_csv_path)
@@ -140,5 +118,6 @@ class FramesDataset:
             sample_ids,
             inds_in_sample,
             frame_dur,
+            input_type,
             item_transform,
         )
