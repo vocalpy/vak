d1443e1d9c920d5f90a6a92c6ac53752691d2b51
David Nicholson <nickledave@users.noreply.github.com>
2023-06-28 11:47:17 -0400

    Import ED_TCN model in vak/models/__init__.py

--- src/vak/models/__init__.py
+++ src/vak/models/__init__.py
@@ -5,6 +5,7 @@ from . import (
 )
 from .base import Model
 from .get import get
+from .ed_tcn import ED_TCN
 from .teenytweetynet import TeenyTweetyNet
 from .tweetynet import TweetyNet
 from .frame_classification_model import FrameClassificationModel
@@ -14,6 +15,7 @@ __all__ = [
     "base",
     "decorator",
     "definition",
+    "ED_TCN",
     "get",
     "Model",
     "TeenyTweetyNet",
