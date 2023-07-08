e796a8d58dcb7506699b7ab56e29d336b9880162
David Nicholson <nickledave@users.noreply.github.com>
2023-07-08 10:15:47 -0400

    Import ParametricUMAPDataset in vak.datasets

--- src/vak/datasets/dimensionality_reduction/__init__.py
+++ src/vak/datasets/dimensionality_reduction/__init__.py
@@ -1,8 +1,10 @@
 from .metadata import Metadata
 from .unit_dataset import UnitDataset
+from .parametric_umap import ParametricUMAPDataset
 
 
 __all__ = [
     "Metadata",
+    "ParametricUMAPDataset",
     "UnitDataset",
 ]
---
+++ src/vak/datasets/dimensionality_reduction/parametric_umap/__init__.py
@@ -0,0 +1,1 @@
+from .parametric_umap import ParametricUMAPDataset
