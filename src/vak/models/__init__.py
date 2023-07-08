7e81e14479cccc952357f88b330b720aa6e860e8
David Nicholson <nickledave@users.noreply.github.com>
2023-07-08 10:16:34 -0400

    Import ParametricUMAPModel and ConvEncoderParametricUMAP in src/vak/models/__init__.py

--- src/vak/models/__init__.py
+++ src/vak/models/__init__.py
@@ -4,21 +4,25 @@ from . import (
     definition,
 )
 from .base import Model
+from .convencoder_parametric_umap import ConvEncoderParametricUMAP
 from .get import get
 from .ed_tcn import ED_TCN
 from .teenytweetynet import TeenyTweetyNet
 from .tweetynet import TweetyNet
 from .frame_classification_model import FrameClassificationModel
+from .parametric_umap_model import ParametricUMAPModel
 
 
 __all__ = [
     "base",
+    "ConvEncoderParametricUMAP",
     "decorator",
     "definition",
     "ED_TCN",
+    "FrameClassificationModel",
     "get",
     "Model",
+    "ParametricUMAPModel",
     "TeenyTweetyNet",
     "TweetyNet",
-    "FrameClassificationModel"
 ]
