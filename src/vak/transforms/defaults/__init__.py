44de24933d32a9a9c85aad320f6caf5fe0843eb0
David Nicholson <nickledave@users.noreply.github.com>
2023-07-09 13:02:41 -0400

    Add transforms/defaults/__init__.py with imports

---
+++ src/vak/transforms/defaults/__init__.py
@@ -0,0 +1,12 @@
+from . import (
+    frame_classification,
+    parametric_umap,
+)
+from .get import get_default_transform
+
+
+__all__ = [
+    "get_default_transform",
+    "frame_classification",
+    "parametric_umap"
+]
