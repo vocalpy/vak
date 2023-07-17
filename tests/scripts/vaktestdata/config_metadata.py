af6e7d3b89fab7cd666f6d4b33f7888f331339f8
David Nicholson <nickledave@users.noreply.github.com>
2023-07-17 12:48:50 -0500

    Add ConfigMetadata dataclass in vaktestdata/config_metadata.py

--- tests/scripts/vaktestdata/config_metadata.py
+++ tests/scripts/vaktestdata/config_metadata.py
@@ -3,6 +3,9 @@ import attrs
 
 @attrs.define
 class ConfigMetadata:
-    config_path: str
-    audio_format: str
-    spect_format: str
+    filename: str = attrs.field()
+    config_type: str = attrs.field()
+    audio_format: str = attrs.field()
+    spect_format: str = attrs.field()
+    annot_format: str = attrs.field()
+    use_dataset_from_config = attrs.field(default=None)
