46f328c282207c1d21284797ac817175bd803b81
David Nicholson <nickledave@users.noreply.github.com>
2023-07-17 12:57:31 -0500

    Add missing field/attribute 'model' to ConfigMetadata

--- tests/scripts/vaktestdata/config_metadata.py
+++ tests/scripts/vaktestdata/config_metadata.py
@@ -4,6 +4,7 @@ import attrs
 @attrs.define
 class ConfigMetadata:
     filename: str = attrs.field()
+    model: str = attrs.field()
     config_type: str = attrs.field()
     audio_format: str = attrs.field()
     spect_format: str = attrs.field()
