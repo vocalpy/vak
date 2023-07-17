32330058d616d4ebcbd422e68da4863c32b49bd4
David Nicholson <nickledave@users.noreply.github.com>
2023-07-17 12:54:24 -0500

    Modify vaktestdata.prep.run_prep to use CONFIG_METADATA to only run prep for configs that have a null 'use_dataset_from_config' field

--- tests/scripts/vaktestdata/prep.py
+++ tests/scripts/vaktestdata/prep.py
@@ -4,9 +4,24 @@ from numba.core.errors import NumbaDeprecationWarning
 warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
 import vak
 
-def run_prep(config_paths):
-    """run ``vak prep`` to generate data for testing"""
-    for config_path in config_paths:
+from . import constants
+
+def run_prep():
+    """Run ``vak prep`` to prepare datasets used with tests.
+
+    This function runs ``prep`` for **only** the configuration files
+    in configs.json that have ``null`` for the field in their metadata
+    ``'use_dataset_from_config'``. The ``null`` indicates that these
+    configs do **not** re-use a dataset prepared from another config.
+    """
+    configs_to_prep = [
+        config_metadata
+        for config_metadata in constants.CONFIG_METADATA
+        if config_metadata.use_dataset_from_config is None
+    ]
+
+    for config_metadata in configs_to_prep:
+        config_path = constants.GENERATED_TEST_CONFIGS_ROOT / config_metadata.filename
         if not config_path.exists():
             raise FileNotFoundError(f"{config_path} not found")
         print(
