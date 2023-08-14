97689082b86e2a2c00eb27cc454740d3ae6147f5
David Nicholson <nickledave@users.noreply.github.com>
2023-08-14 08:39:55 -0400

    Fix imports and __all__ in nn/loss/__init__.py

--- src/vak/nn/loss/__init__.py
+++ src/vak/nn/loss/__init__.py
@@ -1,2 +1,10 @@
-from .dice import *
+from .dice import DiceLoss, dice_loss
 from .umap import UmapLoss, umap_loss
+
+
+__all__ = [
+    "DiceLoss",
+    "dice_loss",
+    "UmapLoss",
+    "umap_loss",
+]
