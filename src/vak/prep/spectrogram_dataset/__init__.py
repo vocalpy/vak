5d5926e835165f770f26d7ea54c93b502e7d63f6
David Nicholson <nickledave@users.noreply.github.com>
2023-08-14 08:28:46 -0400

    Add __all__ in prep/spectrogram_dataset/__init__.py

--- src/vak/prep/spectrogram_dataset/__init__.py
+++ src/vak/prep/spectrogram_dataset/__init__.py
@@ -1,3 +1,8 @@
 """Functions for preparing a dataset for neural network models
 from a dataset of spectrograms."""
 from .prep import prep_spectrogram_dataset
+
+
+__all__ = [
+    "prep_spectrogram_dataset",
+]
