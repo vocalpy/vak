65901ffe838bd43b33c71df45fa596729c6dc377
David Nicholson <nickledave@users.noreply.github.com>
2023-08-14 08:36:55 -0400

    Add 'annotation' to __all__ in plot/__init__.py

--- src/vak/plot/__init__.py
+++ src/vak/plot/__init__.py
@@ -3,6 +3,7 @@ from .annot import annotation, plot_labels, plot_segments
 from .spect import spect, spect_annot
 
 __all__ = [
+    "annotation",
     "learncurve",
     "plot_labels",
     "plot_segments",
