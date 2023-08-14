d0de6524698d3e7be2e1a7db80dae107b618fe88
David Nicholson <nickledave@users.noreply.github.com>
2023-08-13 22:24:33 -0400

    flake8 fixes in frame_labels/__init__.py, noqa on 'unused' imports

--- src/vak/transforms/frame_labels/__init__.py
+++ src/vak/transforms/frame_labels/__init__.py
@@ -1,2 +1,2 @@
-from .functional import *
-from .transforms import FromSegments, PostProcess, ToLabels, ToSegments
+from .functional import *  # noqa : F401
+from .transforms import FromSegments, PostProcess, ToLabels, ToSegments  # noqa : F401
