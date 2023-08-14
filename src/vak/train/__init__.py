61f03069a24a38517b0200d02f6c0e1a6b41d2ff
David Nicholson <nickledave@users.noreply.github.com>
2023-08-14 08:25:32 -0400

    make flake8 fixes in train/__init__.py -- fix imports, add __all__

--- src/vak/train/__init__.py
+++ src/vak/train/__init__.py
@@ -1,5 +1,8 @@
-from .train import *
+from . import frame_classification, parametric_umap
+from .train import train
 
 __all__ = [
+    "frame_classification",
+    "parametric_umap",
     "train",
 ]
