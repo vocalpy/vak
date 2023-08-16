f907a594abe4ce51876425aac4ae12a46f7c655c
David Nicholson <nickledave@users.noreply.github.com>
2023-08-16 08:13:50 -0400

    WIP: fix tests/test_prep/test_frame_classification/test_dataset_arrays.py

--- tests/test_prep/test_frame_classification/test_dataset_arrays.py
+++ tests/test_prep/test_frame_classification/test_dataset_arrays.py
@@ -59,15 +59,15 @@ def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_pat
 
 
 @pytest.mark.parametrize(
-    'config_type, model_name, audio_format, spect_format, annot_format',
+    'config_type, model_name, audio_format, spect_format, annot_format, input_type',
     [
-        ('train', 'teenytweetynet', 'cbin', None, 'notmat'),
-        ('train', 'teenytweetynet', None, 'mat', 'yarden'),
-        ('learncurve', 'teenytweetynet', 'cbin', None, 'notmat'),
+        ('train', 'teenytweetynet', 'cbin', None, 'notmat', 'spect'),
+        ('train', 'teenytweetynet', None, 'mat', 'yarden', 'spect'),
+        ('learncurve', 'teenytweetynet', 'cbin', None, 'notmat', 'spect'),
     ]
 )
 def test_make_npy_files_for_each_split(config_type, model_name, audio_format, spect_format, annot_format,
-                                       tmp_path, specific_dataset_df, specific_dataset_path):
+                                       input_type, tmp_path, specific_dataset_df, specific_dataset_path):
     dataset_df = specific_dataset_df(config_type, model_name, annot_format, audio_format, spect_format)
     dataset_path = specific_dataset_path(config_type, model_name, annot_format, audio_format, spect_format)
     tmp_path_data_dir = tmp_path / 'data_dir'
@@ -82,33 +82,36 @@ def test_make_npy_files_for_each_split(config_type, model_name, audio_format, sp
 
     purpose = config_type
 
-    vak.prep.frame_classification.helper.make_frame_classification_arrays_from_spectrogram_dataset(
+    vak.prep.frame_classification.dataset_arrays.make_npy_files_for_each_split(
         dataset_df,
         tmp_dataset_path,
+        input_type,
         purpose,
         labelmap,
-        annot_format
+        audio_format,
     )
 
-    for split in dataset_df['split'].dropna().unique():
+    for row in dataset_df[not dataset_df['split'].isna()]:
+        split = row.split
         split_subdir = tmp_dataset_path / split
         if split != 'None':
             assert split_subdir.exists()
         elif split == 'None':
             assert not split_subdir.exists()
 
-        for array_file_that_should_exist in (
-            vak.datasets.frame_classification.INPUT_ARRAY_FILENAME,
-            vak.datasets.frame_classification.SOURCE_IDS_ARRAY_FILENAME,
-            vak.datasets.frame_classification.INDS_IN_SOURCE_ARRAY_FILENAME,
+        # TODO: fix below so it works with predict, as in commented out code
+        for col_name in (
+                vak.datasets.frame_classification.constants.FRAMES_NPY_PATH_COL_NAME,
+                vak.datasets.frame_classification.constants.FRAME_LABELS_NPY_PATH_COL_NAME,
         ):
+            array_file_that_should_exist = getattr(row, col_name)
             expected_array_path = split_subdir / array_file_that_should_exist
             assert expected_array_path.exists()
 
-        if purpose != 'predict':
-            for file_that_should_exist in (
-                    vak.datasets.frame_classification.FRAME_LABELS_ARRAY_FILENAME,
-                    vak.datasets.frame_classification.ANNOTATION_CSV_FILENAME,
-            ):
-                expected_path = split_subdir / file_that_should_exist
-                assert expected_path.exists()
+        # if purpose != 'predict':
+        #     for file_that_should_exist in (
+        #             vak.datasets.frame_classification.FRAME_LABELS_ARRAY_FILENAME,
+        #             vak.datasets.frame_classification.ANNOTATION_CSV_FILENAME,
+        #     ):
+        #         expected_path = split_subdir / file_that_should_exist
+        #         assert expected_path.exists()
