6845a84de7652e5ff09fc4fde3c988ff51986307
David Nicholson <nickledave@users.noreply.github.com>
2023-08-17 11:37:29 -0400

    Remove a .lower in a test in tests/test_models/test_windowed_frame_classification_model.py so that it doesn't fail

--- tests/test_models/test_windowed_frame_classification_model.py
+++ tests/test_models/test_windowed_frame_classification_model.py
@@ -87,7 +87,7 @@ class TestWindowedFrameClassificationModel:
                          monkeypatch,
                          ):
         definition = vak.models.definition.validate(definition)
-        model_name = definition.__name__.replace('Definition', '').lower()
+        model_name = definition.__name__.replace('Definition', '')
         toml_path = specific_config('train', model_name, audio_format='cbin', annot_format='notmat')
         cfg = vak.config.parse.from_toml_path(toml_path)
 
