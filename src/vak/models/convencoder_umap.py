a084714a05f56f9ebd217f7e3a59f5fd901f6c81
David Nicholson <nickledave@users.noreply.github.com>
2023-08-18 10:49:35 -0400

    Remove get_default_padding function from src/vak/models/convencoder_umap.py -- deciding to not do this

--- src/vak/models/convencoder_umap.py
+++ src/vak/models/convencoder_umap.py
@@ -7,8 +7,6 @@ https://github.com/lmcinnes/umap/issues/580#issuecomment-1368649550.
 """
 from __future__ import annotations
 
-import math
-
 import torch
 
 from .. import metrics, nets, nn
@@ -65,16 +63,3 @@ class ConvEncoderUMAP:
     default_config = {
         "optimizer": {"lr": 1e-3},
     }
-
-
-def get_default_padding(shape):
-    """Get default padding for input to ConvEncoderUMAP model.
-
-    Rounds up to nearest tens place
-    """
-    rounded_up = tuple(10 * math.ceil(x / 10) for x in shape)
-    padding = tuple(
-        rounded_up_x - shape_x
-        for (rounded_up_x, shape_x) in zip(rounded_up, shape)
-    )
-    return padding
