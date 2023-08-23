b1e8d3da5d4d71900742404cf75b854ce384d56b
David Nicholson <nickledave@users.noreply.github.com>
2023-08-23 18:59:43 -0400

    Fix tests in tests/test_prep/test_frame_classification/test_dataset_arrays.py

--- tests/test_prep/test_frame_classification/test_dataset_arrays.py
+++ tests/test_prep/test_frame_classification/test_dataset_arrays.py
@@ -1,5 +1,7 @@
 """Unit tests for vak.prep.frame_classification.dataset_arrays"""
 import json
+import pathlib
+import shutil
 
 import crowsetta
 import pytest
@@ -43,12 +45,18 @@ def test_argsort_by_label_freq(annots, expected_sort_inds):
     assert out == expected_sort_inds
 
 
-def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_path_data_dir):
+def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, config_type, input_type, tmp_path_data_dir):
     """Copy all the files in a dataset DataFrame to a `tmp_path_data_dir`,
     and change the paths in the Dataframe, so that we can then call
     `vak.prep.frame_classification.helper.move_files_into_split_subdirs`."""
-    # TODO: rewrite to handle case where 'source' files of dataset are audio
-    for paths_col in ('spect_path', 'annot_path'):
+    paths_cols = []
+    if input_type == 'spect':
+        paths_cols.append('spect_path')
+    elif input_type == 'audio':
+        paths_cols.append('audio_path')
+    if config_type != 'predict':
+        paths_cols.append('annot_path')
+    for paths_col in paths_cols:
         paths = dataset_df[paths_col].values
         new_paths = []
         for path in paths:
@@ -62,8 +70,11 @@ def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_pat
     'config_type, model_name, audio_format, spect_format, annot_format, input_type',
     [
         ('train', 'TeenyTweetyNet', 'cbin', None, 'notmat', 'spect'),
+        ('predict', 'TeenyTweetyNet', 'cbin', None, 'notmat', 'spect'),
+        ('eval', 'TeenyTweetyNet', 'cbin', None, 'notmat', 'spect'),
         ('train', 'TeenyTweetyNet', None, 'mat', 'yarden', 'spect'),
         ('learncurve', 'TeenyTweetyNet', 'cbin', None, 'notmat', 'spect'),
+        # TODO: add audio cases
     ]
 )
 def test_make_npy_files_for_each_split(config_type, model_name, audio_format, spect_format, annot_format,
@@ -72,13 +83,16 @@ def test_make_npy_files_for_each_split(config_type, model_name, audio_format, sp
     dataset_path = specific_dataset_path(config_type, model_name, annot_format, audio_format, spect_format)
     tmp_path_data_dir = tmp_path / 'data_dir'
     tmp_path_data_dir.mkdir()
-    copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, tmp_path_data_dir)
+    copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, config_type, input_type, tmp_path_data_dir)
 
     tmp_dataset_path = tmp_path / 'dataset_dir'
     tmp_dataset_path.mkdir()
 
-    with (dataset_path / 'labelmap.json').open('r') as fp:
-        labelmap = json.load(fp)
+    if config_type != 'predict':
+        with (dataset_path / 'labelmap.json').open('r') as fp:
+            labelmap = json.load(fp)
+    else:
+        labelmap = None
 
     purpose = config_type
 
@@ -91,27 +105,66 @@ def test_make_npy_files_for_each_split(config_type, model_name, audio_format, sp
         audio_format,
     )
 
-    for row in dataset_df[not dataset_df['split'].isna()]:
-        split = row.split
+    splits = [
+        split
+        for split in sorted(dataset_df.split.dropna().unique())
+        if split != "None"
+    ]
+
+    for split in splits:
         split_subdir = tmp_dataset_path / split
         if split != 'None':
             assert split_subdir.exists()
         elif split == 'None':
             assert not split_subdir.exists()
 
-        # TODO: fix below so it works with predict, as in commented out code
-        for col_name in (
-                vak.datasets.frame_classification.constants.FRAMES_NPY_PATH_COL_NAME,
-                vak.datasets.frame_classification.constants.FRAME_LABELS_NPY_PATH_COL_NAME,
-        ):
-            array_file_that_should_exist = getattr(row, col_name)
-            expected_array_path = split_subdir / array_file_that_should_exist
-            assert expected_array_path.exists()
+        split_df = dataset_df[dataset_df.split == split].copy()
 
-        # if purpose != 'predict':
-        #     for file_that_should_exist in (
-        #             vak.datasets.frame_classification.FRAME_LABELS_ARRAY_FILENAME,
-        #             vak.datasets.frame_classification.ANNOTATION_CSV_FILENAME,
-        #     ):
-        #         expected_path = split_subdir / file_that_should_exist
-        #         assert expected_path.exists()
+        if purpose != "predict":
+            annots = vak.common.annotation.from_df(split_df)
+        else:
+            annots = None
+
+        if input_type == "audio":
+            source_paths = split_df["audio_path"].values
+        elif input_type == "spect":
+            source_paths = split_df["spect_path"].values
+
+        source_paths = [pathlib.Path(source_path) for source_path in source_paths]
+
+        if annots:
+            source_path_annot_tups = [
+                (source_path, annot)
+                for source_path, annot in zip(source_paths, annots)
+            ]
+        else:
+            source_path_annot_tups = [
+                (source_path, None)
+                for source_path in source_paths
+            ]
+
+        for source_path_annot_tup in source_path_annot_tups:
+            source_path, annot = source_path_annot_tup
+            frames_array_file_that_should_exist = split_subdir / (
+                source_path.stem
+                + vak.datasets.frame_classification.constants.FRAMES_ARRAY_EXT
+            )
+            assert frames_array_file_that_should_exist.exists()
+            if annot:
+                frame_labels_file_that_should_exist = split_subdir / (
+                    source_path.stem
+                    + vak.datasets.frame_classification.constants.FRAME_LABELS_EXT
+                )
+                assert frame_labels_file_that_should_exist.exists()
+
+        sample_id_vec_path = (
+            split_subdir /
+            vak.datasets.frame_classification.constants.SAMPLE_IDS_ARRAY_FILENAME
+        )
+        assert sample_id_vec_path.exists()
+
+        inds_in_sample_vec_path = (
+            split_subdir /
+            vak.datasets.frame_classification.constants.INDS_IN_SAMPLE_ARRAY_FILENAME
+        )
+        assert inds_in_sample_vec_path.exists()
