cd04e359d4f0017ecbb7e827077241c76d7f1e00
David Nicholson <nickledave@users.noreply.github.com>
2023-08-31 13:21:39 -0400

    Remove models command-line argument from tests, no longer used

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,6 @@
-import json
-import pathlib
-
-
 from . import fixtures
 # keep this import here, we need it for fixtures
 from .fixtures import *
-
-
-with pathlib.Path('./tests/vak.tests.config.json').open('r') as fp:
-    VAK_TESTS_CONFIG_TOML = json.load(fp)
-DEFAULT_MODELS = VAK_TESTS_CONFIG_TOML['models']
 
 
 def by_slow_marker(item):
@@ -18,13 +9,6 @@ def by_slow_marker(item):
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--models",
-        action="store",
-        default=DEFAULT_MODELS,
-        nargs="+",
-        help="vak models to test, space-separated list of names",
-    )
     parser.addoption('--dtype', action="store", default="float32")
     parser.addoption('--slow-last', action='store_true', default=False)
 
@@ -35,14 +19,6 @@ def pytest_collection_modifyitems(items, config):
 
 
 def pytest_generate_tests(metafunc):
-    models = metafunc.config.option.models
-    if isinstance(models, str):
-        # wrap a single model name in a list
-        models = [models]
-    # **note!** fixture name is singular even though cmdopt is plural
-    if "model" in metafunc.fixturenames and models is not None:
-        metafunc.parametrize("model", models)
-
     if 'dtype_name' in metafunc.fixturenames:
         raw_value = metafunc.config.getoption('--dtype')
         if raw_value == 'all':
