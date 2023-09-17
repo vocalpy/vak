4de1b0979686ac1dff5c5ddefc4aebcbe600c41c
MB <maris.basha1@gmail.com>
2023-09-17 21:58:58 +0200

    dev: Added suggestions and tested network forward result.

--- src/vak/nets/__init__.py
+++ src/vak/nets/__init__.py
@@ -11,5 +11,5 @@ __all__ = [
     "ED_TCN",
     "tweetynet",
     "TweetyNet",
-    "Ava"
+    "Ava",
 ]
--- src/vak/nn/loss/__init__.py
+++ src/vak/nn/loss/__init__.py
@@ -1,12 +1,12 @@
 from .dice import DiceLoss, dice_loss
 from .umap import UmapLoss, umap_loss
-from .vae import VaeLoss, vae_loss
+from .vae import VaeElboLoss, vae_elbo_loss
 
 __all__ = [
     "DiceLoss",
     "dice_loss",
     "UmapLoss",
     "umap_loss",
-    "VaeLoss",
-    "vae_loss"
+    "VaeElboLoss",
+    "vae_elbo_loss"
 ]
