e1222c3cd645a565a11ee79b6af4a9865bf92d57
David Nicholson <nickledave@users.noreply.github.com>
2023-09-29 08:16:20 -0400

    Add src/vak/models/ava.py

--- src/vak/models/ava.py
+++ src/vak/models/ava.py
@@ -5,17 +5,17 @@ import torch
 from .. import metrics, nets
 from .decorator import model
 from .vae_model import VAEModel
-from ..nn.loss import VaeElboLoss
+from ..nn.loss import VaeLoss
 
 @model(family=VAEModel)
 class AVA:
     """
     """
-    network = nets.Ava
-    loss = VaeElboLoss
+    network = Ava
+    loss = VaeLoss
     optimizer = torch.optim.Adam
     metrics = {
-        "loss": VaeElboLoss,
+        "loss": VaeLoss,
         "kl": torch.nn.functional.kl_div
     }
     default_config = {"optimizer": {"lr": 0.003}}
