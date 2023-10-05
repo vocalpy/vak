bbb10b1e4f1725fbca01ced82080fd65bd9cb946
David Nicholson <nickledave@users.noreply.github.com>
2023-10-05 14:24:08 -0400

    Fix how we use subset in FramesDataset.__init__

--- src/vak/datasets/frame_classification/frames_dataset.py
+++ src/vak/datasets/frame_classification/frames_dataset.py
@@ -63,7 +63,7 @@ class FramesDataset:
         self.subset = subset
         # subset takes precedence over split, if specified
         if subset:
-            dataset_df = dataset_df[dataset_df.split == split].copy()
+            dataset_df = dataset_df[dataset_df.subset == subset].copy()
         else:
             dataset_df = dataset_df[dataset_df.split == split].copy()
         self.dataset_df = dataset_df
