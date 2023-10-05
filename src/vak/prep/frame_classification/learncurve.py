2952ee076a63a265442a9b923ba8028f2a877f15
David Nicholson <nickledave@users.noreply.github.com>
2023-10-05 11:24:29 -0400

    Add function `make_index_vectors_for_each_subset` to src/vak/prep/frame_classification/learncurve.py, rename `make_learncurve_splits` to `make_subsets_from_dataset_df` and have it call `make_index_vectors`

--- src/vak/prep/frame_classification/learncurve.py
+++ src/vak/prep/frame_classification/learncurve.py
@@ -6,42 +6,220 @@ import logging
 import pathlib
 from typing import Sequence
 
+import attrs
+import dask.bag as db
+import numpy as np
 import pandas as pd
+from dask.diagnostics import ProgressBar
 
-from ... import common
+from ... import common, datasets
+from .. import constants as prep_constants
 from .. import split
 
 
 logger = logging.getLogger(__name__)
 
 
-def make_learncurve_splits_from_dataset_df(
+@attrs.define(frozen=True)
+class Sample:
+    """Dataclass representing one sample
+    in a frame classification dataset.
+
+    Used to add paths for arrays from the sample
+    to a ``dataset_df``, and to build
+    the ``sample_ids`` vector and ``inds_in_sample`` vector
+    for the entire dataset."""
+
+    source_id: int = attrs.field()
+    sample_id_vec: np.ndarray
+    inds_in_sample_vec: np.ndarray
+
+
+def make_index_vectors_for_each_subset(
     dataset_df: pd.DataFrame,
+    dataset_path: str | pathlib.Path,
     input_type: str,
+    audio_format: str,
+    spect_key: str = "s",
+) -> pd.DataFrame:
+    r"""Make npy files containing indexing vectors
+    for each subset of the training data 
+    used to generate a learning curve 
+    with a frame classification dataset.
+
+    This function is basically the same as
+    :func:`vak.prep.frame_classification.dataset_arrays.make_npy_files_for_each_split`,
+    *except* that it only makes the indexing vectors
+    for each subset of the training data.
+    These indexing vectors are needed for each subset
+    to properly grab windows from the npy files during training.
+    There is no need to remake the npy files themselves though.
+
+    All the indexing vectors for each split are saved
+    in the "train" directory split inside ``dataset_path``.
+
+    The indexing vectors are used by
+    :class:`vak.datasets.frame_classification.WindowDataset`
+    and :class:`vak.datasets.frame_classification.FramesDataset`.
+    These vectors make it possible to work with files,
+    to avoid loading the entire dataset into memory,
+    and to avoid working with memory-mapped arrays.
+    The first is the ``sample_ids`` vector,
+    that represents the "ID" of any sample :math:`(x, y)` in the split.
+    We use these IDs to load the array files corresponding to the samples.
+    For a split with :math:`m` samples, this will be an array of length :math:`T`,
+    the total number of frames across all samples,
+    with elements :math:`i \in (0, 1, ..., m - 1)`
+    indicating which frames correspond to which sample :math:`m_i`:
+    :math:`(0, 0, 0, ..., 1, 1, ..., m - 1, m -1)`.
+    The second vector is the ``inds_in_sample`` vector.
+    This vector is the same length as ``sample_ids``, but its values represent
+    the indices of frames within each sample :math:`x_t`.
+    For a data set with :math:`T` total frames across all samples,
+    where :math:`t_i` indicates the number of frames in each :math:`x_i`,
+    this vector will look like :math:`(0, 1, ..., t_0, 0, 1, ..., t_1, ... t_m)`.
+
+    Parameters
+    ----------
+    dataset_df : pandas.DataFrame
+        A ``pandas.DataFrame`` returned by :func:`vak.io.dataframe.from_files`
+        with a ``'split'`` column added, as a result of calling
+        :func:`vak.io.dataframe.from_files` or because it was added "manually"
+        by calling :func:`vak.core.prep.prep_helper.add_split_col` (as is done
+        for 'predict' when the entire ``DataFrame`` belongs to this
+        "split").
+    dataset_path : pathlib.Path
+        Path to directory that represents dataset.
+    input_type : str
+        The type of input to the neural network model.
+        One of {'audio', 'spect'}.
+    audio_format : str
+        A :class:`string` representing the format of audio files.
+        One of :constant:`vak.common.constants.VALID_AUDIO_FORMATS`.
+    spect_key : str
+        Key for accessing spectrogram in files. Default is 's'.
+
+    Returns
+    -------
+    None
+    """
+    if input_type not in prep_constants.INPUT_TYPES:
+        raise ValueError(
+            f"``input_type`` must be one of: {prep_constants.INPUT_TYPES}\n"
+            f"Value for ``input_type`` was: {input_type}"
+        )
+
+    subsets = [
+        subset
+        for subset in sorted(dataset_df.subset.dropna().unique())
+    ]
+    for subset in subsets:
+        subset_df = dataset_df[dataset_df.subset == subset].copy()
+
+        if input_type == "audio":
+            source_paths = subset_df["audio_path"].values
+        elif input_type == "spect":
+            source_paths = subset_df["spect_path"].values
+        else:
+            raise ValueError(f"Invalid ``input_type``: {input_type}")
+
+        def _return_index_arrays(
+            source_id_path_tup,
+        ):
+            """Function we use with dask to parallelize.
+            Defined in-line so variables are in scope.
+            """
+            source_id, source_path = source_id_path_tup
+            source_path = pathlib.Path(source_path)
+
+            if input_type == "audio":
+                frames, samplefreq = common.constants.AUDIO_FORMAT_FUNC_MAP[
+                    audio_format
+                ](source_path)
+                if (
+                    audio_format == "cbin"
+                ):  # convert to ~wav, from int16 to float64
+                    frames = frames.astype(np.float64) / 32768.0
+            elif input_type == "spect":
+                spect_dict = np.load(source_path)
+                frames = spect_dict[spect_key]
+
+            n_frames = frames.shape[-1]
+            sample_id_vec = np.ones((n_frames,)).astype(np.int32) * source_id
+            inds_in_sample_vec = np.arange(n_frames)
+
+            return Sample(
+                source_id,
+                sample_id_vec,
+                inds_in_sample_vec,
+            )
+
+        # ---- make npy files for this split, parallelized with dask
+        # using nested function just defined
+        source_path_annot_tups = [
+            (source_id, source_path)
+            for source_id, source_path in enumerate(source_paths)
+        ]
+
+        source_path_annot_bag = db.from_sequence(source_path_annot_tups)
+        with ProgressBar():
+            samples = list(
+                source_path_annot_bag.map(
+                    _return_index_arrays
+                )
+            )
+        samples = sorted(samples, key=lambda sample: sample.source_id)
+
+        # ---- save indexing vectors in split directory
+        sample_id_vec = np.concatenate(
+            list(sample.sample_id_vec for sample in samples)
+        )
+        np.save(
+            dataset_path / "train" /
+            datasets.frame_classification.constants.SAMPLE_IDS_ARRAY_FILENAME.replace(
+                '.npy', f'-{subset}.npy'
+            ),
+            sample_id_vec,
+        )
+        inds_in_sample_vec = np.concatenate(
+            list(sample.inds_in_sample_vec for sample in samples)
+        )
+        np.save(
+            dataset_path / "train" /
+            datasets.frame_classification.constants.INDS_IN_SAMPLE_ARRAY_FILENAME.replace(
+                '.npy', f'-{subset}.npy'
+            ),
+            inds_in_sample_vec,
+        )
+
+
+def make_subsets_from_dataset_df(
+    dataset_df: pd.DataFrame,
     train_set_durs: Sequence[float],
     num_replicates: int,
     dataset_path: pathlib.Path,
     labelmap: dict,
-    audio_format: str | None = None,
+    input_type: str,
+    audio_format: str,
     spect_key: str = "s",
-    timebins_key: str = "t",
 ) -> pd.DataFrame:
-    """Make splits for a learning curve
-    from a dataframe representing the entire dataset,
-    one split for each combination of (training set duration,
-    replicate number).
-    Each split is a randomly drawn subset of data
+    """Make subsets of the training data split for a learning curve.
+
+    Makes subsets given a dataframe representing the entire dataset,
+    with one subset for each combination of (training set duration,
+    replicate number). Each subset is randomly drawn
     from the total training split.
 
     Uses :func:`vak.prep.split.frame_classification_dataframe` to make
-    splits/subsets of the training data
-    from ``dataset_df``, and then uses
-    :func:`vak.prep.frame_classification.dataset_arrays.make_npy_files_for_each_split`
-    to make the array files for each split.
+    subsets of the training data from ``dataset_df``.
 
-    A new directory will be made for each combination of
-    (training set duration, replicate number) as shown below,
-    for ``train_durs=[4.0, 6.0], num_replicates=2``.
+    A new column will be added to the dataframe, `'subset'`,
+    and additional rows for each subset.
+    The dataframe is returned with these subsets added.
+    (The `'split'` for these rows will still be `'train'`.)
+    Additionally, a separate set of indexing vectors
+    will be made for each subset,
+    using
 
     .. code-block:: console
         032312-vak-frame-classification-dataset-generated-230820_144833
@@ -64,12 +242,9 @@ def make_learncurve_splits_from_dataset_df(
     ----------
     dataset_df : pandas.DataFrame
         Representing an entire dataset of vocalizations.
-    input_type : str
-        The type of input to the neural network model.
-        One of {'audio', 'spect'}.
     train_set_durs : list
-        of int, durations in seconds of subsets taken from training data
-        to create a learning curve, e.g. [5, 10, 15, 20].
+        Durations in seconds of subsets taken from training data
+        to create a learning curve, e.g., `[5., 10., 15., 20.]`.
     num_replicates : int
         number of times to replicate training for each training set duration
         to better estimate metrics for a training set of that size.
@@ -77,17 +252,14 @@ def make_learncurve_splits_from_dataset_df(
         data (but of the same duration).
     dataset_path : str, pathlib.Path
         Directory where splits will be saved.
-    labelmap : dict
-        A :class:`dict` that maps a set of human-readable
-        string labels to the integer classes predicted by a neural
-        network model. As returned by :func:`vak.labels.to_map`.
+    input_type : str
+        The type of input to the neural network model.
+        One of {'audio', 'spect'}.
     audio_format : str
         A :class:`string` representing the format of audio files.
         One of :constant:`vak.common.constants.VALID_AUDIO_FORMATS`.
     spect_key : str
         Key for accessing spectrogram in files. Default is 's'.
-    timebins_key : str
-        Key for accessing vector of time bins in files. Default is 't'.
 
     Returns
     -------
@@ -146,12 +318,22 @@ def make_learncurve_splits_from_dataset_df(
         all_train_durs_and_replicates_df
     )
 
+    make_index_vectors_for_each_subset(
+        all_train_durs_and_replicates_df,
+        input_type,
+        audio_format,
+        spect_key,
+    )
+
     # keep the same validation, test, and total train sets by concatenating them with the train subsets
     dataset_df["subset"] = None  # add column but have it be empty
     dataset_df = pd.concat(
         (
             all_train_durs_and_replicates_df,
             dataset_df,
+            input_type,
+            audio_format,
+            spect_key,
         )
     )
     # We reset the entire index across all splits, instead of repeating indices,
