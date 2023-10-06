dbf27f32df5c08c0803b8ecb616930f3e4288507
David Nicholson <nickledave@users.noreply.github.com>
2023-10-06 09:17:12 -0400

    Fix FramesDataset the same way as WindowDataset: load frame paths with constant, load inside __getitem__ with helper function _load_frames, validate input type, fix order of attributes in docstring

--- src/vak/datasets/frame_classification/frames_dataset.py
+++ src/vak/datasets/frame_classification/frames_dataset.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 from . import constants, helper
 from .metadata import Metadata
+from ... import common
 
 
 class FramesDataset:
@@ -31,9 +32,11 @@ class FramesDataset:
     split : str
         The name of a split from the dataset,
         one of {'train', 'val', 'test'}.
-    sample_ids : numpy.ndarray
-        Indexing vector representing which sample
-        from the dataset every frame belongs to.
+    subset : str, optional
+        Name of subset to use.
+        If specified, this takes precedence over split.
+        Subsets are typically taken from the training data
+        for use when generating a learning curve.
     dataset_df : pandas.DataFrame
         A frame classification dataset,
         represented as a :class:`pandas.DataFrame`.
@@ -41,7 +44,7 @@ class FramesDataset:
         to either ``subset`` or ``split`` from the
         ``dataset_df`` that was passed in when
         instantiating the class.
-    frame_paths : numpy.ndarray
+    frames_paths : numpy.ndarray
         Paths to npy files containing frames,
         either spectrograms or audio signals
         that are input to the model.
@@ -49,6 +52,9 @@ class FramesDataset:
         Paths to npy files containing vectors
         with a label for each frame.
         The targets for the outputs of the model.
+    input_type : str
+        The type of input to the neural network model.
+        One of {'audio', 'spect'}.
     sample_ids : numpy.ndarray
         Indexing vector representing which sample
         from the dataset every frame belongs to.
@@ -68,11 +74,11 @@ class FramesDataset:
         self,
         dataset_path: str | pathlib.Path,
         dataset_df: pd.DataFrame,
+        input_type: str,
         split: str,
         sample_ids: npt.NDArray,
         inds_in_sample: npt.NDArray,
         frame_dur: float,
-        input_type: str,
         subset: str | None = None,
         item_transform: Callable | None = None,
     ):
@@ -88,6 +94,9 @@ class FramesDataset:
         dataset_df : pandas.DataFrame
             A frame classification dataset,
             represented as a :class:`pandas.DataFrame`.
+        input_type : str
+            The type of input to the neural network model.
+            One of {'audio', 'spect'}.
         split : str
             The name of a split from the dataset,
             one of {'train', 'val', 'test'}.
@@ -101,9 +110,6 @@ class FramesDataset:
         frame_dur: float
             Duration of a frame, i.e., a single sample in audio
             or a single timebin in a spectrogram.
-        input_type : str
-            The type of input to the neural network model.
-            One of {'audio', 'spect'}.
         subset : str, optional
             Name of subset to use.
             If specified, this takes precedence over split.
@@ -113,8 +119,14 @@ class FramesDataset:
             Transform applied to each item :math:`(x, y)`
             returned by :meth:`FramesDataset.__getitem__`.
         """
-        self.dataset_path = pathlib.Path(dataset_path)
+        from ... import prep  # avoid circular import, use for constants.INPUT_TYPES
+        if input_type not in prep.constants.INPUT_TYPES:
+            raise ValueError(
+                f"``input_type`` must be one of: {prep.constants.INPUT_TYPES}\n"
+                f"Value for ``input_type`` was: {input_type}"
+            )
 
+        self.dataset_path = pathlib.Path(dataset_path)
         self.split = split
         self.subset = subset
         # subset takes precedence over split, if specified
@@ -123,8 +135,9 @@ class FramesDataset:
         else:
             dataset_df = dataset_df[dataset_df.split == split].copy()
         self.dataset_df = dataset_df
+        self.input_type = input_type
         self.frames_paths = self.dataset_df[
-            constants.FRAMES_NPY_PATH_COL_NAME
+            constants.FRAMES_PATH_COL_NAME
         ].values
         if split != "predict":
             self.frame_labels_paths = self.dataset_df[
@@ -132,16 +145,6 @@ class FramesDataset:
             ].values
         else:
             self.frame_labels_paths = None
-
-        if input_type == "audio":
-            self.source_paths = self.dataset_df["audio_path"].values
-        elif input_type == "spect":
-            self.source_paths = self.dataset_df["spect_path"].values
-        else:
-            raise ValueError(
-                f"Invalid `input_type`: {input_type}. Must be one of {{'audio', 'spect'}}."
-            )
-
         self.sample_ids = sample_ids
         self.inds_in_sample = inds_in_sample
         self.frame_dur = float(frame_dur)
@@ -157,9 +160,26 @@ class FramesDataset:
         tmp_item = self.__getitem__(tmp_x_ind)
         return tmp_item["frames"].shape
 
+    def _load_frames(self, source_path):
+        """Helper function that loads "frames",
+        the input to the frame classification model.
+        Loads audio or spectrogram, depending on
+        :attr:`self.input_type`.
+        This function assumes that audio is in wav format 
+        and spectrograms are in npz files.
+        """
+        if self.input_type == "audio":
+            frames, _ = common.constants.AUDIO_FORMAT_FUNC_MAP[
+                "wav"
+            ](source_path)
+        elif self.input_type == "spect":
+            spect_dict = common.files.spect.load(source_path)
+            frames = spect_dict["s"]
+        return frames
+
     def __getitem__(self, idx):
-        source_path = self.source_paths[idx]
-        frames = np.load(self.dataset_path / self.frames_paths[idx])
+        frames_path = self.frames_paths[idx]
+        frames = self._load_frames(frames_path)
         item = {"frames": frames, "source_path": source_path}
         if self.frame_labels_paths is not None:
             frame_labels = np.load(
@@ -184,7 +204,8 @@ class FramesDataset:
         subset: str | None = None,
         item_transform: Callable | None = None,
     ):
-        """
+        """Make a :class:`FramesDataset` instance,
+        given the path to a frame classification dataset.
 
         Parameters
         ----------
@@ -233,11 +254,11 @@ class FramesDataset:
         return cls(
             dataset_path,
             dataset_df,
+            input_type,
             split,
             sample_ids,
             inds_in_sample,
             frame_dur,
-            input_type,
             subset,
             item_transform,
         )
