79322cf98713671f418cd346acd184c8de62eef1
David Nicholson <nickledave@users.noreply.github.com>
2023-10-06 11:44:57 -0400

    Add function load_frames to src/vak/datasets/frame_classification/helper.py

--- src/vak/datasets/frame_classification/helper.py
+++ src/vak/datasets/frame_classification/helper.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from . import constants
+from ... import common
 
 
 def sample_ids_array_filename_for_subset(subset: str) -> str:
@@ -16,3 +17,21 @@ def inds_in_sample_array_filename_for_subset(subset: str) -> str:
     return constants.INDS_IN_SAMPLE_ARRAY_FILENAME.replace(
         '.npy', f'-{subset}.npy'
     )
+
+
+def load_frames(frames_path, input_type):
+    """Helper function that loads "frames",
+    the input to the frame classification model.
+    Loads audio or spectrogram, depending on
+    :attr:`self.input_type`.
+    This function assumes that audio is in wav format
+    and spectrograms are in npz files.
+    """
+    if input_type == "audio":
+        frames, _ = common.constants.AUDIO_FORMAT_FUNC_MAP[
+            constants.FRAME_CLASSIFICATION_DATASET_AUDIO_FORMAT
+        ](frames_path)
+    elif input_type == "spect":
+        spect_dict = common.files.spect.load(frames_path)
+        frames = spect_dict[common.constants.SPECT_KEY]
+    return frames
