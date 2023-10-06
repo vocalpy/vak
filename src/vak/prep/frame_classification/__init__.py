dd3383643ab149ca7b927b6782d9601cf171b5e7
David Nicholson <nickledave@users.noreply.github.com>
2023-10-05 21:15:09 -0400

    Fix imports in src/vak/prep/frame_classification/__init__.py after renaming module to 'make_splits'

--- src/vak/prep/frame_classification/__init__.py
+++ src/vak/prep/frame_classification/__init__.py
@@ -1,10 +1,10 @@
-from . import dataset_arrays, frame_classification, learncurve, validators
+from . import frame_classification, learncurve, make_splits, validators
 from .frame_classification import prep_frame_classification_dataset
 
 __all__ = [
-    "dataset_arrays",
     "frame_classification",
     "learncurve",
+    "make_splits",
     "prep_frame_classification_dataset",
     "validators",
 ]
