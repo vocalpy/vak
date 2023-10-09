03c30558d78e5f6bb723e3858e56e36a5493c03a
David Nicholson <nickledave@users.noreply.github.com>
2023-10-09 17:03:36 -0400

    Add tests/fixtures/source_files with fixtures to get csv files

---
+++ tests/fixtures/source_files.py
@@ -0,0 +1,60 @@
+"""Fixtures having to do with source files, i.e.,
+the "raw" files that go into a data set
+used with neural networks
+"""
+import pytest
+
+from .test_data import GENERATED_TEST_DATA_ROOT
+
+# copied from vaktestdata.constants; could we add this to that with sys.path? or vice versa
+GENERATED_SOURCE_FILES_CSV_DIR = GENERATED_TEST_DATA_ROOT / "source-files-csv"
+GENERATED_SOURCE_FILES_WITH_SPLITS_CSV_DIR = GENERATED_TEST_DATA_ROOT / "source-files-with-splits-csv"
+
+
+@pytest.fixture
+def specific_source_file_csv_path(
+        model_name,
+        config_type,
+        annot_format,
+        audio_format=None,
+        spect_format=None,
+):
+    """Factory fixture that returns a specific source file csv"""
+    def _specific_source_file_csv():
+        if audio_format and spect_format:
+            raise ValueError(
+                "Specify audio_format or spect_format, not both"
+            )
+        if audio_format:
+            csv_filename = f'{model_name}_{config_type}_audio_{audio_format}_annot_{annot_format}.toml-source-files.csv'
+        elif spect_format:
+            csv_filename = f'{model_name}_{config_type}_spect_{spect_format}_annot_{annot_format}.toml-source-files.csv'
+        csv_path = GENERATED_SOURCE_FILES_CSV_DIR / csv_filename
+        return csv_path
+
+    return _specific_source_file_csv
+
+
+@pytest.fixture
+def specific_source_file_with_split_csv_path(
+        model_name,
+        config_type,
+        annot_format,
+        audio_format=None,
+        spect_format=None,
+):
+    """Factory fixture that returns a specific source file csv"""
+
+    def _specific_source_file_with_split_csv_path():
+        if audio_format and spect_format:
+            raise ValueError(
+                "Specify audio_format or spect_format, not both"
+            )
+        if audio_format:
+            csv_filename = f'{model_name}_{config_type}_audio_{audio_format}_annot_{annot_format}.toml-source-files.csv'
+        elif spect_format:
+            csv_filename = f'{model_name}_{config_type}_spect_{spect_format}_annot_{annot_format}.toml-source-files.csv'
+        csv_path = GENERATED_SOURCE_FILES_WITH_SPLITS_CSV_DIR / csv_filename
+        return csv_path
+
+    return _specific_source_file_csv
