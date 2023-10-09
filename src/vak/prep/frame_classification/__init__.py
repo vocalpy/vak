db6f3b8382032eacfb28b391a30ed2da84f364b3
David Nicholson <nickledave@users.noreply.github.com>
2023-10-09 16:39:13 -0400

    Fix import in src/vak/prep/frame_classification/__init__.py

--- src/vak/prep/frame_classification/__init__.py
+++ src/vak/prep/frame_classification/__init__.py
@@ -1,7 +1,7 @@
 from . import frame_classification, learncurve, make_splits, validators
 from .assign_samples_to_splits import assign_samples_to_splits
 from .frame_classification import prep_frame_classification_dataset
-from .get_or_make_source_files import get_or_make_source_files
+from .source_files import get_or_make_source_files
 
 __all__ = [
     "assign_samples_to_splits",
