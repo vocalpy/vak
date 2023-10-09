6686b56849187bac2eaf13e23d9c912c376ff528
David Nicholson <nickledave@users.noreply.github.com>
2023-10-09 17:36:43 -0400

    Fix unit test in tests/test_prep/test_frame_classification/test_make_splits.py to use fixture so it's faster and less verbose

--- tests/test_prep/test_frame_classification/test_make_splits.py
+++ tests/test_prep/test_frame_classification/test_make_splits.py
@@ -80,7 +80,7 @@ def copy_dataset_df_files_to_tmp_path_data_dir(dataset_df, dataset_path, config_
     ]
 )
 def test_make_splits(config_type, model_name, audio_format, spect_format, annot_format,
-                     input_type, tmp_path, specific_config_toml_path):
+                     input_type, tmp_path, specific_config_toml_path, specific_source_files_with_split_df):
     toml_path = specific_config_toml_path(
         config_type,
         model_name,
@@ -96,113 +96,23 @@ def test_make_splits(config_type, model_name, audio_format, spect_format, annot_
 
     purpose = config_type
 
-    def _prep_steps_up_to_make_splits():
-        """copied almost verbatim from vak.prep.frame_classification
-        this is all the stuff that happens before we call make_splits
-        """
-        if input_type == "spect":
-            dataset_df = vak.prep.spectrogram_dataset.prep_spectrogram_dataset(
-                labelset=cfg.prep.labelset,
-                data_dir=cfg.prep.data_dir,
-                annot_format=cfg.prep.annot_format,
-                annot_file=cfg.prep.annot_file,
-                audio_format=cfg.prep.audio_format,
-                spect_format=cfg.prep.spect_format,
-                spect_params=cfg.spect_params,
-                spect_output_dir=tmp_dataset_path,
-                audio_dask_bag_kwargs=cfg.prep.audio_dask_bag_kwargs,
-            )
-        elif input_type == "audio":
-            dataset_df = vak.prep.audio_dataset.prep_audio_dataset(
-                audio_format=cfg.prep.audio_format,
-                data_dir=cfg.prep.data_dir,
-                annot_format=cfg.prep.annot_format,
-                labelset=cfg.prep.labelset,
-            )
-
-        if dataset_df.empty:
-            raise ValueError(
-                "Calling `vak.prep.spectrogram_dataset.prep_spectrogram_dataset` "
-                "with arguments passed to `vak.core.prep` "
-                "returned an empty dataframe.\n"
-                "Please double-check arguments to `vak.core.prep` function."
-            )
-
-        # ---- (possibly) split into train / val / test sets ---------------------------------------------
-        # catch case where user specified duration for just training set, raise a helpful error instead of failing silently
-        if (purpose == "train" or purpose == "learncurve") and (
-                (cfg.prep.train_dur is not None and cfg.prep.train_dur > 0)
-                and (cfg.prep.val_dur is None or cfg.prep.val_dur == 0)
-                and (cfg.prep.test_dur is None or cfg.prep.val_dur == 0)
-        ):
-            raise ValueError(
-                "A duration specified for just training set, but prep function does not currently support creating a "
-                "single split of a specified duration. Either remove the train_dur option from the prep section and "
-                "rerun, in which case all data will be included in the training set, or specify values greater than "
-                "zero for test_dur (and val_dur, if a validation set will be used)"
-            )
-
-        if all(
-                [dur is None for dur in (cfg.prep.train_dur, cfg.prep.val_dur, cfg.prep.test_dur)]
-        ) or purpose in (
-                "eval",
-                "predict",
-        ):
-            # then we're not going to split
-            do_split = False
-        else:
-            if cfg.prep.val_dur is not None and cfg.prep.train_dur is None and cfg.prep.test_dur is None:
-                raise ValueError(
-                    "cannot specify only val_dur, unclear how to split dataset into training and test sets"
-                )
-            else:
-                do_split = True
-
-        if do_split:
-            dataset_df = vak.prep.split.frame_classification_dataframe(
-                dataset_df,
-                tmp_dataset_path,
-                labelset=cfg.prep.labelset,
-                train_dur=cfg.prep.train_dur,
-                val_dur=cfg.prep.val_dur,
-                test_dur=cfg.prep.test_dur,
-            )
-
-        elif (
-                do_split is False
-        ):  # add a split column, but assign everything to the same 'split'
-            # ideally we would just say split=purpose in call to add_split_col, but
-            # we have to special case, because "eval" looks for a 'test' split (not an "eval" split)
-            if purpose == "eval":
-                split_name = (
-                    "test"  # 'split_name' to avoid name clash with split package
-                )
-            elif purpose == "predict":
-                split_name = "predict"
-
-            dataset_df = vak.prep.dataset_df_helper.add_split_col(
-                dataset_df, split=split_name
-            )
-
-        # ---- create and save labelmap ------------------------------------------------------------------------------------
-        # we do this before creating array files since we need to load the labelmap to make frame label vectors
-        if purpose != "predict":
-            # TODO: add option to generate predict using existing dataset, so we can get labelmap from it
-            map_unlabeled_segments = vak.prep.sequence_dataset.has_unlabeled_segments(
-                dataset_df
-            )
-            labelmap = vak.common.labels.to_map(
-                cfg.prep.labelset, map_unlabeled=map_unlabeled_segments
-            )
-            # save labelmap in case we need it later
-            with (tmp_dataset_path / "labelmap.json").open("w") as fp:
-                json.dump(labelmap, fp)
-        else:
-            labelmap = None
-
-        return dataset_df, labelmap
-
-    dataset_df, labelmap = _prep_steps_up_to_make_splits()
+    dataset_df = specific_source_files_with_split_df(
+        config_type,
+        model_name,
+        annot_format,
+        audio_format,
+        spect_format,
+    )
+    if purpose != "predict":
+        # TODO: add option to generate predict using existing dataset, so we can get labelmap from it
+        map_unlabeled_segments = vak.prep.sequence_dataset.has_unlabeled_segments(
+            dataset_df
+        )
+        labelmap = vak.common.labels.to_map(
+            cfg.prep.labelset, map_unlabeled=map_unlabeled_segments
+        )
+    else:
+        labelmap = None
 
     dataset_df_with_splits = vak.prep.frame_classification.make_splits.make_splits(
         dataset_df,
