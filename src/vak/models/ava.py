b2b854d8d25913c77ff34d4c5e38c8657855af99
David Nicholson <nickledave@users.noreply.github.com>
2023-11-22 12:53:41 -0500

    Fix default learning rate in src/vak/models/ava.py

--- src/vak/models/ava.py
+++ src/vak/models/ava.py
@@ -1,15 +1,26 @@
+"""Autoencoded Vocal Analysis (AVA) model [1]_.
+
+.. [1] Goffinet, J., Brudner, S., Mooney, R., & Pearson, J. (2021).
+   Low-dimensional learned feature spaces quantify individual and group differences in vocal repertoires.
+   eLife, 10:e67855. https://doi.org/10.7554/eLife.67855
+"""
 from __future__ import annotations
 
 import torch
 from torchmetrics import KLDivergence
-from .. import metrics, nets
+from .. import nets
 from .decorator import model
 from .vae_model import VAEModel
 from ..nn.loss import VaeElboLoss
 
+
 @model(family=VAEModel)
 class AVA:
-    """
+    """Autoencoded Vocal Analysis (AVA) model [1]_.
+
+    .. [1] Goffinet, J., Brudner, S., Mooney, R., & Pearson, J. (2021).
+       Low-dimensional learned feature spaces quantify individual and group differences in vocal repertoires.
+       eLife, 10:e67855. https://doi.org/10.7554/eLife.67855
     """
     network = nets.AVA
     loss = VaeElboLoss
@@ -18,4 +29,4 @@ class AVA:
         "loss": VaeElboLoss,
         "kl": KLDivergence
     }
-    default_config = {"optimizer": {"lr": 0.003}}
+    default_config = {"optimizer": {"lr": 1e-3}}
