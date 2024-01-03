8ec3b1eb19b454e7ff28cf3219ae384e9b980c5d
David Nicholson <nickledave@users.noreply.github.com>
2024-01-02 19:26:09 -0500

    Add vae-window and vae-segment to DATASET_TYPE_FUNCTION_MAP in vae.prep_vae_dataset

--- src/vak/prep/constants.py
+++ src/vak/prep/constants.py
@@ -2,7 +2,7 @@
 
 Defined in a separate module to minimize circular imports.
 """
-from . import frame_classification, parametric_umap
+from . import frame_classification, parametric_umap, vae
 
 VALID_PURPOSES = frozenset(
     [
@@ -18,6 +18,8 @@ INPUT_TYPES = {"audio", "spect"}
 DATASET_TYPE_FUNCTION_MAP = {
     "frame classification": frame_classification.prep_frame_classification_dataset,
     "parametric umap": parametric_umap.prep_parametric_umap_dataset,
+    "vae-window": vae.prep_vae_dataset,
+    "vae-segment": vae.prep_vae_dataset,
 }
 
 DATASET_TYPES = tuple(DATASET_TYPE_FUNCTION_MAP.keys())
