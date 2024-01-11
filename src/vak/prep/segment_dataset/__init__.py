7f70781309dac7c4dcc29da261f6681d77a15926
David Nicholson <nickledave@users.noreply.github.com>
2024-01-11 11:39:58 -0500

    Import make_splits function in src/vak/prep/segment_dataset/__init__.py

--- src/vak/prep/segment_dataset/__init__.py
+++ src/vak/prep/segment_dataset/__init__.py
@@ -1,4 +1,5 @@
 from . import segment_dataset
+from .make_splits import make_splits
 from .segment_dataset import prep_segment_dataset
 
-__all__ = ["prep_segment_dataset", "segment_dataset"]
+__all__ = ["make_splits", "prep_segment_dataset", "segment_dataset"]
