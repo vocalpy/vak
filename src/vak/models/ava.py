a4272aab886ba97e917c0ab8ecc7cfe062aa0f54
David Nicholson <nickledave@users.noreply.github.com>
2024-01-19 13:09:13 -0500

    Add ref to AVA code in src/vak/models/ava.py

--- src/vak/models/ava.py
+++ src/vak/models/ava.py
@@ -1,8 +1,11 @@
 """Autoencoded Vocal Analysis (AVA) model [1]_.
+Code is adapted from [2]_.
 
 .. [1] Goffinet, J., Brudner, S., Mooney, R., & Pearson, J. (2021).
    Low-dimensional learned feature spaces quantify individual and group differences in vocal repertoires.
    eLife, 10:e67855. https://doi.org/10.7554/eLife.67855
+
+.. [2] https://github.com/pearsonlab/autoencoded-vocal-analysis
 """
 from __future__ import annotations
 
