53447f5f8fbf80a2478ae89493a747e86c5e06e2
David Nicholson <nickledave@users.noreply.github.com>
2024-05-01 09:57:45 -0400

    WIP: Add tests/test_config/test_dataset.py

---
+++ tests/test_config/test_dataset.py
@@ -0,0 +1,10 @@
+import pytest
+
+import vak.config.dataset
+
+class TestDatasetConfig:
+    def test_init(self):
+        assert False
+
+    def test_from_config_dict(self, config_dict):
+        assert False
