60f7b02f6b8ad007a1359b64a1f248f6787d9729
David Nicholson <nickledave@users.noreply.github.com>
2024-05-01 09:26:54 -0400

    Add ModelConfig class to config/model.py, add type annotations, fix config_from_toml_dict to look in specific section

--- src/vak/config/model.py
+++ src/vak/config/model.py
@@ -1,10 +1,12 @@
+"""Class representing the model table of a toml configuration file."""
 from __future__ import annotations
 
 import pathlib
 
-import toml
+from attrs import define, field
 
 from .. import models
+from . import parse, validators
 
 MODEL_TABLES = [
     "network",
@@ -14,7 +16,90 @@ MODEL_TABLES = [
 ]
 
 
-def config_from_toml_dict(toml_dict: dict, model_name: str) -> dict:
+@define
+class ModelConfig:
+    """Class representing the model table of a toml configuration file.
+
+    Attributes
+    ----------
+    name : str
+    network : dict
+        Keyword arguments for the network class,
+        or a :class:`dict` of ``dict``s mapping
+        network names to keyword arguments.
+    optimizer: dict
+        Keyword arguments for the optimizer class.
+    loss : dict
+        Keyword arguments for the class representing the loss function.
+    metrics: dict
+        A :class:`dict` of ``dict``s mapping
+        metric names to keyword arguments.
+    """
+    name: str
+    network: dict = field(validators=isinstance(dict))
+    optimizer: dict = field(validators=isinstance(dict))
+    loss: dict = field(validators=isinstance(dict))
+    metrics: dict = field(validators=isinstance(dict))
+
+    @classmethod
+    def from_config_dict(cls, config_dict: dict):
+        """Return :class:`ModelConfig` instance from a :class:`dict`.
+
+        The :class:`dict` passed in should be the one found
+        by loading a valid configuration toml file with
+        :func:`vak.config.parse.from_toml_path`,
+        and then using a top-level table key,
+        followed by key ``'model'``.
+        E.g., ``config_dict['train']['model']` or
+        ``config_dict['predict']['model']``.
+
+        Examples
+        --------
+        config_dict = vak.config.parse.from_toml_path(toml_path)
+        model_config = vak.config.Model.from_config_dict(config_dict['train'])
+        """
+        model_name = list(config_dict.keys())
+        if len(model_name) == 0:
+            raise ValueError(
+                "Did not find a single key in `config_dict` corresponding to model name. "
+                f"Instead found no keys. Config dict:\n{config_dict}\n"
+                "A configuration file should specify a single model per top-level table."
+            )
+        if len(model_name) > 1:
+            raise ValueError(
+                "Did not find a single key in `config_dict` corresponding to model name. "
+                f"Instead found multiple keys: {model_name}.\nConfig dict:\n{config_dict}.\n"
+                "A configuration file should specify a single model per top-level table."
+            )
+        model_name = model_name[0]
+        MODEL_NAMES = list(models.registry.MODEL_NAMES)
+        if model_name not in MODEL_NAMES:
+            raise ValueError(
+                f"Model name not found in registry: {model_name}\n"
+                f"Model names in registry:\n{MODEL_NAMES}"
+            )
+        model_config = config_dict[model_name]
+        if not all(
+            key in MODEL_TABLES for key in model_config.keys()
+        ):
+            invalid_keys = (
+                key for key in model_config.keys() if key not in MODEL_TABLES
+            )
+            raise ValueError(
+                f"The following sub-tables in the model config are not valid: {invalid_keys}\n"
+                f"Valid sub-table names are: {MODEL_TABLES}"
+            )
+        # for any tables not specified, default to empty dict so we can still use ``**`` operator on it
+        for model_table in MODEL_TABLES:
+            if model_table not in config_dict:
+                model_config[model_table] = {}
+        return cls(
+            name=model_name,
+            **model_config
+        )
+
+
+def config_from_toml_dict(toml_dict: dict, table: str, model_name: str) -> dict:
     """Get configuration for a model from a .toml configuration file
     loaded into a ``dict``.
 
@@ -39,9 +124,10 @@ def config_from_toml_dict(toml_dict: dict, model_name: str) -> dict:
         raise ValueError(
             f"Invalid model name: {model_name}.\nValid model names are: {models.registry.MODEL_NAMES}"
         )
+    validators.are_tables_valid(toml_dict)
 
     try:
-        model_config = toml_dict[model_name]
+        model_config = toml_dict[table][model_name]
     except KeyError as e:
         raise ValueError(
             f"A config section specifies the model name '{model_name}', "
@@ -58,7 +144,7 @@ def config_from_toml_dict(toml_dict: dict, model_name: str) -> dict:
 
 
 def config_from_toml_path(
-    toml_path: str | pathlib.Path, model_name: str
+    toml_path: str | pathlib.Path, table: str, model_name: str
 ) -> dict:
     """Get configuration for a model from a .toml configuration file,
     given the path to the file.
@@ -79,12 +165,5 @@ def config_from_toml_path(
         as loaded from a .toml file,
         and used by the model method ``from_config``.
     """
-    toml_path = pathlib.Path(toml_path)
-    if not toml_path.is_file():
-        raise FileNotFoundError(
-            f"File not found, or not recognized as a file: {toml_path}"
-        )
-
-    with toml_path.open("r") as fp:
-        config_dict = toml.load(fp)
-    return config_from_toml_dict(config_dict, model_name)
+    toml_dict = parse._load_toml_from_path(toml_path)
+    return config_from_toml_dict(toml_dict, model_name)
