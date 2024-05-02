249cf1665414ade6002932792c937d5c6950638a
David Nicholson <nickledave@users.noreply.github.com>
2024-05-01 21:37:16 -0400

    Remove spect_params attribute from Config in config/config.py, fix class' docstring

--- src/vak/config/config.py
+++ src/vak/config/config.py
@@ -5,33 +5,26 @@ from .eval import EvalConfig
 from .learncurve import LearncurveConfig
 from .predict import PredictConfig
 from .prep import PrepConfig
-from .spect_params import SpectParamsConfig
 from .train import TrainConfig
 
 
 @attr.s
 class Config:
-    """class to represent config.toml file
+    """Class that represents a configuration file.
 
     Attributes
     ----------
     prep : vak.config.prep.PrepConfig
-        represents ``[vak.prep]`` table of config.toml file
-    spect_params : vak.config.spect_params.SpectParamsConfig
-        represents ``[SPECT_PARAMS]`` table of config.toml file
+        Represents ``[vak.prep]`` table of config.toml file
     train : vak.config.train.TrainConfig
-        represents ``[vak.train]`` table of config.toml file
+        Represents ``[vak.train]`` table of config.toml file
     eval : vak.config.eval.EvalConfig
-        represents ``[vak.eval]`` table of config.toml file
+        Represents ``[vak.eval]`` table of config.toml file
     predict : vak.config.predict.PredictConfig
-        represents ``[vak.predict]`` table of config.toml file.
+        Represents ``[vak.predict]`` table of config.toml file.
     learncurve : vak.config.learncurve.LearncurveConfig
-        represents ``[vak.learncurve]`` table of config.toml file
+        Represents ``[vak.learncurve]`` table of config.toml file
     """
-
-    spect_params = attr.ib(
-        validator=instance_of(SpectParamsConfig), default=SpectParamsConfig()
-    )
     prep = attr.ib(validator=optional(instance_of(PrepConfig)), default=None)
     train = attr.ib(validator=optional(instance_of(TrainConfig)), default=None)
     eval = attr.ib(validator=optional(instance_of(EvalConfig)), default=None)
