ad1bbf3355c100c5d3e2b1f5218bdc1ad3a0b82c
David Nicholson <nickledave@users.noreply.github.com>
2024-05-01 21:36:35 -0400

    Use tomlkit in tests/test_config/test_validators.py

--- tests/test_config/test_validators.py
+++ tests/test_config/test_validators.py
@@ -1,5 +1,5 @@
 import pytest
-import toml
+import tomlkit
 
 import vak.config.validators
 
@@ -7,7 +7,7 @@ import vak.config.validators
 def test_are_sections_valid(invalid_section_config_path):
     """test that invalid section name raises a ValueError"""
     with invalid_section_config_path.open("r") as fp:
-        config_toml = toml.load(fp)
+        config_toml = tomlkit.load(fp)
     with pytest.raises(ValueError):
         vak.config.validators.are_sections_valid(
             config_toml, invalid_section_config_path
@@ -18,7 +18,7 @@ def test_are_options_valid(invalid_option_config_path):
     """test that section with an invalid option name raises a ValueError"""
     section_with_invalid_option = "PREP"
     with invalid_option_config_path.open("r") as fp:
-        config_toml = toml.load(fp)
+        config_toml = tomlkit.load(fp)
     with pytest.raises(ValueError):
         vak.config.validators.are_options_valid(
             config_toml, section_with_invalid_option, invalid_option_config_path
