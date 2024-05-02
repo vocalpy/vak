6654a0698fd4970d8cad1f1bca1014d92d499bc2
David Nicholson <nickledave@users.noreply.github.com>
2024-05-01 21:46:50 -0400

    Reorder attributes, fix typo in docstring of DatasetConfig

--- src/vak/config/dataset.py
+++ src/vak/config/dataset.py
@@ -13,28 +13,28 @@ class DatasetConfig:
 
     Attributes
     ----------
-    name : str, optional
-        Name of dataset. Only required for built-in datasets
-        from the :mod:`~vak.datasets` module.
     path : pathlib.Path
         Path to the directory that contains the dataset.
-        Equivalent to the `root` parameter of :module:`torchvision`
+        Equivalent to the `root` parameter of :mod:`torchvision`
         datasets.
     splits_path : pathlib.Path, optional
         Path to file representing splits.
+    name : str, optional
+        Name of dataset. Only required for built-in datasets
+        from the :mod:`~vak.datasets` module.
     """
     path: pathlib.Path = field(converter=pathlib.Path)
-    name: str | None = field(
-        converter=attr.converters.optional(str), default=None
-        )
     splits_path: pathlib.Path | None = field(
         converter=attr.converters.optional(pathlib.Path), default=None
+        )
+    name: str | None = field(
+        converter=attr.converters.optional(str), default=None
         )
 
     @classmethod
     def from_config_dict(cls, dict_: dict) -> DatasetConfig:
         return cls(
             path=dict_.get('path'),
+            splits_path=dict_.get('splits_path'),
             name=dict_.get('name'),
-            splits_path=dict_.get('splits_path')
         )
