d175436e2d6ecd2cffbf94c462e13501cfa8864a
David Nicholson <nickledave@users.noreply.github.com>
2024-05-02 16:40:01 -0400

    Make functions in config/parse.py into classmethods on Config class

--- src/vak/config/config.py
+++ src/vak/config/config.py
@@ -1,16 +1,64 @@
-import attr
+"""Class that represents the TOML configuration file used with the vak command-line interface."""
+from __future__ import annotations
+
+import pathlib
+
+from attrs import define, field
 from attr.validators import instance_of, optional
 
+from . import load
 from .eval import EvalConfig
 from .learncurve import LearncurveConfig
 from .predict import PredictConfig
 from .prep import PrepConfig
 from .train import TrainConfig
+from .validators import are_keys_valid, are_tables_valid
 
 
-@attr.s
+TABLE_CLASSES_MAP = {
+    "eval": EvalConfig,
+    "learncurve": LearncurveConfig,
+    "predict": PredictConfig,
+    "prep": PrepConfig,
+    "train": TrainConfig,
+}
+
+
+def _validate_tables_to_parse_arg_convert_list(tables_to_parse: str | list[str]) -> list[str]:
+    """Helper function used by :func:`from_toml` that
+    validates the ``tables_to_parse`` argument,
+    and returns it as a list of strings."""
+    if isinstance(tables_to_parse, str):
+        tables_to_parse = [tables_to_parse]
+
+    if not isinstance(tables_to_parse, list):
+        raise TypeError(
+            f"`tables_to_parse` should be a string or list of strings but type was: {type(tables_to_parse)}"
+        )
+
+    if not all(
+        [isinstance(table_name, str) for table_name in tables_to_parse]
+    ):
+        raise ValueError(
+            "All table names in 'tables_to_parse' should be strings"
+        )
+    if not all(
+        [
+            table_name in list(TABLE_CLASSES_MAP.keys())
+            for table_name in tables_to_parse
+        ]
+    ):
+        raise ValueError(
+            "All table names in 'tables_to_parse' should be valid names of tables. "
+            f"Values for 'tables were: {tables_to_parse}.\n"
+            f"Valid table names are: {list(TABLE_CLASSES_MAP.keys())}"
+        )
+    return tables_to_parse
+
+
+@define
 class Config:
-    """Class that represents a configuration file.
+    """Class that represents the TOML configuration file used with the vak command-line interface.
 
     Attributes
     ----------
@@ -25,12 +73,112 @@ class Config:
     learncurve : vak.config.learncurve.LearncurveConfig
         Represents ``[vak.learncurve]`` table of config.toml file
     """
-    prep = attr.ib(validator=optional(instance_of(PrepConfig)), default=None)
-    train = attr.ib(validator=optional(instance_of(TrainConfig)), default=None)
-    eval = attr.ib(validator=optional(instance_of(EvalConfig)), default=None)
-    predict = attr.ib(
+    prep = field(validator=optional(instance_of(PrepConfig)), default=None)
+    train = field(validator=optional(instance_of(TrainConfig)), default=None)
+    eval = field(validator=optional(instance_of(EvalConfig)), default=None)
+    predict = field(
         validator=optional(instance_of(PredictConfig)), default=None
     )
-    learncurve = attr.ib(
+    learncurve = field(
         validator=optional(instance_of(LearncurveConfig)), default=None
     )
+
+    @classmethod
+    def from_config_dict(
+            cls,
+            config_dict: dict,
+            toml_path: str | pathlib.Path | None = None,
+            tables_to_parse: str | list[str] | None = None
+            ) -> "Config":
+        """Return instance of :class:`Config` class,
+        given a :class:`dict` containing the contents of
+        a TOML configuration file.
+
+        This :func:`classmethod` expects the output
+        of :func:`vak.config.load._load_from_toml_path`,
+        that converts a :class:`tomlkit.TOMLDocument`
+        to a :class:`dict`.
+
+        Parameters
+        ----------
+        config_dict : dict
+            Python ``dict`` containing a .toml configuration file,
+            parsed by the ``toml`` library.
+        toml_path : str, pathlib.Path
+            path to a configuration file in TOML format. Default is None.
+            Not required, used only to make any error messages clearer.
+        tables_to_parse : str, list
+            Name of top-level table or tables from configuration
+            file that should be parsed. Can be a string
+            (single table) or list of strings (multiple
+            tables). Default is None,
+            in which case all are validated and parsed.
+
+        Returns
+        -------
+        config : vak.config.parse.Config
+            instance of :class:`Config` class,
+            whose attributes correspond to the
+            top-level tables in a config.toml file.
+        """
+        try:
+            config_dict = config_dict['vak']
+        except KeyError as e:
+            raise KeyError(
+                "Did not find key 'vak' in `config_dict`."
+                "All top-level tables in toml configuration file must "
+                "use dotted names that begin with 'vak', e.g. ``[vak.eval]``.\n"
+                f"`config_dict`:\n{config_dict}"
+            )
+        are_tables_valid(config_dict, toml_path)
+        if tables_to_parse is None:
+            tables_to_parse = list(
+                TABLE_CLASSES_MAP.keys()
+            )  # i.e., parse all tables
+        else:
+            tables_to_parse = _validate_tables_to_parse_arg_convert_list(tables_to_parse)
+
+        config_kwargs = {}
+        for table_name in tables_to_parse:
+            if table_name in config_dict:
+                are_keys_valid(config_dict, table_name, toml_path)
+                table_config_dict = config_dict[table_name]
+                config_kwargs[table_name] = TABLE_CLASSES_MAP[table_name].from_config_dict(table_config_dict)
+            else:
+                raise KeyError(
+                    f"A table specified in `tables_to_parse` was not found in the config: {table_name}"
+                )
+
+        return cls(**config_kwargs)
+
+    @classmethod
+    def from_toml_path(
+        cls,
+        toml_path: str | pathlib.Path,
+        tables_to_parse: list[str] | None = None
+    ) -> "Config":
+        """Return instance of :class:`Config` class,
+        given the path to a TOML configuration file.
+
+        Parameters
+        ----------
+        toml_path : str, pathlib.Path
+            Path to a configuration file in TOML format.
+            Parsed by ``toml`` library, then converted to an
+            instance of ``vak.config.parse.Config`` by
+            calling ``vak.parse.from_toml``
+        tables_to_parse : str, list
+            Name of table or tables from configuration
+            file that should be parsed. Can be a string
+            (single table) or list of strings (multiple
+            tables). Default is None,
+            in which case all are validated and parsed.
+
+        Returns
+        -------
+        config : vak.config.parse.Config
+            instance of :class:`Config` class, whose attributes correspond to
+            tables in a config.toml file.
+        """
+        config_dict: dict = load._load_toml_from_path(toml_path)
+        return cls.from_toml(config_dict, toml_path, tables_to_parse)
