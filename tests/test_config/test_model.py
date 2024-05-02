10823a53b2961abf41c63666f4136a876a3ac499
David Nicholson <nickledave@users.noreply.github.com>
2024-05-02 10:30:49 -0400

    Finish unit tests in tests/test_config/test_model.py

--- tests/test_config/test_model.py
+++ tests/test_config/test_model.py
@@ -1,62 +1,105 @@
-import copy
 import pytest
-
-from ..fixtures import (
-    ALL_GENERATED_CONFIGS_TOML,
-    ALL_GENERATED_CONFIGS_TOML_PATH_PAIRS
-)
 
 import vak.config.model
 
 
-def _make_expected_config(model_config: dict) -> dict:
-    for attr in vak.config.model.MODEL_TABLES:
-        if attr not in model_config:
-            model_config[attr] = {}
-    return model_config
-
-
-@pytest.mark.parametrize(
-    'toml_dict',
-    ALL_GENERATED_CONFIGS_TOML
-)
-def test_config_from_toml_dict(toml_dict):
-    for section_name in ('TRAIN', 'EVAL', 'LEARNCURVE', 'PREDICT'):
-        try:
-            section = toml_dict[section_name]
-        except KeyError:
-            continue
-    model_name = section['model']
-    # we need to copy so that we don't silently fail to detect mistakes
-    # by comparing a reference to the dict with itself
-    expected_model_config = copy.deepcopy(
-        toml_dict[model_name]
+class TestModelConfig:
+    @pytest.mark.parametrize(
+            'config_dict',
+            [
+                {
+                    'NonExistentModel': {
+                        'network': {},
+                        'optimizer': {},
+                        'loss': {},
+                        'metrics': {},
+                    }
+                },
+                {
+                    'TweetyNet': {
+                        'network': {},
+                        'optimizer': {'lr': 1e-3},
+                        'loss': {},
+                        'metrics': {},
+                    }
+                },
+            ]
     )
-    expected_model_config = _make_expected_config(expected_model_config)
+    def test_init(self, config_dict):
+        name=list(config_dict.keys())[0]
+        config_dict_from_name = config_dict[name]
 
-    model_config = vak.config.model.config_from_toml_dict(toml_dict, model_name)
+        model_config = vak.config.model.ModelConfig(
+            name=name,
+            **config_dict_from_name
+            )
 
-    assert model_config == expected_model_config
+        assert isinstance(model_config, vak.config.model.ModelConfig)
+        assert model_config.name == name
+        for key, val in config_dict_from_name.items():
+            assert hasattr(model_config, key)
+            assert getattr(model_config, key) == val
 
-
-@pytest.mark.parametrize(
-    'toml_dict, toml_path',
-    ALL_GENERATED_CONFIGS_TOML_PATH_PAIRS
-)
-def test_config_from_toml_path(toml_dict, toml_path):
-    for section_name in ('TRAIN', 'EVAL', 'LEARNCURVE', 'PREDICT'):
-        try:
-            section = toml_dict[section_name]
-        except KeyError:
-            continue
-    model_name = section['model']
-    # we need to copy so that we don't silently fail to detect mistakes
-    # by comparing a reference to the dict with itself
-    expected_model_config = copy.deepcopy(
-        toml_dict[model_name]
+    @pytest.mark.parametrize(
+            'config_dict',
+            [
+                {
+                    'TweetyNet': {
+                        'optimizer': {'lr': 1e-3},
+                    }
+                },
+                {
+                    'TweetyNet': {
+                        'network': {},
+                        'optimizer': {'lr': 1e-3},
+                        'loss': {},
+                        'metrics': {},
+                    }
+                },
+                {
+                    'ED_TCN': {
+                        'optimizer': {'lr': 1e-3},
+                    }
+                },
+                {
+                    "ConvEncoderUMAP": {
+                        "optimizer": 1e-3
+                    }
+                }
+            ]
     )
-    expected_model_config = _make_expected_config(expected_model_config)
+    def test_from_config_dict(self, config_dict):
+        model_config = vak.config.model.ModelConfig.from_config_dict(config_dict)
 
-    model_config = vak.config.model.config_from_toml_path(toml_path, model_name)
+        name=list(config_dict.keys())[0]
+        config_dict_from_name = config_dict[name]
+        assert model_config.name == name
+        for attr in ('network', 'optimizer', 'loss', 'metrics'):
+            assert hasattr(model_config, attr)
+            if attr in config_dict_from_name:
+                assert getattr(model_config, attr) == config_dict_from_name[attr]
+            else:
+                assert getattr(model_config, attr) == {}
 
-    assert model_config == expected_model_config
+    def test_from_config_dict_real_config(self, a_generated_config_dict):
+        config_dict = None
+        for table_name in ('train', 'eval', 'predict', 'learncurve'):
+            if table_name in a_generated_config_dict:
+                config_dict = a_generated_config_dict[table_name]['model']
+        if config_dict is None:
+            raise ValueError(
+                f"Didn't find top-level table for config: {a_generated_config_dict}"
+            )
+
+        model_config = vak.config.model.ModelConfig.from_config_dict(config_dict)
+
+        name=list(config_dict.keys())[0]
+        config_dict_from_name = config_dict[name]
+        assert model_config.name == name
+        for attr in ('network', 'optimizer', 'loss', 'metrics'):
+            assert hasattr(model_config, attr)
+            if attr in config_dict_from_name:
+                assert getattr(model_config, attr) == config_dict_from_name[attr]
+            else:
+                assert getattr(model_config, attr) == {}
+
