7c2a1f07783ef023d698bdb5e0b647a0d7bceceb
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 07:19:09 -0400

    Rewrite tests in tests/test_config/test_config.py

--- tests/test_config/test_config.py
+++ tests/test_config/test_config.py
@@ -1,26 +1,27 @@
 import vak.config
 
 
-def test_config_attrs_class(
-    all_generated_configs_toml_path_pairs,
-    default_model,
-):
-    """test that instantiating Config class works as expected"""
-    for config_toml, toml_path in all_generated_configs_toml_path_pairs:
-        if default_model not in str(toml_path):
-            continue  # only need to check configs for one model
-            # also avoids FileNotFoundError on CI
-        # this is basically the body of the ``config.parse.from_toml`` function.
-        config_dict = {}
-        for section_name in list(vak.config.parse.SECTION_CLASSES.keys()):
-            if section_name in config_toml:
-                vak.config.validators.are_options_valid(
-                    config_toml, section_name, toml_path
-                )
-                section = vak.config.parse.parse_config_section(
-                    config_toml, section_name, toml_path
-                )
-                config_dict[section_name.lower()] = section
+class TestConfig:
+    def test_from_config_dict_with_real_config(
+        a_generated_config_dict,
+    ):
+        """test that instantiating Config class works as expected"""
+        # this is basically the body of the ``config.load.from_toml`` function.
+        config_kwargs = {}
+        for table_name in a_generated_config_dict:
+            config_kwargs[table_name] = vak.config.load.TABLE_CLASSES_MAP[table_name].from_config_dict(
+                a_generated_config_dict[table_name]
+            )
 
-        config = vak.config.parse.Config(**config_dict)
-        assert isinstance(config, vak.config.parse.Config)
+        config = vak.config.load.Config(**config_kwargs)
+
+        assert isinstance(config, vak.config.load.Config)
+        # we already test that config loading works for EvalConfig, et al.,
+        # so here we just test that the logic of Config works as expected:
+        # we should get an attribute for each top-level table that we pass in;
+        # if we don't pass one in, then its corresponding attribute should be None
+        for table_name in ('eval', 'learncurve', 'predict', 'prep', 'train'):
+            if table_name in a_generated_config_dict:
+                assert hasattr(config, table_name)
+            else:
+                assert getattr(config, table_name) is None
