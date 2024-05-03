ffd0a4ea5c2528954b78410ecf1a1c33162f8dd9
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 10:04:42 -0400

    Fix/add unit tests in tests/fixtures/config.py

--- tests/test_config/test_config.py
+++ tests/test_config/test_config.py
@@ -4,16 +4,32 @@ import vak.config
 
 
 class TestConfig:
-    def test_from_config_dict_with_real_config(
-        self, a_generated_config_dict,
+    @pytest.mark.parametrize(
+            'tables_to_parse',
+            [
+                None,
+                'prep',
+                ['prep'],
+            ]
+    )
+    def test_init_with_real_config(
+        self, a_generated_config_dict, tables_to_parse
     ):
-        """test that instantiating Config class works as expected"""
-        # this is basically the body of the ``config.load.from_toml`` function.
+        """Test that instantiating Config class works as expected"""
+        # this is basically the body of the ``Config.from_config_dict`` function.
         config_kwargs = {}
-        for table_name in a_generated_config_dict:
-            config_kwargs[table_name] = vak.config.config.TABLE_CLASSES_MAP[table_name].from_config_dict(
-                a_generated_config_dict[table_name]
-            )
+
+        if tables_to_parse is None:
+            for table_name in a_generated_config_dict:
+                config_kwargs[table_name] = vak.config.config.TABLE_CLASSES_MAP[table_name].from_config_dict(
+                    a_generated_config_dict[table_name]
+                )
+        else:
+            for table_name in a_generated_config_dict:
+                if table_name in tables_to_parse:
+                    config_kwargs[table_name] = vak.config.config.TABLE_CLASSES_MAP[table_name].from_config_dict(
+                        a_generated_config_dict[table_name]
+                    )
 
         config = vak.config.Config(**config_kwargs)
 
@@ -22,15 +38,76 @@ class TestConfig:
         # so here we just test that the logic of Config works as expected:
         # we should get an attribute for each top-level table that we pass in;
         # if we don't pass one in, then its corresponding attribute should be None
-        for table_name in ('eval', 'learncurve', 'predict', 'prep', 'train'):
-            if table_name in a_generated_config_dict:
-                assert hasattr(config, table_name)
+        for attr in ('eval', 'learncurve', 'predict', 'prep', 'train'):
+            if tables_to_parse is not None:
+                if attr in a_generated_config_dict and attr in tables_to_parse:
+                    assert hasattr(config, attr)
+                else:
+                    assert getattr(config, attr) is None
             else:
-                assert getattr(config, table_name) is None
+                if attr in a_generated_config_dict:
+                    assert hasattr(config, attr)
 
-    def test_from_toml_path(self, a_generated_config_path):
-        config_toml = vak.config.load._load_toml_from_path(a_generated_config_path)
-        assert isinstance(config_toml, dict)
+    @pytest.mark.parametrize(
+            'tables_to_parse',
+            [
+                None,
+                'prep',
+                ['prep'],
+            ]
+    )
+    def test_from_config_dict_with_real_config(
+        self, a_generated_config_dict, tables_to_parse
+    ):
+        """Test :meth:`Config.from_config_dict`"""
+        config = vak.config.Config.from_config_dict(
+            a_generated_config_dict, tables_to_parse=tables_to_parse
+            )
+
+        assert isinstance(config, vak.config.Config)
+        # we already test that config loading works for EvalConfig, et al.,
+        # so here we just test that the logic of Config works as expected:
+        # we should get an attribute for each top-level table that we pass in;
+        # if we don't pass one in, then its corresponding attribute should be None
+        for attr in ('eval', 'learncurve', 'predict', 'prep', 'train'):
+            if tables_to_parse is not None:
+                if attr in a_generated_config_dict and attr in tables_to_parse:
+                    assert hasattr(config, attr)
+                else:
+                    assert getattr(config, attr) is None
+            else:
+                if attr in a_generated_config_dict:
+                    assert hasattr(config, attr)
+
+    @pytest.mark.parametrize(
+            'tables_to_parse',
+            [
+                None,
+                'prep',
+                ['prep'],
+            ]
+    )
+    def test_from_toml_path(self, a_generated_config_path, tables_to_parse):
+        config = vak.config.Config.from_toml_path(
+            a_generated_config_path, tables_to_parse=tables_to_parse
+            )
+
+        assert isinstance(config, vak.config.Config)
+
+        a_generated_config_dict = vak.config.load._load_toml_from_path(a_generated_config_path)
+        # we already test that config loading works for EvalConfig, et al.,
+        # so here we just test that the logic of Config works as expected:
+        # we should get an attribute for each top-level table that we pass in;
+        # if we don't pass one in, then its corresponding attribute should be None
+        for attr in ('eval', 'learncurve', 'predict', 'prep', 'train'):
+            if tables_to_parse is not None:
+                if attr in a_generated_config_dict and attr in tables_to_parse:
+                    assert hasattr(config, attr)
+                else:
+                    assert getattr(config, attr) is None
+            else:
+                if attr in a_generated_config_dict:
+                    assert hasattr(config, attr)
 
     def test_from_toml_path_raises_when_config_doesnt_exist(self, config_that_doesnt_exist):
         with pytest.raises(FileNotFoundError):
