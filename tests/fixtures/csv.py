2ed3763db3253cd5785fa413b673e71b09e21e69
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 13:35:11 -0400

    Fix how we get 'path' from 'dataset' table in configs, in tests/fixtures/csv.py

--- tests/fixtures/csv.py
+++ tests/fixtures/csv.py
@@ -25,11 +25,11 @@ def specific_dataset_csv_path(specific_config_toml):
         config_toml = specific_config_toml(
             config_type, model, annot_format, audio_format, spect_format
         )
-        dataset_path = Path(config_toml[config_type.upper()]["dataset_path"])
+        dataset_path = Path(config_toml[config_type]["dataset"]["path"])
         # TODO: make this more general -- dataset registry?
-        if config_toml['PREP']['dataset_type'] == 'frame classification':
+        if config_toml['prep']['dataset_type'] == 'frame classification':
             metadata = vak.datasets.frame_classification.Metadata.from_dataset_path(dataset_path)
-        elif config_toml['PREP']['dataset_type'] == 'parametric umap':
+        elif config_toml['prep']['dataset_type'] == 'parametric umap':
             metadata = vak.datasets.parametric_umap.Metadata.from_dataset_path(dataset_path)
         dataset_csv_path = dataset_path / metadata.dataset_csv_filename
         return dataset_csv_path
