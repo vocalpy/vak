d96d7827c7caaaa4f1681ec2ded718101df8bf1c
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 07:45:49 -0400

    Add unit test for exceptions in tests/test_config/test_learncurve.py

--- tests/test_config/test_learncurve.py
+++ tests/test_config/test_learncurve.py
@@ -108,3 +108,69 @@ class TestLearncurveConfig:
         )
 
         assert isinstance(config, vak.config.learncurve.LearncurveConfig)
+
+    @pytest.mark.parametrize(
+            'config_dict, expected_exception',
+            [
+                # missing 'model', should raise KeyError
+                (
+                    {
+                        'normalize_spectrograms': True,
+                        'batch_size': 11,
+                        'num_epochs': 2,
+                        'val_step': 50,
+                        'ckpt_step': 200,
+                        'patience': 4,
+                        'num_workers': 16,
+                        'device': 'cuda',
+                        'root_results_dir': './tests/data_for_tests/generated/results/train/audio_cbin_annot_notmat/TweetyNet',
+                        'post_tfm_kwargs': {'majority_vote': True, 'min_segment_dur': 0.02},
+                        'train_dataset_params': {'window_size': 88},
+                        'val_transform_params': {'window_size': 88},
+                        'dataset': {
+                            'path': 'tests/data_for_tests/generated/prep/train/audio_cbin_annot_notmat/TweetyNet/032312-vak-frame-classification-dataset-generated-240502_234819'
+                        }
+                    },
+                    KeyError
+                ),
+                # missing 'dataset', should raise KeyError
+               (
+                    {
+                        'normalize_spectrograms': True,
+                        'batch_size': 11,
+                        'num_epochs': 2,
+                        'val_step': 50,
+                        'ckpt_step': 200,
+                        'patience': 4,
+                        'num_workers': 16,
+                        'device': 'cuda',
+                        'root_results_dir': './tests/data_for_tests/generated/results/train/audio_cbin_annot_notmat/TweetyNet',
+                        'post_tfm_kwargs': {'majority_vote': True, 'min_segment_dur': 0.02},
+                        'train_dataset_params': {'window_size': 88},
+                        'val_transform_params': {'window_size': 88},
+                        'model': {
+                            'TweetyNet': {
+                                'network': {
+                                    'conv1_filters': 8,
+                                    'conv1_kernel_size': [3, 3],
+                                    'conv2_filters': 16,
+                                    'conv2_kernel_size': [5, 5],
+                                    'pool1_size': [4, 1],
+                                    'pool1_stride': [4, 1],
+                                    'pool2_size': [4, 1],
+                                    'pool2_stride': [4, 1],
+                                    'hidden_size': 32
+                                },
+                                'optimizer': {
+                                    'lr': 0.001
+                                }
+                            }
+                        },
+                    },
+                    KeyError
+                ),
+            ]
+    )
+    def test_from_config_dict_raises(self, config_dict, expected_exception):
+        with pytest.raises(expected_exception):
+            vak.config.LearncurveConfig.from_config_dict(config_dict)
