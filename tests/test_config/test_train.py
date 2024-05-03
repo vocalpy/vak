3927c5b914d89751b14f8f2d391577b4d9b6c85d
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 07:46:02 -0400

    Add unit test for exceptions in tests/test_config/test_train.py

--- tests/test_config/test_train.py
+++ tests/test_config/test_train.py
@@ -103,3 +103,65 @@ class TestTrainConfig:
         train_config = vak.config.train.TrainConfig.from_config_dict(train_table)
 
         assert isinstance(train_config, vak.config.train.TrainConfig)
+
+    @pytest.mark.parametrize(
+            'config_dict',
+            [
+                (
+                    {
+                        'normalize_spectrograms': True,
+                        'batch_size': 11,
+                        'num_epochs': 2,
+                        'val_step': 50,
+                        'ckpt_step': 200,
+                        'patience': 4,
+                        'num_workers': 16,
+                        'device': 'cuda',
+                        'root_results_dir': './tests/data_for_tests/generated/results/train/audio_cbin_annot_notmat/TweetyNet',
+                        'train_dataset_params': {'window_size': 88},
+                        'val_transform_params': {'window_size': 88},
+                        'dataset': {
+                            'path': 'tests/data_for_tests/generated/prep/train/audio_cbin_annot_notmat/TweetyNet/032312-vak-frame-classification-dataset-generated-240502_234819'
+                        }
+                    },
+                    KeyError
+                ),
+                (
+                    {
+                        'normalize_spectrograms': True,
+                        'batch_size': 11,
+                        'num_epochs': 2,
+                        'val_step': 50,
+                        'ckpt_step': 200,
+                        'patience': 4,
+                        'num_workers': 16,
+                        'device': 'cuda',
+                        'root_results_dir': './tests/data_for_tests/generated/results/train/audio_cbin_annot_notmat/TweetyNet',
+                        'train_dataset_params': {'window_size': 88},
+                        'val_transform_params': {'window_size': 88},
+                        'model': {
+                            'TweetyNet': {
+                                'network': {
+                                    'conv1_filters': 8,
+                                    'conv1_kernel_size': [3, 3],
+                                    'conv2_filters': 16,
+                                    'conv2_kernel_size': [5, 5],
+                                    'pool1_size': [4, 1],
+                                    'pool1_stride': [4, 1],
+                                    'pool2_size': [4, 1],
+                                    'pool2_stride': [4, 1],
+                                    'hidden_size': 32
+                                },
+                                'optimizer': {
+                                    'lr': 0.001
+                                }
+                            }
+                        },
+                    },
+                    KeyError
+                )
+            ]
+    )
+    def test_from_config_dict_raises(self, config_dict, expected_exception):
+        with pytest.raises(expected_exception):
+            vak.config.TrainConfig.from_config_dict(config_dict)
