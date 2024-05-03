8e2cc9f1ba5de57e663be7c0fb06acfb65aeee99
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 08:33:24 -0400

    Add two unit tests that PrepConfig raises expected exceptions

--- tests/test_config/test_prep.py
+++ tests/test_config/test_prep.py
@@ -8,39 +8,39 @@ import vak.config.prep
 
 class TestPrepConfig:
     @pytest.mark.parametrize(
-            'config_dict',
-            [
-                {
-                    'annot_format': 'notmat',
-                    'audio_format': 'cbin',
-                    'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032412',
-                    'dataset_type': 'parametric umap',
-                    'input_type': 'spect',
-                    'labelset': 'iabcdefghjk',
-                    'output_dir': './tests/data_for_tests/generated/prep/eval/audio_cbin_annot_notmat/ConvEncoderUMAP',
-                    'spect_params': {'fft_size': 512,
-                                    'step_size': 32,
-                                    'transform_type': 'log_spect_plus_one'},
-                    'test_dur': 0.2
-                },
-                {
-                    'annot_format': 'notmat',
-                    'audio_format': 'cbin',
-                    'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032312',
-                    'dataset_type': 'frame classification',
-                    'input_type': 'spect',
-                    'labelset': 'iabcdefghjk',
-                    'output_dir': './tests/data_for_tests/generated/prep/train/audio_cbin_annot_notmat/TweetyNet',
-                    'spect_params': {'fft_size': 512,
-                                    'freq_cutoffs': [500, 10000],
-                                    'step_size': 64,
-                                    'thresh': 6.25,
-                                    'transform_type': 'log_spect'},
-                    'test_dur': 30,
-                    'train_dur': 50,
-                    'val_dur': 15
-                },
-            ]
+        'config_dict',
+        [
+            {
+                'annot_format': 'notmat',
+                'audio_format': 'cbin',
+                'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032412',
+                'dataset_type': 'parametric umap',
+                'input_type': 'spect',
+                'labelset': 'iabcdefghjk',
+                'output_dir': './tests/data_for_tests/generated/prep/eval/audio_cbin_annot_notmat/ConvEncoderUMAP',
+                'spect_params': {'fft_size': 512,
+                                'step_size': 32,
+                                'transform_type': 'log_spect_plus_one'},
+                'test_dur': 0.2
+            },
+            {
+                'annot_format': 'notmat',
+                'audio_format': 'cbin',
+                'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032312',
+                'dataset_type': 'frame classification',
+                'input_type': 'spect',
+                'labelset': 'iabcdefghjk',
+                'output_dir': './tests/data_for_tests/generated/prep/train/audio_cbin_annot_notmat/TweetyNet',
+                'spect_params': {'fft_size': 512,
+                                'freq_cutoffs': [500, 10000],
+                                'step_size': 64,
+                                'thresh': 6.25,
+                                'transform_type': 'log_spect'},
+                'test_dur': 30,
+                'train_dur': 50,
+                'val_dur': 15
+            },
+        ]
     )
     def test_init(self, config_dict):
         config_dict['spect_params'] = vak.config.SpectParamsConfig(**config_dict['spect_params'])
@@ -56,6 +56,62 @@ class TestPrepConfig:
                 assert getattr(prep_config, key) == vak.common.converters.labelset_to_set(val)
             else:
                 assert getattr(prep_config, key) == val
+
+    @pytest.mark.parametrize(
+        'config_dict',
+            [
+                {
+                    'annot_format': 'notmat',
+                    'audio_format': 'cbin',
+                    'spect_format': 'mat',
+                    'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032412',
+                    'dataset_type': 'parametric umap',
+                    'input_type': 'spect',
+                    'labelset': 'iabcdefghjk',
+                    'output_dir': './tests/data_for_tests/generated/prep/eval/audio_cbin_annot_notmat/ConvEncoderUMAP',
+                    'spect_params': {'fft_size': 512,
+                                    'step_size': 32,
+                                    'transform_type': 'log_spect_plus_one'},
+                    'test_dur': 0.2
+                },
+        ]
+    )
+    def test_both_audio_and_spect_format_raises(
+        self, config_dict,
+    ):
+        """test that a config with both an audio and a spect format raises a ValueError"""
+        # need to do this set-up so we don't mask one error with another
+        config_dict['spect_params'] = vak.config.SpectParamsConfig(**config_dict['spect_params'])
+
+        with pytest.raises(ValueError):
+            prep_config = vak.config.PrepConfig(**config_dict)
+
+    @pytest.mark.parametrize(
+        'config_dict',
+            [
+                {
+                    'annot_format': 'notmat',
+                    'data_dir': './tests/data_for_tests/source/audio_cbin_annot_notmat/gy6or6/032412',
+                    'dataset_type': 'parametric umap',
+                    'input_type': 'spect',
+                    'labelset': 'iabcdefghjk',
+                    'output_dir': './tests/data_for_tests/generated/prep/eval/audio_cbin_annot_notmat/ConvEncoderUMAP',
+                    'spect_params': {'fft_size': 512,
+                                    'step_size': 32,
+                                    'transform_type': 'log_spect_plus_one'},
+                    'test_dur': 0.2
+                },
+        ]
+    )
+    def test_neither_audio_nor_spect_format_raises(
+        self, config_dict
+    ):
+        """test that a config without either an audio or a spect format raises a ValueError"""
+        # need to do this set-up so we don't mask one error with another
+        config_dict['spect_params'] = vak.config.SpectParamsConfig(**config_dict['spect_params'])
+
+        with pytest.raises(ValueError):
+            prep_config = vak.config.PrepConfig(**config_dict)
 
     @pytest.mark.parametrize(
             'config_dict',
