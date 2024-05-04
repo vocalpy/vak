0738b7700c30b4189f6ec469b2f1dbb67002bbd3
David Nicholson <nickledave@users.noreply.github.com>
2024-05-03 22:26:58 -0400

    Add unit tests for ModelConfig.asdict

--- tests/test_config/test_model.py
+++ tests/test_config/test_model.py
@@ -105,6 +105,51 @@ class TestModelConfig:
                 assert getattr(model_config, attr) == {}
 
     @pytest.mark.parametrize(
+            'config_dict',
+            [
+                {
+                    'TweetyNet': {
+                        'optimizer': {'lr': 1e-3},
+                    }
+                },
+                {
+                    'TweetyNet': {
+                        'network': {},
+                        'optimizer': {'lr': 1e-3},
+                        'loss': {},
+                        'metrics': {},
+                    }
+                },
+                {
+                    'ED_TCN': {
+                        'optimizer': {'lr': 1e-3},
+                    }
+                },
+                {
+                    "ConvEncoderUMAP": {
+                        "optimizer": 1e-3
+                    }
+                }
+            ]
+    )
+    def test_asdict(self, config_dict):
+        model_config = vak.config.model.ModelConfig.from_config_dict(config_dict)
+
+        model_config_as_dict = model_config.asdict()
+
+        assert isinstance(model_config_as_dict, dict)
+
+        model_name = list(config_dict.keys())[0]
+        for key in ('name', 'network', 'optimizer', 'loss', 'metrics'):
+            if key == 'name':
+                assert model_config_as_dict[key] == model_name
+            else:
+                if key in config_dict[model_name]:
+                    assert model_config_as_dict[key] == config_dict[model_name][key]
+                else:
+                    assert model_config_as_dict[key] == {}
+
+    @pytest.mark.parametrize(
             'config_dict, expected_exception',
             [
                 # Raises ValueError because model name not in registry
