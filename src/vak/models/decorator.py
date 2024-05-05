a2d63f7d0f670d796ff3bbf8691f2f244e547bf3
David Nicholson <nickledave@users.noreply.github.com>
2024-05-05 16:47:22 -0400

    Change model decorator to not subclass family, and to instead subclass Model, then make a new instance of the subclass, add that instance to the registry, and then return the instance. This makes it possible to call the 'from_config' method of the instance and get a new class instance. Think of the instance as a Singleton

--- src/vak/models/decorator.py
+++ src/vak/models/decorator.py
@@ -13,6 +13,8 @@ from __future__ import annotations
 
 from typing import Type
 
+import lightning
+
 from .base import Model
 from .definition import validate as validate_definition
 from .registry import register_model
@@ -28,7 +30,7 @@ class ModelDefinitionValidationError(Exception):
     pass
 
 
-def model(family: Type[Model]):
+def model(family: lightning.pytorch.LightningModule):
     """Decorator that makes a model class,
     given a definition of the model,
     and another class that represents a
@@ -46,7 +48,11 @@ def model(family: Type[Model]):
         A class with all the class variables required
         by :func:`vak.models.definition.validate`.
         See docstring of that function for specification.
-    family : subclass of vak.models.Model
+        See also :class:`vak.models.definition.ModelDefinition`,
+        but note that it is not necessary to subclass
+        :class:`~vak.models.definition.ModelDefinition` to
+        define a model.
+    family : lightning.pytorch.LightningModule
         The class representing the family of models
         that the new model will belong to.
         E.g., :class:`vak.models.FrameClassificationModel`.
@@ -54,20 +60,23 @@ def model(family: Type[Model]):
     Returns
     -------
     model : type
-        A sub-class of ``model_family``,
-        with attribute ``definition``,
+        A sub-class of :class:`~vak.models.base.Model`,
+        with attribute ``definition``
+        and ``family``,
         that will be used when making
-        new instances of the model.
+        new instances of the model
+        by calling the
+        :meth:`~vak.models.base.Model.from_config` method.
     """
 
     def _model(definition: Type):
-        if not issubclass(family, Model):
+        if not issubclass(family, lightning.pytorch.LightningModule):
             raise TypeError(
                 "The ``family`` argument to the ``vak.models.model`` decorator"
-                "should be a subclass of ``vak.models.base.Model``,"
+                "should be a subclass of ``lightning.pytorch.LightningModule``,"
                 f"but the type was: {type(family)}, "
                 "which was not recognized as a subclass "
-                "of ``vak.models.base.Model``."
+                "of ``lightning.pytorch.LightningModule``."
             )
 
         try:
@@ -83,13 +92,15 @@ def model(family: Type[Model]):
 
         attributes = dict(family.__dict__)
         attributes.update({"definition": definition})
+        attributes.update({"family": family})
         subclass_name = definition.__name__
-        subclass = type(subclass_name, (family,), attributes)
+        subclass = type(subclass_name, (Model,), attributes)
         subclass.__module__ = definition.__module__
 
+        instance = subclass()
         # finally, add model to registry
-        register_model(subclass)
+        register_model(instance)
 
-        return subclass
+        return instance
 
     return _model
