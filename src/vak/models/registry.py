84a9de63af4bd00a0ff65ac05668107d170fc09f
David Nicholson <nickledave@users.noreply.github.com>
2024-05-05 22:10:42 -0400

    Fix how we add a Model instance to MODEL_REGISTRY

--- src/vak/models/registry.py
+++ src/vak/models/registry.py
@@ -7,21 +7,24 @@ with a decorator, so that the model can be used at runtime.
 from __future__ import annotations
 
 import inspect
-from typing import Any, Type
+from typing import Any, Type, TYPE_CHECKING
 
 import lightning
+
+if TYPE_CHECKING:
+    from .base import Model
 
 MODEL_FAMILY_REGISTRY = {}
 
 
 def model_family(family_class: Type) -> None:
     """Decorator that adds a class to the registry of model families."""
-    if not issubclass(family_class, lightning.pytorch.LightningModule):
+    if not issubclass(family_class, lightning.LightningModule):
         raise TypeError(
             "The ``family_class`` provided to the `vak.models.model_family` decorator"
-            "must be a subclass of `lightning.pytorch.LightningModule`, "
+            "must be a subclass of `lightning.LightningModule`, "
             f"but the class specified is not: {family_class}. "
-            f"Subclasses of `lightning.pytorch.LightningModule` are: {lightning.pytorch.LightningModule.__subclasses__()}"
+            f"Subclasses of `lightning.LightningModule` are: {lightning.LightningModule.__subclasses__()}"
         )
 
     model_family_name = family_class.__name__
@@ -40,7 +43,7 @@ def model_family(family_class: Type) -> None:
 MODEL_REGISTRY = {}
 
 
-def register_model(model_class: Type) -> Type:
+def register_model(model: Model) -> Model:
     """Decorator that registers a model in the model registry.
 
     This function is called by :func:`vak.models.decorator.model`,
@@ -49,28 +52,27 @@ def register_model(model_class: Type) -> Type:
     and should prefer to use :func:`vak.models.decorator.model` instead.
     """
     model_family_classes = list(MODEL_FAMILY_REGISTRY.values())
-    model_parent_class = inspect.getmro(model_class)[1]
-    if model_parent_class not in model_family_classes:
+    model_family = model.family
+    if model_family not in model_family_classes:
         raise TypeError(
-            "The parent class of ``model_class`` passed to the ``model`` decorator "
-            f"is not recognized as a model family. Class was: {model_class} and "
-            f"parent is {model_parent_class}, as determined with "
-            f"``inspect.getmro(model_class)[1]``. "
-            f"Please specify a class that is a sub-class of a model family. "
+            "The family of `model` passed to the `register_model` decorator "
+            f"is not recognized as a model family. Class was '{model}' and "
+            f"its family is '{model_family}'. "
+            f"Please specify a valid model family. "
             f"Valid model family classes are: {model_family_classes}"
         )
 
-    model_name = model_class.__name__
+    model_name = model.__name__
     if model_name in MODEL_REGISTRY:
         raise ValueError(
             f"Attempted to register a model family with the name '{model_name}', "
             f"but this name is already in the registry.\n"
         )
 
-    MODEL_REGISTRY[model_name] = model_class
+    MODEL_REGISTRY[model_name] = model
     # need to return class after we register it or we replace it with None
     # when this function is used as a decorator
-    return model_class
+    return model
 
 
 def __getattr__(name: str) -> Any:
