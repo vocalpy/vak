9ca7c7e738e87f47f087cb36de930654708ead9c
David Nicholson <nickledave@users.noreply.github.com>
2024-05-06 10:49:01 -0400

    Rewrite docstring, fix type annotations, rename vars for clarity in src/vak/models/decorator.py

--- src/vak/models/decorator.py
+++ src/vak/models/decorator.py
@@ -1,23 +1,25 @@
-"""Decorator that makes a model class,
+"""Decorator that makes a :class:`vak.models.ModelFactory`,
 given a definition of the model,
-and another class that represents a
+and a :class:`lightning.LightningModule` that represents a
 family of models that the new model belongs to.
 
-The function returns a newly-created subclass
-of the class representing the family of models.
-The subclass can then be instantiated
-and have all model methods.
+The function returns a new instance of :class:`vak.models.ModelFactory`,
+that can create new instances of the model with its
+:meth:`~:class:`vak.models.ModelFactory.from_config` and
+:meth:`~:class:`vak.models.ModelFactory.from_instances` methods.
 """
 
 from __future__ import annotations
 
-from typing import Type
+from typing import Type, TYPE_CHECKING
 
 import lightning
 
 from .definition import validate as validate_definition
 from .registry import register_model
 
+if TYPE_CHECKING:
+    from .factory import ModelFactory
 
 class ModelDefinitionValidationError(Exception):
     """Exception raised when validating a model
@@ -30,15 +32,15 @@ class ModelDefinitionValidationError(Exception):
 
 
 def model(family: lightning.pytorch.LightningModule):
-    """Decorator that makes a model class,
+    """Decorator that makes a :class:`vak.models.ModelFactory`,
     given a definition of the model,
-    and another class that represents a
+    and a :class:`lightning.LightningModule` that represents a
     family of models that the new model belongs to.
 
-    Returns a newly-created subclass
-    of the class representing the family of models.
-    The subclass can then be instantiated
-    and have all model methods.
+    The function returns a new instance of :class:`vak.models.ModelFactory`,
+    that can create new instances of the model with its
+    :meth:`~:class:`vak.models.ModelFactory.from_config` and
+    :meth:`~:class:`vak.models.ModelFactory.from_instances` methods.
 
     Parameters
     ----------
@@ -51,34 +53,36 @@ def model(family: lightning.pytorch.LightningModule):
         but note that it is not necessary to subclass
         :class:`~vak.models.definition.ModelDefinition` to
         define a model.
-    family : lightning.pytorch.LightningModule
+    family : lightning.LightningModule
         The class representing the family of models
         that the new model will belong to.
         E.g., :class:`vak.models.FrameClassificationModel`.
+        Should be a subclass of :class:`lightning.LightningModule`
+        that was registered with the
+        :func:`vak.models.registry.model_family` decorator.
 
     Returns
     -------
-    model : type
-        A sub-class of :class:`~vak.models.base.Model`,
-        with attribute ``definition``
-        and ``family``,
+    model_factory : vak.models.ModelFactory
+        An instance of :class:`~vak.models.ModelFactory`,
+        with attribute ``definition`` and ``family``,
         that will be used when making
-        new instances of the model
-        by calling the
-        :meth:`~vak.models.base.Model.from_config` method.
+        new instances of the model by calling the
+        :meth:`~vak.models.ModelFactory.from_config` method
+        or the :meth:`~:class:`vak.models.ModelFactory.from_instances` method.
     """
 
-    def _model(definition: Type):
+    def _model(definition: Type) -> ModelFactory:
         from .factory import ModelFactory  # avoid circular import
 
-        model = ModelFactory(
+        model_factory = ModelFactory(
             definition,
             family
         )
-        model.__name__ = definition.__name__
-        model.__doc__ = definition.__doc__
-        model.__module__ = definition.__module__
-        register_model(model)
-        return model
+        model_factory.__name__ = definition.__name__
+        model_factory.__doc__ = definition.__doc__
+        model_factory.__module__ = definition.__module__
+        register_model(model_factory)
+        return model_factory
 
     return _model
