57ce5b4b6ffc14329fa7d4bc354a43e8f17b96e7
David Nicholson <nickledave@users.noreply.github.com>
2024-05-06 11:38:28 -0400

    Fix unit test in tests/test_models/test_convencoder_umap.py

--- tests/test_models/test_convencoder_umap.py
+++ tests/test_models/test_convencoder_umap.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 
 import vak
 
@@ -22,7 +23,7 @@ class TestConvEncoderUMAP:
             attr_from_definition = getattr(vak.models.convencoder_umap.ConvEncoderUMAP.definition, attr)
             if isinstance(attr_from_definition, dict):
                 attr_from_model = getattr(model, attr)
-                assert isinstance(attr_from_model, dict)
+                assert isinstance(attr_from_model, (dict, torch.nn.ModuleDict))
                 assert attr_from_model.keys() == attr_from_definition.keys()
                 for net_name, net_instance in attr_from_model.items():
                     assert isinstance(net_instance, attr_from_definition[net_name])
