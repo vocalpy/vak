855e0cf1475f8cbdc1435248fb1a26c893c5ded4
David Nicholson <nickledave@users.noreply.github.com>
2024-05-06 14:42:08 -0400

    Rewrite transforms.defaults.frames_classification.EvalItemTransform and PredictItemTransform as a single class, InferItemTransform, and remname spect_standardizer -> frames_standardizer in that module

--- src/vak/transforms/defaults/frame_classification.py
+++ src/vak/transforms/defaults/frame_classification.py
@@ -13,9 +13,11 @@ from __future__ import annotations
 
 from typing import Callable
 
+import torch
 import torchvision.transforms
 
 from .. import transforms as vak_transforms
+from ..transforms import FramesStandardizer
 
 
 class TrainItemTransform:
@@ -23,14 +25,14 @@ class TrainItemTransform:
 
     def __init__(
         self,
-        spect_standardizer=None,
+        frames_standardizer: FramesStandardizer | None = None,
     ):
-        if spect_standardizer is not None:
-            if isinstance(spect_standardizer, vak_transforms.FramesStandardizer):
-                frames_transform = [spect_standardizer]
+        if frames_standardizer is not None:
+            if isinstance(frames_standardizer, vak_transforms.FramesStandardizer):
+                frames_transform = [frames_standardizer]
             else:
                 raise TypeError(
-                    f"invalid type for spect_standardizer: {type(spect_standardizer)}. "
+                    f"invalid type for frames_standardizer: {type(frames_standardizer)}. "
                     "Should be an instance of vak.transforms.StandardizeSpect"
                 )
         else:
@@ -61,110 +63,58 @@ class TrainItemTransform:
         return item
 
 
-class EvalItemTransform:
-    """Default transform used when evaluating frame classification models.
+class InferItemTransform:
+    """Default transform used when running inference on frame classification models,
+    for evaluation or to generate new predictions.
 
-    Returned item includes "source" spectrogram reshaped into a stack of windows,
-    with padded added to make reshaping possible, and annotation also padded and
-    reshaped.
-    If return_padding_mask is True, item includes 'padding_mask' that
-    can be used to crop off any predictions made on the padding.
-    """
-
-    def __init__(
-        self,
-        window_size,
-        spect_standardizer=None,
-        padval=0.0,
-        return_padding_mask=True,
-        channel_dim=1,
-    ):
-        if spect_standardizer is not None:
-            if not isinstance(
-                spect_standardizer, vak_transforms.FramesStandardizer
-            ):
-                raise TypeError(
-                    f"invalid type for spect_standardizer: {type(spect_standardizer)}. "
-                    "Should be an instance of vak.transforms.StandardizeSpect"
-                )
-        self.spect_standardizer = spect_standardizer
-
-        self.pad_to_window = vak_transforms.PadToWindow(
-            window_size, padval, return_padding_mask=return_padding_mask
-        )
-
-        self.source_transform_after_pad = torchvision.transforms.Compose(
-            [
-                vak_transforms.ViewAsWindowBatch(window_size),
-                vak_transforms.ToFloatTensor(),
-                # below, add channel at first dimension because windows become batch
-                vak_transforms.AddChannel(channel_dim=channel_dim),
-            ]
-        )
-
-        self.annot_transform = vak_transforms.ToLongTensor()
-
-    def __call__(self, frames, frame_labels, frames_path=None):
-        if self.spect_standardizer:
-            frames = self.spect_standardizer(frames)
-
-        if self.pad_to_window.return_padding_mask:
-            frames, padding_mask = self.pad_to_window(frames)
-        else:
-            frames = self.pad_to_window(frames)
-            padding_mask = None
-        frames = self.source_transform_after_pad(frames)
-
-        frame_labels = self.annot_transform(frame_labels)
-
-        item = {
-            "frames": frames,
-            "frame_labels": frame_labels,
-        }
-
-        if padding_mask is not None:
-            item["padding_mask"] = padding_mask
-
-        if frames_path is not None:
-            # make sure frames_path is a str, not a pathlib.Path
-            item["frames_path"] = str(frames_path)
-
-        return item
-
-
-class PredictItemTransform:
-    """Default transform used when using trained frame classification models
-    to make predictions.
-
-    Returned item includes "source" spectrogram reshaped into a stack of windows,
+    Returned item includes frames reshaped into a stack of windows,
     with padded added to make reshaping possible.
+    If `frame_labels` are supplied, then these are also padded and reshaped.
     If return_padding_mask is True, item includes 'padding_mask' that
     can be used to crop off any predictions made on the padding.
+
+    Attributes
+    ----------
+    frames_standardizer : vak.transforms.FramesStandardizer
+        instance that has already been fit to dataset, using fit_df method.
+        Default is None, in which case no standardization transform is applied.
+    window_size : int
+        width of window in number of elements. Argument to PadToWindow transform.
+    padval : float
+        value to pad with. Added to end of array, the "right side" if 2-dimensional.
+        Argument to PadToWindow transform. Default is 0.
+    return_padding_mask : bool
+        if True, the dictionary returned by ItemTransform classes will include
+        a boolean vector to use for cropping back down to size before padding.
+        padding_mask has size equal to width of padded array, i.e. original size
+        plus padding at the end, and has values of 1 where
+        columns in padded are from the original array,
+        and values of 0 where columns were added for padding.
     """
 
     def __init__(
         self,
         window_size,
-        spect_standardizer=None,
+        frames_standardizer=None,
         padval=0.0,
         return_padding_mask=True,
         channel_dim=1,
     ):
-        if spect_standardizer is not None:
+        if frames_standardizer is not None:
             if not isinstance(
-                spect_standardizer, vak_transforms.FramesStandardizer
+                frames_standardizer, vak_transforms.FramesStandardizer
             ):
                 raise TypeError(
-                    f"invalid type for spect_standardizer: {type(spect_standardizer)}. "
+                    f"invalid type for frames_standardizer: {type(frames_standardizer)}. "
                     "Should be an instance of vak.transforms.StandardizeSpect"
                 )
-        self.spect_standardizer = spect_standardizer
+        self.frames_standardizer = frames_standardizer
 
         self.pad_to_window = vak_transforms.PadToWindow(
             window_size, padval, return_padding_mask=return_padding_mask
         )
 
-        self.source_transform_after_pad = torchvision.transforms.Compose(
+        self.frames_transform_after_pad = torchvision.transforms.Compose(
             [
                 vak_transforms.ViewAsWindowBatch(window_size),
                 vak_transforms.ToFloatTensor(),
@@ -173,21 +123,26 @@ class PredictItemTransform:
             ]
         )
 
-    def __call__(self, frames, frames_path=None):
-        if self.spect_standardizer:
-            frames = self.spect_standardizer(frames)
+        self.frame_labels_transform = vak_transforms.ToLongTensor()
+
+    def __call__(self, frames: torch.Tensor, frame_labels: torch.Tensor | None = None, frames_path=None) -> dict:
+        if self.frames_standardizer:
+            frames = self.frames_standardizer(frames)
 
         if self.pad_to_window.return_padding_mask:
             frames, padding_mask = self.pad_to_window(frames)
         else:
             frames = self.pad_to_window(frames)
             padding_mask = None
-
-        frames = self.source_transform_after_pad(frames)
+        frames = self.frames_transform_after_pad(frames)
 
         item = {
             "frames": frames,
         }
+
+        if frame_labels is not None:
+            frame_labels = self.frame_labels_transform(frame_labels)
+            item["frame_labels"] = frame_labels
 
         if padding_mask is not None:
             item["padding_mask"] = padding_mask
@@ -212,7 +167,7 @@ def get_default_frame_classification_transform(
         Default is None.
         If supplied, should be a :class:`dict`,
         that can include the following key-value pairs:
-            spect_standardizer : vak.transforms.StandardizeSpect
+            frames_standardizer : vak.transforms.StandardizeSpect
                 instance that has already been fit to dataset, using fit_df method.
                 Default is None, in which case no standardization transform is applied.
             window_size : int
@@ -234,21 +189,21 @@ def get_default_frame_classification_transform(
     """
     if transform_kwargs is None:
         transform_kwargs = {}
-    spect_standardizer = transform_kwargs.get("spect_standardizer", None)
+    frames_standardizer = transform_kwargs.get("frames_standardizer", None)
     # regardless of mode, transform always starts with StandardizeSpect, if used
-    if spect_standardizer is not None:
-        if not isinstance(spect_standardizer, vak_transforms.FramesStandardizer):
+    if frames_standardizer is not None:
+        if not isinstance(frames_standardizer, vak_transforms.FramesStandardizer):
             raise TypeError(
-                f"invalid type for spect_standardizer: {type(spect_standardizer)}. "
+                f"invalid type for frames_standardizer: {type(frames_standardizer)}. "
                 "Should be an instance of vak.transforms.StandardizeSpect"
             )
 
     if mode == "train":
-        return TrainItemTransform(spect_standardizer)
+        return TrainItemTransform(frames_standardizer)
 
     elif mode == "predict":
         item_transform = PredictItemTransform(
-            spect_standardizer=spect_standardizer,
+            frames_standardizer=frames_standardizer,
             window_size=transform_kwargs["window_size"],
             padval=transform_kwargs.get("padval", 0.0),
             return_padding_mask=transform_kwargs.get(
@@ -259,7 +214,7 @@ def get_default_frame_classification_transform(
 
     elif mode == "eval":
         item_transform = EvalItemTransform(
-            spect_standardizer=spect_standardizer,
+            frames_standardizer=frames_standardizer,
             window_size=transform_kwargs["window_size"],
             padval=transform_kwargs.get("padval", 0.0),
             return_padding_mask=transform_kwargs.get(
