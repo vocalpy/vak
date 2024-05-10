17b2df99dc2d554de717b1f082b3cba7b6161734
David Nicholson <nickledave@users.noreply.github.com>
2024-05-09 22:57:16 -0400

    Add nn/loss/crossentropy.py, wraps torch.nn.CrossEntropy, but converts weight arg as list to tensor

---
+++ src/vak/nn/loss/crossentropy.py
@@ -0,0 +1,17 @@
+import torch
+
+class CrossEntropyLoss(torch.nn.CrossEntropyLoss):
+    """Wrapper around :class:`torch.nn.CrossEntropyLoss`
+
+    Converts the argument ``weight`` to a :class:`torch.Tensor`
+    if it is a :class:`list`.
+    """
+    def __init__(self, weight=None, size_average=None, ignore_index=-100, reduce=None,
+                 reduction='mean', label_smoothing=0.0):
+
+        if weight is not None:
+            if isinstance(weight, torch.Tensor):
+                pass
+            elif isinstance(weight, list):
+                weight = torch.Tensor(weight)
+        super().__init__(weight, size_average, ignore_index, reduce, reduction, label_smoothing)
