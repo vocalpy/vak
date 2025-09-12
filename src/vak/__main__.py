70da5df33124ccc2168deb40630a681ece71aeba
David Nicholson <NickleDave@users.noreply.github.com>
2025-09-12 16:20:48 -0400

    Rewrite __main__/cli so argparse uses sub-parsers
    To add a `configfile` command,
    we can no longer assume that we have just
    two arguments, `command` and `configfile`.
    
    Instead we need to move to sub-parsers,
    even though must of the sub-parsers
    will just be the same named commands as before,
    and will only have a single positional argument
    of `configfile`.
    As of now, only the new `configfile` command will
    be different, although I haven't figured out
    what its args will be yet.
    
    To do this I ended up representing
    cli commands as a dataclass inside the
    `vak.cli.cli` module,
    with attributes `name`, `help`,
    `func`, and `add_parser_args_func`.
    
    I am choosing to send each sub-parser
    to `dest='command'`, and then dispatching
    based on the value of `command` inside the
    `vak.cli.cli.cli` function, instead of letting
    `argparse` do all the work,
    because I want to be able to raise a
    somewhat friendly error message.
    Maybe this is not needed since argparse
    validates arguments anyways.
    But this requires the least rewiring of
    what we already had in place.

--- src/vak/__main__.py
+++ src/vak/__main__.py
@@ -2,36 +2,7 @@
 Invokes __main__ when the module is run as a script.
 Example: python -m vak --help
 """
-
-import argparse
-from pathlib import Path
-
 from .cli import cli
-
-
-def get_parser():
-    """returns ArgumentParser instance used by main()"""
-    parser = argparse.ArgumentParser(
-        prog="vak",
-        description="vak command-line interface",
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-    parser.add_argument(
-        "command",
-        type=str,
-        metavar="command",
-        choices=cli.CLI_COMMANDS,
-        help="Command to run, valid options are:\n"
-        f"{cli.CLI_COMMANDS}\n"
-        "$ vak train ./configs/config_2018-12-17.toml",
-    )
-    parser.add_argument(
-        "configfile",
-        type=Path,
-        help="name of config.toml file to use \n"
-        "$ vak train ./configs/config_2018-12-17.toml",
-    )
-    return parser
 
 
 def main(args=None):
@@ -44,7 +15,7 @@ def main(args=None):
     ``args`` is used for unit testing only
     """
     if args is None:
-        parser = get_parser()
+        parser = cli.get_parser()
         args = parser.parse_args()
     cli.cli(command=args.command, config_file=args.configfile)
 
--- src/vak/cli/cli.py
+++ src/vak/cli/cli.py
@@ -1,46 +1,141 @@
-def eval(toml_path):
+"""Implements the vak command-line interface"""
+import argparse
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable
+
+
+def eval(args):
     from .eval import eval
 
-    eval(toml_path=toml_path)
+    eval(toml_path=args.config_file)
 
 
-def train(toml_path):
+def train(args):
     from .train import train
 
-    train(toml_path=toml_path)
+    train(toml_path=args.config_file)
 
 
-def learncurve(toml_path):
+def learncurve(args):
     from .learncurve import learning_curve
 
-    learning_curve(toml_path=toml_path)
+    learning_curve(toml_path=args.config_file)
 
 
-def predict(toml_path):
+def predict(args):
     from .predict import predict
 
-    predict(toml_path=toml_path)
+    predict(toml_path=args.config_file)
 
 
-def prep(toml_path):
+def prep(args):
     from .prep import prep
 
-    prep(toml_path=toml_path)
+    prep(toml_path=args.config_file)
 
 
-COMMAND_FUNCTION_MAP = {
-    "prep": prep,
-    "train": train,
-    "eval": eval,
-    "predict": predict,
-    "learncurve": learncurve,
+@dataclass
+class CLICommand:
+    """Dataclass representing a cli command
+    
+    Attributes
+    ----------
+    name : str
+        Name of the command, that gets added to the CLI as a sub-parser
+    help : str
+        Help for the command, that gets added to the CLI as a sub-parser
+    func : Callable
+        Function to call for command
+    add_parser_args_func: Callable
+        Function to call to add arguments to sub-parser representing command
+    """
+    name: str
+    help: str
+    func: Callable
+    add_parser_args_func : Callable
+
+
+def add_configfile_arg(
+    cli_command,
+    cli_command_parser
+):
+        cli_command_parser.add_argument(
+            "configfile",
+            type=Path,
+            help="name of TOML configuration file to use \n"
+            f"$ vak {cli_command.name} ./configs/config_rat01337.toml",
+        )
+
+
+CLI_COMMANDS = [
+    CLICommand(
+        name='prep',
+        help='prepare a dataset',
+        func=prep,
+        add_parser_args_func=add_configfile_arg,
+    ),
+    CLICommand(
+        name='train',
+        help='train a model',
+        func=train,
+        add_parser_args_func=add_configfile_arg,
+    ),
+    CLICommand(
+        name='eval',
+        help='evaluate a trained model',
+        func=eval,
+        add_parser_args_func=add_configfile_arg,
+    ),
+    CLICommand(
+        name='predict',
+        help='generate predictions from trained model',
+        func=predict,
+        add_parser_args_func=add_configfile_arg,
+    ),
+    CLICommand(
+        name='learncurve',
+        help='run a learning curve',
+        func=learncurve,
+        add_parser_args_func=add_configfile_arg,
+    ),
+]
+
+
+def get_parser():
+    """returns ArgumentParser instance used by main()"""
+    parser = argparse.ArgumentParser(
+        prog="vak",
+        description="vak command-line interface",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    # create sub-parser
+    sub_parsers = parser.add_subparsers(
+        help='Commands for vak command-line interface',
+        dest="command",
+    )
+
+    for cli_command in CLI_COMMANDS:
+        cli_command_parser = sub_parsers.add_parser(
+            cli_command.name,
+            help=cli_command.help
+        )
+        cli_command.add_parser_args_func(
+            cli_command,
+            cli_command_parser
+        )
+
+    return parser
+
+
+CLI_COMMAND_FUNCTION_MAP = {
+    cli_command.name: cli_command.func 
+    for cli_command in CLI_COMMANDS
 }
 
 
-CLI_COMMANDS = tuple(COMMAND_FUNCTION_MAP.keys())
-
-
-def cli(command, config_file):
+def cli(args):
     """Execute the commands of the command-line interface.
 
     Parameters
@@ -50,7 +145,7 @@ def cli(command, config_file):
     config_file : str, Path
         path to a config.toml file
     """
-    if command in COMMAND_FUNCTION_MAP:
-        COMMAND_FUNCTION_MAP[command](toml_path=config_file)
+    if args.command in CLI_COMMAND_FUNCTION_MAP:
+        CLI_COMMAND_FUNCTION_MAP[args.command](args)
     else:
-        raise ValueError(f"command not recognized: {command}")
+        raise ValueError(f"command not recognized: {args.command}")
