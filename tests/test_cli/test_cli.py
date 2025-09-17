12b31f3bce77ecd250fdd9fc7334b00c1366b14e
David Nicholson <nickledave@users.noreply.github.com>
2025-09-17 12:57:46 -0400

    Add tests/test_cli/test_cli.py with unit tests moved from tests/test__main__.py

---
+++ tests/test_cli/test_cli.py
@@ -0,0 +1,50 @@
+import pathlib
+
+import pytest
+
+
+def test_parser_usage(parser,
+                      capsys):
+    with pytest.raises(SystemExit):
+        parser.parse_args(args=[''])
+    captured = capsys.readouterr()
+    assert captured.err.startswith(
+        "usage: vak [-h] {prep,train,eval,predict,learncurve,configfile} ..."
+    )
+
+
+def test_parser_help(parser,
+                     capsys):
+    with pytest.raises(SystemExit):
+        parser.parse_args(['-h'])
+    captured = capsys.readouterr()
+    assert captured.out.startswith(
+        "usage: vak [-h] {prep,train,eval,predict,learncurve,configfile} ..."
+    )
+
+
+DUMMY_CONFIGFILE = './configs/config_2018-12-17.toml'
+
+
+@pytest.mark.parametrize(
+    'command, raises',
+    [
+        ('prep', False),
+        ('train', False),
+        ('learncurve', False),
+        ('eval', False),
+        ('predict', False),
+        ('not-a-valid-command', True),
+    ]
+)
+def test_parser(command,
+                raises,
+                parser,
+                capsys):
+    if raises:
+        with pytest.raises(SystemExit):
+            parser.parse_args([command, DUMMY_CONFIGFILE])
+    else:
+        args = parser.parse_args([command, DUMMY_CONFIGFILE])
+        assert args.command == command
+        assert args.configfile == pathlib.Path(DUMMY_CONFIGFILE)
