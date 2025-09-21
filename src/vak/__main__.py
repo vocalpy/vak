5fffe81116b561a83eb72b429f34b3b6a647a96c
David Nicholson <nickledave@users.noreply.github.com>
2025-09-21 10:22:35 -0400

    Rewrite __main__.main to take `args_list` for clarity

--- src/vak/__main__.py
+++ src/vak/__main__.py
@@ -7,14 +7,14 @@ import sys
 from .cli import cli
 
 
-def main(args=None):
+def main(args_list:list[str] | None = None):
     """Main function called when run as script or through command-line interface
 
     called when package is run with `python -m vak` or
     alternatively just calling `vak` at the command line (because this
     function is installed under just `vak` as a console script)
 
-    ``args`` is used for unit testing only
+    ``args_list`` is used for unit testing only
     """
     parser = cli.get_parser()
 
@@ -22,10 +22,10 @@ def main(args=None):
         parser.print_help()
         sys.exit(1)
 
-    if args is None:
+    if args_list is None:
         args = parser.parse_args()
     else:
-        args = parser.parse_args(args)
+        args = parser.parse_args(args_list)
     cli.cli(args)
 
 
